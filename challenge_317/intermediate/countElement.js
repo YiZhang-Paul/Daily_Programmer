af3a02745857b6a4b1bc870fb5c9f531718ed596
YI ZHANG <scaccterran@gmail.com>
2017-06-01 03:56:24 -0400

    basic functions

---
+++ challenge_317/intermediate/countElement.js
@@ -0,0 +1,24 @@
+/* jslint esversion: 6 */
+(() => {
+	document.addEventListener("DOMContentLoaded", () => {
+		//all formulas
+		let inputs = ["C6H12O6", "CCl2F2", "NaHCO3", "C4H8(OH)2", "PbCl(NH3)2(COOH)2"];
+		/**
+		 * reform formula string
+		 * @param String
+		 * 
+		 * formula : formula string to be reformed
+		 */
+		function reform(formula) {
+			
+		}
+
+		function breakDown() {
+
+		}
+
+		function count() {
+
+		}
+	});
+})();
