ec549892e4c45a7e07052b444ce602e3250b67a0
YI ZHANG <scaccterran@gmail.com>
2017-06-09 17:01:37 -0400

    create all teams and record all opponents for each team

--- challenge_318/intermediate/nbaRevolution.js
+++ challenge_318/intermediate/nbaRevolution.js
@@ -3,25 +3,60 @@
 	document.addEventListener("DOMContentLoaded", () => {
 		/**
 		 * team class
+		 * @param String
+		 *
+		 * name : team name
 		 */
 		class Team {
 			constructor(name) {
 				this.name = name;
-				this.lastOpponent = null;
 				this.lastGameLocation = null;
-				this.remainingOpponents = null;
+				this.allOpponents = [];
 			}
 		} 
 		/**
 		 * schedule manager class
+		 * @param array []
+		 *
+		 * allTeams : all participating teams
 		 */
 		class ScheduleManager {
 			constructor(allTeams) {
 				this.allTeams = allTeams;
-				this.totalGames = 0;
-				this.homeTeamList = [];
-				this.awayTeamList = [];
+				this.currentRound = 0;
+				this.totalRounds = (allTeams.length - 1) * 2;
+				this.consecutiveRemain = 1;
+				//update current remaining opponent for every team 
+				this.updateAllOpponent();
 			}
+			/**
+			 * update remaining opponents for each team
+			 */
+			updateAllOpponent() {
+				this.allTeams.forEach((team, index, array) => {
+					team.allOpponents = [...array.slice(0, index), ...array.slice(index + 1)];
+					team.allOpponents = team.allOpponents.map(opponent => {
+						return {opponent : opponent, home : false, away : false};
+					});
+				});
+			}
+			/**
+			 * pick teams for each round
+			 */ 
+			pickRound() {
+				
+			}
+			/**
+			 * print out schedule for one round
+			 */
+			printRound() {
+
+			} 
 		} 
+		//default input
+		let allTeamNames = ["Cleveland Cavaliers", "Golden State Warriors", "San Antonio Spurs", "Toronto raptors"];
+		//create manager for all teams
+		let allTeams = allTeamNames.map(name => new Team(name));
+		let scheduleManager = new ScheduleManager(allTeams);
 	});
 })();
