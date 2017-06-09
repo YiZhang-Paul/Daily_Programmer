250a861812ab712f6f1bc506ae6d2585edb2bd24
YI ZHANG <scaccterran@gmail.com>
2017-06-08 23:57:54 -0400

    basket ball team class

--- challenge_318/intermediate/nbaRevolution.js
+++ challenge_318/intermediate/nbaRevolution.js
@@ -1,6 +1,16 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * team class
+		 */
+		class Team {
+			constructor(name) {
+				this.name = name;
+				this.lastOpponent = null;
+				this.lastGameLocation = null;
+				this.remainingOpponents = null;
+			}
+		} 
 	});
 })();
