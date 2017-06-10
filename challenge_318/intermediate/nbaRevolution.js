2207a14d52f49b415cc279c59b3c6a5c0a5deb86
YI ZHANG <scaccterran@gmail.com>
2017-06-09 23:33:25 -0400

    get all match ups using permutation

--- challenge_318/intermediate/nbaRevolution.js
+++ challenge_318/intermediate/nbaRevolution.js
@@ -2,199 +2,30 @@
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
 		/**
-		 * team class
-		 * @param String
+		 * permute all possible match-ups
+		 * @param array [], array []
 		 *
-		 * name : team name
-		 */
-		class Team {
-			constructor(name) {
-				this.name = name;
-				this.lastGameLocation = null;
-				this.allOpponents = [];
-				this.consecutiveRemain = 1;
-			}
-			/**
-			 * find opponents who have not played
-			 * home games with current team
-			 * @param obj {}
-			 *
-			 * pickedTeam : team already picked by other teams 
-			 *
-			 * returns array []
-			 */
-			findHomeTeam(pickedTeam) {
-				return this.allOpponents.slice().filter(opponent => 
-					!opponent.home && !pickedTeam.has(opponent.opponent));
-			} 
-			/**
-			 * find opponents who have not played
-			 * away games with current team
-			 * @param obj {}
-			 *
-			 * pickedTeam : team already picked by other teams 
-			 *
-			 * returns array []
-			 */
-			findAwayTeam(pickedTeam) {
-				return this.allOpponents.slice().filter(opponent => 
-					!opponent.away && !pickedTeam.has(opponent.opponent));
-			}   
-			/**
-			 * update all opponent status
-			 * param String, obj {}
-			 *
-			 * location : location of current game
-			 * opponent : opponent picked
-			 */
-			updateOpponentStatus(location, opponent) {
-				//update game location information for current team
-				this.lastGameLocation = location;
-				opponent[location] = true;
-				//update game locaotion for opponent team
-				let opponentLocation = location == "home" ? "away" : "home";
-				opponent.opponent.lastGameLocation = opponentLocation;
-				let index = opponent.opponent.allOpponents.findIndex(opponent => opponent.opponent.name == this.name);
-				opponent.opponent.allOpponents[index][opponentLocation] = true;
-			} 
-			/**
-			 * pick home team
-			 * @param array []
-			 *
-			 * homeTeams : all available home teams
-			 *
-			 * returns obj {}
-			 */
-			pickHomeTeam(homeTeams) {
-				if(!homeTeams.length) {
-					return;
-				}
-				let opponent = homeTeams[Math.floor(Math.random() * homeTeams.length)];
-				this.updateOpponentStatus("home", opponent);
-				return opponent;
-			} 
-			/**
-			 * pick away team
-			 * @param array []
-			 *
-			 * awayTeams : all available away teams
-			 *
-			 * returns obj {}
-			 */
-			pickAwayTeam(awayTeams) {
-				if(!awayTeams.length) {
-					return;
-				}
-				let opponent = awayTeams[Math.floor(Math.random() * awayTeams.length)];
-				this.updateOpponentStatus("away", opponent);
-				return opponent;
-			}  
-			/**
-			 * pick opponents
-			 * @param obj {}
-			 *
-			 * pickedTeam : team already picked by other teams 
-			 *
-			 * returns obj {}
-			 */
-			pickOpponent(pickedTeam) {
-				let opponent;
-				//find all opponent who had not played home/away game
-				let homeTeams = this.findHomeTeam(pickedTeam);
-				let awayTeams = this.findAwayTeam(pickedTeam); 
-				if(!this.lastGameLocation) {
-					if(homeTeams.length > awayTeams.length) {
-						opponent = this.pickHomeTeam(homeTeams);
-					} else {
-						opponent = this.pickAwayTeam(awayTeams);
-					}
-				} else if(this.lastGameLocation == "home") {
-					opponent = awayTeams.length ? this.pickAwayTeam(awayTeams) : this.pickHomeTeam(homeTeams);
-				} else {
-					opponent = homeTeams.length ? this.pickHomeTeam(homeTeams) : this.pickAwayTeam(awayTeams);
-				}
-				return opponent ? opponent.opponent : opponent;
-			} 
-		} 
-		/**
-		 * schedule manager class
-		 * @param array []
+		 * teamList   : list of all team names
+		 * curMatchUp : current match-up
 		 *
-		 * allTeams : all participating teams
+		 * returns array []
 		 */
-		class ScheduleManager {
-			constructor(allTeams) {
-				this.allTeams = allTeams;
-				this.totalRounds = (allTeams.length - 1) * 2;
-				//update current remaining opponent for every team 
-				this.updateAllOpponent();
-				//post schedule
-				this.postSchedule();
+		function getAllMatchUp(teamList, curMatchUp = []) {
+			if(curMatchUp.length == 2) {
+				return curMatchUp;
 			}
-			/**
-			 * update remaining opponents for each team
-			 */
-			updateAllOpponent() {
-				this.allTeams.forEach((team, index, array) => {
-					team.allOpponents = [...array.slice(0, index), ...array.slice(index + 1)];
-					team.allOpponents = team.allOpponents.map(opponent => {
-						return {opponent : opponent, home : false, away : false};
-					});
-				});
+			let permutation = [];
+			for(let i = 0; i < teamList.length; i++) {
+				let curTeam = teamList[i];
+				let otherTeam = [...teamList.slice(0, i), ...teamList.slice(i + 1)];
+				permutation.push(getAllMatchUp(otherTeam, [...curMatchUp, curTeam]));
 			}
-			/**
-			 * pick teams for each round
-			 *
-			 * returns array []
-			 */ 
-			scheduleRound() {
-				let curRound = [], scheduledTeams = new Set();
-				this.allTeams.forEach(team => {
-					if(!scheduledTeams.has(team)) {
-						let opponent = team.pickOpponent(scheduledTeams);
-						if(opponent) {
-							let schedule = opponent.lastGameLocation == "home" ? 
-								`${opponent.name} - ${team.name}` : `${team.name} - ${opponent.name}`;
-							//record new schedule
-							curRound.push(schedule);
-							//record team already picked
-							scheduledTeams.add(team);
-							scheduledTeams.add(opponent);
-						}
-					}
-				});
-				return curRound.length == this.allTeams.length * 0.5 ? curRound : this.scheduleRound();
-			}
-			/**
-			 * print out schedule for one round
-			 * @param int, array []
-			 *
-			 * roundNum : current round number
-			 * curRound : schedule information for current round
-			 */
-			printRound(roundNum, curRound) {
-				console.log(`Round ${roundNum}`);
-				curRound.forEach(round => {
-					console.log(round);
-				});
-			} 
-			/**
-			 * post schedule for all rounds
-			 */
-			postSchedule() {
-				for(let i = 0; i < this.totalRounds; i++) {
-					this.printRound(i + 1, this.scheduleRound());
-				}
-			} 
-		} 
+			return permutation;
+		}
 		//default input
 		let allTeamNames = ["Cleveland Cavaliers", "Golden State Warriors", "San Antonio Spurs", "Toronto raptors"];
-		//create manager for all teams
-		let allTeams = allTeamNames.map(name => new Team(name));
-		let scheduleManager = new ScheduleManager(allTeams);
+		let allMatchUps = getAllMatchUp(allTeamNames);
 		//challenge input
 		allTeamNames = ["Atlanta Hawks", "Boston Celtics", "Brooklyn Nets", "Charlotte Hornets", "Chicago Bulls", "Cleveland Cavaliers", "Dallas Mavericks", "Denver Nuggets", "Detroit Pistons", "Golden State Warriors", "Houston Rockets", "Indiana Pacers", "Los Angeles Clippers", "Los Angeles Lakers", "Memphis Grizzlies", "Miami Heat", "Milwaukee Bucks", "Minnesota Timberwolves", "New Orleans Pelicans", "New York Knicks", "Oklahoma City Thunder", "Orlando Magic", "Philadelphia 76ers", "Phoenix Suns", "Portland Trail Blazers", "Sacramento Kings", "San Antonio Spurs", "Toronto Raptors", "Utah Jazz", "Washington Wizards"];
-		allTeams = allTeamNames.map(name => new Team(name));
-		scheduleManager = new ScheduleManager(allTeams);
 	});
 })();
