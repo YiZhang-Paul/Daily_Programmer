630d0687d21d0fbd76d0a1fdb698d6862ae8a53b
YI ZHANG <scaccterran@gmail.com>
2017-06-09 23:52:50 -0400

    simplified get match-ups function

--- challenge_318/intermediate/nbaRevolution.js
+++ challenge_318/intermediate/nbaRevolution.js
@@ -10,7 +10,7 @@
 		 *
 		 * returns array []
 		 */
-		function getAllMatchUp(teamList, curMatchUp = []) {
+		function getMatchUp(teamList, curMatchUp = []) {
 			if(curMatchUp.length == 2) {
 				return curMatchUp;
 			}
@@ -18,33 +18,34 @@
 			for(let i = 0; i < teamList.length; i++) {
 				let curTeam = teamList[i];
 				let otherTeam = [...teamList.slice(0, i), ...teamList.slice(i + 1)];
-				permutation.push(getAllMatchUp(otherTeam, [...curMatchUp, curTeam]));
+				permutation.push(getMatchUp(otherTeam, [...curMatchUp, curTeam]));
 			}
 			return permutation;
 		}
 		/**
 		 * group all match-ups by teams
-		 * @param array [], int
+		 * @param array []
 		 *
-		 * allMatchUp : all match-ups for all teams
-		 * totalTeam  : total number of teams
+		 * teamList : list of all team names
 		 *
 		 * returns array []
 		 */
-		function getTeamMatchUp(allMatchUp, totalTeam) {
-			let teamMatchUp = [];
-			let totalTeamMatch = allMatchUp.length / totalTeam;
-			for(let i = 0; i < allMatchUp.length; i += totalTeamMatch) {
-				teamMatchUp.push(allMatchUp.slice(i, i + totalTeamMatch));
+		function getTeamMatchUp(teamList) {
+			let allMatchUps = getMatchUp(teamList);
+			let teamMatchUps = [];
+			let totalTeamMatch = allMatchUps.length / teamList.length;
+			for(let i = 0; i < allMatchUps.length; i += totalTeamMatch) {
+				teamMatchUps.push(...allMatchUps.slice(i, i + totalTeamMatch));
 			}
-			return teamMatchUp;
+			return teamMatchUps;
 		}
 		//default input
 		let allTeamNames = ["Cleveland Cavaliers", "Golden State Warriors", "San Antonio Spurs", "Toronto raptors"];
-		let allMatchUps = getAllMatchUp(allTeamNames);
-		let teamMatchUps = getTeamMatchUp(allMatchUps, allTeamNames.length);
+		let teamMatchUps = getTeamMatchUp(allTeamNames);
 		console.log(teamMatchUps);
 		//challenge input
 		allTeamNames = ["Atlanta Hawks", "Boston Celtics", "Brooklyn Nets", "Charlotte Hornets", "Chicago Bulls", "Cleveland Cavaliers", "Dallas Mavericks", "Denver Nuggets", "Detroit Pistons", "Golden State Warriors", "Houston Rockets", "Indiana Pacers", "Los Angeles Clippers", "Los Angeles Lakers", "Memphis Grizzlies", "Miami Heat", "Milwaukee Bucks", "Minnesota Timberwolves", "New Orleans Pelicans", "New York Knicks", "Oklahoma City Thunder", "Orlando Magic", "Philadelphia 76ers", "Phoenix Suns", "Portland Trail Blazers", "Sacramento Kings", "San Antonio Spurs", "Toronto Raptors", "Utah Jazz", "Washington Wizards"];
+		teamMatchUps = getTeamMatchUp(allTeamNames);
+		console.log(teamMatchUps);
 	});
 })();
