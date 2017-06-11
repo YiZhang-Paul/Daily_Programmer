6e5e065ab42e7ecffd48aa1a2cbb7dddf9326372
YI ZHANG <scaccterran@gmail.com>
2017-06-10 22:30:43 -0400

    calculate result from left to right

--- challenge_318/easy/countDown.js
+++ challenge_318/easy/countDown.js
@@ -13,7 +13,7 @@
 				this.numList = numList.splice(" ");
 				this.operatorList = operatorList; 
 				this.totalOperators = this.numList.length - 2;
-				console.log(this.calculateResult());
+				this.operatorPattern = this.permuteOperator();
 			}
 			/**
 			 * generate all possible permutation of operators
@@ -39,16 +39,60 @@
 				return permutation;
 			} 
 			/**
-			 * calculate result
+			 * calculate result base on operator
+			 * @param float, float, String
+			 *
+			 * num1     : first number  
+			 * num2     : second number
+			 * operator : operator to be used
+			 *
+			 * returns float 
 			 */
-			calculateResult() {
-				let operatorPattern = this.permuteOperator();
-				console.log(operatorPattern);
+			calculateResult(num1, num2, operator) {
+				let result = 0;
+				switch(operator) {
+					case "+" :
+						result = num1 + num2;
+						break;
+					case "-" :
+						result = num1 - num2;
+						break;
+					case "*" :
+						result = num1 * num2;
+						break;
+					case "/" :
+						result = num1 / num2;
+						break;
+				}
+				return result;
 			} 
+			/**
+			 * calculate result for entire list
+			 * @param String
+			 *
+			 * operatorPattern : operator patterns to be applied
+			 *
+			 * returns float
+			 */
+			getListResult(operatorPattern) {
+				return this.numList.slice(0, this.numList.length - 1).reduce((acc, val, index) =>
+					this.calculateResult(acc, val, operatorPattern[index - 1]));
+			} 
+			/**
+			 * get all valid count down
+			 *
+			 * returns array []
+			 */
+			getCountDown() {
+				return this.operatorPattern.slice().filter(pattern =>
+					this.getListResult(pattern) == this.numList[this.numList.length - 1]);
+			}
 		} 
 		//default input
 		let operatorList = ["+", "-", "*", "/"];
 		let numList = [1, 3, 7, 6, 8, 3, 250];
 		let countDown = new CountDown(numList, operatorList);
+		let result = countDown.getCountDown();
+		console.log(result);
 	});
 })();
