03e0c61992d951cf4b84815d89063e30c4eae67a
YI ZHANG <scaccterran@gmail.com>
2017-06-10 21:56:10 -0400

    count down class

--- challenge_318/easy/countDown.js
+++ challenge_318/easy/countDown.js
@@ -1,6 +1,25 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-
+		/**
+		 * count down class
+		 * @param array [], array []
+		 *
+		 * numList      : list of all number inputs and target result
+		 * operatorList : list of all available operators
+		 */
+		class CounteDown {
+			constructor(numList, operatorList) {
+				this.numList = numList.splice(" ");
+				this.operatorList = operatorList; 
+				this.totalOperators = this.numList.length - 2;
+			}
+			/**
+			 * generate all possible permutation of operators
+			 */
+			permuteOperator() {
+				
+			} 
+		} 
 	});
 })();
