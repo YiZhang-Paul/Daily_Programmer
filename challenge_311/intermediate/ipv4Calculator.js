039b9c4a249e56599b50e55eb87d4d880cd4cb2a
YI ZHANG <scaccterran@gmail.com>
2017-06-12 10:02:23 -0400

    IPv4 address class

--- challenge_311/intermediate/ipv4Calculator.js
+++ challenge_311/intermediate/ipv4Calculator.js
@@ -1,6 +1,30 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * IPv4 address class
+		 * @param String
+		 *
+		 * addr : IPv4 address in decimal form
+		 */
+		class IPv4 {
+			constructor(addr) {
+				this.addr = addr.split("/");
+				this.netAndHost = this.toBinary(this.addr[0]);
+				this.mask = this.addr[1];
+			}
+			/**
+			 * convert IPv4 address to binary notation
+			 * @param String
+			 * 
+			 * addr : IPv4 address without mask
+			 *
+			 * returns String
+			 */	
+			toBinary(addr) {
+				return addr.split(".").map(quad => Number(quad).toString(2)).join("");
+			} 
+		} 
+		let addr1 = new IPv4("172.26.32.0/24");
 	});
 })();
