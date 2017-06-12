306974eda74bb788cb2dc89e562e7d642ad34eaf
YI ZHANG <scaccterran@gmail.com>
2017-06-12 18:04:50 -0400

    function for bonus challenge

--- challenge_313/easy/subsetSum.js
+++ challenge_313/easy/subsetSum.js
@@ -15,6 +15,36 @@
 			let set = new Set(numList.map(num => Math.abs(num)));
 			return set.size != numList.length || set.has(0);
 		}
+		/**
+		 * check if exists a subset that add up to 0
+		 * @param array []
+		 * 
+		 * numList : list of all numbers
+		 *
+		 * returns boolean
+		 */
+		function checkZeroSumSubset(numList) {
+			if(numList[0] > 0) {
+				return false;
+			} else if(numList[0] === 0) {
+				return true;
+			}
+			let startIndex = 0, curIndex = startIndex, sum = 0;
+			while(startIndex < numList.length) {
+				while(curIndex < numList.length) {
+					sum += numList[curIndex];
+					if(sum > 0) {
+						return false;
+					}
+					let expectedNum = -sum;
+					if(numList[curIndex + 1] == expectedNum) {
+						return true;
+					}
+					curIndex++;
+				}
+				curIndex = ++startIndex;
+			}
+		} 
 		//base challenge input
 		let input = [1, 2, 3];
 		console.log(`[${input}] -> ${checkZeroSum(input)}`);
@@ -28,5 +58,12 @@
 		console.log(`[${input}] -> ${checkZeroSum(input)}`);
 		input = [-53974, -39140, -36561, -23935, -15680, 0];
 		console.log(`[${input}] -> ${checkZeroSum(input)}`);
+		//bonus challenge input
+		input = [0];
+		console.log(`[${input}] -> ${checkZeroSumSubset(input)}`);
+		input = [-3, 1, 2];
+		console.log(`[${input}] -> ${checkZeroSumSubset(input)}`);
+		input = [-98634, -86888, -48841, -40483, 2612, 9225, 17848, 71967, 84319, 88875];
+		console.log(`[${input}] -> ${checkZeroSumSubset(input)}`);
 	});
 })();
