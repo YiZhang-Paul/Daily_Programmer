15a214bd1f1deea7afd0766e3c1cbd0fd0f82cb9
YI ZHANG <scaccterran@gmail.com>
2017-06-12 10:51:11 -0400

    function to check if an address is covered by other

--- challenge_311/intermediate/ipv4Calculator.js
+++ challenge_311/intermediate/ipv4Calculator.js
@@ -12,6 +12,8 @@
 				this.addr = addr.split("/");
 				this.netAndHost = this.toBinary(this.addr[0]);
 				this.mask = this.addr[1];
+				this.netPrefix = this.netAndHost.slice(0, this.mask); 
+				this.hosts = this.netAndHost.slice(this.mask);
 			}
 			/**
 			 * convert IPv4 address to binary notation
@@ -22,7 +24,37 @@
 			 * returns String
 			 */	
 			toBinary(addr) {
-				return addr.split(".").map(quad => Number(quad).toString(2)).join("");
+				return addr.split(".").map(quad => {
+					let bits = Number(quad).toString(2);
+					return "0".repeat(8 - bits.length) + bits;	
+				}).join("");
+			} 
+			/**
+			 * check if current address contains a given address
+			 * @param obj {}
+			 *
+			 * addr : address to be checked
+			 *
+			 * returns boolean
+			 */
+			coverAddr(addr) {
+				if(this.mask < addr.mask) {
+					return this.netPrefix == addr.netAndHost.slice(0, this.mask);
+				} else if(this.mask == addr.mask) {
+					//find identical portion of network prefix
+					let i = 1, identical;
+					do {
+						identical = this.netPrefix.slice(0, i) == add.netPrefix.slice(0, i);
+					} while(!identical || i <= this.mask);
+					if(identical) {
+						let remainQuadBits = Math.min((Math.floor(i / 8) + 1), 4) * 8 - i;
+						return this.netPrefix.slice(i, i + remainQuadBits) > addr.netPrefix.slice(i, i + remainQuadBits);
+					} else {
+						return false; 
+					}
+				} else {
+					return false;
+				}
 			} 
 		} 
 		let addr1 = new IPv4("172.26.32.0/24");
