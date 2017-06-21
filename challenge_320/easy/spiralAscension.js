f2858280674bdb091a7baf3ae87bedd7fcb2c5ea
YI ZHANG <scaccterran@gmail.com>
2017-06-21 02:07:40 -0400

    tidy up

--- challenge_320/easy/spiralAscension.js
+++ challenge_320/easy/spiralAscension.js
@@ -2,6 +2,86 @@
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
 		/**
+		 * make square to hold all numbers
+		 * @param int, void
+		 *
+		 * num  : dimension of square
+		 * init : initial value of all elements
+		 *
+		 * returns array []
+		 */
+		function makeSquare(num, init = 0) {
+			let square = [];
+			for(let i = 0; i < num; i++) {
+				square.push(new Array(num).fill(init));
+			}
+			return square;
+		} 
+		/**
+		 * pad infront of a number to given length
+		 * @param int, int
+		 *
+		 * number : number to be padded
+		 * length : pre-determined length
+		 *
+		 * returns String
+		 */
+		function padNumber(number, length) {
+			return " ".repeat(length - number.toString().length) + number;
+		} 
+		/**
+		 * print result
+		 * @param array []
+		 *
+		 * result : result to be printed
+		 */
+		function printResult(result) {
+			result.forEach(row => {console.log(row);});
+		} 
+		/**
+		 * get current direction
+		 * @param String, int, int, int
+		 *
+		 * curDir : current direction
+		 * row    : current row
+		 * col    : current column
+		 * square : square to be traversed
+		 *
+		 * returns String
+		 */
+		function getDirection(curDir, row, col, square) {
+			if((curDir == "right" || curDir == "left") && row + col == square.length - 1) {
+				curDir = curDir == "right" ? "down" : "up";	
+			} else if(curDir == "down" && row == col) {
+				curDir = "left";
+			} else if(curDir == "up" && square[row - 1][col]) {
+				curDir = "right";
+			}
+			return curDir; 
+		} 
+		/**
+		 * traverse in square
+		 * @param String, int, int, array []
+		 *
+		 * curDir : current direction
+		 * row    : current row
+		 * col    : current column
+		 * square : square to be traversed
+		 *
+		 * returns array []
+		 */
+		function traverse(curDir, row, col, square) {
+			switch(curDir) {
+				case "right" : case "left" :
+					col = curDir == "right" ? col + 1 : col - 1;
+					break;
+				case "up" : case "down" :
+					row = curDir == "up" ? row - 1 : row + 1;
+					break;	
+			}
+			return [row, col];
+		} 
+		/**
 		 * fill square with spiral
 		 * @param int
 		 *
@@ -10,45 +90,21 @@
 		 * returns array [] 
 		 */
 		function fillSpiral(num) {
-			let square = new Array(num);
-			for(let i = 0; i < square.length; i++) {
-				square[i] = new Array(num).fill(0);
-			}
-			let space = " ", maxLength = Math.pow(num, 2).toString().length;
-			let curRow = 0, curCol = 0, filled = 1;
-			square[curRow][curCol] = space.repeat(maxLength - filled.toString().length) + filled;
+			//make square to hold all numbers
+			let square = makeSquare(num);
+			let maxNum = Math.pow(num, 2), maxLength = maxNum.toString().length;
+			let curRow = 0, curCol = 0, curNum = 1;
 			let direction = "right";
-			while(filled != Math.pow(num, 2)) {
-				//determine direction
-				if(direction == "right" && curRow + curCol == square.length - 1) direction = "down";
-				else if(direction == "down" && curRow == curCol) direction = "left";
-				else if(direction == "left" && curRow + curCol == square.length - 1) direction = "up";
-				else if(direction == "up" && square[curRow - 1][curCol]) direction = "right";   
+			while(curNum <= maxNum) {
+				square[curRow][curCol] = padNumber(curNum++, maxLength);
+				//get direction
+				direction = getDirection(direction, curRow, curCol, square);	
 				//move through square
-				switch(direction) {
-					case "right" : case "left" :
-						curCol = direction == "right" ? curCol + 1 : curCol - 1;
-						break;
-					case "up" : case "down" :
-						curRow = direction == "up" ? curRow - 1 : curRow + 1;
-						break;	
-				}
-				filled++;
-				square[curRow][curCol] = space.repeat(maxLength - filled.toString().length) + filled;
+				[curRow, curCol] = traverse(direction, curRow, curCol, square);
 			}
 			printResult(square);
 		} 
-		/**
-		 * print result
-		 * @param array []
-		 * 
-		 * result : result to be printed
-		 */
-		function printResult(result) {
-			result.forEach(row => {
-				console.log(row);
-			});
-		} 
+		//get user input
 		let input = prompt("Please Enter a Number: ");
 		fillSpiral(Number(input));
 	});
