da6d03048d9ec5bc981d726d85d4551f9eb9c879
YI ZHANG <scaccterran@gmail.com>
2017-06-21 19:57:30 -0400

    embed shorter word in a longer word

--- challenge_313/hard/embedWord.js
+++ challenge_313/hard/embedWord.js
@@ -2,68 +2,39 @@
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
 		/**
-		 * remove duplicate letters
-		 * @param array []
+		 * merge two words
+		 * @param String, String
 		 *
-		 * combinedList : combined word list according to letter weights
+		 * word1 : word 1 (usually a longer word)
+		 * word2 : word 2
 		 *
 		 * returns String
-		 */
-		function removeDuplicate(combinedList) {
-			return combinedList.map(block => Array.from(new Set(block.split(""))).join("")).join("");
-		} 
-		/**
-		 * combine all words according to 
-		 * weights of each letter
-		 * @param array []
-		 *
-		 * wordList : list of all words
-		 *
-		 * returns array []
-		 */
-		function combineWords(wordList) {
-			let maxLength = Math.max(...wordList.map(word => word.length));
-			let wordArr = new Array(maxLength).fill("");
-			wordList.forEach(word => {
-				for(let modifier = maxLength - word.length, i = 0; i < word.length; i++) {
-					wordArr[i + modifier] += word[i];
+		 */		
+		function merge(word1, word2) {
+			let merged = word1.length > word2.length ? word1 : word2;
+			let toMerge = merged == word1 ? word2 : word1;
+			let start = 0;
+			while(toMerge.length) {
+				for(let i = 0; i < toMerge.length; i++) {
+					let index = merged.indexOf(toMerge[i], start);
+					if(index == -1) {
+						if(i != toMerge.length - 1) {
+							continue;						
+						} else {
+							merged += toMerge;
+							break;
+						}
+					} else {
+						start = index + 1;
+						merged = merged.slice(0, index) + toMerge.slice(0, i) + merged.slice(index);
+						toMerge = toMerge.slice(i + 1);
+						break;
+					}
 				}
-			});
-			return removeDuplicate(wordArr);
-		} 
-		/**
-		 * test letter used to form all words
-		 * @param array [], String
-		 *
-		 * wordList : list of all words
-		 * letters  : string of combined letters
-		 *
-		 * returns String
-		 */
-		function letterUsed(wordList, letters) {
-			let usedFlags = new Array(letters.length).fill(0);
-			wordList.forEach(word => {
-				for(let start = 0, i = 0; i < word.length; i++) {
-					start = letters.indexOf(word[i], start);
-					usedFlags[start]++;
-				}
-			});
-			return letters.split("").reduce((acc, val, index) => acc + (usedFlags[index] ? val : "")); 
-		} 
-		/**
-		 * embed word
-		 * @param array []
-		 *
-		 * wordList : list of all words
-		 *
-		 * returns String
-		 */
-		function embedWord(wordList) {
-			return letterUsed(input, combineWords(input));
+			}
+			return merged;
 		} 
 		//default input
 		let input = ["one", "two", "three", "four", "five"];
-		let embedded = embedWord(input);
-		console.log(embedded, embedded.length);
 	});
 })();		
