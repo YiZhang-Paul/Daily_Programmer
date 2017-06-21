ce26b0922072007ea0bd2d19fa4ec95da9ceb661
YI ZHANG <scaccterran@gmail.com>
2017-06-20 23:39:10 -0400

    make square to hold all numbers

--- challenge_320/easy/spiralAscension.js
+++ challenge_320/easy/spiralAscension.js
@@ -1,5 +1,20 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
+		/**
+		 * make two dimensional array to hold all numbers
+		 * @param int
+		 *
+		 * num : total number of digits on each row and column
+		 *
+		 * returns array []  
+		 */
+		function makeSquare(num) {
+			return new Array(num).fill(new Array(num).fill(0));
+		}
+		//let input = prompt("Please Enter a Number: ");
+		let input = 5;
+		let square = makeSquare(input);
+		console.log(square);
 	});
 })();				
