5e773156714da9ca4ce4f678c81c4796d71b8809
YI ZHANG <scaccterran@gmail.com>
2017-06-21 17:41:46 -0400

    embed words

--- challenge_313/hard/embedWord.js
+++ challenge_313/hard/embedWord.js
@@ -2,6 +2,17 @@
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
 		/**
+		 * remove duplicate letters
+		 * @param array []
+		 *
+		 * combinedList : combined word list according to letter weights
+		 *
+		 * returns String
+		 */
+		function removeDuplicate(combinedList) {
+			return combinedList.map(block => Array.from(new Set(block.split(""))).join("")).join("");
+		} 
+		/**
 		 * combine all words according to 
 		 * weights of each letter
 		 * @param array []
@@ -14,27 +25,45 @@
 			let maxLength = Math.max(...wordList.map(word => word.length));
 			let wordArr = new Array(maxLength).fill("");
 			wordList.forEach(word => {
-				let modifier = maxLength - word.length;
-				for(let i = 0; i < word.length; i++) {
+				for(let modifier = maxLength - word.length, i = 0; i < word.length; i++) {
 					wordArr[i + modifier] += word[i];
 				}
 			});
-			return wordArr;
+			return removeDuplicate(wordArr);
 		} 
 		/**
-		 * remove duplicate letters
+		 * test letter used to form all words
+		 * @param array [], String
+		 *
+		 * wordList : list of all words
+		 * letters  : string of combined letters
+		 *
+		 * returns String
+		 */
+		function letterUsed(wordList, letters) {
+			let usedFlags = new Array(letters.length).fill(0);
+			wordList.forEach(word => {
+				for(let start = 0, i = 0; i < word.length; i++) {
+					start = letters.indexOf(word[i], start);
+					usedFlags[start]++;
+				}
+			});
+			return letters.split("").reduce((acc, val, index) => acc + (usedFlags[index] ? val : "")); 
+		} 
+		/**
+		 * embed word
 		 * @param array []
 		 *
-		 * combinedList : combined word list according to letter weights
+		 * wordList : list of all words
 		 *
-		 * returns array []
+		 * returns String
 		 */
-		function removeDuplicate(combinedList) {
-			return combinedList.map(block => Array.from(new Set(block.split(""))).join(""));
+		function embedWord(wordList) {
+			return letterUsed(input, combineWords(input));
 		} 
 		//default input
 		let input = ["one", "two", "three", "four", "five"];
-		let embedWord = removeDuplicate(combineWords(input)).join("");
-		console.log(embedWord, embedWord.length);
+		let embedded = embedWord(input);
+		console.log(embedded, embedded.length);
 	});
 })();		
