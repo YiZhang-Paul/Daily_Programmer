c180682f9c26469da20b6750eff9e35c2caed91a
YI ZHANG <scaccterran@gmail.com>
2017-06-21 20:22:33 -0400

    default input and challenge input

--- challenge_313/hard/embedWord.js
+++ challenge_313/hard/embedWord.js
@@ -34,7 +34,48 @@
 			}
 			return merged;
 		} 
+		/**
+		 * embed words
+		 * @param array []
+		 * 
+		 * wordList : list of all words
+		 *
+		 * returns String
+		 */
+		function embed(wordList) {
+			wordList = wordList.sort((word1, word2) => word1.length - word2.length);
+			wordList[0] = merge(wordList.pop(), wordList[0]);
+			return wordList.reduce((acc, val) => merge(acc, val));
+		} 
+		/**
+		 * get text file
+		 * @param String
+		 *
+		 * url : text file URL
+		 *
+		 * returns obj {}
+		 */
+		function getText(url) {
+			return new Promise((resolve, reject) => {
+				let xhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+				xhttp.onreadystatechange = function() {
+					if(this.readyState === 4 && this.status === 200) {
+						resolve(this.responseText.split("\n").map(word => word.trim()));
+					}
+				};
+				xhttp.open("GET", url, true);
+				xhttp.send();
+			});
+		} 
 		//default input
 		let input = ["one", "two", "three", "four", "five"];
+		let embedded = embed(input);
+		console.log(embedded, embedded.length);
+		//challenge input
+		let promise = getText("wordList.txt");
+		promise.then(result => {
+			let embedded = embed(result);
+			console.log(embedded, embedded.length); 	
+		});
 	});
 })();		
