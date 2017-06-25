f33ca56bf126c635d1e61f992ce8ed3b73bf737c
YI ZHANG <scaccterran@gmail.com>
2017-06-25 19:24:36 -0400

    all done

--- challenge_320/intermediate/war.js
+++ challenge_320/intermediate/war.js
@@ -12,10 +12,8 @@
 			constructor(deck1, deck2) {
 				this.player1 = new Player(deck1, 1);
 				this.player2 = new Player(deck2, 2);
-				console.log("initial");
-				console.log(this.player1.deck);
-				console.log(this.player2.deck);
-				this.cardPool = new CardPool();
+				this.battleCard = [];
+				this.warCard = [];
 				this.state = "battle";
 				this.proceedGame();
 			}
@@ -23,13 +21,8 @@
 			 * proceed a game
 			 */
 			proceedGame() {
-				let round = 1;
 				while(this.state != "finished") {
 					this[this.state]();
-					console.log("round " + round);
-					console.log(this.player1.deck);
-					console.log(this.player2.deck);
-					round++;
 				}
 			} 
 			/**
@@ -50,18 +43,29 @@
 				}
 			} 
 			/**
+			 * clear pool
+			 * @param array []
+			 *
+			 * pool : pool to be cleared
+			 *
+			 * returns array []
+			 */
+			clearPool(pool) {
+				return pool.splice(0, pool.length);
+			} 
+			/**
 			 * game states
 			 */
 			//battle state
 			battle() {
-				let card1 = this.player1.drawCard(1)[0];
-				let card2 = this.player2.drawCard(1)[0];
-				this.cardPool.battleCard.push(...[card1, card2].sort((a, b) => b - a));
-				if(card1 == card2) {
+				let faceUp1 = this.player1.drawCard(1)[0];
+				let faceUp2 = this.player2.drawCard(1)[0];
+				this.battleCard = [faceUp1, faceUp2].sort((a, b) => b - a);
+				if(faceUp1 == faceUp2) {
 					this.state = "war";
 				} else {
-					let winner = card1 > card2 ? this.player1 : this.player2;
-					winner.retrieveCard(this.state, this.cardPool);
+					let winner = faceUp1 > faceUp2 ? this.player1 : this.player2;
+					winner.retrieveCard(this);
 					this.checkGameEnd();
 				}
 			} 
@@ -69,22 +73,17 @@
 			war() {
 				this.checkGameEnd();
 				if(this.state != "finished") {
-					let warDecks = Math.min(this.player1.deck.length, this.player2.deck.length, 4);
-					let faceDowns1 = this.player1.drawCard(warDecks - 1);
-					let faceDowns2 = this.player2.drawCard(warDecks - 1);
+					let totalFaceDown = Math.min(this.player1.deck.length, this.player2.deck.length, 4) - 1;
+					let faceDowns1 = this.player1.drawCard(totalFaceDown);
+					let faceDowns2 = this.player2.drawCard(totalFaceDown);
 					let faceUp1 = this.player1.drawCard(1)[0];
 					let faceUp2 = this.player2.drawCard(1)[0];
-					if(faceUp1 == faceUp2) {
-						this.cardPool.battleCard.push(faceUp1, faceUp2);
-						this.cardPool.warCard.push([faceDowns1, faceDowns2]);
-					} else {
-						this.cardPool.warCard.push([[...faceDowns1, faceUp1], [...faceDowns2, faceUp2]]);
-					}
+					this.warCard.push([[...faceDowns1, faceUp1], [...faceDowns2, faceUp2]]);
 					if(faceUp1 == faceUp2) {
 						this.war();
 					} else {
 						let winner = faceUp1 > faceUp2 ? this.player1 : this.player2;
-						winner.retrieveCard(this.state, this.cardPool);
+						winner.retrieveCard(this);
 						this.state = "battle";
 						this.checkGameEnd();
 					}
@@ -120,48 +119,35 @@
 			} 
 			/**
 			 * retrieve card after a round
-			 * @param String, obj {}
+			 * @param obj {}
 			 *
-			 * state : current state of game
-			 * pool  : pool of current cards
+			 * game  : current game object
 			 */
-			retrieveCard(state, pool) {
-				if(state == "battle") {
-					this.deck = [...this.deck, ...pool.clearPool(pool.battleCard)];
-				} else {
-					for(let i = 0; i < pool.warCard.length; i++) {
-						let warCards = [pool.clearPool(pool.warCard[i])][0];
-						warCards = this.number == 1 ? [...warCards[0], ...warCards[1]] : [...warCards[1], ...warCards[0]];
-						this.deck = [...this.deck, ...warCards];
+			retrieveCard(game) {
+				if(game.state == "war") {
+					for(let i = game.warCard.length - 1; i >= 0; i--) {
+						let warCards = [game.clearPool(game.warCard[i])][0];
+						this.deck = [...this.deck, ...(this.number == 1 ? [...warCards[0], ...warCards[1]] : [...warCards[1], ...warCards[0]])];
 					}
-					this.deck = [...this.deck, ...pool.clearPool(pool.battleCard)];
-					pool.warCard = [];
+					game.warCard = [];
 				}
+				this.deck = [...this.deck, ...game.clearPool(game.battleCard)];
 			} 
 		} 
-		/**
-		 * card pool class
+		/** 
+		 * challenge input
 		 */
-		class CardPool {
-			constructor() {
-				this.battleCard = [];
-				this.warCard = [];
-			}
-			/**
-			 * clear pool
-			 * @param array []
-			 *
-			 * pool : pool to be cleared
-			 *
-			 * returns array []
-			 */
-			clearPool(pool) {
-				return pool.splice(0, pool.length);
-			} 
-		}
-		//challenge input
+		//game 1 
 		let deck1 = [5, 1, 13, 10, 11, 3, 2, 10, 4, 12, 5, 11, 10, 5, 7, 6, 6, 11, 9, 6, 3, 13, 6, 1, 8, 1]; 
     let deck2 = [9, 12, 8, 3, 11, 10, 1, 4, 2, 4, 7, 9, 13, 8, 2, 13, 7, 4, 2, 8, 9, 12, 3, 12, 7, 5];   
     let war = new War(deck1, deck2);
+    //game 2
+		deck1 = [3, 11, 6, 12, 2, 13, 5, 7, 10, 3, 10, 4, 12, 11, 1, 13, 12, 2, 1, 7, 10, 6, 12, 5, 8, 1]; 
+		deck2 = [9, 10, 7, 9, 5, 2, 6, 1, 11, 11, 7, 9, 3, 4, 8, 3, 4, 8, 8, 4, 6, 9, 13, 2, 13, 5]; 
+		war = new War(deck1, deck2);
+		//game 3
+		deck1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]; 
+		deck2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]; 
+   	war = new War(deck1, deck2);
 	});
 })();
