06866e21eead7ff1f8e3fdd43e7bf0a514b32525
YI ZHANG <scaccterran@gmail.com>
2017-06-25 16:22:50 -0400

    War, Player and CardPool class

--- challenge_320/intermediate/war.js
+++ challenge_320/intermediate/war.js
@@ -1,6 +1,40 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * war game class
+		 * @param array [], array []
+		 *
+		 * deck1 : deck for player 1
+		 * deck2 : deck for player 2
+		 */	
+		class War {
+			constructor(deck1, deck2) {
+				this.player1 = null;
+				this.player2 = null;
+				this.cardPool = null;
+			}
+		} 
+		/**
+		 * player class
+		 * @param array []
+		 *
+		 * deck : holding deck
+		 */
+		class Player {
+			constructor(deck) {
+				this.deck = deck;
+			}
+		} 
+		/**
+		 * card pool class
+		 */
+		class CardPool {
+			constructor() {
+				this.battleCard = [];
+				this.tiedCard = [];
+				this.warCard = [];
+			}
+		}  
 	});
 })();
