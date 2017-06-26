6a7e0bc2cdfbb66d18771e22f528e8d9db77c83c
YI ZHANG <scaccterran@gmail.com>
2017-06-26 00:22:46 -0400

    function to generate collatz tag

--- challenge_317/easy/collatzTag.js
+++ challenge_317/easy/collatzTag.js
@@ -1,6 +1,31 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * generate collatz tag 
+		 * @param String
+		 *
+		 * aString : string of a's
+		 * 
+		 * returns array []
+		 */ 
+		function collatzTag(aString) {
+			let tags = [];
+			while(aString.length > 1) {
+				aString = aString.slice(2) + (aString[0] == "a" ? "bc" : (aString[0] == "b" ? "a" : "aaa"));
+				tags.push(aString);
+			}
+			return tags;
+		} 
+		//challenge input 1
+		console.log("Challenge 1:");
+		collatzTag("aaa").forEach(tag => {
+			console.log(tag);
+		});
+		//challenge input 2
+		console.log("Challenge 2:");
+		collatzTag("aaaaaaa").forEach(tag => {
+			console.log(tag);
+		});
 	});
 })(); 		
