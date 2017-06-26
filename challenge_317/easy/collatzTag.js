db3a1c9d445c5e87d6eac17e28b42c6dbd72d973
YI ZHANG <scaccterran@gmail.com>
2017-06-26 02:29:28 -0400

    encoder/decoder

--- challenge_317/easy/collatzTag.js
+++ challenge_317/easy/collatzTag.js
@@ -16,16 +16,60 @@
 				tags.push(aString);
 			}
 			return tags;
+		}
+		/**
+		 * encoder/decoder
+		 * @param String
+		 *
+		 * code : code to be encoded/decoded
+		 *
+		 * returns String
+		 */
+		function converter(code) {
+			let converted;
+			switch(code) {
+				case "a" : case "100" :
+					converted = code == "a" ? "100" : "a";
+					break;
+				case "b" : case "010" :
+					converted = code == "b" ? "010" : "b";
+					break;
+				case "c" : case "001" :
+					converted = code == "c" ? "001" : "c";
+					break;		
+			}
+			return converted;
 		} 
+		/**
+		 * generate cyclic tag
+		 * @param String
+		 * 
+		 * aEncode : encoded string of a's
+		 *
+		 * returns array [] 
+		 */
+		function cyclicTag(aEncode) {
+			let splitEncode = encode => {
+				let segment = [];
+				for(let i = 0; i < encode.length; i += 3) {
+					segment.push(encode.slice(i, i + 3));
+				}	
+				return segment;
+			};
+			console.log(splitEncode(aEncode));
+			let tags = [];
+
+		} 
+		cyclicTag("100100100");
 		//challenge input 1
-		console.log("Challenge 1:");
+		//console.log("Challenge 1:");
 		collatzTag("aaa").forEach(tag => {
-			console.log(tag);
+			//console.log(tag);
 		});
 		//challenge input 2
-		console.log("Challenge 2:");
+		//console.log("Challenge 2:");
 		collatzTag("aaaaaaa").forEach(tag => {
-			console.log(tag);
+			//console.log(tag);
 		});
 	});
 })(); 		
