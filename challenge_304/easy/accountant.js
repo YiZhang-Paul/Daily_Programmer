6ff739a13c088853a5ed0b3cd71e0d82ba0ddede
YI ZHANG <scaccterran@gmail.com>
2017-06-27 02:17:29 -0400

    record class

--- challenge_304/easy/accountant.js
+++ challenge_304/easy/accountant.js
@@ -1,6 +1,22 @@
 /* jslint esversion: 6 */
 (() => {
   document.addEventListener("DOMContentLoaded", () => {
- 		
+ 		/**
+ 		 * journal record class
+ 		 * @param String, obj {}, float, float
+ 		 * 
+ 		 * acc    : account number
+ 		 * period : record period
+ 		 * debit  : debit entry 
+ 		 * credit : credit entry
+ 		 */
+ 		class Record {
+ 			constructor(acc, period, debit, credit) {
+ 				this.acc = acc;
+ 				this.period = period;
+ 				this.debit = debit;
+ 				this.credit = credit;
+ 			}
+ 		} 
   });
 })();    	  	
