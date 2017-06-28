f9828bfc50d7074f19472509f031f3d366eac9d6
YI ZHANG <scaccterran@gmail.com>
2017-06-28 02:07:14 -0400

    move function

--- challenge_303/easy/ricochet.js
+++ challenge_303/easy/ricochet.js
@@ -19,6 +19,21 @@
   			this.minY = 0;
   			this.maxY = grid.height;
   		}
+  		/**
+  		 * bounce particle
+  		 */
+  		bounce() {
+  			this.hVelocity *= this.xCord === 0 || this.xCord == this.maxX ? -1 : 1;
+  			this.vVelocity *= this.yCord === 0 || this.yCord == this.maxY ? -1 : 1;
+  		} 
+  		/**
+  		 * move particle
+  		 */
+  		move() {
+  			this.bounce();
+  			this.xCord += this.hVelocity;
+  			this.yCord += this.vVelocity;
+  		} 
   	} 
   	/**
   	 * grid class
@@ -33,6 +48,24 @@
   			this.height = height;
   		}
   	} 
+  	/**
+  	 * calculate ending corner, bounce time
+  	 * and total time traveld for a particle
+  	 * @param int, int, int
+  	 *
+  	 * width    : width of grid
+  	 * height   : height of grid
+  	 * velocity : particle movement speed
+  	 */
+  	function ricochet(width, height, velocity) {
+  		let grid = new Grid(width, height);
+  		let particle = new Particle(grid, velocity);
+  		console.log(grid, particle);
 
+  	} 
+  	//default input
+  	let input = [8, 3, 1];
+  	ricochet(...input);
+    input = [15, 4, 2];
   });
 })();  	
