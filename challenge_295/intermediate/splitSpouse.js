b850adabc7a0e20959d243d12d2b85c6f3d397ac
YI ZHANG <scaccterran@gmail.com>
2017-06-28 18:12:11 -0400

    get all seating arrangements

--- challenge_295/intermediate/splitSpouse.js
+++ challenge_295/intermediate/splitSpouse.js
@@ -16,5 +16,33 @@
 			}
 			return spouses.split("");
 		} 
+		/**
+		 * get permutation of all seatings
+		 * @param array [], int, String
+		 *
+		 * list       : list of all guests
+		 * totalGuest : total number of guests
+		 * curPermute : current permutation 
+		 *
+		 * returns array []
+		 */
+		function permuteGuest(list, totalGuest, curPermute = "") {
+			if(curPermute.length == totalGuest) {
+				return curPermute;
+			}
+			let permutation = [];
+			for(let i = 0; i < list.length; i++) {
+				let curGuest = list[i];
+				let otherGuest = [...list.slice(0, i), ...list.slice(i + 1)];
+				let result = permuteGuest(otherGuest, totalGuest, curPermute + curGuest);
+				if(Array.isArray(result)) {
+					permutation.push(...result);
+				} else {
+					permutation.push(result);
+				}
+			}
+			return permutation;
+		} 
+		console.log(permuteGuest(getSpouseList(3), 6));
 	});
 })();				
