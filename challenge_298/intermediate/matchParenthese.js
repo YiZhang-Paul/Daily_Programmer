492d50a1fb70cef6f18d08bcd2d164da3d4f728e
YI ZHANG <scaccterran@gmail.com>
2017-06-29 08:57:43 -0400

    all done

--- challenge_298/intermediate/matchParenthese.js
+++ challenge_298/intermediate/matchParenthese.js
@@ -13,7 +13,7 @@
 			let flags = new Array(test.length).fill(0).map((flag, index) => 
 				test[index] == "(" ? 1 : (test[index] == ")" ? 2 : 0));
 			let closeParenthese = flags.filter(flag => flag == 2).length;
-			for(let i = 0, openIndex = 0, closeIndex = -1; i < closeParenthese; i++) {
+			for(let i = 0, openIndex, closeIndex = -1; i < closeParenthese; i++) {
 				closeIndex = flags.indexOf(2, closeIndex + 1);
 				openIndex = flags.lastIndexOf(1, closeIndex);
 				if(openIndex != -1) {
@@ -21,24 +21,42 @@
 					flags[closeIndex] = 0;
 				}
 			}	
-			console.log(flags);		
+			return flags;		
+		} 
+		/**
+		 * highlight first unmatched parenthese found
+		 * @param String
+		 *
+		 * test : string to be tested
+		 */
+		function showUnmatched(test) {
+			let flags = matchParenthese(test);
+			let flagType = new Set(flags), index = null;
+			if(flagType.size > 1) {
+				index = flagType.has(1) ? flags.lastIndexOf(1) : flags.indexOf(2);
+			}
+			if(index === null) {
+				console.log(test);
+			} else {
+				console.log(`${test.slice(0, index)}%c${test[index]}%c${test.slice(index + 1)}`, "color : tomato;", "");
+			}
 		} 
 		//challenge input
 		let input = ")(asdf)))";
-		matchParenthese(input);
+		showUnmatched(input);
 		input = "((((asdf)))";
-		matchParenthese(input);
+		showUnmatched(input);
 		input = "((((asdf))";
-		matchParenthese(input);
+		showUnmatched(input);
 		input = "(ab)((cd)(asdf)))";
-		matchParenthese(input);
+		showUnmatched(input);
 		input = "(ab)((cd)(asdf)())";
-		matchParenthese(input);
+		showUnmatched(input);
 		input = "(ab)(((cd)(asdf)";
-		matchParenthese(input);
+		showUnmatched(input);
 		input = "(ab)(((cd)(asdf";
-		matchParenthese(input);
+		showUnmatched(input);
 		input = "(ab)(((cd)(asdf)))))";
-		matchParenthese(input);
+		showUnmatched(input);
 	});
 })();				
