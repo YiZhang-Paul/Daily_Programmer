e42a7fac69bb8b101be4da5e73085949a46aee11
YI ZHANG <scaccterran@gmail.com>
2017-06-29 06:26:31 -0400

    all done

--- challenge_302/intermediate/barChart.js
+++ challenge_302/intermediate/barChart.js
@@ -52,7 +52,10 @@
 			addXLabel(x1, x2, recordNum, board) {
 				let intervalX = (x2 - x1) / recordNum;
 				for(let i = 1, j = 0; i < board[board.length - 1].length; i += 2) {
-					board[board.length - 1][i] = x1 + intervalX * j++;
+					let label = (x1 + intervalX * j++).toString();
+					for(let k = 0; k < board.length; k++) {
+						board[k][i] = k == board.length - 1 ? label : " ".repeat(label.length);
+					}
 				}
 			} 
 			/**
@@ -63,8 +66,10 @@
 			 * board    : chart board
 			 */ 
 			addYLabel(y2, board) {
-				for(let i = 0, j = 0; i < board.length - 1; i++) {
-					board[i][0] = y2 - j++;
+				let colWidth = y2.toString().length;
+				for(let i = 0, j = 0; i < board.length; i++) {
+					let label = (y2 - j++).toString();
+					board[i][0] = i < board.length - 1 ? " ".repeat(colWidth - label.length) + label : " ".repeat(colWidth);
 				}
 			} 
 			/**
@@ -76,9 +81,9 @@
 			 */	
 			addRecord(record, board) {
 				record = record.split(" ");
-				let col = board[board.length - 1].indexOf(Number(record[0])) + 1;
-				for(let i = board.length - 2; i > board.length - 2 - record[2]; i--) {
-					board[i][col] = "*";
+				let col = board[board.length - 1].indexOf(record[0]) + 1;
+				for(let i = 0; i < board.length; i++) {
+					board[i][col] = i > board.length - 2 - record[2] && i <= board.length - 2 ? "*" : " ";
 				}
 			}
 			/**
@@ -93,7 +98,13 @@
 				});
 			} 
 		} 
+		//default input
+		console.log("Default Input: ");
 		let chart = new Chart(...[140, 190, 1, 8, 5, "140 150 1", "150 160 0", "160 170 7", "170 180 6", "180 190 2"]);
 		chart.displayChart();	
+		//challenge input
+		console.log("Challenge Input: ");
+    chart = new Chart(...[0, 50, 1, 10, 5, "0 10 1", "10 20 3", "20 30 6", "30 40 4", "40 50 2"]);
+    chart.displayChart();
 	});
 })();				
