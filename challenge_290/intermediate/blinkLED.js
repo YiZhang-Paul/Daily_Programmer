63bad3e8fb9c50b8c9b641adc8a958cb6cfd70a1
YI ZHANG <scaccterran@gmail.com>
2017-06-30 01:50:43 -0400

    allow loops and rotations

--- challenge_290/intermediate/blinkLED.js
+++ challenge_290/intermediate/blinkLED.js
@@ -33,6 +33,18 @@
 				this.registers[register.toUpperCase()] = Number(number);
 			} 
 			/**
+			 * loop instructions
+			 * @param String, String
+			 *
+			 * instructions : all instructions
+			 * loop         : loop label
+			 *
+			 * returns int
+			 */
+			enterLoop(instructions, loop) {
+				return instructions.findIndex(instruction => instruction.search(loop) === 0);
+			} 
+			/**
 			 * display LEDs
 			 * @param String
 			 *
@@ -42,23 +54,47 @@
 				console.log(this.toBinary(register).split("").map(bit => Number(bit) === 0 ? "." : "*").join(""));
 			} 
 			/**
+			 * rotate left
+			 */
+			rotateLeft() {
+				let registerA = this.toBinary(this.registers.A);
+				this.registers.A = parseInt(registerA.slice(1) + registerA.slice(0, 1), 2);
+			} 
+			/**
+			 * rotate right
+			 */
+			rotateRight() {
+				let registerA = this.toBinary(this.registers.A);
+				this.registers.A = parseInt(registerA.slice(-1) + registerA.slice(0, -1), 2);
+			} 
+			/**
 			 * process instructions
 			 * @param String
 			 *
 			 * instructions : instructions to be processed
 			 */
 			processInstruction(instructions) {
-				instructions.split("\n").forEach(instruction => {
-					instruction = instruction.trim().split(/[, ]/);
+				instructions = instructions.split("\n").map(instruction => instruction.trim());
+				for(let i = 0; i < instructions.length; i++) {
+					let instruction = instructions[i].split(/[, ]/);
 					if(instruction[0] == "ld") {
 						this.setRegister(instruction[1], instruction[2]);
 					} else if(instruction[0] == "out") {
 						this.displayLED(this.registers[instruction[2].toUpperCase()]);
+					} else if(instruction[0] == "djnz") {
+						if(--this.registers.B) {
+							i = this.enterLoop(instructions, instruction[1]) - 1;
+						}
+					} else if(instruction[0] == "rlca") {
+						this.rotateLeft();
+					} else if(instruction[0] == "rrca") {
+						this.rotateRight();
 					}
-				});
+				}
 			} 
 		} 
 		//part 1 challenge input
+		console.log("Challenge Input 1: ");
 		let input = `ld a,14
 								 out (0),a
 								 ld a,12
@@ -72,6 +108,37 @@
 								 ld a,14
 								 out (0),a`;	
 		let led = new LED();	
-		led.processInstruction(input);						 	
+		led.processInstruction(input);	
+		//part 2 challenge input
+		console.log("Challenge Input 2: ");
+		input = `ld b,3
+
+					 triple:
+					   ld a,126
+					   out (0),a
+					   ld a,60
+					   out (0),a
+					   ld a,24
+					   out (0),a
+					   djnz triple`;	
+		led.processInstruction(input);			   
+		console.log("Challenge Input 3: ");
+	  input = `  ld a,1
+						   ld b,9
+						 
+						 loop:
+						   out (0),a
+						   rlca
+						   djnz loop`;
+		led.processInstruction(input);			   
+		console.log("Challenge Input 4: ");
+		input = `  ld a,2
+						   ld b,9
+						 
+						 loop:
+						   out (0),a
+						   rrca
+						   djnz loop`;				   					   			 	
+		led.processInstruction(input);			   
 	});
 })();			
