2517b69cfab1f95efd892fe135fba9eddbfbb615
YI ZHANG <scaccterran@gmail.com>
2017-07-02 00:24:35 -0400

    change minute to word

--- challenge_321/easy/talkingClock.js
+++ challenge_321/easy/talkingClock.js
@@ -11,10 +11,10 @@
  		 */
  		function timeToWord(time) {
  			time = time.split(":");	
- 			let hour = hourToWord(Number(time[0]));
- 			let minute = time[1] <= 20 ? numToWord(Number(time[1])) : numToWord(Math.floor(Number(time[1]) / 10) * 10) + numToWord(Number(time[1]) % 10);
- 			let dayNight = time[0] / 12 >= 0 ? "PM" : "AM";
- 			return `It's ${hour} ${minute} ${dayNight}`;
+ 			let hour = hourToWord(time[0]);
+ 			let minute = minuteToWord(time[1]);
+ 			let dayNight = Number(time[0]) / 12 >= 1 ? "pm" : "am";
+ 			return `It's ${hour}${minute ? " " + minute : minute} ${dayNight}`;
  		} 
  		/**
  		 * translate hour to word
@@ -25,8 +25,8 @@
  		 * returns String
  		 */
  		function hourToWord(hour) {
- 			let word;
- 			switch(hour % 12) {
+ 			let word = "";
+ 			switch(Number(hour) % 12) {
  				case 0 : case 12 : 
  					word = "twelve";
  					break;
@@ -63,6 +63,98 @@
  				case 11 : 
  					word = "eleven";
  					break;										
+ 			}
+ 			return word;
+ 		} 
+ 		/**
+ 		 *
+ 		 * translate minute to word
+ 		 * @param String
+ 		 *
+ 		 * minute : minute to be translated
+ 		 *
+ 		 * returns String
+ 		 */
+ 		function minuteToWord(minute) {
+ 			let word = "";
+ 			if(Number(minute) < 10 && minute.length == 2) {
+ 				return Number(minute) === 0 ? word : "oh " + minuteToWord(minute[1]);
+ 			} else if(Number(minute) > 20 && Number(minute) % 10 && minute.length == 2) {
+ 				return minuteToWord(Math.floor(Number(minute) / 10) * 10) + " " + minuteToWord(Number(minute) % 10);
+ 			}
+ 			switch(Number(minute)) {
+ 				case 0 : 
+ 				 	word = "oh";
+ 				 	break;
+ 				case 1 : 
+ 					word = "one";
+ 					break;
+ 				case 2 : 
+ 					word = "two";
+ 					break;
+ 				case 3 : 
+ 					word = "three";
+ 					break;
+ 				case 4 : 
+ 					word = "four";
+ 					break;
+ 				case 5 : 
+ 					word = "five";
+ 					break;	
+ 				case 6 : 
+ 					word = "six";
+ 					break;	
+ 				case 7 : 
+ 					word = "seven";
+ 					break;
+ 				case 8 : 
+ 					word = "eight";
+ 					break;
+ 				case 9 : 
+ 					word = "nine";
+ 					break;
+ 				case 10 : 
+ 					word = "ten";
+ 					break;
+ 				case 11 : 
+ 					word = "eleven";
+ 					break;	
+ 				case 12 : 
+ 					word = "twelve";
+ 					break;	
+ 				case 13 : 
+ 					word = "thirteen";
+ 					break;
+ 				case 14 : 
+ 					word = "fourteen";
+ 					break;
+ 				case 15 : 
+ 					word = "fifteen";
+ 					break;
+ 				case 16 : 
+ 					word = "sixteen";
+ 					break;
+ 				case 17 : 
+ 					word = "seventeen";
+ 					break;
+ 				case 18 : 
+ 					word = "eighteen";
+ 					break;		
+ 				case 19 : 
+ 					word = "nineteen";
+ 					break;
+ 				case 20 : 
+ 					word = "twenty";
+ 					break;	
+ 				case 30 : 
+ 					word = "thirty";
+ 					break;
+ 				case 40 : 
+ 					word = "fourty";
+ 					break;
+ 				case 50 : 
+ 					word = "fifty";
+ 					break;						 
  			}
  			return word;
  		} 
