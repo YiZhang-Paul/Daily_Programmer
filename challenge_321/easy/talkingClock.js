1b689dab668b1d7669e37fdbc31a1f7a6014ed50
YI ZHANG <scaccterran@gmail.com>
2017-07-02 23:42:19 -0400

    translate table from time to word

--- challenge_321/easy/talkingClock.js
+++ challenge_321/easy/talkingClock.js
@@ -2,6 +2,17 @@
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
 		/**
+		 * look up table for time to word convertion
+		 */		
+		let translateTable = {
+			0 : "oh", 1 : "one", 2 : "two", 3 : "three", 4 : "four",
+			5 : "five", 6 : "six", 7 : "seven", 8 : "eight", 9 : "nine", 
+			10 : "ten", 11 : "eleven", 12 : "twelve", 13 : "thirteen", 
+			14 : "fourteen", 15 : "fifteen", 16 : "sixteen", 17 : "seventeen",
+			18 : "eighteen", 19 : "nineteen", 20 : "twenty", 30 : "thirty",
+			40 : "fourty", 50 : "fifty"
+		};
+		/**
 		 * output time in words
 		 * @param String
 		 *
@@ -158,92 +169,6 @@
  			}
  			return word;
  		} 
- 		/**
- 		 * traslate number to words
- 		 * @param String
- 		 *
- 		 * number : number to be translated
- 		 *
- 		 * returns String
- 		 */
- 		function numToWord(number) {
- 			let word;
- 			switch(number) {
- 				case 0 :
- 					word = "";
- 					break;
- 				case 1 : 
- 					word = "one";
- 					break;
- 				case 2 : 
- 					word = "two";
- 					break;
- 				case 3 : 
- 					word = "three";
- 					break;
- 				case 4 : 
- 					word = "four";
- 					break;
- 				case 5 : 
- 					word = "five";
- 					break;	
- 				case 6 : 
- 					word = "six";
- 					break;	
- 				case 7 : 
- 					word = "seven";
- 					break;
- 				case 8 : 
- 					word = "eight";
- 					break;
- 				case 9 : 
- 					word = "nine";
- 					break;
- 				case 10 : 
- 					word = "ten";
- 					break;
- 				case 11 : 
- 					word = "eleven";
- 					break;	
- 				case 12 : 
- 					word = "twelve";
- 					break;	
- 				case 13 : 
- 					word = "thirteen";
- 					break;
- 				case 14 : 
- 					word = "fourteen";
- 					break;
- 				case 15 : 
- 					word = "fifteen";
- 					break;
- 				case 16 : 
- 					word = "sixteen";
- 					break;
- 				case 17 : 
- 					word = "seventeen";
- 					break;
- 				case 18 : 
- 					word = "eighteen";
- 					break;		
- 				case 19 : 
- 					word = "nineteen";
- 					break;
- 				case 20 : 
- 					word = "twenty";
- 					break;	
- 				case 30 : 
- 					word = "thirty";
- 					break;
- 				case 40 : 
- 					word = "fourty";
- 					break;
- 				case 50 : 
- 					word = "fifty";
- 					break;														
- 			}
- 			return word;
- 		}
  		//default inputs
  		let input = "00:00";
  		console.log(timeToWord(input));
