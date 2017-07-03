54e4865d6f7d02ca51309dcc248c3827919f5dae
YI ZHANG <scaccterran@gmail.com>
2017-07-03 01:12:19 -0400

    construct dictionary

--- challenge_321/intermediate/cipherSolver.js
+++ challenge_321/intermediate/cipherSolver.js
@@ -1,5 +1,18 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
+		/**
+		 * construct dictionary
+		 *
+		 * returns obj {}
+		 */
+		function makeDictionary() {
+			let dictionary = new Map();
+			for(let i = 0, j = "a".charCodeAt(); i < 26; i++) {
+				dictionary.set(String.fromCharCode(j), i + 1);
+				dictionary.set(i + 1, String.fromCharCode(j++));
+			}
+			return dictionary;
+		} 
 	});
 })();		
