c7f264e1260e29887ea2187f3f65136e4c37ff2c
YI ZHANG <scaccterran@gmail.com>
2017-07-04 07:34:06 -0400

    fill up mutual relationships

--- challenge_304/intermediate/sortNumber.js
+++ challenge_304/intermediate/sortNumber.js
@@ -94,6 +94,40 @@
 		 */
 		function fillTable(num1, num2, table) {
 			table[getKey(num1, num2, table)] = Math.min(num1, num2);
+			fillMutual(num1, num2, table);
+			fillMutual(num2, num1, table);
+		} 
+		/**
+		 * check mutual relationship between three numbers
+		 * @param int, int, int, table
+		 * 
+		 * num1  : number 1 
+		 * num2  : number 2
+		 * num3  : number 3
+		 * table : relation table 
+		 *
+		 * returns boolean
+		 */
+		function canLink(num1, num2, num3, table) {
+			let ascend = isSmaller(num1, num2, table) && isSmaller(num2, num3, table);
+			let descend = isLarger(num1, num2, table) && isLarger(num2, num3, table);
+			return ascend || descend;
+		} 
+		/**
+		 * check and fill up mutual relationship
+		 * @param int, int, obj {}
+		 *
+		 * num1  : number 1
+		 * num2  : number 2
+		 * table : relation table 
+		 */
+		function fillMutual(num1, num2, table) {
+			let num1Relation = knownRelation(num1, table);
+			for(let i = 0; i < num1Relation.length; i++) {
+				if(canLink(num2, num1, num1Relation[i], table)) {
+					table[getKey(num2, num1Relation[i], table)] = Math.min(num2, num1Relation[i]);
+				}
+			}
 		} 
 		/**
 		 * compare numbers 5 at a time
@@ -114,7 +148,9 @@
 		let input = [107, 47, 102, 64, 50, 100, 28, 91, 27, 5, 22, 114, 23, 42, 13, 3, 93, 8, 92, 79, 53, 83, 63, 7, 15, 66, 105, 57, 14, 65, 58, 113, 112, 1, 62, 103, 120, 72, 111, 51, 9, 36, 119, 99, 30, 20, 25, 84, 16, 116, 98, 18, 37, 108, 10, 80, 101, 35, 75, 39, 109, 17, 38, 117, 60, 46, 85, 31, 41, 12, 29, 26, 74, 77, 21, 4, 70, 61, 88, 44, 49, 94, 122, 2, 97, 73, 69, 71, 86, 45, 96, 104, 89, 68, 40, 6, 87, 115, 54, 123, 125, 90, 32, 118, 52, 11, 33, 106, 95, 76, 19, 82, 56, 121, 55, 34, 24, 43, 124, 81, 48, 110, 78, 67, 59];
 		let table = makeTable(input);
 		compareNumber(input.slice(0, 5), table);
-		compareNumber(input.slice(5, 10), table);
+		compareNumber(input.slice(4, 9), table);
 		fillTable(102, 100, table);
+		console.log(table);
+		console.log(Object.keys(table).filter(key => table[key]));
 	});
 })();		
