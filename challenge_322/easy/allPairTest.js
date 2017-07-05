ecba488beb6921083833a09d36fee8e1df9073cc
YI ZHANG <scaccterran@gmail.com>
2017-07-05 07:42:52 -0400

    permute all test pairs for an input set

--- challenge_322/easy/allPairTest.js
+++ challenge_322/easy/allPairTest.js
@@ -25,10 +25,49 @@
 			}
 			return permutation;
 		}
+		/**
+		 * get permutation of testing pairs 
+		 * for every input set
+		 * @param String 
+		 *
+		 * set     : input set
+		 * curPair : current pair
+		 * 
+		 * returns array [] 
+		 */
+		function permutePairs(set, curPair = "") {
+			if(curPair.length == 2) {
+				return curPair;
+			}
+			let permutation = [];
+			for(let i = 0; i < set.length; i++) {
+				let result = permutePairs(set.slice(i + 1), curPair + set[i]);
+				if(Array.isArray(result)) {
+					permutation.push(...result);
+				} else {
+					permutation.push(result);
+				}
+			}
+			return permutation;
+		} 
+		/**
+		 * generate all testing pairs
+		 * @param array []
+		 *
+		 * input : input sets
+		 *
+		 * returns array [] 
+		 */
+		function getTestPairs(input) {
+			let allInput = permuteInput(input);
+			console.log(allInput);
+		} 
 		//challenge input
 		let input = [['0', '1'], ['A', 'B', 'C'], ['D', 'E', 'F', 'G']];
-		console.log(permuteInput(input));
+		console.log(getTestPairs(input));
 		input = [['0', '1', '2', '3'], ['A', 'B', 'C', 'D'], ['E', 'F', 'G', 'H', 'I']];
+		console.log(getTestPairs(input));
 		input = [['0', '1', '2', '3', '4'], ['A', 'B', 'C', 'D', 'E'], ['F', 'G', 'H', 'I'], ['J', 'K', 'L']];
+		console.log(getTestPairs(input));
 	});
 })();			
