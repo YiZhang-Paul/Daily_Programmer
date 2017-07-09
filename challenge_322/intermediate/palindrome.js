0d658805fe3f038cd779f9a7c7bb5deba4672d91
YI ZHANG <scaccterran@gmail.com>
2017-07-09 05:27:05 -0400

    return all numbers in range

--- challenge_322/intermediate/palindrome.js
+++ challenge_322/intermediate/palindrome.js
@@ -1,6 +1,23 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * generate all possible values that 
+		 * have factors of a given string length
+		 * @param int
+		 *
+		 * length : string length of given number
+		 *
+		 * returns array []
+		 */
+		function allNumber(length) {
+			let lowest = length == 1 ? 0 : Math.pow(10, length - 1);
+			let highest = Number("9".repeat(length));
+			let allNums = [];
+			for(let i = Math.pow(lowest, 2), j = Math.pow(highest, 2); i <= j; i++) {
+				allNums.push(i);
+			}
+			return allNums;
+		} 
 	});
 })();		
