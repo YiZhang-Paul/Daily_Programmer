d22e3cbf844e89b7d5afcdf8fdae4107b1c0f648
YI ZHANG <scaccterran@gmail.com>
2017-07-10 08:35:13 -0400

    all done

--- challenge_292/easy/rangeParsing.js
+++ challenge_292/easy/rangeParsing.js
@@ -29,9 +29,9 @@
 		 * returns array []
 		 */
 		function numInRange(num1, num2, step = 1) {
-			num2 = Number(nextNum(num1, num2)); 
+			let [start, end] = [Number(num1) + step, Number(nextNum(num1, num2))];
 			let nums = [];
-			for(let i = Number(num1) + step; i <= num2; i += step) {
+			for(let i = start; i <= end; i += step) {
 				nums.push(i.toString());
 			}
 			return nums;
@@ -45,24 +45,21 @@
 	   * returns String
 	   */
 	  function expandList(notation) {
-	  	notation = notation.match(/\d+|[,.:-]/g);
+	  	notation = notation.match(/\d+|[,:-]|\.{2}/g);
 	  	let list = [notation[0]];
 	  	for(let i = 1; i < notation.length; i++) {
-	  		let [lastNum, curNum] = [list[list.length - 1], notation[i + 1]];
-	  		switch(notation[i]) {
+	  		let [lastNum, operator, curNum] = [list[list.length - 1], notation[i], notation[i + 1]];
+	  		switch(operator) {
 	  			case "," :
 	  				list.push(nextNum(lastNum, curNum));
 	  				break;
-	  			case "-" :
-	  				list = [...list, ...numInRange(lastNum, curNum)];
-	  				break;
+	  			case "-" : 
+	  			case ".." : 
 	  			case ":" :
-	  				let step = notation[i + 2] == ":" ? Number(notation[i + 3]) : 1;
-	  				i += notation[i + 2] == ":" ? 3 : 0;
+	  				let hasStep = operator == ":" && notation[i + 2] == ":";
+	  				let step = hasStep ? Number(notation[i + 3]) : 1;
+	  				i += hasStep ? 3 : 0;
 	  				list = [...list, ...numInRange(lastNum, curNum, step)];
-	  				break;
-	  			case "." :
-	  				list = [...list, ...numInRange(lastNum, notation[i++ + 2])];
 	  				break;
 	  		}
 	  	}
@@ -70,16 +67,16 @@
 	  }  
 	  //challenge input
 	  let input = "1,3,7,2,4,1";
-	  console.log(`${input} => ${expandList(input)}`);
+	  console.log(`${input} => %c${expandList(input)}`, "color : red;");
 		input = "1-3,1-2";
-	  console.log(`${input} => ${expandList(input)}`);
+	  console.log(`${input} => %c${expandList(input)}`, "color : red;");
 		input = "1:5:2";
-	  console.log(`${input} => ${expandList(input)}`);
+	  console.log(`${input} => %c${expandList(input)}`, "color : red;");
 		input = "104-2";
-	  console.log(`${input} => ${expandList(input)}`);
+	  console.log(`${input} => %c${expandList(input)}`, "color : red;");
 		input = "104..02";
-	  console.log(`${input} => ${expandList(input)}`);
+	  console.log(`${input} => %c${expandList(input)}`, "color : red;");
 		input = "545,64:11";
-	  console.log(`${input} => ${expandList(input)}`);
+	  console.log(`${input} => %c${expandList(input)}`, "color : red;");
 	});
 })();		
