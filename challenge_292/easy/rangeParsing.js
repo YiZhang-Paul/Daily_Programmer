236c917d64c609d8a917c52f221cb3ffe53b58f0
YI ZHANG <scaccterran@gmail.com>
2017-07-10 05:38:20 -0400

    function to process '-' separator

--- challenge_292/easy/rangeParsing.js
+++ challenge_292/easy/rangeParsing.js
@@ -16,6 +16,23 @@
 			}
 			return num2;
 		} 
+		/**
+		 * find all numbers within a range
+		 * @param int, int
+		 *
+		 * num1 : starting number
+		 * num2 : ending number
+		 *
+		 * returns array []
+		 */
+		function numInRange(num1, num2) {
+			num2 = nextNum(num1, num2); 
+			let nums = [];
+			for(let i = num1 + 1; i <= num2; i++) {
+				nums.push(i);
+			}
+			return nums;
+		}
 	  /**
 	   * expand number list 
 	   * @param String
@@ -27,13 +44,15 @@
 	  function expandList(notation) {
 	  	notation = notation.split("");
 	  	let list = [Number(notation[0])];
-	  	for(let i = 0; i < notation.length; i++) {
+	  	for(let i = 1; i < notation.length; i++) {
 	  		let [lastNum, curNum] = [list[list.length - 1], Number(notation[i + 1])];
 	  		switch(notation[i]) {
 	  			case "," :
 	  				list.push(nextNum(lastNum, curNum));
 	  				break;
 	  			case "-" :
+	  				list = [...list, ...numInRange(lastNum, curNum)];
+	  				break;
 	  			case ":" :
 	  			case "." :
 	  		}
@@ -42,8 +61,9 @@
 	  }  
 	  //challenge input
 	  let input = "1,3,7,2,4,1";
-	  console.log(expandList("1,3,7,2,4,1"));
+	  console.log(`${input} => ${expandList(input)}`);
 		input = "1-3,1-2";
+	  console.log(`${input} => ${expandList(input)}`);
 		input = "1:5:2";
 		input = "104-2";
 		input = "104..02";
