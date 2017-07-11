8606892ef690b44df027b8abf5a0e013ca42b9f0
YI ZHANG <scaccterran@gmail.com>
2017-07-11 00:31:24 -0400

    basic challenge complete

--- challenge_287/easy/kaprekarRoutine.js
+++ challenge_287/easy/kaprekarRoutine.js
@@ -1,6 +1,22 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * find largest digit in a number
+		 * @param int
+		 * 
+		 * number : number to be examined
+		 *
+		 * returns int
+		 */
+		function largestDigit(number) {
+			return Math.max(...number.toString().split("").map(digit => Number(digit)));
+		} 
+		//default input
+		console.log(`largestDigit(1234) -> ${largestDigit(1234)}`);
+		console.log(`largestDigit(3253) -> ${largestDigit(3253)}`);
+		console.log(`largestDigit(9800) -> ${largestDigit(9800)}`);
+		console.log(`largestDigit(3333) -> ${largestDigit(3333)}`);
+		console.log(`largestDigit(120) -> ${largestDigit(120)}`);
 	});
 })();			
