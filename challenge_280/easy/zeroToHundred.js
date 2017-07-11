66e824f2e7bea3bbbebe4c878717e7e2ea78a4a0
YI ZHANG <scaccterran@gmail.com>
2017-07-11 14:26:01 -0400

    all done

--- challenge_280/easy/zeroToHundred.js
+++ challenge_280/easy/zeroToHundred.js
@@ -10,16 +10,7 @@
 		 * returns boolean
 		 */
 		function rightHandValid(pattern) {
-			if(!pattern[1] && pattern.slice(2).some(num => num)) {
-				return false;
-			} else {
-				for(let i = 2; i < pattern.length; i++) {
-					if(!pattern[i - 1] && pattern[i]) {
-						return false;
-					}
-				}
-			}
-			return true;
+			return !pattern.slice(2).some((digit, index) => digit && !pattern[index + 2 - 1]);
 		}
 		/**
 		 * count with right hand
@@ -44,21 +35,19 @@
 			pattern = pattern.split("").map(digit => Number(digit));
 			let leftHand = pattern.slice(0, pattern.length * 0.5).reverse();
 			let rightHand = pattern.slice(pattern.length * 0.5);
-			if(!rightHandValid(leftHand) || !rightHandValid(rightHand)) {
-				return "Invalid";
-			}
-			console.log(rightHandCount(rightHand));
+			let invalid = !rightHandValid(leftHand) || !rightHandValid(rightHand);
+			return invalid ? "Invalid" : rightHandCount(leftHand) * 10 + rightHandCount(rightHand);
 		} 	
 		//challenge input
 		let input = "0111011100";
-		console.log(countWithHand(input));
+		console.log(`${input} -> %c${countWithHand(input)}`, "color : red;");
     input = "1010010000";
-		console.log(countWithHand(input));
+		console.log(`${input} -> %c${countWithHand(input)}`, "color : red;");
     input = "0011101110";
-		console.log(countWithHand(input));
+		console.log(`${input} -> %c${countWithHand(input)}`, "color : red;");
     input = "0000110000";
-		console.log(countWithHand(input));
+		console.log(`${input} -> %c${countWithHand(input)}`, "color : red;");
     input = "1111110001";
-		console.log(countWithHand(input));
+		console.log(`${input} -> %c${countWithHand(input)}`, "color : red;");
 	});
 })();		
