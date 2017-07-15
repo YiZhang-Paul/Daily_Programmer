d6e322ca9184bda5ba7b28454912f649348d7ae7
YI ZHANG <scaccterran@gmail.com>
2017-07-15 11:06:44 -0400

    create inverted table

--- challenge_262/easy/maybeNumeric.js
+++ challenge_262/easy/maybeNumeric.js
@@ -45,6 +45,42 @@
 			return `${testStr} (${isNumber(testStr) ? numType(testStr) : "String"})`;
 		} 
 		/**
+		 * parse separate values
+		 * @param String
+		 *
+		 * values : string containing separated values
+		 *
+		 * returns String
+		 */
+		function parseSeparate(values) {
+			return values.split("\n").map(row => row.trim().split("`"));
+		} 
+		/**
+		 * get a column of elements in two-dimensional array
+		 * @param array [], int
+		 *
+		 * array : two-dimensional array
+		 * index : column index
+		 *
+		 * returns array []
+		 */
+		function getColumn(array, index) {
+			return array.map(row => row[index]);
+		} 
+		/**
+		 * get table dimensions
+		 * @param array []
+		 *
+		 * parsed : value parsed
+		 *
+		 * returns array []
+		 */
+		function getDimension(parsed) {
+			let colLen = parsed[0].map((col, index) => Math.max(...getColumn(parsed, index).map(row => row.length)));
+			let rowLen = colLen.reduce((acc, val) => acc + val) + parsed[0].length - 1;
+			return [colLen, rowLen];
+		} 
+		/**
 		 * make parse table
 		 * @param String
 		 *
@@ -53,15 +89,52 @@
 		 * returns String
 		 */
 		function makeParseTable(values) {
-			let parsed = values.split("\n").map(row => row.trim().split("`"));
-			let colLen = parsed[0].map((col, index) => Math.max(col.length, parsed[1][index].length));
-			let rowLen = colLen.reduce((acc, val) => acc + val) + parsed[0].length - 1;
+			let parsed = parseSeparate(values);
+			let [colLen, rowLen] = [...getDimension(parsed)];
 			let table = `|${"-".repeat(rowLen)}|` + "\n";
 			parsed.forEach(row => {
 				row = row.map((col, index) => col + " ".repeat(colLen[index] - col.length)).join("|");
 				table += `|${row}|` + "\n" + `|${"-".repeat(rowLen)}|` + "\n";
 			});
 			return table;
+		} 
+		/**
+		 * combine non-homogeneous rows
+		 * @param array []
+		 *
+		 * parsed : value parsed
+		 *
+		 * returns array [] 
+		 */
+		function combineRow(parsed) {
+			parsed[0].forEach((col, index) => {
+				let colItems = getColumn(parsed, index);
+				if(!colItems.every(item => isNumber(item)) &&
+					 !colItems.every(item => !isNumber(item))) {
+					parsed[0][index] = colItems.join(" ");
+					for(let i = 1; i < parsed.length; i++) {
+						parsed[i][index] = "";
+					}
+				}
+			});
+			return parsed;
+		} 
+		/**
+		 * make inverted table
+		 * @param String
+		 *
+		 * values : string containing separated values
+		 *
+		 * returns String 
+		 */
+		function makeInvertedTable(values) {
+			let parsed = combineRow(parseSeparate(values));
+			let [colLen, rowLen] = [...getDimension(parsed)];
+			let table = `|${"-".repeat(rowLen)}|` + "\n";
+			parsed.forEach(row => {
+				table += `|${row.map((col, index) => col + " ".repeat(colLen[index] - col.length)).join("|")}|` + "\n";
+			});
+			return table + `|${"-".repeat(rowLen)}|` + "\n";
 		} 
 		//default input
 		console.log(`%cDefault Input: `, "color : red;");
@@ -86,5 +159,11 @@
 		input = `2015 4 4\`Challenge #\`261\`Easy
              234.2\`234ggf 45\`00\`number string number (0)`;
     console.log(makeParseTable(input));
+    //bonus 3 input
+		console.log(`%cBonus 3 Input: `, "color : red;");
+		//console.log(makeInvertedTable(input));
+		input = `2015 4 4\`Challenge #\`261\`Easy
+ 						 234.2\`234ggf 45\`0\`8`;
+		console.log(makeInvertedTable(input));
 	});
 })();		
