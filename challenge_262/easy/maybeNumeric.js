292a511b9d51a165373075c9204b160ddc03f971
YI ZHANG <scaccterran@gmail.com>
2017-07-15 08:21:02 -0400

    determine special numbers

--- challenge_262/easy/maybeNumeric.js
+++ challenge_262/easy/maybeNumeric.js
@@ -12,15 +12,27 @@
 		function maybeNumeric(test) {
 			let strings = test.trim().split(" ");
 			let result = strings.every(string => /^(\d+\.?\d+|\.\d+)(e|e[-+])?\d+$/g.test(string)) ? "Number" : "String";
-			if(strings.length > 1) {
-				result += " -> Array";
-			} else if(strings[0].indexOf("e") != -1) {
-				result += " -> Exponent Notation";
-			} else if(strings[0].length >= 10) {
-				result += " -> Big Number";
-			}
-			return `${test.trim()} (${result})`;
+			return `${test.trim()} (${result}${result == "Number" ? isSpecial(test) : ""})`;
 		}
+		/**
+		 * check special numbers
+		 * @param String
+		 *
+		 * test : number to be tested
+		 *
+		 * returns String
+		 */
+		function isSpecial(test) {
+			let result = "";
+			if(test.trim().split(" ").length > 1) {
+				result = " -> Array";
+			} else if(test.indexOf("e") != -1) {
+				result = " -> Exponent Notation";
+			} else if(test.length >= 10) {
+				result = " -> Big Number";
+			}
+			return result;
+		}  
 		//default input
 		console.log(`%cDefault Input: `, "color : red;");
 		let input = "123";
@@ -39,5 +51,10 @@
 		console.log(maybeNumeric(input));
 		input = ".25";
 		console.log(maybeNumeric(input)); 
+		//bonus 2 input
+		console.log(`%cBonus 2 Input: `, "color : red;");
+		input = `2015 4 4\`Challenge #\`261\`Easy
+             234.2\`234ggf 45\`00\`number string number (0)`;
+             console.log(input);
 	});
 })();		
