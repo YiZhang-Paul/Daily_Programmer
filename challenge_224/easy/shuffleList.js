ca28c2997022837ee1f42cba5b581afc4e5b51f2
YI ZHANG <scaccterran@gmail.com>
2017-07-16 11:06:50 -0400

    fisher-yates shuffle

--- challenge_224/easy/shuffleList.js
+++ challenge_224/easy/shuffleList.js
@@ -2,25 +2,58 @@
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
 		/**
-		 * shuffle an array of items
-		 * @param array []
+		 * shuffle an array of items using 
+		 * Fisher-Yates shuffles
+		 * @param array [], array []
 		 *
-		 * array : array to be shuffled
+		 * items    : items to be shuffled
+		 * shuffled : shuffled array
 		 *
 		 * returns array []
-		 */
-		function shuffleArray(array, shuffled = []) {
-			if(!array.length) {
+		 */ 
+		function fisherYatesShuffle(items, shuffled = []) {
+			if(!items.length) {
 				return shuffled;
 			}
-			let pickIndex = Math.floor(Math.random() * array.length);
-			return shuffleArray([...array.slice(0, pickIndex), ...array.slice(pickIndex + 1)], [...shuffled, array[pickIndex]]);
+			let index = Math.floor(Math.random() * items.length);
+			return fisherYatesShuffle([...items.slice(0, index), ...items.slice(index + 1)], [...shuffled, items[index]]);
 		} 
-		console.log(shuffleArray([1, 2, 3, 4, 5, 6, 7, 8]));
-		console.log(shuffleArray([1, 2, 3, 4, 5, 6, 7, 8]));
-		console.log(shuffleArray([1, 2, 3, 4, 5, 6, 7, 8]));
-		console.log(shuffleArray([1, 2, 3, 4, 5, 6, 7, 8]));
-		console.log(shuffleArray([1, 2, 3, 4, 5, 6, 7, 8]));
-		console.log(shuffleArray([1, 2, 3, 4, 5, 6, 7, 8]));
+		console.log(`%cFisher-Yates Shuffles: `, "color : red;");
+		let input = [1, 2, 3, 4, 5, 6, 7, 8];
+		console.log(`%c${input.join(" ")}`, "color : yellow;");
+		console.log(fisherYatesShuffle(input).join(" "));
+		console.log(fisherYatesShuffle(input).join(" "));
+		console.log(fisherYatesShuffle(input).join(" "));
+		input = ["apple", "blackberry", "cherry", "dragonfruit", "grapefruit", "kumquat", "mango", "nectarine", "persimmon", "raspberry", "raspberry"];
+		console.log(`%c${input.join(" ")}`, "color : yellow;");
+		console.log(fisherYatesShuffle(input).join(" "));
+		console.log(fisherYatesShuffle(input).join(" "));
+		console.log(fisherYatesShuffle(input).join(" "));
+		input = ["a", "e", "i", "o", "u"];
+		console.log(`%c${input.join(" ")}`, "color : yellow;");
+		console.log(fisherYatesShuffle(input).join(" "));
+		console.log(fisherYatesShuffle(input).join(" "));
+		console.log(fisherYatesShuffle(input).join(" "));
+		//console.log(`%cDurstenfeld Shuffles: `, "color : red;");
+		//input = [1, 2, 3, 4, 5, 6, 7, 8];
+		//console.log(`%c${input.join(" ")}`, "color : yellow;");
+		//input = ["apple", "blackberry", "cherry", "dragonfruit", "grapefruit", "kumquat", "mango", "nectarine", "persimmon", "raspberry", "raspberry"];
+		//console.log(`%c${input.join(" ")}`, "color : yellow;");
+		//input = ["a", "e", "i", "o", "u"];
+		//console.log(`%c${input.join(" ")}`, "color : yellow;");
+		//console.log(`%cFaro Shuffles(out): `, "color : red;");
+		//input = [1, 2, 3, 4, 5, 6, 7, 8];
+		//console.log(`%c${input.join(" ")}`, "color : yellow;");
+		//input = ["apple", "blackberry", "cherry", "dragonfruit", "grapefruit", "kumquat", "mango", "nectarine", "persimmon", "raspberry", "raspberry"];
+		//console.log(`%c${input.join(" ")}`, "color : yellow;");
+		//input = ["a", "e", "i", "o", "u"];
+		//console.log(`%c${input.join(" ")}`, "color : yellow;");
+		//console.log(`%cFaro Shuffles(in): `, "color : red;");
+		//input = [1, 2, 3, 4, 5, 6, 7, 8];
+		//console.log(`%c${input.join(" ")}`, "color : yellow;");
+		//input = ["apple", "blackberry", "cherry", "dragonfruit", "grapefruit", "kumquat", "mango", "nectarine", "persimmon", "raspberry", "raspberry"];
+		//console.log(`%c${input.join(" ")}`, "color : yellow;");
+		//input = ["a", "e", "i", "o", "u"];
+		//console.log(`%c${input.join(" ")}`, "color : yellow;");
 	});
 })();		
