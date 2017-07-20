5d7f9039d928990c5f43df7084ee2e65d45d4fa1
YI ZHANG <scaccterran@gmail.com>
2017-07-20 18:23:55 -0400

    base & bonus 1 done

--- challenge_305/intermediate/bestConjunction.js
+++ challenge_305/intermediate/bestConjunction.js
@@ -8,7 +8,7 @@
   	 * url : URL of word list file
   	 *
   	 * returns array []
-  	 */ 
+  	 */
   	function getWordList(url) {
   		return new Promise((resolve, reject) => {
   			let xhttp = window.XMLHttpRequest ? new window.XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
@@ -22,35 +22,24 @@
   		});
   	} 
   	/**
-  	 * filter word list to narrow down search area
-  	 * @param array [], int
+  	 * slice a string in all possible ways
+  	 * @param int, String, int, int, array []
   	 *
-  	 * list   : list of all words
-  	 * minLen : minimum length of word
-  	 *
-  	 * returns array []
-  	 */
-  	function filterList(list, minLen) {
-  		return list.filter(word => word.length >= minLen);
-  	} 
-  	/**
-  	 * slices a string in all possible ways
-  	 * @param int, String, int, array []
-  	 *
-  	 * minLen   : minimum length of word slice
+  	 * minSize  : minimum length of word slice
   	 * string   : string to be sliced
+  	 * share    : number of letters can be shared be between sub-words
   	 * curLen   : current total length of slices
   	 * curSlice : current slice pattern
-  	 * 
+  	 *
   	 * returns array []
   	 */
-  	function sliceString(minLen, string, curLen = 0, curSlice = []) {
-  		if(string.length - curLen < minLen) {
+  	function sliceString(minSize, string, share, curLen = share, curSlice = []) {
+  		if(curLen == string.length || string.length - curLen + share < minSize) {
   			return curLen == string.length ? curSlice : null;
   		}
   		let slices = [];
-  		for(let i = minLen; i <= string.length - curLen; i++) {
-  			let result = sliceString(minLen, string, curLen + i, [...curSlice, string.slice(curLen, curLen + i)]);
+  		for(let i = minSize; i <= string.length - curLen + share; i++) {
+  			let result = sliceString(minSize, string, share, curLen + i - share, [...curSlice, string.slice(curLen - share, curLen + i - share)]);
   			if(result) {
   				if(Array.isArray(result[0])) {
   					slices.push(...result);
@@ -59,67 +48,75 @@
   				}
   			}
   		}
-  		return slices;
-  	} 
+  		return slices;	
+  	}
   	/**
-  	 * check if all slices of a word
+  	 * check if all slices of a word 
   	 * can be found in the dictionary
   	 * @param array [], obj {}
   	 *
   	 * slices     : word slices
   	 * dictionary : dictionary of all words
   	 *
-  	 * returns boolean 
-  	 */
+  	 * returns boolean
+  	 */ 
   	function isValidSlice(slices, dictionary) {
   		return !slices.some(slice => !dictionary.has(slice));
   	} 
   	/**
-  	 * find best conjunction for a given word
-  	 * @param int, String, obj {}
+  	 * find maximum conjunction for a given word
+  	 * @param int, String, obj {}, int
   	 *
-  	 * minLen     : minimum length of sub-words
-  	 * word       : word to be sliced
+  	 * minSize    : minimum length of sub-words
+  	 * word       : word to be checked
   	 * dictionary : dictionary of all words
+  	 * share      : number of letters can be shared be between sub-words
   	 *
-  	 * returns array []
+  	 * returns array [] 
   	 */
-  	function maxWordConjunction(minLen, word, dictionary) {
-  		return sliceString(minLen, word).sort((a, b) => b.length - a.length)
-  		                                .find(slices => isValidSlice(slices, dictionary)) || [];
+  	function maxConjunction(minSize, word, dictionary, share) {
+  		return sliceString(minSize, word, share)
+  					   .sort((a, b) => b.length - a.length)
+  		  		   .find(slices => isValidSlice(slices, dictionary)) || [];
   	} 
   	/**
   	 * find best conjunction from a dictionary
-  	 * @param int, array []
+  	 * @param int, array [], int
   	 *
-  	 * minLen : minimum length of sub-words
-  	 * list   : list of all words
+  	 * minSize : minimum length of sub-words
+  	 * list    : list of all words
+  	 * share   : number of letters can be shared be between sub-words
   	 *
   	 * returns array []
   	 */
-  	function bestConjunction(minLen, list) {
-  		let descList = filterList(list.slice().sort((b, a) => a.length - b.length), minLen);
+  	function bestConjunction(minSize, list, share = 0) {
+  		let descList = list.filter(word => word.length >= minSize).sort((a, b) => b.length - a.length);
   		let dictionary = new Set(descList);
   		let best = [];
   		for(let i = 0; i < descList.length; i++) {
-  			if(Math.floor(descList[i].length / minLen) > best.length) {
-  				let curConjunction = maxWordConjunction(minLen, descList[i], dictionary);
-  				best = curConjunction.length > best.length ? curConjunction : best;
+  			if(Math.floor(descList[i].length / minSize) > best.length) {
+  				let conjunction = maxConjunction(minSize, descList[i], dictionary, share);
+  				best = conjunction.length > best.length ? conjunction : best; 
   			}
   		}
   		return best;
   	} 
-  	//challenge input
-  	getWordList("wordList.txt").then(result => {
-  		let displayOutput = minSize => {
-  			let time = new Date().getTime();
-  			let best = bestConjunction(minSize, result);
-		  	console.log(`minSize %c${minSize}%c: ${best.join("")} %c(${best.length}: ${best.join(", ")})`, "color : red;", "", "color : yellow;");
-  			console.log(`Time Spent: %c${new Date().getTime() - time}ms`, "color : red;");
-  		};
-  		for(let i = 2; i <= 10; i++) {
-  			displayOutput(i);
-  		}
-  	});
+  	//challenge & bonus input
+ 		getWordList("wordList.txt").then(list => {
+ 			let displayResult = (minSize, share = 0) => {
+ 				let time = new Date().getTime();
+ 				let result = bestConjunction(minSize, list, share);
+ 				console.log(`minSize %c${minSize}%c: ${result.join("")} %c(${result.length}: ${result.join(", ")})`, "color : red;", "", "color : yellow;");
+ 				console.log(`Time Spent: %c${new Date().getTime() - time}ms`, "color : red;");
+ 			};
+ 			console.log(`%cChallenge Input: `, "color : red;");
+ 			for(let i = 2; i <= 10; i++) {
+ 				displayResult(i);
+ 			}
+ 			console.log(`%cBonus Input: `, "color : red;");
+ 			for(let i = 3; i <= 10; i++) {
+ 				displayResult(i, 1);
+ 			}
+ 		});	
   });
 })();  	
