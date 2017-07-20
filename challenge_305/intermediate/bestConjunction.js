96c04889d08bd411c90b126a66338013464afe83
YI ZHANG <scaccterran@gmail.com>
2017-07-20 15:09:17 -0400

    find best conjunction for a string

--- challenge_305/intermediate/bestConjunction.js
+++ challenge_305/intermediate/bestConjunction.js
@@ -7,8 +7,8 @@
   	 *
   	 * url : URL of word list file
   	 *
-  	 * returns obj {}
-  	 */
+  	 * returns array []
+  	 */ 
   	function getWordList(url) {
   		return new Promise((resolve, reject) => {
   			let xhttp = window.XMLHttpRequest ? new window.XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
@@ -20,31 +20,31 @@
   			xhttp.open("GET", url, true);
   			xhttp.send();
   		});
-  	}
+  	} 
   	/**
   	 * filter word list to narrow down search area
   	 * @param array []
   	 *
-  	 * list  : list of all words
-  	 * min   : minimum length of word
-  	 * input : input string to be checked
+  	 * list   : list of all words
+  	 * minLen : minimum length of word
+  	 * input  : input string to be checked
   	 *
   	 * returns array []
   	 */
-  	function filterWord(list, min, input) {
+  	function filterList(list, minLen, input) {
   		let letters = new Set(input.split(""));
-  		return list.filter(word => 
-  			letters.has(word[0]) && word.length >= min && word.length <= input.length);
+  		return list.filter(word =>
+  			letters.has(word[0]) && word.length >= minLen && word.length <= input.length);
   	} 
   	/**
-  	 * get all slice patterns for a string
+  	 * get all possible slice patterns for a string
   	 * @param int, int, int, array []
   	 *
-  	 * minLen   : minimum length of fragment
+  	 * minLen   : minimum length of word slice
   	 * length   : total length of string
-  	 * curLen   : current total slice length
-  	 * curSlice : current slices
-  	 *
+  	 * curLen   : current total length of slices
+  	 * curSlice : current slice pattern
+  	 * 
   	 * returns array []
   	 */
   	function slicePattern(minLen, length, curLen = 0, curSlice = []) {
@@ -65,27 +65,57 @@
   		return slices;
   	} 
   	/**
-  	 * slice string following slice patterns
-  	 * @param int, String
+  	 * slice string using a slice pattern
+  	 * @param String, array []
   	 *
-  	 * minLen : minimum length of fragment  	 
-  	 * string : string to be sliced
-  	 * 
+  	 * string  : string to be sliced
+  	 * pattern : slice pattern
+  	 *
   	 * returns array []
   	 */
-  	function sliceString(minLen, string) {
-  		return slicePattern(minLen, string.length).map(pattern => {
-  			let curPosition = 0;
-  			return pattern.map(sliceLen => {
-  				curPosition += sliceLen;
-  				return string.slice(curPosition - sliceLen, curPosition);
-  			});
+  	function sliceString(string, pattern) {
+  		let curPosition = 0;
+  		return pattern.map(sliceLen => {
+  			curPosition += sliceLen;
+  			return string.slice(curPosition - sliceLen, curPosition);
   		});
   	} 
-  	console.log(sliceString(3, "disproportionateness"));
+  	/**
+  	 * check if all slices of a word
+  	 * can be found in the dictionary
+  	 * @param array [], array []
+  	 *
+  	 * slices : word slices
+  	 * list   : list of all words
+  	 *
+  	 * returns boolean 
+  	 */
+  	function isValidSlice(slices, list) {
+  		return !slices.some(slice => list.indexOf(slice) == -1);
+  	} 
+  	/**
+  	 * find best conjunction
+  	 * @param int, String, array []
+  	 *
+  	 * minLen : minimum length of sub-words
+  	 * string : string to be checked
+  	 * list   : list of all words
+  	 *
+  	 * returns array []
+  	 */
+  	function maxWordConjunction(minLen, string, list) {
+  		list = filterList(list, minLen, string).sort((a, b) => a.length - b.length);
+  		let allSlice = slicePattern(minLen, string.length).sort((a, b) => b.length - a.length);
+  		let bestSlice = allSlice.find(slice => isValidSlice(sliceString(string, slice), list));
+  		return sliceString(string, bestSlice);
+  	} 
   	//challenge input
   	getWordList("wordList.txt").then(result => {
-  		console.log(filterWord(result, 3, "disproportionateness"));
+  		let time = new Date().getTime();
+  		let input = "disproportionateness";
+  		let best = maxWordConjunction(3, input, result);
+	  	console.log(`minSize 3: ${input} %c(${best.length}: ${best.join(", ")})`, "color : yellow;");
+  		console.log(`Time Spent: %c${new Date().getTime() - time}ms`, "color : red;");
   	});
   });
 })();  	
