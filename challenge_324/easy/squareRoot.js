438227b0160bdbd6f63af45b882f8a33c0caf515
YI ZHANG <scaccterran@gmail.com>
2017-07-21 19:44:52 -0400

    all done

--- challenge_324/easy/squareRoot.js
+++ challenge_324/easy/squareRoot.js
@@ -2,26 +2,29 @@
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
 		/**
-		 * adjust input number
-		 * @param String
+		 * prepend number with zero
+		 * @param {int} [num] [number to be prepended]
 		 *
-		 * numStr : number string to be adjusted
-		 *
-		 * returns array []
+		 * @return {String} [prepended number]
 		 */
-		function adjustNumber(numStr) {
-			let numParts = numStr.split(".");
-			return ["0".repeat(numParts[0].length % 2) + numParts[0], 
-							numParts[1] ? numParts[1] + "0".repeat(numParts[1].length % 2): "00"];
-		} 
+		function prependNum(num) {
+			return "0".repeat(String(num).length % 2) + num;
+		}
 		/**
-		 * find largest whole number whose 
-		 * square is closest to a given number
-		 * @param int
+		 * append number with zerp
+		 * @param {int} [num] [number to be appended]
 		 *
-		 * limit : upper limit
+		 * @return {String} [appended number]
+		 */
+		function appendNum(num) {
+			return num + "0".repeat(String(num).length % 2);
+		}
+		/**
+		 * find largest whole number square root
+		 * for a given number limit
+		 * @param {int} [limit] [upper limit]
 		 *
-		 * returns int
+		 * @return {int} [largest whole number square root]
 		 */
 		function maxWholeSqrt(limit) {
 			let sqrt = 0;
@@ -29,60 +32,91 @@
 				sqrt++;
 			}
 			return sqrt - 1;
-		} 
+		}
 		/**
-		 * find largest factor for a given number
-		 * @param int, int
-		 *
-		 * sideA : length of side A
-		 * limit : upper limit 
-		 *
-		 * returns int
+		 * find largest factor for a given expression
+		 * within the range of a given number limit
+		 * @param {int} [prev] [previous result from square root]
+		 * @param {int} [limit] [upper limit]
+		 * 
+		 * @return {int} [largest factor] 
 		 */
-		function maxFactor(sideA, limit) {
-			let factor = 0, num = 20 * sideA;
-			while(factor * (num + factor) <= limit) {
+		function maxFactor(prev, limit) {
+			let factor = 0;
+			while(factor * (prev + factor) <= limit) {
 				factor++;
 			}
 			return factor - 1;
-		} 
+		}
 		/**
-		 * find square root
-		 * @param int, float
-		 *
-		 * precision : precision of square root
-		 * number    : number to square root
-		 *
-		 * returns float
-		 */ 
-		function squareRoot(precision, number) {
-			let [whole, decimal] = adjustNumber(number.toString());
-			let result = maxWholeSqrt(Number(whole.slice(0, 2))).toString();
-			let remain = ((Number(whole.slice(0, 2)) - Math.pow(Number(result), 2)) || "").toString() + whole.slice(2, 4);
-			whole = whole.slice(2);
-			while(whole.length) {
-				let factor = maxFactor(Number(result), Number(remain));
-				remain = (Number(remain) - (20 * Number(result) + factor) * factor).toString() + whole.slice(2, 4);
-				whole = whole.slice(2);
-				result += factor;
+		 * find remaining digits for square root
+		 * @param {String} [curSqrt] [current square root]
+		 * @param {String} [remain] [remaining value]
+		 * @param {String} [digits] [remaining digits]
+		 * 
+		 * @return {array []} [square root and remaining value]
+		 */
+		function restSqrt(curSqrt, remain, digits) {
+			while(digits.length) {
+				let factor = maxFactor(Number(curSqrt) * 20, Number(remain));
+				remain = String(Number(remain) - (Number(curSqrt) * 20 + factor) * factor) + digits.slice(2, 4);
+				curSqrt += String(factor);
+				digits = digits.slice(2);
 			}
-			return Number(result);
+			return [curSqrt, remain];
+		}
+		/**
+		 * find square root for whole number part
+		 * @param {String} [whole] [whole number part string]
+		 * 
+		 * @return {array []} [square root and remaining value]
+		 */
+		function getWholeSqrt(whole) {
+			let sqrt = maxWholeSqrt(whole.slice(0, 2));
+			let remain = String(Number(whole.slice(0, 2)) - Math.pow(Number(sqrt), 2)) + whole.slice(2, 4);
+			return restSqrt(sqrt, remain, whole.slice(2));
+		}
+		/**
+		 * find square root for decimal number part
+		 * @param {String} [decimal] [decimal number part string]
+		 * @param {String} [prev] [square root from whole number part]
+		 * @param {String} [remain] [remaining value from whole number part]
+		 * @param {int} [precision] [precision of square root]
+		 * 
+		 * @return {int} [square root]
+		 */
+		function getDecimalSqrt(decimal, prev, remain, precision) {
+			let prevLen = prev.length;
+			decimal += "0".repeat(precision * 2 - decimal.length);
+			remain += decimal.slice(0, 2);
+			return restSqrt(prev, remain, decimal)[0].slice(prevLen);
+		}
+		/**
+		 * find square root for a given number
+		 * @param {int} [precision] [precision of square root]
+		 * @param {int} [number] [number to square root]
+		 *
+		 * @return {float} [square root]
+		 */
+		function squareRoot(precision, number) {
+			number = String(number).split(".");
+			let [whole, decimal] = [prependNum(number[0]), appendNum(number[1] || "00")];
+			let [wholeSqrt, remain] = getWholeSqrt(whole);
+			return precision === 0 ? wholeSqrt : wholeSqrt + "." + getDecimalSqrt(decimal, wholeSqrt, remain, precision);
 		}
 		//default input
 		console.log(`%cDefault Input: `, "color : red;");
 		let input = "0 7720.17";
-		console.log(`${input} => ${squareRoot(...input.split(" ").map(num => Number(num)))}`);
+		console.log(`${input} => %c${squareRoot(...input.split(" ").map(num => Number(num)))}`, "color : yellow;");
 		input = "1 7720.17";
-		console.log(`${input} => ${squareRoot(...input.split(" ").map(num => Number(num)))}`);
+		console.log(`${input} => %c${squareRoot(...input.split(" ").map(num => Number(num)))}`, "color : yellow;");
 		input = "2 7720.17"; 
-		console.log(`${input} => ${squareRoot(...input.split(" ").map(num => Number(num)))}`);
+		console.log(`${input} => %c${squareRoot(...input.split(" ").map(num => Number(num)))}`, "color : yellow;");
 		//challenge input
 		console.log(`%cChallenge Input: `, "color : red;");
 		input = "0 12345";
-		console.log(`${input} => ${squareRoot(...input.split(" ").map(num => Number(num)))}`);
+		console.log(`${input} => %c${squareRoot(...input.split(" ").map(num => Number(num)))}`, "color : yellow;");
 		input = "8 123456";
-		console.log(`${input} => ${squareRoot(...input.split(" ").map(num => Number(num)))}`);
-		input = "1 12345678901234567890123456789"; 
-		console.log(`${input} => ${squareRoot(...input.split(" ").map(num => Number(num)))}`);
+		console.log(`${input} => %c${squareRoot(...input.split(" ").map(num => Number(num)))}`, "color : yellow;");
 	});
 })();		
