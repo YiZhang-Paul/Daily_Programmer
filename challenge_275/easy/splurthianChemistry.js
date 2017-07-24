221da8b38387f41c9277994a244632c531eed1de
YI ZHANG <scaccterran@gmail.com>
2017-07-24 15:04:54 -0400

    all done

--- challenge_275/easy/splurthianChemistry.js
+++ challenge_275/easy/splurthianChemistry.js
@@ -40,19 +40,70 @@
 			return chars[startIndex].toUpperCase() + minCharCodeLetter(chars.slice(startIndex + 1));               
 		}
 		/**
-		 * find total number of unique symbols for a given element name
+		 * group letters in an element name and record 
+		 * total number of each letter
 		 * @param {String} [element] - element name
 		 *
-		 * @return {int} [total number of vaild symbols]
+		 * @return {Object} [letter group]
 		 */
-		function totalUniqueSymbol(element) {
-			let chars = element.toLowerCase().split("")
-												 .reduce((acc, val) => {
-			                     acc[val] = acc[val] ? acc[val] + 1 : 1; 
-			                     return acc;
-			                   }, {});
+		function groupLetter(element) {
+			return element.toLowerCase().split("")
+									  .reduce((acc, val) => {
+			                acc[val] = acc[val] ? acc[val] + 1 : 1; 
+			                return acc;
+			              }, {});
+		}
+		/**
+		 * find total number of unique symbols with length of 2
+		 * for a given element name
+		 * @param {String} [element] - element name
+		 *
+		 * @return {int} [total number of valid symbols]
+		 */
+		function totalUniqueSymbol1(element) {
+			let chars = groupLetter(element);
       let charKeys = Object.keys(chars);			                   
       return charKeys.length * (charKeys.length - 1) * 0.5 + charKeys.filter(key => chars[key] > 1).length;
+		}
+		/**
+		 * slice a group of letters from a given element name 
+		 * @param {String} [element] - element name
+		 * @param {int} [len] - length of slice
+		 * @param {Array} [curSlice] - current slice
+		 *
+		 * @return {Array} [sliced groups]
+		 */
+		function sliceElement(elements, len = 1, curSlice = []) {
+			if(curSlice.length == len || !elements) {
+				return curSlice.length == len ? curSlice.join("") : null;
+			}
+			let slices = [];
+			for(let i = 0; i < elements.length; i++) {
+				let result = sliceElement(elements.slice(i + 1), len, [...curSlice, elements[i]]);
+				if(result) {
+					if(Array.isArray(result)) {
+						slices.push(...result);
+					} else {
+						slices.push(result);
+					}
+				}
+			}
+			return slices;
+		}
+		/**
+		 * find total number of unique symbols of any length
+		 * for a given element name
+		 * @param {String} [element] - element name
+		 *
+		 * @return {int} [total number of valid symbols]
+		 */
+		function totalUniqueSymbol2(element) {
+			element = element.toLowerCase();
+			let symbols = new Set();
+			for(let i = 1; i <= element.length; i++) {
+				symbols = new Set([...Array.from(symbols), ...sliceElement(element, i)]);
+			}
+			return symbols.size;
 		}
 		//challenge input
 		console.log(`%cChallenge Input: `, "color : red;");
@@ -77,6 +128,9 @@
 		//bonus 2 input
 		console.log(`%cBonus 2 Input: `, "color : red;");
 		input = "Zuulon";
-		console.log(`${input} -> %c${totalUniqueSymbol(input)}`, "color : yellow;");
+		console.log(`${input} -> %c${totalUniqueSymbol1(input)}`, "color : yellow;");
+		//bonus 3 input
+		console.log(`%cBonus 3 Input: `, "color : red;");
+		console.log(`${input} -> %c${totalUniqueSymbol2(input)}`, "color : yellow;");
 	});
 })();		
