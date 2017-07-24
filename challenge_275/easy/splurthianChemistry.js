5c7a03d8a5e1f76da905fdcedf0b005837d32d45
YI ZHANG <scaccterran@gmail.com>
2017-07-23 22:37:24 -0400

    bonus 1

--- challenge_275/easy/splurthianChemistry.js
+++ challenge_275/easy/splurthianChemistry.js
@@ -18,6 +18,27 @@
 			}
 			return true;
 		}
+		/**
+		 * find letter in the word that 
+		 * comes first in alphabetical order
+		 * @param {array} [chars] - characters to be tested
+		 *
+		 * @return {Char} [letter with lowest ASCII code]
+		 */
+		function minCharCodeLetter(chars) {
+			return String.fromCharCode(Math.min(...chars.map(char => char.charCodeAt())));
+		}	
+		/**
+		 * find valid symbol that's first in alphabetical order
+		 * @param {String} [element] - element name
+		 *
+		 * @return {String} [valid symbol]
+		 */
+		function validOrderedSymbol(element) {
+			let chars = Array.from(new Set(element.toLowerCase()));
+			let startIndex = chars.indexOf(minCharCodeLetter(chars.slice(0, -1)));
+			return chars[startIndex].toUpperCase() + minCharCodeLetter(chars.slice(startIndex + 1));               
+		}
 		//challenge input
 		console.log(`%cChallenge Input: `, "color : red;");
 		let input = ["Spenglerium", "Ee"]; 
@@ -34,5 +55,9 @@
 		console.log(`${input.join(", ")} -> %c${isValidSymbol(...input)}`, "color : red;");
 		//bonus input
 		console.log(`%cBonus Input: `, "color : red;");
+		input = "Gozerium";
+		console.log(`${input} -> %c${validOrderedSymbol(input)}`, "color : red;");
+		input = "Slimyrine";
+		console.log(`${input} -> %c${validOrderedSymbol(input)}`, "color : red;");
 	});
 })();		
