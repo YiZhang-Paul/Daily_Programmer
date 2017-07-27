f575a746b7869d06230ab4596135bd1d3c2e34aa
YI ZHANG <scaccterran@gmail.com>
2017-07-27 15:41:33 -0400

    base challenge complete

--- challenge_306/intermediate/grayCode.js
+++ challenge_306/intermediate/grayCode.js
@@ -1,6 +1,28 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * construct n-bit gray oode
+		 * @param {int} [bitLen] - bit length of gray code
+		 * @param {Array} [bits] - current gray code constructed (default 2-bit)
+		 *
+		 * @return {Array} [generated code]
+		 */
+		function makeGrayCode(bitLen, bits = ["00", "01", "11", "10"]) {
+			return bits[0].length == bitLen ? 
+				bits : makeGrayCode(bitLen, [...bits.slice().map(bit => "0" + bit), ...bits.slice().reverse().map(bit => "1" + bit)]);
+		}
+		//challenge input
+		console.log(`%cChallenge Input: `, "color : red;");
+		let input = 8;
+		console.log(`%c${input} -> `, "color : yellow;");
+		makeGrayCode(input).forEach(row => {
+			console.log(row);
+		});
+		input = 16;
+		console.log(`%c${input} -> `, "color : yellow;");
+		makeGrayCode(input).forEach(row => {
+			console.log(row);
+		});
 	});
 })();		
