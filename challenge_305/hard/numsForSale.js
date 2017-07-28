3fd1258817fb3b8150b184cbee15cbd204a9c0c1
YI ZHANG <scaccterran@gmail.com>
2017-07-28 04:31:06 -0400

    all done

--- challenge_305/hard/numsForSale.js
+++ challenge_305/hard/numsForSale.js
@@ -11,7 +11,7 @@
   	 *
   	 * @return {Array} [all number combinations]
   	 */
-  	function getNumCombine(totalDigit, goal = 69, curSum = 0, curNum = []) {
+  	function getNumCombine(totalDigit, goal, curSum = 0, curNum = []) {
   		if(curNum.length == totalDigit && curSum == goal) {
   			return curNum;
   		}
@@ -32,5 +32,55 @@
   		}
   		return combination;
   	}
+  	/**
+  	 * get factorial of a number
+  	 * @param {int} [base] - base number
+  	 *
+  	 * @return {int} [factorial]
+  	 */
+  	function factorial(base) {
+  		return base == 1 ? base : base * factorial(base - 1);
+  	}
+  	/**
+  	 * get total number of distinct numbers 
+  	 * that can be formed by a set of digits
+  	 * @param {Array} [set] - digit set
+  	 *
+  	 * @return {int} [total number of distinct numbers]
+  	 */
+  	function countDistinct(set) {
+  		let digitCount = set.reduce((acc, val) => {
+  			acc[val] = acc[val] ? acc[val] + 1 : 1;
+  			return acc;
+  		}, {});
+  		let total = (set.length - (digitCount[0] || 0)) * factorial(set.length - 1);
+  		for(let digit in digitCount) {
+  			if(digitCount[digit] > 1) {
+  				total /= factorial(digitCount[digit]);
+  			}
+  		}
+  		return total;
+  	}
+		/**
+		 * get total number of all distinct numbers 
+		 * of a given length whose digits add up to a given number
+		 * @param {int} [len] - number of digits
+		 * @param {int} [goal] - desired sum of all digits
+		 *
+		 * @return {[type]} [description]
+		 */
+		function allDistinctNum(len, goal = 69) {
+      let distinctByLen = combination => combination.reduce((acc, val) => acc + countDistinct(val), 0);
+			let distinct = 0;
+			for(let i = 1; i <= len; i++) {
+				let allNum = getNumCombine(i, goal);
+				distinct += allNum ? distinctByLen(allNum) : 0;				
+			}
+			return distinct;
+		}
+    //challenge solution
+    let time = new Date().getTime();
+		console.log(`Total Number of Possible Values: %c${allDistinctNum(15)}`, "color : orange;");
+    console.log(`Time Spent: %c${new Date().getTime() - time}ms`, "color : red;");
   });
-})();  	
+})();
