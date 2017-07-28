30f5546a9c40ddaf34508ea13bbb3e7239dfda13
YI ZHANG <scaccterran@gmail.com>
2017-07-28 17:09:53 -0400

    all done

--- challenge_246/intermediate/letterSplits.js
+++ challenge_246/intermediate/letterSplits.js
@@ -60,7 +60,61 @@
 			}
 			return selection;
 		}
+		/**
+		 * check if a word is a real English word
+		 * @param {String} [word] - word to be tested
+		 * @param {Object} [dictionary] - dictionary for reference
+		 *
+		 * @return {boolean} [test result]
+		 */
+		function isRealWord(word, dictionary) {
+			return dictionary.has(word.toLowerCase());
+		}
+		/**
+		 * find word arrangements that are real English words
+		 * @param {String} [chars] - all characters
+		 * @param {Obejct} [dictionary] - dictionary for reference
+		 * @param {Array} [curWord] - current words found
+		 *
+		 * @return {Array} [all real English words]
+		 */
+		function findRealWord(chars, dictionary, curWord = []) { 
+			if(curWord[0] && !isRealWord(curWord[0].toLowerCase(), dictionary)) {
+				return null;
+			}
+			if(!chars.length) {
+				return curWord.join(" ");
+			}			
+			let realWord = [];
+			for(let i = chars.length; i >= 4; i--) {
+				let result = findRealWord(chars.slice(0, -i), dictionary, [chars.slice(-i), ...curWord]);
+				if(result) {
+					if(Array.isArray(result)) {
+						realWord.push(...result);
+					} else {
+						realWord.push(result);
+					}
+				}
+			}
+			return realWord;
+		}
+		/**
+		 * find all real words that can be formed by given character codes
+		 * @param {String} [charCode] - character codes
+		 * @param {Object} [dictionary] - dictionary for reference
+		 *
+		 * @return {Array} [all real words]
+		 */
+		function findAllRealWord(charCode, dictionary) {
+			let arrangements = arrangeWord(charCode);
+			let wholeWord = arrangements.filter(word => isRealWord(word, dictionary));
+			return wholeWord.length ? 
+				wholeWord : arrangements.map(word => findRealWord(word, dictionary))
+															  .filter(word => word.length)
+															  .reduce((acc, val) => [...acc, ...val], []);
+		}
 		//challenge input
+		console.log(`%cChallenge Input: `, "color : red;");
 		let input = "1234";
 		console.log(`${input} -> `);
 		arrangeWord(input).forEach(word => {
@@ -76,10 +130,30 @@
 		arrangeWord(input).forEach(word => {
 			console.log(`%c${word}`, "color : orange;");
 		});
-		
+		//bonus input
+		console.log(`%cBonus Input: `, "color : red;");
 		getWordList("wordList.txt").then(result => {
 			let dictionary = makeDictionary(result);
-			console.log(dictionary);
+			let input = "1321205";
+			console.log(`${input} -> `);
+			findAllRealWord(input, dictionary).forEach(word => {
+				console.log(`%c${word}`, "color : orange;");
+			});
+			input = "1252020518";
+			console.log(`${input} -> `);
+			findAllRealWord(input, dictionary).forEach(word => {
+				console.log(`%c${word}`, "color : orange;");
+			});
+			input = "85121215231518124";
+			console.log(`${input} -> `);
+			findAllRealWord(input, dictionary).forEach(word => {
+				console.log(`%c${word}`, "color : orange;");
+			});
+			input = "811616258151294125194191225161815718113";
+			console.log(`${input} -> `);
+			findAllRealWord(input, dictionary).forEach(word => {
+				console.log(`%c${word}`, "color : orange;");
+			});
 		});
 	});
 })();		
