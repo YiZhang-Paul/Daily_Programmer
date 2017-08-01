b079c4b657588cdffdadc80208df62e417febea6
YI ZHANG <scaccterran@gmail.com>
2017-08-01 03:25:44 -0400

    all done

--- challenge_250/intermediate/selfNumbers.js
+++ challenge_250/intermediate/selfNumbers.js
@@ -2,56 +2,109 @@
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
 		/**
-		 * check if a number exists inside of an array from backward
+		 * check if a number exists inside of an array
 		 * @param {int} [number] - number to be checked
-		 * @param {Array} [arr] - array to be checked against
+		 * @param {Array} [array] - array to be checked against
 		 *
 		 * @return {boolean} [test result]
 		 */
-		function hasNum(number, arr) {
-			return arr.lastIndexOf(number) != -1;
+		function hasNumber(number, array) {
+			return array.lastIndexOf(number) != -1;
 		}
 		/**
 		 * count total number of a given digit in an array
-		 * @param {int} [digit] - digit to be checked
-		 * @param {Array} [allNum] - all numbers 
+		 * @param {int} [digit] - digit to be checked 
+		 * @param {Array} [array] - array to be checked against
 		 *
-		 * @return {int} [total number of digits]
+		 * @return {int} [total number of digit in the array]
 		 */
-		function countDigit(digit, allNum) {
-			return allNum.filter(number => number == digit).length;
+		function countDigit(digit, array) {
+			return array.filter(number => number == digit).length;
+		}
+		/**
+		 * count occurrence of all each digit in an array
+		 * @param {Array} [array] - array containing all digits
+		 *
+		 * @return {Object} [digit counts]
+		 */
+		function countAllDigits(array) {
+			return Array.from(new Set(array)).reduce((acc, val) => {
+				let curCount = countDigit(val, array);
+				acc[val] = {total : curCount, remain : curCount};
+				return acc;
+			}, {});
 		}
 		/**
 		 * find all combination of numbers that add up to a given value
-		 * @param {int} [digits] - total number of digits
+		 * @param {int} [digits] - total number of digits to be selected
 		 * @param {int} [curSum] - current sum of all digits
-		 * @param {Array} [curNum] - current number selections
+		 * @param {Array} [curSelect] - current digits selection
 		 *
 		 * @return {Array} [all number combinations]
 		 */
-		function getNumCombine(digits, curSum = 0, curNum = []) {
-			if(curNum.length == digits && curSum == digits) {
-				let [zeros, ones] = [countDigit(0, curNum), countDigit(1, curNum)];
-				return hasNum(zeros, curNum) && hasNum(ones, curNum) ? curNum.reverse() : null;
+		function getNumCombine(digits, curSum = 0, curSelect = []) {
+			if(curSum == digits && curSelect.length == digits) {
+				let [zeros, ones] = [countDigit(0, curSelect), countDigit(0, curSelect)];
+				return hasNumber(zeros, curSelect) && hasNumber(ones, curSelect) ? curSelect.reverse() : null;
 			}
-			if(curSum > digits || curSum + (digits - curNum.length) * 9 < digits) {
+			if(curSum > digits || curSum + (digits - curSelect.length) * 9 < digits) {
 				return null;
 			}
-			let selections = [], lastNum = curNum[curNum.length - 1];
-			for(let i = lastNum === undefined ? 0 : lastNum; i <= 9; i++) {
-				let result = getNumCombine(digits, curSum + i, [...curNum, i]);
+			let combinations = [], lastDigit = curSelect[curSelect.length - 1];
+			for(let i = lastDigit === undefined ? 0 : lastDigit; i <= 9; i++) {
+				let result = getNumCombine(digits, curSum + i, [...curSelect, i]);
 				if(result && result.length) {
 					if(Array.isArray(result[0])) {
-						selections.push(...result);
+						combinations.push(...result);
 					} else {
-						selections.push(result);
+						combinations.push(result);
 					}
 				}
 			}
-			return selections;
+			return combinations;
 		}
-		console.log(getNumCombine(4));
-		console.log(getNumCombine(10));
-		console.log(getNumCombine(15));
+		/**
+		 * construct self-descriptive number using given set of digits
+		 * @param {Array} [digits] - all available digits
+		 *
+		 * @return {int} [valid self-descriptive number]
+		 */
+		function makeSelfDesNumber(digits) {
+			let number = "";
+			for(let i = 0, counts = countAllDigits(digits); i < digits.length; i++) {
+				let curDigit = counts[i] ? counts[i].total : 0;
+				if(!counts[curDigit] || !counts[curDigit].remain) {
+					return null;
+				}
+				counts[curDigit].remain--;
+				number += curDigit;
+			}
+			return Number(number);
+		}
+		/**	
+		 * find all self-descriptive numbers in a given range
+		 * @param {int} [length] - total digits in the numbers
+		 *
+		 * @return {Array} [all valid self-descriptive numbers in range]
+		 */
+		function findSelfDesNumber(length) {
+			let digitSets = getNumCombine(length);
+			if(!digitSets.length) {
+				return ["No Self-descriptive Number Found."];
+			}
+			let numbers = digitSets.map(set => makeSelfDesNumber(set)).filter(number => number);
+			return numbers.length ? numbers : ["No Self-descriptive Number Found."];
+		}
+		//challenge & bonus input
+		console.log(`%cChallenge & Bonus Input: `, "color : red;");
+		for(let i = 1; i <= 15; i++) {
+			let time = new Date().getTime();
+			console.log(`%c${i} -> `, "color : orange;");
+			let result = findSelfDesNumber(i);
+			for(let j = 0; j < result.length; j++) {
+				console.log(result[j]);
+			}
+			console.log(`Time Spent: %c${new Date().getTime() - time}ms`, "color : orange;");
+		}
 	});
 })();		
