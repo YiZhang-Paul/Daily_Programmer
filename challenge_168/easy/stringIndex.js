fdc0b2d316b46e1c5e172a5ec8e3b7e244e8815a
YI ZHANG <scaccterran@gmail.com>
2017-08-01 11:30:33 -0400

    all done

--- challenge_168/easy/stringIndex.js
+++ challenge_168/easy/stringIndex.js
@@ -1,6 +1,21 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * display all words with given indexes
+		 * @param {String} [string] - string to be referred to
+		 * @param {Array} [indexes] - index of all words
+		 *
+		 * @return {String} [words joined by spaces]
+		 */
+		function displayWord(string, indexes) {
+			let words = string.match(/\w+/g);
+			return indexes.map(index => words[index - 1] ? words[index - 1] : "").filter(word => word).join(" ");
+		}
+		//challenge input
+		console.log(`%cChallenge Input: `, "color : red;");
+		let input = `...You...!!!@!3124131212 Hello have this is a --- string Solved !!...? to test @\n\n\n#!#@#@%$**#$@ Congratz this!!!!!!!!!!!!!!!!one ---Problem\n\n`;
+		let indexes = [12, -1, 1, -100, 4, 1000, 9, -1000, 16, 13, 17, 15];
+		console.log(displayWord(input, indexes));
 	});
 })();		
