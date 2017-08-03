95ab842a80cf114eb23d5a278be7a2f051d5b577
YI ZHANG <scaccterran@gmail.com>
2017-08-03 16:07:30 -0400

    all done

--- challenge_286/easy/reverseFactorial.js
+++ challenge_286/easy/reverseFactorial.js
@@ -1,6 +1,31 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * reverse factorial
+		 * @param {int} [number] - number to be reversed
+		 * @param {int} [curFact] - current factorial
+		 *
+		 * @return {int} [reversed number]
+		 */
+		function reverseFactorial(number, curFact = 2) {
+			return number == curFact ? curFact + "!" : (number % curFact || number < curFact ? "NONE" : reverseFactorial(number / curFact, curFact + 1));
+		}
+		//default input
+		console.log(`%cDefault Input: `, "color : red;");
+		let input = 120;
+		console.log(`${input} -> %c${reverseFactorial(input)}`, "color : orange;");
+    input = 150;
+		console.log(`${input} -> %c${reverseFactorial(input)}`, "color : orange;");
+    //challenge input
+		console.log(`%cChallenge Input: `, "color : red;");
+    input = 3628800;
+		console.log(`${input} -> %c${reverseFactorial(input)}`, "color : orange;");
+    input = 479001600;
+		console.log(`${input} -> %c${reverseFactorial(input)}`, "color : orange;");
+    input = 6;
+		console.log(`${input} -> %c${reverseFactorial(input)}`, "color : orange;");
+    input = 18;
+		console.log(`${input} -> %c${reverseFactorial(input)}`, "color : orange;");
 	});
 })();			
