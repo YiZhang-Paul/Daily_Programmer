35d9cae0d763983dfd3f887a466051042f750ac8
YI ZHANG <scaccterran@gmail.com>
2017-08-07 00:53:28 -0400

    all done

--- challenge_239/easy/gameOfThrees.js
+++ challenge_239/easy/gameOfThrees.js
@@ -1,6 +1,30 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * play game of three
+		 * @param {int} [number] - starting number
+		 *
+		 * @return {String} [steps to 1]
+		 */
+		function gameOfThree(number) {
+			let steps = "";
+			while(number != 1) {
+				let change = number % 3 === 0 ? 0 : (number + 1) % 3 === 0 ? 1 : -1;
+				steps += number + " " + change + "\n";
+				number = (number + change) / 3;
+			}
+			return steps + "1";
+		}
+		//default input
+		console.log(`%cDefault Input: `, "color : red;");
+		let input = 100;
+		console.log(`${input} ->`);
+		console.log(`%c${gameOfThree(input)}`, "color : orange;");
+		//challenge input
+		console.log(`%cChallenge Input: `, "color : red;");
+		input = 31337357;
+		console.log(`${input} ->`);
+		console.log(`%c${gameOfThree(input)}`, "color : orange;");
 	});
 })();		
