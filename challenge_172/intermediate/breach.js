0922b4e9ad775db546a9158ab3a777303e7bcd80
YI ZHANG <scaccterran@gmail.com>
2017-08-12 12:23:44 -0400

    get GUID

--- challenge_172/intermediate/breach.js
+++ challenge_172/intermediate/breach.js
@@ -1,6 +1,16 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * create GUID
+		 *
+		 * @return {String} [GUID] 
+		 */
+		function getGUID() {
+			return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, char => {
+				let seed = Math.random() * 16 | 0;
+				return char == "x" ? seed.toString(16) : (seed&0x3|0x8).toString(16);
+			});
+		}
 	});
 })();			
