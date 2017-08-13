46945199614f209ae13955d197c6fa3dec40105e
YI ZHANG <scaccterran@gmail.com>
2017-08-13 05:38:04 -0400

    read and record all schedules

--- challenge_218/hard/elevatorScheduling.js
+++ challenge_218/hard/elevatorScheduling.js
@@ -2,44 +2,65 @@
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
 		/**
-		 * passenger class
-		 * @param {String} [id] - passenger identifier
-		 * @param {String} [curTime] - current time
-		 * @param {String} [source] - source floor
-		 * @param {String} [destination] - destination floor
-		 */
-		class Passenger {
-			constructor(id, curTime, source, destination) {
-				this.id = id;
-				this.curTime = Number(curTime);
-				this.source = Number(source);
-				this.destination = Number(destination);
-				this.curFloor = this.source;
-			}
-		}
-		/**
 		 * elevator class
-		 * @param {String} [id] - elevator identifier
-		 * @param {String} [capacity] - elevator capacity
-		 * @param {String} [speed] - movement speed
-		 * @param {String} [curFloor] - current floor
+		 * @param {String} [id] - elevator ID
+		 * @param {int} [capacity] - elevator capacity
+		 * @param {int} [speed] - movement speed
+		 * @param {int} [curFloor] - current floor
 		 */
 		class Elevator {
 			constructor(id, capacity, speed, curFloor) {
 				this.id = id;
-				this.capacity = Number(capacity);
-				this.speed = Number(speed);
-				this.curFloor = Number(curFloor);
+				this.capacity = capacity;
+				this.speed = speed;
+				this.curFloor = curFloor;
+			}
+		}
+		/**
+		 * passenger class
+		 * @param {String} [id] - passenger ID
+		 * @param {Array} [fromTo] - source floor and destination floor
+		 */
+		class Passenger {
+			constructor(id, fromTo) {
+				this.id = id;
+				this.rides = [fromTo];
 			}
 		}
 		/**
 		 * manager class
-		 * @param {Array} [info] - elevator specs and schedules
+		 * @param {Array} [specs] - car specifications
+		 * @param {Array} [schedules] - all schedules
 		 */
 		class Manager {
-			constructor(info) {
-				this.cars = info.slice(1, 1 + Number(info[0])).map(spec => new Elevator(...spec.split(" ")));
-				this.schedules = info.slice(-Number(info[Number(info[0]) + 1]));
+			constructor(specs, schedules) {
+				this.cars = specs.map(spec => new Elevator(...spec));
+				this.schedules = schedules;
+				this.passengers = new Map();
+				this.curTime = 0;
+				this.run();
+			}
+			/**
+			 * run elevators
+			 */
+			run() {
+				while(this.schedules.length) {
+					this.readSchedule();
+					this.curTime++;
+				}
+			}
+			/**
+			 * read and record schedules 
+			 */
+			readSchedule() {
+				while(this.schedules.length && this.curTime == this.schedules[0][1]) {
+					let curRide = this.schedules.shift();
+					if(this.passengers.has(curRide[0])) {
+						this.passengers.get(curRide[0]).rides.push(curRide.slice(2));
+					} else {
+						this.passengers.set(curRide[0], new Passenger(curRide[0], curRide.slice(2)));
+					}
+				}
 			}
 		}
 		/**
@@ -62,7 +83,11 @@
 		getSchedule("schedule.txt").then(result => {
 			//challenge input
 			console.log(`%cChallenge Input: `, "color : red;");
-			let manager = new Manager(result);
+			let [totalCars, totalPassengers] = [Number(result[0]), Number(result[Number(result[0]) + 1])];
+			let [carSpecs, schedules] = [result.slice(1, 1 + totalCars), result.slice(-totalPassengers)];
+			carSpecs = carSpecs.map(spec => spec.split(" ").map((item, index) => index > 0 ? Number(item) : item));
+			schedules = schedules.map(schedule => schedule.split(" ").map((item, index) => index > 0 ? Number(item) : item));
+			let manager = new Manager(carSpecs, schedules);
 		}).catch(error => {console.log(error);});
 	});
 })();		
