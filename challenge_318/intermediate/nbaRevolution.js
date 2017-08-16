2162d2516a8575a1e2a1f180e3264d9bc0e0a9a9
YI ZHANG <scaccterran@gmail.com>
2017-08-15 20:06:52 -0400

    split match-ups into equal halves for the whole season

--- challenge_318/intermediate/nbaRevolution.js
+++ challenge_318/intermediate/nbaRevolution.js
@@ -18,6 +18,20 @@
 			return matchUps;
 		}
 		/**
+		 * split all match-ups for both season halves
+		 * @param {Array} [matchUps] - all match-ups
+		 *
+		 * @return {Array} [match-ups split into equal halves]
+		 */
+		function splitSeason(matchUps) {
+			let pairs = [];
+			for(let i = 0; i < matchUps.length - 1; i += 2) {
+				pairs.push(matchUps.slice(i, i + 2));
+			}
+			let picks = pairs.map(pair => Math.floor(Math.random() * 2));
+			return [picks.map((pick, index) => pairs[index][pick]), picks.map((pick, index) => pairs[index][pick == 1 ? 0 : 1])];
+		}
+		/**
 		 * assign games for all teams
 		 * @param {String} [names] - names of all teams
 		 *
@@ -26,6 +40,7 @@
 		function assignGames(names) {
 			let matchUps = allMatchUps(names);
 			console.log(matchUps);
+			console.log(splitSeason(matchUps));
 		}
 		//default input
 		console.log(`%cDefault Input: `, "color : red;");
@@ -33,6 +48,39 @@
                  Golden State Warriors
                  San Antonio Spurs
                  Toronto raptors`;
-		console.log(assignGames(input));              
+		console.log(assignGames(input)); 
+		//challenge input
+		console.log(`%cChallenge Input: `, "color : red;");
+		input = `Atlanta Hawks
+             Boston Celtics
+             Brooklyn Nets
+             Charlotte Hornets
+             Chicago Bulls
+             Cleveland Cavaliers
+             Dallas Mavericks
+             Denver Nuggets
+             Detroit Pistons
+             Golden State Warriors
+             Houston Rockets
+             Indiana Pacers
+             Los Angeles Clippers
+             Los Angeles Lakers
+             Memphis Grizzlies
+             Miami Heat
+             Milwaukee Bucks
+             Minnesota Timberwolves
+             New Orleans Pelicans
+             New York Knicks
+             Oklahoma City Thunder
+             Orlando Magic
+             Philadelphia 76ers
+             Phoenix Suns
+             Portland Trail Blazers
+             Sacramento Kings
+             San Antonio Spurs
+             Toronto Raptors
+             Utah Jazz
+             Washington Wizards`;
+		console.log(assignGames(input));            
 	});
 })();
