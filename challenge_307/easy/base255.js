07898a7bedee5198df9be412c03dfdb8e1446080
YI ZHANG <scaccterran@gmail.com>
2017-08-20 02:39:58 -0400

    encode/decode function 3 & 4

--- challenge_307/easy/base255.js
+++ challenge_307/easy/base255.js
@@ -42,12 +42,60 @@
 			let decodedMsg = messages.map(msg => 
 				msg.map(char => {
 					if(/\+/.test(char)) {
-						let total = char.slice(1);
-						return "+".repeat(total.length == 1 ? Number(total[0]) : total.split("").reduce((acc, val) => acc + Number(val), 0));  
+						return "+".repeat(char.length == 2 ? Number(char[1]) : char.slice(1).split("").reduce((acc, val) => acc + Number(val), 0));  
 					}
 					return char;
 				}).join(""));
 			return decodedMsg.join("\n");
+		}
+		/**
+		 * encode message
+		 * @param {String} [msg] - message to be encoded
+		 *
+		 * @return {String} [encoded message]
+		 */
+		function encode3(msg) {
+			let message = msg.trim();
+			let tail = message[message.length - 1];
+			return message.slice(0, -1).replace(/\+/g, "++").split("\n").map(line => line.trim()).join("+") + (tail == "+" ? "+++" : tail);
+		}
+		/**
+		 * decode message
+		 * @param {String} [encoded] - encoded message
+		 *
+		 * @return {String} [decoded message]
+		 */
+		function decode3(encoded) {
+			let messages = encoded.match(/(\w|\+{2,})+/g);
+			let decodedMsg = messages.map(msg => 
+				msg.match(/\w+|\++/g).map(char => {
+					return /\+/.test(char) ? (char.length % 2 ? "+" : char.slice(0, char.length * 0.5)) : char;
+				}).join(""));	
+			return decodedMsg.join("\n");
+		}
+		/**
+		 * encode message
+		 * @param {String} [msg] - message to be encoded
+		 *
+		 * @return {String} [encoded message]
+		 */
+		function encode4(msg) {
+			let message = msg.trim();
+			let tail = message[message.length - 1];
+			return message.slice(0, -1).split("\n").map(line => 
+				line.trim().replace(/^\+/, "+1").replace(/\+/g, "++")).join("+") + (tail == "+" ? "+++0" : tail);
+		}
+		/**
+		 * decode message
+		 * @param {String} [encoded] - encoded message
+		 *
+		 * @return {String} [decoded message]
+		 */
+		function decode4(encoded) {
+			let messages = encoded.split(/\++1/g).map((msg, index) => index === 0 ? msg : "+" + msg);
+			let decodedMsg = messages.map(msg => 
+				msg.match(/(\w|\+{2,})+/g).map(char => char.replace(/\+{2}/g, "+"))).reduce((acc, val) => [...acc, ...val], []);
+			return decodedMsg.join("\n").replace(/\++0/g, "+");
 		}
 		//challenge input 1
 		console.log(`%cChallenge Input 1: `, "color : red;");
@@ -68,5 +116,21 @@
     console.log(`%c${encoded}`, "color : orange;");
     console.log("Decode Into ->");
     console.log(`%c${decode2(encoded)}`, "color : orange;");
+    //challenge input 3
+		console.log(`%cChallenge Input 3: `, "color : red;");
+		console.log(`%c${input.split("\n").map(line => line.trim()).join("\n")}`, "color : skyblue;");
+    console.log("Encode Into ->");
+    encoded = encode3(input);
+    console.log(`%c${encoded}`, "color : orange;");
+    console.log("Decode Into ->");
+    console.log(`%c${decode3(encoded)}`, "color : orange;");
+    //challenge input 4
+		console.log(`%cChallenge Input 4: `, "color : red;");
+		console.log(`%c${input.split("\n").map(line => line.trim()).join("\n")}`, "color : skyblue;");
+    console.log("Encode Into ->");
+    encoded = encode4(input);
+    console.log(`%c${encoded}`, "color : orange;");
+    console.log("Decode Into ->");
+    console.log(`%c${decode4(encoded)}`, "color : orange;");
 	});
 })();		
