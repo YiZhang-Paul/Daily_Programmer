0e5e92924b019826495980089a0d6c3d334ea5e7
YI ZHANG <scaccterran@gmail.com>
2017-08-19 23:54:20 -0400

    encode/decode function 1

--- challenge_307/easy/base255.js
+++ challenge_307/easy/base255.js
@@ -1,6 +1,34 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * encode message 
+		 * @param {String} [msg] - message to be encoded
+		 *
+		 * @return {String} [encoded message]
+		 */
+		function encode1(msg) {
+			return msg.replace(/\+/g, "++").split("\n").map(line => line.trim()).join("+");
+		}
+		/**
+		 * decode message
+		 * @param {String} [encoded] - encoded message
+		 *
+		 * @return {String} [decoded message]
+		 */
+		function decode1(encoded) {
+			return encoded.match(/(\w|\+{2})+/g).map(msg => msg.replace(/\+{2}/g, "+")).join("\n");
+		}
+		//challenge input 1
+		console.log(`%cChallenge Input 1: `, "color : red;");
+		let input = `abc+def
+                 ghij
+                 klmno++p+`;
+    console.log(`%c${input.split("\n").map(line => line.trim()).join("\n")}`, "color : skyblue;");
+    console.log("Encode Into ->");
+    let encoded = encode1(input);
+    console.log(`%c${encoded}`, "color : orange;");
+    console.log("Decode Into ->");
+    console.log(`%c${decode1(encoded)}`, "color : orange;");
 	});
 })();		
