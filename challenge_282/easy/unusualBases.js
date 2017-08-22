4b4337a7a93cf5d9d624ba930be89d73040921d8
YI ZHANG <scaccterran@gmail.com>
2017-08-22 14:38:46 -0400

    generate fibonacci numbers

--- challenge_282/easy/unusualBases.js
+++ challenge_282/easy/unusualBases.js
@@ -1,6 +1,31 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * generate a fixed number of Fibonacci numbers
+		 * @param {int} [len] - total length of number list
+		 *
+		 * @return {Array} [Fibonacci number list]
+		 */
+		function getFibonacci(len) {
+			let list = [1, 1];
+			for(let i = 0; i < len - 2; i++) {
+				list.push(list[list.length - 1] + list[list.length - 2]);
+			}
+			return list;
+		}
+		/**
+		 * generate Fibonacci numbers until a given limit
+		 * @param {int} [limit] - maximum number of Fibonacci number
+		 *
+		 * @return {Array} [Fibonacci number list]
+		 */
+		function maxFibonacci(limit) {
+			let list = [1, 1];
+			while(list[list.length - 1] <= limit) {
+				list.push(list[list.length - 1] + list[list.length - 2]);
+			}
+			return list.slice(0, -1);
+		}
 	});
 })();			
