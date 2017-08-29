b6f75fd49b35cb9541b37fee94e8f89966c802c1
YI ZHANG <scaccterran@gmail.com>
2017-08-29 00:49:44 -0400

    fixed algorithm to correctly find lukcy numbers

--- challenge_329/easy/nearestLuckyNumber.js
+++ challenge_329/easy/nearestLuckyNumber.js
@@ -9,24 +9,52 @@
 		 */
 		function getLuckyNumbers(limit) {
 			let allNums = new Array(limit).fill(0).map((num, index) => index + 1);
-			let curPick = 2;
+			let curPick = 2, curStep = 0;
 			while(curPick <= allNums.length) {
-				let newNums = [], nextPick = allNums[curPick];
+				let newNums = [];
 				for(let i = 0; i < allNums.length; i++) {
 					if((i + 1) % curPick) {
 						newNums.push(allNums[i]);
 					}
 				}
-				[allNums, curPick] = [newNums, nextPick];
+				[allNums, curPick] = [newNums, newNums[++curStep]];
 			}
 			return allNums;
 		}
-		console.log(getLuckyNumbers(25));
-		let time = new Date().getTime();
-		console.log(getLuckyNumbers(10000000));
-		console.log(`${new Date().getTime() - time}ms`);
-		time = new Date().getTime();
-		console.log(getLuckyNumbers(100000));
-		console.log(`${new Date().getTime() - time}ms`);
+		/**
+		 * find nearest lucky numbers
+		 * @param {int} [target] - target number
+		 *
+		 * @return {String} [nearest numbers]
+		 */
+		function nearestLuckyNumber(target) {
+			let limit = Math.pow(10, String(target).length);
+			let luckyNums = getLuckyNumbers(limit);
+			let index = luckyNums.findIndex(num => target <= num);
+			return luckyNums[index] == target ? 
+				`${target} is a Lucky Number.` : `${luckyNums[index - 1]} < ${target} < ${luckyNums[index]}`;
+		}
+		//challenge input
+		console.log(`%cChallenge Input: `, "color : red;");
+		let input = 103;
+		console.log(`%c${input} -> %c${nearestLuckyNumber(input)}`, "color : skyblue;", "color : orange;");
+    input = 225;
+		console.log(`%c${input} -> %c${nearestLuckyNumber(input)}`, "color : skyblue;", "color : orange;");
+    input = 997;
+		console.log(`%c${input} -> %c${nearestLuckyNumber(input)}`, "color : skyblue;", "color : orange;");
+		//bonus input
+		//console.log(`%cBonus Input: `, "color : red;");
+		//let time = new Date().getTime();
+		//input = 100000;
+		//console.log(getLuckyNumbers(input));
+		//console.log(`%c${input} Found in: %c${new Date().getTime() - time}ms`, "color : skyblue;", "color : orange;");
+		//time = new Date().getTime();
+		//input = 1000000;
+		//console.log(getLuckyNumbers(input));
+		//console.log(`%c${input} Found in: %c${new Date().getTime() - time}ms`, "color : skyblue;", "color : orange;");
+		//time = new Date().getTime();
+		//input = 10000000;
+		//console.log(getLuckyNumbers(input));
+		//console.log(`%c${input} Found in: %c${new Date().getTime() - time}ms`, "color : skyblue;", "color : orange;");
 	});
 })();		
