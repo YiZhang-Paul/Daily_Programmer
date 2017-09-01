de2a68dcfa00903fb5695bbfb22de5e3c4079185
YI ZHANG <scaccterran@gmail.com>
2017-09-01 00:53:21 -0400

    all done

--- challenge_252/easy/sailorMonkeyCoconut.js
+++ challenge_252/easy/sailorMonkeyCoconut.js
@@ -8,7 +8,7 @@
 		 * 
 		 * @return {boolean} [test result]
 		 */
-		function hasValidCoconut(coconut, sailors) {
+		function validCoconutCount(coconut, sailors) {
 			for(let i = 0; i < sailors; i++) {
 				if(coconut % sailors != 1) {
 					return false;
@@ -17,5 +17,22 @@
 			}
 			return coconut % sailors === 0;
 		}
+		/**
+		 * find minimum amount of coconut
+		 * @param {int} [sailors] - total number of sailors
+		 *
+		 * @return {int} [minimum amount of coconuts]
+		 */
+		function getMinCoconut(sailors) {
+			let coconut = Math.pow(sailors, 2) + 1;
+			while(!validCoconutCount(coconut, sailors)) {
+				coconut += sailors;
+			}
+			return coconut;
+		}
+		//challenge input
+		console.log(`%cChallenge Input: `, "color : red;");
+		let input = 5;
+		console.log(`%c${input} Sailors -> %c${getMinCoconut(5)} Coconuts`, "color : skyblue;", "color : orange;");
 	});
 })();			
