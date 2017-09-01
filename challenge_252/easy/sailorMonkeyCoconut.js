19a637441f058723d6e14ca8ca2d6ffe43f37ce4
YI ZHANG <scaccterran@gmail.com>
2017-09-01 00:39:32 -0400

    check if a coconut amount is valid amount

--- challenge_252/easy/sailorMonkeyCoconut.js
+++ challenge_252/easy/sailorMonkeyCoconut.js
@@ -1,6 +1,21 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * check if coconut amount is valid
+		 * @param {int} [coconut] - total number of coconuts
+		 * @param {int} [sailors] - total number of sailors
+		 * 
+		 * @return {boolean} [test result]
+		 */
+		function hasValidCoconut(coconut, sailors) {
+			for(let i = 0; i < sailors; i++) {
+				if(coconut % sailors != 1) {
+					return false;
+				}
+				coconut -= ((coconut - 1) / sailors + 1);
+			}
+			return coconut % sailors === 0;
+		}
 	});
 })();			
