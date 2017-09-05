c50ffd8b32ba62d354aeef375a824d363e687dff
YI ZHANG <scaccterran@gmail.com>
2017-09-04 20:43:05 -0400

    part 3 complete

--- challenge_246/easy/xMassLights.js
+++ challenge_246/easy/xMassLights.js
@@ -34,6 +34,19 @@
 			}
 			return circuit + "\n";
 		}
+		/**
+		 * calculate resistence
+		 * @param {int} [hours] - total hours to lit up lights
+		 * @param {int} [capacity] - battery capacity
+		 * @param {float} [batteryVolt] - battery voltage
+		 * @param {float} [ledVolt] - LED voltage
+		 * @param {int} [current] - LED current
+		 *
+		 * @return {float} [total resistence]
+		 */
+		function getResistence(hours, capacity = 1200, batteryVolt = 9, ledVolt = 1.7, current = 20) {
+			return Math.round(batteryVolt % ledVolt / Math.floor(capacity / hours) * 1000 * 1000) / 1000;
+		}
 		//part 1 input
 		console.log(`%cPart 1 Input: `, "color : red;");
 		let input = 1;
@@ -54,5 +67,13 @@
 		console.log(`%c${drawCircuit(input)}`, "color : orange;");
 		input = 100;
 		console.log(`%c${drawCircuit(input)}`, "color : orange;");
+		//part 3 input
+		console.log(`%cPart 3 Input: `, "color : red;");
+		input = 1;
+		console.log(`%c${getResistence(input)}`, "color : orange;");
+		input = 4;
+		console.log(`%c${getResistence(input)}`, "color : orange;");
+		input = 8;		
+		console.log(`%c${getResistence(input)}`, "color : orange;");
 	});
 })();		
