0419a40156d068667ddad39372151c8897c13c0e
YI ZHANG <scaccterran@gmail.com>
2017-09-05 23:48:25 -0400

    find covering rectangle

--- challenge_330/easy/surroundCircles.js
+++ challenge_330/easy/surroundCircles.js
@@ -1,6 +1,26 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * find rectangle to cover all circles
+		 * @param {String} [circles] - location and radius of all circles
+		 *
+		 * @return {Array} [rectangle vertices]
+		 */
+		function getCoverRectangle(circles) {
+			circles = circles.split("\n").map(line => line.trim().split(",").map(Number));
+			let top = Math.max(...circles.map(circle => circle[1] + circle[2])).toFixed(3);
+			let bottom = Math.min(...circles.map(circle => circle[1] - circle[2])).toFixed(3);
+			let left = Math.min(...circles.map(circle => circle[0] - circle[2])).toFixed(3);
+			let right = Math.max(...circles.map(circle => circle[0] + circle[2])).toFixed(3);
+			return [`(${left}, ${bottom})`, `(${left}, ${top})`, `(${right}, ${top})`, `(${right}, ${bottom})`];
+		}
+		//challenge input
+		console.log(`%cChallenge Input: `, "color : red;");
+		let input = `1,1,2
+								 2,2,0.5
+								 -1,-3,2
+								 5,2,1`;
+		console.log(`%c${getCoverRectangle(input).join(", ")}`, "color : orange;");								 
 	});
 })();		
