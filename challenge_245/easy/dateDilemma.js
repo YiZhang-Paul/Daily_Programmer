871195bef2525fb5f956940fdf40509ef643622e
YI ZHANG <scaccterran@gmail.com>
2017-09-09 23:43:46 -0400

    custom date class

--- challenge_245/easy/dateDilemma.js
+++ challenge_245/easy/dateDilemma.js
@@ -7,246 +7,59 @@
 		 */
 		class CustomDate {
 			constructor(date) {
-				[this.year, this.month, this.dayOfMonth] = this.getDate(date);
-				this.dayOfWeek = this.getDayOfWeek();
+				[this.year, this.month, this.dayOfMonth, this.dayOfWeek] = this.getDate(date);
 			}
 			/**
-			 * append zero to dates when necessary
-			 * @param {int} [dateNum] - date values
+			 * append number to format year to 4 digits
+			 * @param {int} [year] - year to be appended
 			 *
-			 * @return {String} [reformatted date value]
+			 * @return {int} [appended year]
 			 */
-			appendZero(dateNum) {
-				return dateNum < 10 ? "0" + dateNum : dateNum;
+			appendYear(year) {
+				return year < 1000 ? year + 2000 : year;
 			}
 			/**
-			 * retrieve date
+			 * append zero to number smaller than 10
+			 * @param {int} [number] - number to be appended
+			 *
+			 * @return {String} [appended number]
+			 */
+			appendZero(number) {
+				return number < 10 ? "0" + number : number;
+			}
+			/**
+			 * get day of week
+			 * @param {int} [year] - current year
+			 * @param {int} [month] - current month in 0-index
+			 * @param {int} [dayOfMonth] - day in month
+			 *
+			 * @return {int} [day of week in 0-index, counting from Sunday]
+			 */
+			getDayOfWeek(year, month, dayOfMonth) {
+				return new Date(year, month, dayOfMonth).getDay();
+			}
+			/**
+			 * get date 
 			 * @param {String} [date] - date string
 			 *
-			 * @return {Array} [given year, month and day]
+			 * @return {Array} [date represented by date string]
 			 */
 			getDate(date) {
-				return reformatDate(date).split("-").map(Number);
+				const dateValues = date.match(/\d+/g).map(Number); 
+				const [year, month, dayOfMonth] = dateValues[0] < 1000 ? [dateValues[2], ...dateValues.slice(0, 2)] : dateValues;
+				return [this.appendYear(year), month, dayOfMonth, this.getDayOfWeek(this.appendYear(year), month - 1, dayOfMonth)];
 			}
 			/**
-			 * retrieve day of week
-			 *
-			 * @return {int} [day of week]
-			 */
-			getDayOfWeek() {
-				return new Date(this.year, this.month - 1, this.dayOfMonth).getDay();
-			}
-			/**
-			 * retrieve date string
+			 * get date string representing current date
 			 *
 			 * @return {String} [date string]
 			 */
-			getDateStr() {
-				return [this.year, this.appendZero(this.month), this.appendZero(this.dayOfMonth)].join("-");
-			}
-			/**
-			 * get total days in a month
-			 * @param {int} [month] - current month
-			 *
-			 * @return {int} [total days in given month]
-			 */
-			totalDaysOfMonth(month = this.month) {
-				const daysPerMonth = {1 : 31, 2 : 28, 3 : 31, 4 : 30, 5 : 31, 6 : 30, 7 : 31, 8 : 31, 9 : 30, 10 : 31, 11 : 30, 12 : 31};
-				return daysPerMonth[month];
-			}
-			/**
-			 * add days
-			 * @param {int} [days] - days to add
-			 */
-			addDays(days) {
-				if(this.totalDaysOfMonth() - this.dayOfMonth >= days) {
-					this.dayOfMonth += days;
-					return;
-				}
-				days -= this.totalDaysOfMonth() - this.dayOfMonth + 1;
-				if(++this.month == 13) {
-					[this.year, this.month] = [this.year + 1, 1];
-				}
-				this.dayOfMonth = 1;
-				this.addDays(days);
-			}
-			/**
-			 * reduce days
-			 * @param {int} [day] - days to reduce
-			 */
-			reduceDays(days) {
-				if(this.dayOfMonth > days) {
-					this.dayOfMonth -= days;
-					return;
-				}
-				days -= this.dayOfMonth;
-				if(--this.month === 0) {
-					[this.year, this.month] = [this.year - 1, 12];
-				}
-				this.dayOfMonth = this.totalDaysOfMonth();
-				this.reduceDays(days);
-			}
-			/**
-			 * get adjacent date
-			 * @param {String} [changes] - changes to date
-			 */
-			getAdjacentDate(changes) {
-				if(/\byesterday\b/.test(changes)) {
-					this.reduceDays(/\bbefore\b/.test(changes) ? 2 : 1);
-				} else if(/\btomorrow\b/.test(changes)) {
-					this.addDays(/\bafter\b/.test(changes) ? 2 : 1);
-				} 
-			}
-			/**
-			 * translate month name to month number
-			 * @param {String} [name] - name of month
-			 *
-			 * @return {int} [month number]
-			 */
-			getMonthNumber(name) {
-				const monthTable = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
-				return monthTable.indexOf(name.slice(0, 3)) + 1;
-			}
-			/**
-			 * switch current date
-			 * @param {String} [changes] - changes to date
-			 */
-			switchDate(changes) {
-				const dateNums = changes.match(/\d+/g).map(Number).sort((a, b) => b - a);
-				this.year = dateNums.length == 1 ? this.year : dateNums[0];
-				this.month = this.getMonthNumber(changes.match(/[a-z]+/)[0]);
-				this.dayOfMonth = dateNums[dateNums.length - 1];
-				this.dayOfWeek = this.getDayOfWeek();
-			}
-			/**
-			 * translate day name to day number in week
-			 * @param {String} [name] - name of day
-			 *
-			 * @return {int} [day number in 0-index] 
-			 */
-			getDayNumber(name) {
-				const dayTable = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
-				return dayTable.indexOf(name) ? dayTable.indexOf(name) : 7;
-			}
-			/**
-			 * move date forward
-			 * @param {String} [changes] - changes to date
-			 */
-			addDate(changes) {
-				if(/\bnext\b/.test(changes)) {
-					if(/\bday/.test(changes)) {
-						this.addDays(1);
-					} else if(/\bweek/.test(changes)) {
-						this.addDays(7);
-					} else if(/\byear/.test(changes)) {
-						this.year++;
-					} else if(/\bmonth/.test(changes)) {
-						[this.year, this.month] = this.month == 12 ? [this.year + 1, 1] : [this.year, this.month + 1];
-						this.dayOfMonth = this.dayOfMonth > this.totalDaysOfMonth() ? this.totalDaysOfMonth() : this.dayOfMonth;
-					} else {
-						this.addDays(7 - (this.dayOfWeek ? this.dayOfWeek : 7) + this.getDayNumber(changes.match(/\bmon|\btue|\bwed|\bthu|\bfri|\bsat|\bsun/)[0]));
-					}
-					return;
-				}
-				let [start, end] = changes.split("from").reverse();
-				if(/\btomorrow\b|\byesterday\b/.test(start)) {
-					this.getAdjacentDate(start);
-				}
-				let changeAmount = Number(end.replace(/\ba\b|\ban\b/, "1").match(/\d+/)[0]);
-				if(/\bday/.test(end)) {
-					this.addDays(changeAmount);
-				} else if(/\bweek/.test(end)) {
-					this.addDays(changeAmount * 7);
-				} else if(/\byear/.test(end)) {
-					this.year += changeAmount;
-				} else if(/\bmonth/.test(end)) {
-					[this.year, this.month] = this.month == 12 ? [this.year + 1, 1] : [this.year, this.month + 1];
-					this.dayOfMonth = this.dayOfMonth > this.totalDaysOfMonth() ? this.totalDaysOfMonth() : this.dayOfMonth;
-				}
-				console.log(changes.split("from")[1]);
-			}
-			/**
-			 * move date backward
-			 * @param {String} [changes] - changes to date
-			 */
-			reduceDate(changes) {
-				if(/\blast\b/.test(changes)) {
-					if(/\bday/.test(changes)) {
-						this.reduceDays(1);
-					} else if(/\bweek/.test(changes)) {
-						this.reduceDays(7);
-					} else if(/\byear/.test(changes)) {
-						this.year--;
-					} else if(/\bmonth/.test(changes)) {
-						[this.year, this.month] = this.month == 1 ? [this.year - 1, 12] : [this.year, this.month - 1];
-						this.dayOfMonth = this.dayOfMonth > this.totalDaysOfMonth() ? this.totalDaysOfMonth() : this.dayOfMonth;
-					} else {
-						this.reduceDays(7 - this.getDayNumber(changes.match(/\bmon|\btue|\bwed|\bthu|\bfri|\bsat|\bsun/)[0]) + (this.dayOfWeek ? this.dayOfWeek : 7));
-					} 
-					return;
-				}
-				let changeAmount = Number(changes.replace(/\ba\b|\ban\b/, "1").match(/\d+/)[0]);
-				if(/\bday/.test(changes)) {
-					this.reduceDays(changeAmount);
-				} else if(/\bweek/.test(changes)) {
-					this.reduceDays(changeAmount * 7);
-				}	else if(/\byear/.test(changes)) {
-					this.year -= changeAmount;
-				} else if(/\bmonth/.test(changes)) {
-					while(changeAmount) {
-						if(this.month > changeAmount) {
-							this.month -= changeAmount;
-							break;
-						}
-						changeAmount -= this.month;
-						[this.year, this.month] = [this.year - 1, 12];
-					}
-				}
-			}
-			/**
-			 * change date 
-			 * @param {String} [changes] - changes to date
-			 *
-			 * @return {String} [changed date string]
-			 */
-			changeDate(changes) {
-				if(/\bago\b|\blast\b/.test(changes)) this.reduceDate(changes);
-				else if(/\bfrom\b|\bnext\b/.test(changes)) this.addDate(changes);
-				else if(/\bjan|\bfeb|\bmar|\bapr|\bmay|\bjun|\bjul|\baug|\bsep|\boct|\bnov|\bdec/.test(changes)) this.switchDate(changes);
-				else this.getAdjacentDate(changes);
-				return this.getDateStr();
+			getDateString() {
+				return [this.year, this.appendZero(this.month), this.appendZero(this.day)].join("-");
 			}
 		}
-		/**
-		 * reformat date
-		 * @param {String} [date] - date string to be reformatted
-		 *
-		 * @return {String} [reformatted date]
-		 */
-		function reformatDate(date) {
-			const dateNums = date.match(/\d+/g).map(Number);
-			const [year, month, day] = dateNums[0] < 1000 ? [dateNums[2], ...dateNums.slice(0, 2)] : dateNums;
-			return `${year < 100 ? "20" + year : year}-${month < 10 ? "0" + month : month}-${day < 10 ? "0" + day : day}`;
-		}
-		/**
-		 * validate all dates
-		 * @param {String} [dates] - date strings to be validated
-		 *
-		 * @return {Array} [validated dates]
-		 */
-		function validateDates(dates) {
-			return dates.split("\n").map(date => reformatDate(date));
-		}
-		/**
-		 * translate notes into dates
-		 * @param {String} [notes] - notes to be translated
-		 * @param {String} [baseDate] - base date for reference
-		 *
-		 * @return {Array} [translated dates]
-		 */
-		function translateDate(notes, baseDate) {
-			return notes.split("\n").map(line => new CustomDate(baseDate).changeDate(line.toLowerCase()));
-		}
+		console.log(new CustomDate("2/13/15"));
+		console.log(new CustomDate("2012 3 17"));
 		//challenge input
 		console.log(`%cChallenge Input: `, "color : red;");
 		let input = `2/13/15
