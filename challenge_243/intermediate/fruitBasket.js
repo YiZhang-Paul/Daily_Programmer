1643188c7c6ffe51937a35d4133c53ff94f4dc42
YI ZHANG <scaccterran@gmail.com>
2017-09-11 05:52:02 -0400

    find all possible combinations of fruit to go into basket

--- challenge_243/intermediate/fruitBasket.js
+++ challenge_243/intermediate/fruitBasket.js
@@ -16,6 +16,53 @@
 			});
 			return table;
 		}
+		/**
+		 * find all combinations from a given list
+		 * @param {int} [total] - total number of selections
+		 * @param {Array} [list] - list to choose from
+		 * @param {Array} [selection] - current selections
+		 *
+		 * @return {Array} [all combinations]
+		 */
+		function getCombination(total, list, selection = []) {
+			if(selection.length == total || !list.length) {
+				return selection.length == total ? [selection] : null;
+			}
+			const combination = [];
+			for(let i = 0; i < list.length; i++) {
+				const result = getCombination(total, [...list.slice(i + 1)], [...selection, list[i]]);
+				if(Array.isArray(result)) {
+					combination.push(...result);
+				}
+			}
+			return combination;
+		}
+		/**
+		 * find all combinations of fruits 
+		 * @param {String} [fruits] - fruit information
+		 *
+		 * @return {Array} [all combination of fruits]
+		 */
+		function getFruitCombos(fruits) {
+			const names = fruits.split("\n").map(fruit => fruit.match(/[a-zA-Z\s]+/)[0].trim());
+			const combos = [];
+			for(let i = 1; i <= names.length; i++) {
+				combos.push(...getCombination(i, names));
+			}
+			return combos;
+		}
+		/**
+		 * find all possible ways to buy fruit basket
+		 * @param {String} [fruits] - fruit information
+		 * @param {int} [budget] - available budget
+		 *
+		 * @return {Array} [all possible ways to buy fruit basket]
+		 */
+		function fillBasket(fruits, budget) {
+			const fruitTable = getFruitTable(fruits);
+			const fruitCombos = getFruitCombos(fruits);
+			console.log(fruitCombos);
+		}
 		//default input
 		console.log(`%cDefault Input: `, "color : red;");
 		let input = `banana 32
@@ -23,6 +70,6 @@
 								 mango 97
 								 papaya 254
 								 pineapple 399`;
-		console.log(getFruitTable(input));								 
+		console.log(fillBasket(input, 500));								 
 	});
 })();		
