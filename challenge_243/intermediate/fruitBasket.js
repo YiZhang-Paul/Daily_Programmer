1196c2fd9f9aeba2468aaba1ceba64120fb61ea1
YI ZHANG <scaccterran@gmail.com>
2017-09-11 05:17:26 -0400

    create product table

--- challenge_243/intermediate/fruitBasket.js
+++ challenge_243/intermediate/fruitBasket.js
@@ -1,6 +1,28 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * create product table
+		 * @param {String} [products] - product information
+		 *
+		 * @return {Object} [product table]
+		 */
+		function getProductTable(products) {
+			const table = new Map();
+			products.split("\n").forEach(product => {
+				const name = product.match(/[a-zA-Z\s]+/)[0].trim();
+				const price = Number(product.match(/\d+/)[0]);
+				table.set(name, price);
+			});
+			return table;
+		}
+		//default input
+		console.log(`%cDefault Input: `, "color : red;");
+		let input = `banana 32
+								 kiwi 41
+								 mango 97
+								 papaya 254
+								 pineapple 399`;
+		console.log(getProductTable(input));								 
 	});
 })();		
