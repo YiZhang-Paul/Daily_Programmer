cddf6075485b983cdb8dfaf6d1e12411700c47a3
YI ZHANG <scaccterran@gmail.com>
2017-09-12 09:33:50 -0400

    check for valid pattern

--- challenge_238/easy/consonantsVowels.js
+++ challenge_238/easy/consonantsVowels.js
@@ -1,6 +1,21 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * check if input pattern is valid
+		 * @param {String} [pattern] - input pattern to be checked
+		 * @param {Object} [allowed] - allowed characters in the pattern
+		 *
+		 * @return {boolean} [test result]
+		 */
+		function isValid(pattern, allowed = new Set("cv")) {
+			return pattern.toLowerCase().split("").every(char => allowed.has(char));
+		}
+		//challenge & bonus input
+		console.log(`%cChallenge & Bonus Input: `, "color : red;");
+		let input = "cvcvcc";
+		console.log(isValid(input));
+		input = "CcvV";
+		input = "cvcvcvcvcvcvcvcvcvcv"; 
 	});
 })();		
