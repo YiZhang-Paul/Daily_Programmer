7c0152badbf209f7ce6c8317dde7306704adcf62
YI ZHANG <scaccterran@gmail.com>
2017-09-12 09:41:01 -0400

    generate random words

--- challenge_238/easy/consonantsVowels.js
+++ challenge_238/easy/consonantsVowels.js
@@ -2,6 +2,16 @@
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
 		/**
+		 * pick random character
+		 * @param {char} [type] - type of character
+		 *
+		 * @return {char} [randomly picked character]
+		 */
+		function pickChar(type = "c") {
+			const chars = type == "c" ? "bcdfghjklmnpqrstvwxyz" : "aeiou";
+			return chars[Math.floor(Math.random() * chars.length)];
+		}
+		/**
 		 * check if input pattern is valid
 		 * @param {String} [pattern] - input pattern to be checked
 		 * @param {Object} [allowed] - allowed characters in the pattern
@@ -11,11 +21,25 @@
 		function isValid(pattern, allowed = new Set("cv")) {
 			return pattern.toLowerCase().split("").every(char => allowed.has(char));
 		}
+		/**
+		 * randomly generate words with given pattern
+		 * @param {String} [pattern] - pattern to be followed
+		 *
+		 * @return {String} [randomly generated word]
+		 */
+		function generateWord(pattern) {
+			if(!isValid(pattern)) {
+				return null;
+			}
+			return pattern.split("").map(char => pickChar(char.toLowerCase())).join("");
+		}
 		//challenge & bonus input
 		console.log(`%cChallenge & Bonus Input: `, "color : red;");
 		let input = "cvcvcc";
-		console.log(isValid(input));
+		console.log(generateWord(input));
 		input = "CcvV";
+		console.log(generateWord(input));
 		input = "cvcvcvcvcvcvcvcvcvcv"; 
+		console.log(generateWord(input));
 	});
 })();		
