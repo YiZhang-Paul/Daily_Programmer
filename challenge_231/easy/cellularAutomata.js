6dac49c3b7ff7a3452fe503957ce41064077cee1
YI ZHANG <scaccterran@gmail.com>
2017-09-13 12:13:56 -0400

    fixed algorithm to generate next state

--- challenge_231/easy/cellularAutomata.js
+++ challenge_231/easy/cellularAutomata.js
@@ -3,18 +3,33 @@
 	document.addEventListener("DOMContentLoaded", () => {
 		/**
 		 * get next celular automata states
-		 * @param {String} [state] - current state of celular automata
+		 * @param {String} [state] - current celular automata state
 		 *
 		 * @return {String} [next state of celular automata]
 		 */
 		function getNextState(state) {
-			let curState = "0" + state + "0";
+			let newState = "";
+			const curState = "0" + state + "0";
 			for(let i = 1; i < curState.length - 1; i++) {
-				const newState = curState[i - 1] == curState[i + 2] ? "0" : "1";
-				curState = curState.slice(0, i) + newState + curState.slice(i + 1);
+				newState += curState[i - 1] == curState[i + 1] ? "0" : "1";
 			}
-			return curState.slice(1, -1);
+			return newState;
 		}
-		console.log(getNextState("1101010"));
+		/**
+		 * get states of celular automata for given number of steps
+		 * @param {String} [state] - starting celular automata state
+		 * @param {int} [steps] - number of steps to simulate
+		 *
+		 * @return {Array} [states for given steps]
+		 */
+		function getStates(state, steps) {
+			const states = [state];
+			for(let i = 0; i < steps; i++) {
+				state = getNextState(state);
+				states.push(state);
+			}
+			return states;
+		}
+		console.log(getStates("1101010", 25));
 	});
 })();		
