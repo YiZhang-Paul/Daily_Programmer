7c3d190522c388c88f896bf876e8e048632c64db
YI ZHANG <scaccterran@gmail.com>
2017-09-13 12:04:36 -0400

    get next state

--- challenge_231/easy/cellularAutomata.js
+++ challenge_231/easy/cellularAutomata.js
@@ -1,6 +1,20 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * get next celular automata states
+		 * @param {String} [state] - current state of celular automata
+		 *
+		 * @return {String} [next state of celular automata]
+		 */
+		function getNextState(state) {
+			let curState = "0" + state + "0";
+			for(let i = 1; i < curState.length - 1; i++) {
+				const newState = curState[i - 1] == curState[i + 2] ? "0" : "1";
+				curState = curState.slice(0, i) + newState + curState.slice(i + 1);
+			}
+			return curState.slice(1, -1);
+		}
+		console.log(getNextState("1101010"));
 	});
 })();		
