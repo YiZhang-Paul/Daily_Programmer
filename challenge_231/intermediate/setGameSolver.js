3ed12bf2b62fca98868eca7628bb7f9b393ed62d
YI ZHANG <scaccterran@gmail.com>
2017-09-13 16:06:51 -0400

    check if a set of cards form a valid set

--- challenge_231/intermediate/setGameSolver.js
+++ challenge_231/intermediate/setGameSolver.js
@@ -1,6 +1,17 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * check if three cards form a set
+		 * @param {Array} [cards] - cards to be checked
+		 *
+		 * @return {boolean} [test result]
+		 */
+		function isValidSet(cards) {
+			return cards[0].split("").every((attribute, index) => {
+				const attributes = new Set(cards.map(card => card[index]));
+				return attributes.size == 1 || attributes.size == cards.length;
+			});
+		}
 	});
 })();		
