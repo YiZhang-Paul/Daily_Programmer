c428cb1de711dc23c34e394b96e671f7c3780f40
YI ZHANG <scaccterran@gmail.com>
2017-09-13 16:29:50 -0400

    all done

--- challenge_231/intermediate/setGameSolver.js
+++ challenge_231/intermediate/setGameSolver.js
@@ -2,7 +2,7 @@
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
 		/**
-		 * check if three cards form a set
+		 * check if three cards form a valid set
 		 * @param {Array} [cards] - cards to be checked
 		 *
 		 * @return {boolean} [test result]
@@ -13,5 +13,56 @@
 				return attributes.size == 1 || attributes.size == cards.length;
 			});
 		}
+		/**
+		 * find all valid sets
+		 * @param {Array} [cards] - all cards
+		 * @param {int} [size] - size of a set
+		 * @param {Array} [curSet] - current set
+		 *
+		 * @return {Array} [all valid sets]
+		 */
+		function findSets(cards, size = 3, curSet = []) {
+			if(curSet.length == size || !cards.length) {
+				return curSet.length == size ? (isValidSet(curSet) ? [curSet.join(" ")] : null) : null;
+			}
+			const sets = [];
+			for(let i = 0; i < cards.length; i++) {
+				const result = findSets(cards.slice(i + 1), size, [...curSet, cards[i]]);
+				if(Array.isArray(result)) {
+					sets.push(...result);
+				}
+			}
+			return sets;
+		}
+		//default input
+		console.log(`%cDefault input: `, "color : red;");
+		let input = `SP3F
+							   DP3O
+							   DR2F
+							   SP3H
+							   DG3O
+							   SR1H
+							   SG2O
+							   SP1F
+							   SP3O
+							   OR3O
+							   OR3H
+							   OR2H`;
+		console.log(`%c${findSets(input.split("\n").map(card => card.trim())).join("\n")}`, "color : orange;");				
+		//challenge input
+		console.log(`%cChallenge input: `, "color : red;");
+		input = `DP2H
+			       DP1F
+			       SR2F
+			       SP1O
+			       OG3F
+			       SP3H
+			       OR2O
+			       SG3O
+			       DG2H
+			       DR2H
+			       DR1O
+			       DR3O`;			   
+		console.log(`%c${findSets(input.split("\n").map(card => card.trim())).join("\n")}`, "color : orange;");				
 	});
 })();		
