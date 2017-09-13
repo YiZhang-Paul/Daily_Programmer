e30105016d744b0524d6ef44bd4c80f0b953a9fd
YI ZHANG <scaccterran@gmail.com>
2017-09-13 12:51:53 -0400

    all done

--- challenge_231/easy/cellularAutomata.js
+++ challenge_231/easy/cellularAutomata.js
@@ -16,20 +16,38 @@
 			return newState;
 		}
 		/**
+		 * replace state with characters
+		 * @param {String} [state] - state to be replaced
+		 *
+		 * @return {String} [replaced state]
+		 */
+		function replaceState(state) {
+			return state.split("").map(char => char == "0" ? " " : "X").join("");
+		}
+		/**
 		 * get states of celular automata for given number of steps
 		 * @param {String} [state] - starting celular automata state
 		 * @param {int} [steps] - number of steps to simulate
 		 *
 		 * @return {Array} [states for given steps]
 		 */
-		function getStates(state, steps) {
-			const states = [state];
+		function getStates(state, steps = 25) {
+			const states = [replaceState(state)];
 			for(let i = 0; i < steps; i++) {
 				state = getNextState(state);
-				states.push(state);
+				states.push(replaceState(state));
 			}
-			return states;
+			return states.filter(state => /\w/.test(state));
 		}
-		console.log(getStates("1101010", 25));
+		//default input
+		console.log(`%cDefault Input: `, "color : red;");
+		let input = "1101010";
+		console.log(`%c${input} ->`, "color : skyblue;");
+		console.log(`%c${getStates(input, 25).join("\n")}`, "color : orange;");
+		//challenge input
+		console.log(`%cChallenge Input: `, "color : red;");
+		input = "00000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000";
+		console.log(`%c${input} ->`, "color : skyblue;");
+		console.log(`%c${getStates(input, 25).join("\n")}`, "color : orange;");
 	});
-})();		
+})();
