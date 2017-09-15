7ee10e0bc011e0e3a5640be005a5565be2fea8a6
YI ZHANG <scaccterran@gmail.com>
2017-09-15 13:03:33 -0400

    factor a number

--- challenge_226/easy/addFractions.js
+++ challenge_226/easy/addFractions.js
@@ -2,21 +2,51 @@
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
 		/**
-		 * add fractions together
-		 * @param {String} [fractions] - fractions to add
+		 * find all factors of a number
+		 * @param {int} [number] - number to be factored
 		 *
-		 * @return {String} [resulting fraction]
+		 * @return {Array} [all factors of number]
 		 */
-		function addFraction(fractions) {
-			let allFraction = fractions.split("\n").map(fraction => fraction.match(/\d+/g).map(Number));
-			const denominator = allFraction.reduce((acc, val) => acc * val[1], 1);
-			const numerator = allFraction.reduce((acc, val) => acc + denominator / val[1] * val[0], 0);
-			return numerator + "/" + denominator;
+		function getFactors(number) {
+			let factors = [];
+			while(number != 1) {
+				for(let i = 2; i <= number; i++) {
+					if(number % i === 0) {
+						number /= i;
+						factors.push(i);
+						break;
+					}
+				}
+			}
+			return factors.length ? factors : [1];
 		}
-		//default input
-		console.log(`%cDefault Input: `, "color : red;");
-		let input = `1/6
-							   3/10`;
-		console.log(addFraction(input));							   
+		// //default input
+		// console.log(`%cDefault Input: `, "color : red;");
+		// let input = `1/6
+		// 					   3/10`;
+		// console.log(addFraction(input));		
+		// input = `1/3
+  //            1/4
+  //            1/12`;
+		// console.log(addFraction(input));		
+  //   //challenge input
+		// console.log(`%cChallenge Input: `, "color : red;"); 
+		// input = `2/9
+		// 	       4/35
+		// 	       7/34
+		// 	       1/2
+		// 	       16/33`; 
+		// console.log(addFraction(input));		
+		// input = `1/7
+  //            35/192
+  //            61/124
+  //            90/31
+  //            5/168
+  //            31/51
+  //            69/179
+  //            32/5
+  //            15/188
+  //            10/17`;       					   
+		// console.log(addFraction(input));
 	});
 })();		
