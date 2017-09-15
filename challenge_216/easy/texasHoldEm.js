a438f72bb835cfbb111c7dd634ccde775d8fc0c3
YI ZHANG <scaccterran@gmail.com>
2017-09-15 07:33:32 -0400

    deal cards to players

--- challenge_216/easy/texasHoldEm.js
+++ challenge_216/easy/texasHoldEm.js
@@ -4,17 +4,35 @@
 		/**
 		 * generate deck of cards
 		 *
-		 * @return {Object} [deck of cards]
+		 * @return {Array} [deck of cards]
 		 */
 		function getDeck() {
-			let deck = new Set();
+			let deck = [];
 			for(let i = 1; i <= 13; i++) {
 				for(let j = 1; j <= 4; j++) {
-					deck.add(i * 10 + j);
+					deck.push(i * 10 + j);
 				}
 			}
 			return deck;
 		}
-		console.log(getDeck());
+		/**
+		 * deal cards for a round
+		 * @param {Array} [cards] - all available cards
+		 * @param {int} [players] - total number of players
+		 *
+		 * @return {Object} [all cards dealt for the round]
+		 */
+		function dealCard(cards = getDeck(), players = 8) {
+			let deal = {};
+			for(let i = 0; i < players; i++) {
+				let curDeal = [];
+				for(let j = 0; j < 2; j++) {
+					curDeal.push(cards.splice(Math.floor(Math.random() * cards.length), 1)[0]);
+				}
+				deal.players = deal.players ? [...deal.players, curDeal] : [curDeal];
+			}
+			return deal;
+		}
+		console.log(dealCard());
 	});
 })();		
