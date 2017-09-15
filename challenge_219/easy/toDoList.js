e782af2c45981341fd896ea1f5e0e56a01c3f1f1
YI ZHANG <scaccterran@gmail.com>
2017-09-15 09:42:20 -0400

    all done

--- challenge_219/easy/toDoList.js
+++ challenge_219/easy/toDoList.js
@@ -1,6 +1,50 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * to-do list class
+		 */
+		class ToDoList {
+			constructor() {
+				this.items = new Set();
+			}
+			/**
+			 * add item
+			 * @param {String} [item] - item to add 
+			 */
+			addItem(item) {
+				if(this.items.has(item)) {
+					console.log("Item Already Exist.");
+					return;
+				}
+				this.items.add(item);
+			}
+			/**
+			 * delete item
+			 * @param {String} [item] - item to delete
+			 */
+			deleteItem(item) {
+				if(!this.items.has(item)) {
+					console.log("Item Does Not Exist.");
+					return;
+				}
+				this.items.delete(item);
+			}
+			/**
+			 * view item list
+			 */
+			viewList() {
+				console.log(Array.from(this.items).join("\n"));
+			}
+		}
+		//challenge input
+		console.log(`%cChallenge Input: `, "color : red;");
+		let list = new ToDoList();
+		list.addItem('Take a shower');
+		list.addItem('Go to work');
+		list.viewList();
+		list.addItem('Buy a new phone');
+		list.deleteItem('Go to work');
+		list.viewList();
 	});
 })();				
