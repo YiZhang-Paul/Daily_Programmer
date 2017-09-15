003850a66bc83bebc9848a31b082f679454c3b29
YI ZHANG <scaccterran@gmail.com>
2017-09-15 07:26:00 -0400

    generate full deck of cards

--- challenge_216/easy/texasHoldEm.js
+++ challenge_216/easy/texasHoldEm.js
@@ -1,6 +1,20 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * generate deck of cards
+		 *
+		 * @return {Object} [deck of cards]
+		 */
+		function getDeck() {
+			let deck = new Set();
+			for(let i = 1; i <= 13; i++) {
+				for(let j = 1; j <= 4; j++) {
+					deck.add(i * 10 + j);
+				}
+			}
+			return deck;
+		}
+		console.log(getDeck());
 	});
 })();		
