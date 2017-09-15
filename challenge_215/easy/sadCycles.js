2e03e7d4b917c717d7064d9b71078cd40b0baf26
YI ZHANG <scaccterran@gmail.com>
2017-09-15 06:10:09 -0400

    get sum of all digits raised to a given power

--- challenge_215/easy/sadCycles.js
+++ challenge_215/easy/sadCycles.js
@@ -1,6 +1,18 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * raise all digits of a number to given power and get sum
+		 * @param {int} [number] - number to be manipulated
+		 * @param {int} [power] - target power
+		 *
+		 * @return {int} [result number]
+		 */
+		function getPowerSum(number, power = 2) {
+			return String(number).split("")
+													 .map(digit => Math.pow(Number(digit), power))
+													 .reduce((acc, val) => acc + val);
+		}
+		console.log(getPowerSum(12));
 	});
 })();		
