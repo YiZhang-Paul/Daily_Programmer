30d013d67afaa1eee1d8ecbf6016aaa29263aadc
YI ZHANG <scaccterran@gmail.com>
2017-09-15 13:19:41 -0400

    find least common multiply

--- challenge_226/easy/addFractions.js
+++ challenge_226/easy/addFractions.js
@@ -20,6 +20,29 @@
 			}
 			return factors.length ? factors : [1];
 		}
+		/**
+		 * find least common multiply of all numbers
+		 * @param {Array} [numbers] - numbers to be checked
+		 *
+		 * @return {int} [least common multiply]
+		 */
+		function findLCM(numbers) {
+			let factors = numbers.map(getFactors);
+			let commonFactor = [];
+			for(let i = 0; i < factors.length; i++) {
+				for(let j = i + 1; j < factors.length; j++) {
+					for(let k = factors[i].length - 1; k >= 0; k--) {
+						const index = factors[j].indexOf(factors[i][k]);
+						if(index != -1) {
+							commonFactor.push(factors[j].splice(index, 1)[0]);
+							factors[i].splice(k, 1);
+						}
+					}
+				}
+				commonFactor.push(...factors[i]);
+			} 
+			return commonFactor.reduce((acc, val) => acc * val);
+		}
 		// //default input
 		// console.log(`%cDefault Input: `, "color : red;");
 		// let input = `1/6
