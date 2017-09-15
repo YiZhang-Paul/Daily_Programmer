3b305aada256e8ad4fe8ca6183593f51892acac4
YI ZHANG <scaccterran@gmail.com>
2017-09-15 13:55:01 -0400

    all done

--- challenge_226/easy/addFractions.js
+++ challenge_226/easy/addFractions.js
@@ -43,33 +43,60 @@
 			} 
 			return commonFactor.reduce((acc, val) => acc * val);
 		}
-		// //default input
-		// console.log(`%cDefault Input: `, "color : red;");
-		// let input = `1/6
-		// 					   3/10`;
-		// console.log(addFraction(input));		
-		// input = `1/3
-  //            1/4
-  //            1/12`;
-		// console.log(addFraction(input));		
-  //   //challenge input
-		// console.log(`%cChallenge Input: `, "color : red;"); 
-		// input = `2/9
-		// 	       4/35
-		// 	       7/34
-		// 	       1/2
-		// 	       16/33`; 
-		// console.log(addFraction(input));		
-		// input = `1/7
-  //            35/192
-  //            61/124
-  //            90/31
-  //            5/168
-  //            31/51
-  //            69/179
-  //            32/5
-  //            15/188
-  //            10/17`;       					   
-		// console.log(addFraction(input));
+		/**
+		 * find greatest common divisor
+		 * @param {int} [number1] - number 1
+		 * @param {int} [number2] - number 2
+		 *
+		 * @return {int} [greatest common divisor]
+		 */
+		function findGCD(number1, number2) {
+			return number1 % number2 ? findGCD(number2, number1 % number2) : number2;
+		}
+		/**
+		 * add fractions
+		 * @param {String} [fractions] - fractions to add
+		 *
+		 * @return {String} [resulting fraction]
+		 */
+		function addFraction(fractions) {
+			let allFraction = fractions.split("\n").map(fraction => fraction.match(/\d+/g).map(Number));
+			const denominator = findLCM(allFraction.map(fraction => fraction[1]));
+			const numerator = allFraction.reduce((acc, val) => acc + denominator / val[1] * val[0], 0);
+			const divisor = findGCD(denominator, numerator);
+			return numerator / divisor + "/" + denominator / divisor;
+		}
+		//default input
+		console.log(`%cDefault Input: `, "color : red;");
+		let input = `1/6
+							   3/10`;
+		console.log(`%c${input.split("\n").map(fraction => fraction.trim()).join(" + ")} -> `, "color : skyblue;");							   
+		console.log(`%c${addFraction(input)}`, "color : orange;");		
+		input = `1/3
+             1/4
+             1/12`;
+		console.log(`%c${input.split("\n").map(fraction => fraction.trim()).join(" + ")} -> `, "color : skyblue;");							   
+		console.log(`%c${addFraction(input)}`, "color : orange;");				
+    //challenge input
+		console.log(`%cChallenge Input: `, "color : red;"); 
+		input = `2/9
+			       4/35
+			       7/34
+			       1/2
+			       16/33`; 
+		console.log(`%c${input.split("\n").map(fraction => fraction.trim()).join(" + ")} -> `, "color : skyblue;");							   
+		console.log(`%c${addFraction(input)}`, "color : orange;");				
+		input = `1/7
+             35/192
+             61/124
+             90/31
+             5/168
+             31/51
+             69/179
+             32/5
+             15/188
+             10/17`;       					   
+		console.log(`%c${input.split("\n").map(fraction => fraction.trim()).join(" + ")} -> `, "color : skyblue;");							   
+		console.log(`%c${addFraction(input)}`, "color : orange;");		
 	});
 })();		
