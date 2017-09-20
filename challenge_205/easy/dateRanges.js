b83f9af5023caf1f6e80df3e290f5fe7e94c7953
YI ZHANG <scaccterran@gmail.com>
2017-09-20 17:50:44 -0400

    fix for edge case #2

--- challenge_205/easy/dateRanges.js
+++ challenge_205/easy/dateRanges.js
@@ -114,6 +114,16 @@
 				return date1.month == date2.month;
 			}
 			/**
+			 * check if two dates have the same day
+			 * @param {Object} [date1] - date 1
+			 * @param {Object} [date2] - date 2
+			 *
+			 * @return {boolean} [test result] 
+			 */
+			isSameDay(date1 = this.date1, date2 = this.date2) {
+				return date1.day == date2.day;
+			}
+			/**
 			 * check if two dates are the same 
 			 * @param {Object} [date1] - date 1
 			 * @param {Object} [date2] - date 2
@@ -144,20 +154,39 @@
 				}
 				const [begin, end] = this.sortDate(this.date1, this.date2);
 				if(this.inCurrentYear(begin) && end.year - begin.year <= 1) {
+					if(this.isSameMonth() && this.isSameDay()) {
+						return begin.printDate("YMD", this.format) + " - " + end.printDate("YMD", this.format);
+					}
 					return begin.printDate("MD", this.format) + " - " + end.printDate(this.isSameMonth() ? "D" : "MD", this.format);
 				}
 				return begin.printDate(this.isSameYear() ? "MD" : "YMD", this.format) + " - " + end.printDate("YMD", this.format);
 			}
 		}
-		console.log(new RangeGenerator("2015-07-01", "2015-07-04").getDateRange());
-		console.log(new RangeGenerator("2015-12-01", "2016-02-03").getDateRange());
-		console.log(new RangeGenerator("2015-12-01", "2017-02-03").getDateRange());
-		console.log(new RangeGenerator("2016-03-01", "2016-05-05").getDateRange());
-		console.log(new RangeGenerator("2017-01-01", "2017-01-01").getDateRange());
-		console.log(new RangeGenerator("2022-09-05", "2023-09-04").getDateRange());
-
-		console.log(new RangeGenerator("2015-07-01", "2015-07-04", "DMY").getDateRange());
-		console.log(new RangeGenerator("2016-03-01", "2016-05-05", "YDM").getDateRange());
-		console.log(new RangeGenerator("2022-09-05", "2023-09-04", "YMD").getDateRange());
+		//challenge input
+		console.log(`%cChallenge Input: `, "color : red;");
+		let input = ["2015-07-01", "2015-07-04"];
+		console.log(`%c${input.join(" ~ ")} -> %c${new RangeGenerator(...input).getDateRange()}`, "color : skyblue;", "color : orange;");
+		input = ["2015-12-01", "2016-02-03"];
+		console.log(`%c${input.join(" ~ ")} -> %c${new RangeGenerator(...input).getDateRange()}`, "color : skyblue;", "color : orange;");
+		input = ["2015-12-01", "2017-02-03"];
+		console.log(`%c${input.join(" ~ ")} -> %c${new RangeGenerator(...input).getDateRange()}`, "color : skyblue;", "color : orange;");
+		input = ["2016-03-01", "2016-05-05"];
+		console.log(`%c${input.join(" ~ ")} -> %c${new RangeGenerator(...input).getDateRange()}`, "color : skyblue;", "color : orange;");
+		input = ["2017-01-01", "2017-01-01"];
+		console.log(`%c${input.join(" ~ ")} -> %c${new RangeGenerator(...input).getDateRange()}`, "color : skyblue;", "color : orange;");
+		input = ["2022-09-05", "2023-09-04"];
+		console.log(`%c${input.join(" ~ ")} -> %c${new RangeGenerator(...input).getDateRange()}`, "color : skyblue;", "color : orange;");
+		input = ["2015-04-01", "2020-09-10"];
+		console.log(`%c${input.join(" ~ ")} -> %c${new RangeGenerator(...input).getDateRange()}`, "color : skyblue;", "color : orange;");
+		input = ["2015-12-11", "2016-12-11"];
+		console.log(`%c${input.join(" ~ ")} -> %c${new RangeGenerator(...input).getDateRange()}`, "color : skyblue;", "color : orange;");
+		//bonus input
+		console.log(`%cBonus Input: `, "color : red;");
+		input = ["2015-07-01", "2015-07-04", "DMY"];
+		console.log(`%c${input.slice(0, 2).join(" ~ ")} (${input[2]}) -> %c${new RangeGenerator(...input).getDateRange()}`, "color : skyblue;", "color : orange;");
+		input = ["2016-03-01", "2016-05-05", "YDM"];
+		console.log(`%c${input.slice(0, 2).join(" ~ ")} (${input[2]}) -> %c${new RangeGenerator(...input).getDateRange()}`, "color : skyblue;", "color : orange;");
+		input = ["2022-09-05", "2023-09-04", "YMD"];
+		console.log(`%c${input.slice(0, 2).join(" ~ ")} (${input[2]}) -> %c${new RangeGenerator(...input).getDateRange()}`, "color : skyblue;", "color : orange;");
 	});
 })();			
