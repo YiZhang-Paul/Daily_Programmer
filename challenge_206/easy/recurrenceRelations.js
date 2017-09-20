9bb5d2ebeedfaf669dc761721abb5bbda4dd69d9
YI ZHANG <scaccterran@gmail.com>
2017-09-20 13:55:13 -0400

    read and apply all operations to a term

--- challenge_206/easy/recurrenceRelations.js
+++ challenge_206/easy/recurrenceRelations.js
@@ -1,6 +1,31 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * get all operations on each term
+		 * @param {String} [operations] - all operations on one line
+		 *
+		 * @return {Array} [all operations on each term]
+		 */
+		function getOperation(operations) {
+			return operations.split(" ").map(operation => [operation[0], operation.slice(1)]);
+		}
+		/**
+		 * apply all operations on a term
+		 * @param {float} [term] - current term to apply all operations
+		 * @param {Array} [operations] - all operations to apply
+		 *
+		 * @return {float} [result after all operations]
+		 */
+		function applyOperation(term, operations) {
+			let result = term;
+			for(let i = 0; i < operations.length; i++) {
+				if(operations[i][0] == "+") result += operations[i][1];
+				else if(operations[i][0] == "-") result -= operations[i][1];
+				else if(operations[i][0] == "*") result *= operations[i][1];
+				else result /= operations[i][1];
+			}
+			return result;
+		}
 	});
 })();		
