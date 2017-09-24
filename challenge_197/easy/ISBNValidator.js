727d5eb5b456ee3163e115432d0eacb6363f5223
YI ZHANG <scaccterran@gmail.com>
2017-09-24 19:56:33 -0400

    validate ISBN

--- challenge_197/easy/ISBNValidator.js
+++ challenge_197/easy/ISBNValidator.js
@@ -1,6 +1,17 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * check if an ISBN is valid
+		 * @param {String} [isbn] - ISBN to be validated
+		 *
+		 * @return {boolean} [validation result]
+		 */
+		function isValidISBN(isbn) {
+			let digits = isbn.match(/\w/g);
+			return digits.reduce((acc, val, index) => 
+				acc + (isNaN(Number(val)) ? 10 : Number(val)) * (digits.length - index), 0) % 11 === 0;
+		}
+		console.log(isValidISBN("0-7475-3269-9"));
 	});
 })();		
