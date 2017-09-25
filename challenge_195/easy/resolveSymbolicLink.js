b783599cf02c28d14f3afb9339e194832fa3ac2a
YI ZHANG <scaccterran@gmail.com>
2017-09-25 15:06:39 -0400

    base challenge complete

--- challenge_195/easy/resolveSymbolicLink.js
+++ challenge_195/easy/resolveSymbolicLink.js
@@ -32,15 +32,31 @@
 		 * @return {boolean} [test result]
 		 */
 		function isExtended(path, table) {
-			let directory = path.match(/[^\/]+/g);
-			let curPath = "/" + directory[0];
-			for(let i = 1; i < directory.length; i++) {
-				curPath += "/" + directory[i];
-				if(table.get(curPath)) {
+			let directories = path.match(/[^\/]+/g);
+			for(let i = 0, curPath = ""; i < directories.length; i++) {
+				curPath += "/" + directories[i];
+				if(table.has(curPath)) {
 					return false;
 				}
 			}
 			return true;
+		}
+		/**
+		 * extend a path
+		 * @param {String} [path] - path to be checked
+		 * @param {Object} [table] - symbolic links table
+		 *
+		 * @return {String} [extended path]
+		 */
+		function extendPath(path, table) {
+			let directories = path.match(/[^\/]+/g);
+			for(let i = 0, curPath = ""; i < directories.length; i++) {
+				curPath += "/" + directories[i];
+				if(table.has(curPath)) {
+					return path.replace(curPath, table.get(curPath));
+				}
+			}
+			return path;
 		}
 		/**
 		 * resolve symbolic link
@@ -50,15 +66,26 @@
 		 * @return {String} [resolved path]
 		 */
 		function resolveLink(symbolics, path) {
-			let links = makeTable(symbolics);
-			//while(isExtended(path, links)) {
-			//	
-			//}
-			return links;
+			let links = makeTable(symbolics), counter = 0;
+			while(!isExtended(path, links)) {
+				path = extendPath(path, links);
+				if(++counter == 500) {
+					return "Infinite Loop Detected.";
+				}
+			}
+			return path;
 		}
 		//default input
 		console.log(`%cDefault Input: `, "color : red;");
 		let input = [["/bin/thing:/bin/thing-3", "/bin/thing-3:/bin/thing-3.2", "/bin/thing-3.2/include:/usr/include", "/usr/include/SDL:/usr/local/include/SDL"], "/bin/thing/include/SDL/stan"];
-		console.log(resolveLink(...input));
+		console.log(`%c${input[1]} -> %c${resolveLink(...input)}`, "color : skyblue;", "color : orange;");
+		//challenge input
+		console.log(`%cChallenge Input: `, "color : red;");
+		input = [["/home/elite/documents:/media/mmcstick/docs"], "/home/elite/documents/office"];
+		console.log(`%c${input[1]} -> %c${resolveLink(...input)}`, "color : skyblue;", "color : orange;");
+		input = [["/bin:/usr/bin", "/usr/bin:/usr/local/bin/", "/usr/local/bin/log:/var/log-2014"], "/bin/log/rc"];
+		console.log(`%c${input[1]} -> %c${resolveLink(...input)}`, "color : skyblue;", "color : orange;");
+		input = [["/etc:/tmp/etc", "/tmp/etc/:/etc/"], "/etc/modprobe.d/config/"];
+		console.log(`%c${input[1]} -> %c${resolveLink(...input)}`, "color : skyblue;", "color : orange;");
 	});
 })();		
