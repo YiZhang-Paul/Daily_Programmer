f6dbb70944f4134c10d649a5443f612050731f36
YI ZHANG <scaccterran@gmail.com>
2017-09-25 14:51:24 -0400

    check if a path is fully extended

--- challenge_195/easy/resolveSymbolicLink.js
+++ challenge_195/easy/resolveSymbolicLink.js
@@ -2,6 +2,15 @@
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
 		/**
+		 * remove trailing forward slash
+		 * @param {String} [path] - path to be checked
+		 *
+		 * @return {String} [path with trailing forward slash removed]
+		 */
+		function removeSlash(path) {
+			return path[path.length - 1] == "/" ? path.slice(0, -1) : path;
+		}
+		/**
 		 * create symbolic table
 		 * @param {Array} [symbolics] - symbolic links
 		 *
@@ -11,9 +20,27 @@
 			let table = new Map();
 			symbolics.forEach(symbolic => {
 				let link = symbolic.split(":");
-				table.set(link[0], link[1]);
+				table.set(removeSlash(link[0]), removeSlash(link[1]));
 			});
 			return table;
+		}
+		/**
+		 * check if a path is fully extended
+		 * @param {String} [path] - path to be checked
+		 * @param {Object} [table] - symbolic links table
+		 *
+		 * @return {boolean} [test result]
+		 */
+		function isExtended(path, table) {
+			let directory = path.match(/[^\/]+/g);
+			let curPath = "/" + directory[0];
+			for(let i = 1; i < directory.length; i++) {
+				curPath += "/" + directory[i];
+				if(table.get(curPath)) {
+					return false;
+				}
+			}
+			return true;
 		}
 		/**
 		 * resolve symbolic link
@@ -23,11 +50,15 @@
 		 * @return {String} [resolved path]
 		 */
 		function resolveLink(symbolics, path) {
-			console.log(makeTable(symbolics));
+			let links = makeTable(symbolics);
+			//while(isExtended(path, links)) {
+			//	
+			//}
+			return links;
 		}
 		//default input
 		console.log(`%cDefault Input: `, "color : red;");
-		let input = [["bin/thing:/bin/thing-3", "/bin/thing-3:/bin/thing-3.2", "/bin/thing-3.2/include:/usr/include", "/usr/include/SDL:/usr/local/include/SDL"], "/bin/thing/include/SDL/stan"];
+		let input = [["/bin/thing:/bin/thing-3", "/bin/thing-3:/bin/thing-3.2", "/bin/thing-3.2/include:/usr/include", "/usr/include/SDL:/usr/local/include/SDL"], "/bin/thing/include/SDL/stan"];
 		console.log(resolveLink(...input));
 	});
 })();		
