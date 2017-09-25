15a2b1dcc1287329586600dc5a9eb0771ab9a311
YI ZHANG <scaccterran@gmail.com>
2017-09-25 14:35:12 -0400

    create symbolic link table

--- challenge_195/easy/resolveSymbolicLink.js
+++ challenge_195/easy/resolveSymbolicLink.js
@@ -1,6 +1,33 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * create symbolic table
+		 * @param {Array} [symbolics] - symbolic links
+		 *
+		 * @return {Object} [symbolic table]
+		 */
+		function makeTable(symbolics) {
+			let table = new Map();
+			symbolics.forEach(symbolic => {
+				let link = symbolic.split(":");
+				table.set(link[0], link[1]);
+			});
+			return table;
+		}
+		/**
+		 * resolve symbolic link
+		 * @param {Array} [symbolics] - symbolic links
+		 * @param {String} [path] - path to be resolved
+		 *
+		 * @return {String} [resolved path]
+		 */
+		function resolveLink(symbolics, path) {
+			console.log(makeTable(symbolics));
+		}
+		//default input
+		console.log(`%cDefault Input: `, "color : red;");
+		let input = [["bin/thing:/bin/thing-3", "/bin/thing-3:/bin/thing-3.2", "/bin/thing-3.2/include:/usr/include", "/usr/include/SDL:/usr/local/include/SDL"], "/bin/thing/include/SDL/stan"];
+		console.log(resolveLink(...input));
 	});
 })();		
