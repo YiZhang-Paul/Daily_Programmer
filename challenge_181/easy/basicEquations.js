5f86c8db591cda96df66c3bec0627dcc8a568ba8
YI ZHANG <scaccterran@gmail.com>
2017-09-29 01:16:51 -0400

    extract numbers and coefficients from expression

--- challenge_181/easy/basicEquations.js
+++ challenge_181/easy/basicEquations.js
@@ -1,6 +1,27 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * extract number from string
+		 * @param {String} [numStr] - string to extract number from
+		 *
+		 * @return {Array} [numbers extracted from string]
+		 */
+		function getNumber(numStr) {
+			return /\d/.test(numStr) ? numStr.match(/[+-]{0,1}\d+\.*\d*/g).map(Number) : [1];
+		}
+		/**
+		 * read expression
+		 * @param {String} [expression] - expression to read
+		 *
+		 * @return {Object} [expression broken down]
+		 */
+		function readExpression(expression) {
+			let sides = expression.split("=").map(side => side.trim());
+			const totalY = getNumber(sides[0])[0];
+			const [a, b] = sides[1].split("x").map(number => getNumber(number)[0]);
+			return {totalY, a, b};
+		}
+		console.log(readExpression("y=2x+2"));
 	});
 })();		
