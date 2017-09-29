8438f2542e2a9fbc4c0d584b20cdf8815aec83bb
YI ZHANG <scaccterran@gmail.com>
2017-09-28 23:09:18 -0400

    all done

--- challenge_188/easy/yyyymmdd.js
+++ challenge_188/easy/yyyymmdd.js
@@ -2,8 +2,25 @@
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
 		/**
+		 * retrieve dates
+		 * @param {String} [url] - date string URL
+		 *
+		 * @return {Object} [Promise object]
+		 */
+		function getDates(url) {
+			return new Promise((resolve, reject) => {
+				let xhttp = window.XMLHttpRequest ? new window.XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+				xhttp.onreadystatechange = function() {
+					if(this.readyState == 4 && this.status == 200) resolve(this.responseText.split("\n").map(date => date.trim()));
+					else if(this.status == 404) reject("No Dates Found.");
+				};
+				xhttp.open("GET", url, true);
+				xhttp.send();
+			});
+		}
+		/**
 		 * prepend zero for numbers smaller than 10
-		 * @param {int} [number] - number to prepend zero
+		 * @param {int} [number] - number to prepend
 		 *
 		 * @return {String} [number with zero prepended]
 		 */
@@ -11,30 +28,43 @@
 			return number < 10 ? "0" + number : number;
 		}
 		/**
-		 * get month number for a given month name
+		 * get month number corresponding to month name
 		 * @param {String} [month] - month name
 		 *
-		 * @return {int} [month number]
+		 * @return {int} [corresponding month number]
 		 */
 		function getMonthNumber(month) {
 			const table = {
 				Jan : 1, Feb : 2, Mar : 3, Apr : 4, May : 5, Jun : 6,
 				Jul : 7, Aug : 8, Sep : 9, Oct : 10, Nov : 11, Dec : 12
  			};
-			return table[month];
+ 			return prependZero(table[month]);
 		}
 		/**
 		 * convert date string to number
 		 * @param {String} [date] - date string for conversion
-		 * @param {String} [type] - date string type (year/month/day)
+		 * @param {String} [type] - date string type
 		 *
 		 * @return {int} [number after conversion]
 		 */
-		function toNumber(date, type) {
+		function dateToNumber(date, type) {
 			if(type == "M") {
 				return /\d/.test(date) ? prependZero(Number(date)) : getMonthNumber(date);
 			}
-			return type == "Y" ? date % 1000 + (date % 1000 < 50 ? 2000 : 1900) : prependZero(Number(date));
+			return type == "Y" ? date % 100 + (date % 100 < 50 ? 2000 : 1900) : prependZero(Number(date));
+		}
+		/**
+		 * get format of current date string
+		 * @param {String} [date] - date string
+		 *
+		 * @return {String} [current format of date string]
+		 */
+		function getFormat(date) {
+			const separator = /[/#*]/.test(date) ? date.match(/[/#*]/)[0] : 0;
+			if(separator == "/" || separator === 0) {
+				return "MDY";	
+			}
+			return separator == "#" ? "MYD" : "DMY";
 		}
 		/**
 		 * format date into ISO 8601 standard
@@ -43,19 +73,19 @@
 		 * @return {String} [formatted date]
 		 */
 		function formatDate(date) {
-			let dates = date.match(/\w+/g);
-			let format = "";
-			const separator = /[-/#*]/.test(date) ? date.match(/[-/#*]/)[0] : 0;
-			if(separator == "-") {
+			if(/-/.test(date)) {
 				return date;
 			}
-			if(separator == "/" || separator === 0) format = "MDY";
-			else if(separator == "#") format = "MYD";
-			else if(separator == "*") format = "DMY";
-			return format.split("").map(type => toNumber(dates[format.indexOf(type)], type)).join("-");
+			let dates = date.match(/\w+/g);
+			const format = getFormat(date);
+			return "YMD".split("").map(type => dateToNumber(dates[format.indexOf(type)], type)).join("-");
 		}
-
-		console.log(formatDate("06#72#03"));
-		console.log(formatDate("Dec 26, 75"));
+		getDates("dates.txt").then(dates => {
+			//challenge input
+			console.log(`%cChallenge Input: `, "color : red;");
+			dates.forEach(date => {
+				console.log(`%c${formatDate(date)}`, "color : orange;");
+			});
+		}).catch(error => {console.log(error);});
 	});
 })();		
