9e9f09a8b7ed373998e19b3f071f79f695b26c3b
YI ZHANG <scaccterran@gmail.com>
2017-09-29 19:12:51 -0400

    all done

--- challenge_180/easy/lookAndSay.js
+++ challenge_180/easy/lookAndSay.js
@@ -20,7 +20,24 @@
 			}
 			return description + counter + curNum;
 		}
-
-		console.log(describeNumber("111221"));
+		/**
+		 * generate Nth Look and Say number
+		 * @param {int} [place] - Nth Look and Say number to generate
+		 * @param {int} [seed] - seed number to start with
+		 * 
+		 * @return {String} [Nth Look and Say number]
+		 */
+		function getLookAndSayNumber(place, seed = 1) {
+			let number = String(seed);
+			for(let i = 0; i < place - 1; i++) {
+				number = describeNumber(number);
+			}
+			return number;
+		}
+		//challenge & bonus input
+		console.log(`%cChallenge & Bonus Input: `, "color : red;");
+		for(let i = 1; i <= 10; i++) {
+			console.log(`%cSeed: ${i} -> %cNumber: ${getLookAndSayNumber(6, i)}`, "color : skyblue;", "color : orange;");
+		}
 	});
 })();		
