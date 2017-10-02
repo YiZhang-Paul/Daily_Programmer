1f07ac1a62f30ca6400e13097fef8efcd441133d
YI ZHANG <scaccterran@gmail.com>
2017-10-01 23:26:28 -0400

    all done

--- challenge_174/easy/thueMorseSequences.js
+++ challenge_174/easy/thueMorseSequences.js
@@ -1,6 +1,30 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * generate Thus-Morse sequence
+		 * @param {int} [order] - Nth order of sequence to be generated
+		 *
+		 * @return {Array} [Nth order of sequence]
+		 */
+		function getMorseSequence(order = 0) {
+			if(order <= 1) {
+				return order ? "01" : "0";
+			}
+			let sequence = "01";
+			for(let i = 2; i <= order; i++) {
+				const halfLen = sequence.length * 0.5;
+				sequence += sequence.slice(halfLen) + sequence.slice(0, halfLen);
+			}
+			return [sequence];
+		}
+		//challenge input
+		console.log(`%cChallenge Input: `, "color : red;");
+		for(let i = 0; i <= 6; i++) {
+			console.log(`%c${i}th: ${getMorseSequence(i)}`, "color : orange;");
+		}
+		//bonus input
+		console.log(`%cBonus Input: `, "color : red;");
+		console.log(getMorseSequence(25));
 	});
 })();		
