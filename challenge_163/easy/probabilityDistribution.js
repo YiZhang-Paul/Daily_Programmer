4753267f101320346d7be4ad301aab6ea5b0da21
YI ZHANG <scaccterran@gmail.com>
2017-10-03 02:28:09 -0400

    all done

--- challenge_163/easy/probabilityDistribution.js
+++ challenge_163/easy/probabilityDistribution.js
@@ -20,8 +20,25 @@
 			for(let i = 0; i < rolls; i++) {
 				distribution[rollDice() - 1]++;
 			}
-			return distribution.map(chance => Math.round(chance / rolls * 10000) / 100);
+			return distribution.map(chance => (chance / rolls * 100).toFixed(2) + "%");
 		}
-		console.log(findDistribution(10));
+		/**
+		 * display probability distribution table
+		 * @param {Array} [sizes] - sample sizes for each group
+		 * 
+		 * @return {String} [probability distribution table]
+		 */
+		function getDistributionTable(sizes) {
+			let table = `# of Rolls ${[1, 2, 3, 4, 5, 6].join("s     ")}s\n${"=".repeat(52)}\n`;
+			sizes.forEach(size => {
+				const header = size + " ".repeat(10 - String(size).length);
+				const content = findDistribution(size).map(chance => " ".repeat(6 - chance.length) + chance).join(" ");
+				table += `${header} ${content}\n`;
+			});
+			return table;
+		}
+		//challenge input
+		console.log(`%cChallenge Input: `, "color : red;");
+		console.log(`%c${getDistributionTable([10, 100, 1000, 10000, 100000, 1000000])}`, "color : orange;");
 	});
 })();		
