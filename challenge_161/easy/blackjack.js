6c3773d3400b384cad77c1305fea80cbbe2a755f
YI ZHANG <scaccterran@gmail.com>
2017-10-04 19:57:15 -0400

    create a deck of card

--- challenge_161/easy/blackjack.js
+++ challenge_161/easy/blackjack.js
@@ -1,6 +1,21 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * create a deck of card
+		 *
+		 * @return {Array} [deck of card]
+		 */
+		function makeDeck() {
+			let deck = [];
+			for(let i = 1; i <= 13; i++) {
+				const suit = "DCHS";
+				for(let j = 0; j < suit.length; j++) {
+					deck.push(i + suit[j]);
+				}
+			}
+			return deck;
+		}
+		console.log(makeDeck());
 	});
 })();		
