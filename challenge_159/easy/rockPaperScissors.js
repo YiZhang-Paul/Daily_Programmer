8bbdba209d02cfc0f30ecdbea293347409456b37
YI ZHANG <scaccterran@gmail.com>
2017-10-05 09:14:53 -0400

    play a round

--- challenge_159/easy/rockPaperScissors.js
+++ challenge_159/easy/rockPaperScissors.js
@@ -10,7 +10,7 @@
 			constructor(name = "Computer", type = "AI") {
 				this.name = name;
 				this.type = type;
-				this.win = 0;
+				this.totalWin = 0;
 				this.moves = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
 			}
 			/**
@@ -49,27 +49,29 @@
 		 * play a round of game
 		 * @param {Object} [player1] - player 1
 		 * @param {Object} [player2] - player 2
-		 * @param {Object} [gameRule] - game rule
+		 * @param {Object} [rule] - game rule
 		 *
-		 * @return {Array} [game result]
+		 * @return {String} [round result]
 		 */
-		function playRound(player1, player2, gameRule = rule) {
-			let roundInfo = `${player1.name} Picks: ${player1.move}.\n${player2.name} Picks: ${player2.move}.\n\n`;
-			if(player1.move == player2.move) {
-				return `${roundInfo}Game Tied!`;
+		function playRound(player1, player2, rule) {
+			const move1 = player1.type == "AI" ? player1.autoMove() : player1.getMove();
+			const move2 = player2.type == "AI" ? player2.autoMove() : player2.getMove();
+			const roundInfo = `${player1.name} (${player1.type}) Picks: ${move1}\n${player2.name} (${player2.type}) Picks: ${move2}\n\n`;
+			if(move1 == move2) {
+				return `${roundInfo}Game Tied!\n`;
 			}
-			const [win, lose] = rule[player1.move][player2.move] ? [player1, player2] : [player2, player1];
-			return [win, lose, `${roundInfo}${win.move} ${rule[win.move][lose.move]} ${lose.move}. ${win.name} Wins!`];
+			const [winMove, loseMove] = rule[move1][move2] ? [move1, move2] : [move2, move1];
+			let [winner, loser] = winMove == move1 ? [player1, player2] : [player2, player1];
+			winner.totalWin++;
+			return `${roundInfo}${winMove} ${rule[winMove][loseMove]} ${loseMove}. ${winner.name} (${winner.type}) Wins!\n`;
 		}
-
-
 		/**
-		 * play a given number of games and calculate result
+		 * play a given number of games
 		 * @param {Array} [player1Info] - player 1 information
 		 * @param {Array} [player2Info] - player 2 information
 		 * @param {int} [rounds] - total rounds of game
 		 *
-		 * @return {String} [game result]
+		 * @return {Array} [game result]
 		 */
 		function playGame(player1Info, player2Info, rounds = 5) {
 			const rule = Object.freeze({
@@ -80,11 +82,12 @@
 				Spock : {Scissors : "Smashes", Rock : "Vaporizes"}
 			});
 			let [player1, player2] = [new Player(...player1Info), new Player(...player2Info)];
+			let result = "";
 			for(let i = 0; i < rounds; i++) {
-				playRound(player1, player2, rule);
+				result += `Round ${i + 1}:\n${playRound(player1, player2, rule)}\n`;
 			}
-			return [player1.win, player2.win, rounds - player1.win - player2.win];
+			return [player1, player2, rounds, result];
 		}
-		console.log(playGame(["John", "Human"], ["Tim", "AI"]));
+		console.log(playGame(["John", "AI"], ["Tim", "AI"]));
 	});
 })();		
