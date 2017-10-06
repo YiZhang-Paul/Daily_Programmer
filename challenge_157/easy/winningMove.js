23b87aad89fc6b5593a9d7954ce47fbf5ea2e1b6
YI ZHANG <scaccterran@gmail.com>
2017-10-06 00:13:48 -0400

    read current board

--- challenge_157/easy/winningMove.js
+++ challenge_157/easy/winningMove.js
@@ -1,6 +1,18 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * read current board
+		 * @param {String} [layout] - current layout
+		 *
+		 * @return {Array} [current board]
+		 */
+		function readBoard(layout) {
+			return layout.match(/\S+(?=\n)|\S+(?!.)/g).map(row => row.split(""));
+		}
+		let board = `XX-
+                 -XO
+                 OO-`;
+		console.log(readBoard(board));
 	});
 })();		
