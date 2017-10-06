8dad435036d8f16681e8edc1c5552fc1aedf2807
YI ZHANG <scaccterran@gmail.com>
2017-10-06 05:09:48 -0400

    challenge 334 intermediate

--- challenge_158/easy/tornNumber.js
+++ challenge_158/easy/tornNumber.js
@@ -13,8 +13,26 @@
 				return false;
 			}
 			const [half1, half2] = [numStr.slice(0, numStr.length * 0.5), numStr.slice(numStr.length * 0.5)];
-			return Math.pow(Number(half1) + Number(half2), 2) == number;
+			return Math.pow(Number(half1) + Number(half2), 2)  == number;
 		}
-		console.log(isTornNumber(3025));
+		/**
+		 * find torn numbers from all numbers of a given length
+		 * @param {int} [digits] - total number of digits in number
+		 *
+		 * @return {Array} [all torn numbers]
+		 */
+		function findTornNumber(digits) {
+			const [start, end] = [Math.pow(10, digits - 1), Math.pow(10, digits) - 1];
+			let tornNumber = [];
+			for(let i = start; i <= end; i++) {
+				if(isTornNumber(i)){
+					tornNumber.push(i);
+				}
+			}
+			return tornNumber;
+		}
+		//challenge & bonus input
+		console.log(`%cChallenge & Bonus Input: `, "color : red;");
+		console.log(`%cTorn Numbers: %c${findTornNumber(4).join(", ")}`, "color : skyblue;", "color : orange;");
 	});
 })();		
