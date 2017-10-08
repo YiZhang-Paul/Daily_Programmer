c0f1ab15e84a3b1ffb66bbdc6aa1d1d09b45724e
YI ZHANG <scaccterran@gmail.com>
2017-10-08 07:16:57 -0400

    all done

--- challenge_146/easy/polygonPerimeter.js
+++ challenge_146/easy/polygonPerimeter.js
@@ -1,6 +1,21 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * find permitter of a polygon
+		 * @param {String} [specs] - polygon specifications
+		 *
+		 * @return {float} [permitter of polygon]
+		 */
+		function getPermitter(specs) {
+			const [side, circumradius] = specs.match(/\d+\.{0,1}\d*/g).map(Number);
+			return (Math.sin(Math.PI / side) * circumradius * 2 * side).toFixed(3);
+		}
+		//challenge input
+		console.log(`%cChalleng Input: `, "color : red;");
+		let input = "5 3.7";
+		console.log(`%c${getPermitter(input)}`, "color : orange;");
+		input = "100 1.0";
+		console.log(`%c${getPermitter(input)}`, "color : orange;");
 	});
 })();
