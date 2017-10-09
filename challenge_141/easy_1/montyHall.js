4992472db53e22306c580bfe919ceb50ebdde4d6
YI ZHANG <scaccterran@gmail.com>
2017-10-09 09:36:08 -0400

    initalize round and pick a random door

--- challenge_141/easy_1/montyHall.js
+++ challenge_141/easy_1/montyHall.js
@@ -1,6 +1,40 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
+		/**
+		 * pick random number 
+		 * @param {int} [min] - minimum number
+		 * @param {int} [max] - maximum number
+		 *
+		 * @return {int} [random number]
+		 */
+		function randomNumber(min = 0, max = 3) {
+			return Math.floor(Math.random() * (max + 1 - min)) + min;
+		}
+		/**
+		 * initialize round
+		 *
+		 * @return {Array} [objects behind each door]
+		 */
+		function initRound() {
+			let objects = ["goat", "goat", "car"];
+			let arrangement = [];
+			while(objects.length) {
+				const index = randomNumber(0, objects.length - 1);
+				arrangement.push(objects.splice(index, 1)[0]);
+			}
+			return arrangement;
+		}
+		/**
+		 * pick a random door
+		 * @param {int} [doors] - total number of doors
+		 *
+		 * @return {int} [random door number]
+		 */
+		function pickDoor(doors = 3) {
+			return randomNumber(0, doors - 1);
+		}
 		
+		console.log(pickDoor());
 	});
 })();		
