6355e60d71bb91553fd758a139777afaabf4a752
YI ZHANG <scaccterran@gmail.com>
2017-10-09 11:41:30 -0400

    determine final landing row

--- challenge_142/easy/fallingSand.js
+++ challenge_142/easy/fallingSand.js
@@ -1,6 +1,25 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		
+		/**
+		 * check if target row can be landed on
+		 * @param {char} [target] - target row
+		 *
+		 * @return {boolean} [test result]
+		 */
+		function canLand(target) {
+			return /\S/.test(target);
+		}
+		/**
+		 * find landing row for sand
+		 * @param {int} [curRow] - current row of sand
+		 * @param {Array} [column] - current sand column
+		 *
+		 * @return {int} [landing row index]
+		 */
+		function getLandingRow(curRow, column) {
+			return column.slice(curRow + 1).findIndex(row => canLand(row)) + curRow;
+		}
+		console.log(getLandingRow(1, [" ", ".", " ", " ", ".", "#"]));
 	});
 })();		
