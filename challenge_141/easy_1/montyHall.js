a3ab9217713ee4de457d8b58885cfa04e0a5951d
YI ZHANG <scaccterran@gmail.com>
2017-10-09 10:44:08 -0400

    all done

--- challenge_141/easy_1/montyHall.js
+++ challenge_141/easy_1/montyHall.js
@@ -2,7 +2,7 @@
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
 		/**
-		 * pick random number 
+		 * generate random number
 		 * @param {int} [min] - minimum number
 		 * @param {int} [max] - maximum number
 		 *
@@ -13,17 +13,17 @@
 		}
 		/**
 		 * initialize round
-		 * @param {Array} [objects] - avaliable objects for the round
+		 * @param {Array} [objects] - available objects for the round
 		 *
 		 * @return {Array} [objects behind each door]
 		 */
 		function initRound(objects = ["goat", "goat", "car"]) {
-			let arrangement = [];
+			let arrange = [];
 			while(objects.length) {
 				const index = randomNumber(0, objects.length - 1);
-				arrangement.push(objects.splice(index, 1)[0]);
+				arrange.push(objects.splice(index, 1)[0]);
 			}
-			return arrangement;
+			return arrange;
 		}
 		/**
 		 * pick a random door
@@ -36,26 +36,26 @@
 		}
 		/**
 		 * suggest a door to player
-		 * @param {Array} [arrangement] - current arrangement
-		 * @param {int} [curPick] - current player pick
+		 * @param {Array} [arrange] - arrangement of objects
+		 * @param {int} [pick] - player's pick
 		 *
 		 * @return {int} [suggested door]
 		 */
-		function suggestDoor(arrangement, curPick) {
-			let remainDoors = arrangement.map((door, index) => index).filter(door => door != curPick);
-			let validDoors = remainDoors.filter(door => arrangement[door] != "car");
+		function suggestDoor(arrange, pick) {
+			let remainDoors = arrange.map((door, index) => index).filter(door => door != pick);
+			let validDoors = remainDoors.filter(door => arrange[door] != "car");
 			const doorShown = validDoors[randomNumber(0, validDoors.length - 1)];
 			return remainDoors.filter(door => door != doorShown);
 		}
 		/**
 		 * check if a player wins a round
-		 * @param {Array} [arrangement] - current arrangement
-		 * @param {int} [curPick] - current player pick
+		 * @param {Array} [arrange] - arrangement of objects
+		 * @param {int} [pick] - player's pick
 		 *
 		 * @return {boolean} [test result]
 		 */
-		function isWin(arrangement, curPick) {
-			return arrangement[curPick] == "car";
+		function isWin(arrange, pick) {
+			return arrange[pick] == "car";
 		}
 		/**
 		 * simulate a round with both tactics
@@ -63,13 +63,32 @@
 		 * @return {Object} [round result with both tactics]
 		 */
 		function simulateRound() {
-			let arrangement = initRound();
-			const doorPick = pickDoor();
+			let arrange = initRound();
+			const pick = pickDoor();
 			return {
-				stick : isWin(arrangement, doorPick),
-				change : isWin(arrangement, suggestDoor(arrangement, doorPick))
+				stick : isWin(arrange, pick),
+				change : isWin(arrange, suggestDoor(arrange, pick))
 			};
 		}
-		console.log(simulateRound());
+		/**
+		 * simulate games and calculate success rate with both tactics
+		 * @param {int} [rounds] - total number of rounds to simulate
+		 *
+		 * @return {String} [winning rates for different tactics]
+		 */
+		function getSuccessRate(rounds) {
+			let [tactic1Win, tactic2Win] = [0, 0];
+			for(let i = 0; i < rounds; i++) {
+				let result = simulateRound();
+				tactic1Win += result.stick ? 1 : 0;
+				tactic2Win += result.change ? 1 : 0;
+			}
+			return [tactic1Win, tactic2Win].map((wins, index) => {
+				return `Tactic ${index + 1}: ${(wins / rounds * 100).toFixed(1)}% Winning Chance`;
+			}).join("\n");
+		}
+		//challenge input
+		console.log(`%cChallenge Input: `, "color : red;");
+		console.log(`%c1000000 Games ->\n%c${getSuccessRate(1000000)}`, "color : skyblue;", "color : orange;");
 	});
 })();		
