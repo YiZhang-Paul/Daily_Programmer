ced2122a53def0e48aec2ba5c98189d9781af28d
YI ZHANG <scaccterran@gmail.com>
2017-10-12 11:16:57 -0400

    check if a sentence is a pangram

---
+++ challenge_139/easy/pangrams/pangrams/PangramChecker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pangrams {
+    class PangramChecker {
+        /**
+         * check if a string is pangram
+         * @param {string} [testStr] - string to test
+         * @param {Dictionary<char, int>} [counter] - letter counter
+         * @param {int} [totalLetter] - total number of letters in a writing system
+         * 
+         * @return {bool} [test result]
+         */
+        public bool IsPangram(string testStr, Dictionary<char, int> counter, int totalLetter = 26) {
+            string lowerStr = testStr.ToLower();
+            foreach(char character in lowerStr) {
+                if(Char.IsLetter(character)) {
+                    counter[character] = counter.ContainsKey(character) ? counter[character] + 1 : 1;
+                }
+            }
+            return counter.Count >= totalLetter;
+        }
+    }
+}
--- challenge_139/easy/pangrams/pangrams/Program.cs
+++ challenge_139/easy/pangrams/pangrams/Program.cs
@@ -7,6 +7,19 @@ using System.Threading.Tasks;
 namespace pangrams {
     class Program {
         static void Main(string[] args) {
+            //default & challenge & bonus input
+            string[] sentences = new string[] { 
+                "The quick brown fox jumps over the lazy dog",
+                "The quick brown fox jumps over the lazy dog.",
+                "Pack my box with five dozen liquor jugs",
+                "Saxophones quickly blew over my jazzy hair"
+            };
+
+            PangramChecker checker = new PangramChecker();
+
+            foreach(string sentence in sentences) {
+                Console.WriteLine(checker.IsPangram(sentence, new Dictionary<char,int>()));
+            }
         }
     }
 }
