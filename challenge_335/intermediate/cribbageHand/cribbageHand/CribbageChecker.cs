6d34cec502578f0be50a1ba1940a381c9df5a822
YI ZHANG <scaccterran@gmail.com>
2017-10-12 16:22:03 -0400

    check pairs

--- challenge_335/intermediate/cribbageHand/cribbageHand/CribbageChecker.cs
+++ challenge_335/intermediate/cribbageHand/cribbageHand/CribbageChecker.cs
@@ -6,11 +6,8 @@ using System.Threading.Tasks;
 
 namespace cribbageHand {
     class CribbageChecker {
-        public StandardDeck Deck { get; set; }
+        private StandardDeck deck = new StandardDeck();
 
-        public CribbageChecker() {
-            Deck = new StandardDeck();
-        }
         /**
          * get game score
          * @param {string[]} [hand] - current hand
@@ -18,9 +15,38 @@ namespace cribbageHand {
          * @return {int} [game score]
          */
         public int GetScore(string[] hand) {
+            List<string[]> pairs = GetPairs(hand);
             int flush = GetFlush(hand);
             List<string> nobs = GetNobs(hand);
             return nobs.Count;
+        }
+        /**
+         * count occurrence of each suit
+         * @param {string[]} [hand] - current hand to check
+         * 
+         * @return {Dictionary<char, List<string>>} [occurrence of each suit]
+         */
+        public Dictionary<char, List<string>> GetOccurrence(string[] hand) { 
+            var counter = new Dictionary<char, List<string>>();
+            foreach(string card in hand) {
+                char suit = this.deck.GetSuit(card);
+                counter[suit] = counter.ContainsKey(suit) ? counter[suit] : new List<string>();
+                counter[suit].Add(card);
+            }
+            return counter;
+        }
+        /**
+         * check for pairs in a hand
+         * @param {string[]} [hand] - current hand to check
+         * 
+         * @return {List<string[]>} [all pairs]
+         */
+        public List<string[]> GetPairs(string[] hand) {
+            var counter = GetOccurrence(hand);
+            var pairs = from pair in counter
+                       where pair.Value.Count >= 2
+                      select pair.Value.ToArray();
+            return pairs.ToList();
         }
         /**
          * check for flush in a hand
@@ -29,7 +55,7 @@ namespace cribbageHand {
          * @return {int} [flush count]
          */
         public int GetFlush(string[] hand) {
-            var suits = hand.Select(card => Deck.GetSuit(card));
+            var suits = hand.Select(card => this.deck.GetSuit(card));
             if(new HashSet<char>(suits).Count == 1) {
                 return 5;
             }
@@ -44,8 +70,8 @@ namespace cribbageHand {
         public List<string> GetNobs(string[] hand) {
             List<string> nobs = new List<string>();
             for(int i = 0; i < hand.Length - 1; i++) {
-                bool isJack = Deck.GetRank(hand[i]) == 11;
-                bool sameSuit = Deck.GetSuit(hand[i]) == Deck.GetSuit(hand.Last());
+                bool isJack = this.deck.GetRank(hand[i]) == 11;
+                bool sameSuit = this.deck.GetSuit(hand[i]) == this.deck.GetSuit(hand.Last());
                 if(isJack && sameSuit) {
                     nobs.Add(hand[i]);
                 }
