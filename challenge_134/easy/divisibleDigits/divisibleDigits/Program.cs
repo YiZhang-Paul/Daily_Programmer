9a6e5cd8e3992dddf9a5c1df999e3ac706b73ad6
YI ZHANG <scaccterran@gmail.com>
2017-10-14 21:17:43 -0400

    all done

--- challenge_134/easy/divisibleDigits/divisibleDigits/Program.cs
+++ challenge_134/easy/divisibleDigits/divisibleDigits/Program.cs
@@ -7,6 +7,24 @@ using System.Threading.Tasks;
 namespace divisibleDigits {
     class Program {
         static void Main(string[] args) {
+            //default input
+            int result = GetMaxDivisible(2, 101);
+            Console.WriteLine(result == 0 ? "No Solution Found." : "Result: " + result);
+            //challenge input
+            result = GetMaxDivisible(3, 2);
+            Console.WriteLine(result == 0 ? "No Solution Found." : "Result: " + result);
+            result = GetMaxDivisible(7, 4241275);
+            Console.WriteLine(result == 0 ? "No Solution Found." : "Result: " + result);
+        }
+        /*
+         * find maximum divisible integer of given places
+         * @param {int} [places] - total digits in the number
+         * @param {int} [divisor] - integer to divide
+         *
+         * @return {int} [maximum divisible integer]
+         */
+        public static int GetMaxDivisible(int places, int divisor) {
+            return (int)(Math.Pow(10, places) - 1) / divisor * divisor;
         }
     }
 }
