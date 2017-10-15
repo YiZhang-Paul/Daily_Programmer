3493bd9a10e4f8e95de2e36d0940c7e4c2b80953
YI ZHANG <scaccterran@gmail.com>
2017-10-15 12:55:00 -0400

    read student grades

--- challenge_136/easy/studentManagement/studentManagement/GradeCalculator.cs
+++ challenge_136/easy/studentManagement/studentManagement/GradeCalculator.cs
@@ -5,7 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace studentManagement {
-    class GradeCalculator {
-
+    class GradeManager {
+        public Dictionary<string, int[]> Records { get; set; }
+        /*
+         * read and organize student grades
+         * @param {string} [grades] - student grades
+         */
+        public void ReadGrades(string grades) {
+            //format grades
+            List<string[]> allGrades = grades.Split(new char[] { '\n' })
+                                             .Select(line => line.Trim())
+                                             .Select(line => line.Split(new char[] { ' ' }))
+                                             .ToList();
+            //record grades
+            Records = new Dictionary<string, int[]>();
+            foreach(string[] grade in allGrades) {
+                Records.Add(grade[0], grade.Skip(1).Select(item => Int32.Parse(item)).ToArray());
+            }
+        }
     }
 }
