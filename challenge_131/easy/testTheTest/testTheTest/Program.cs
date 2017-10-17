8159505deab2695a8af27f7d476691ae101bfe2b
YI ZHANG <scaccterran@gmail.com>
2017-10-16 21:48:12 -0400

    all done

--- challenge_131/easy/testTheTest/testTheTest/Program.cs
+++ challenge_131/easy/testTheTest/testTheTest/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace testTheTest {
     class Program {
@@ -15,10 +16,38 @@ namespace testTheTest {
                              1 Batman BATMAN
                              1 Graph GRAPH
                              1 One one";
-            Console.WriteLine(ToUpperCase("Batman"));
-            Console.WriteLine(ToUpperCase("Graph"));
-            Console.WriteLine(ToUpperCase("One"));
-            //Console.WriteLine(Test(input));
+            
+            Console.WriteLine(TestData(input));
+        }
+        /*
+         * validate test data for unit tests
+         * @param {string} [data] - test data for unit tests
+         *
+         * @return {string} [test result]
+         */
+        public static string TestData(string data) {
+
+            var result = new StringBuilder();
+            var allData = data.Split('\n');
+            //counter for total pass/fail
+            int pass = 0;
+            int fail = 0;
+
+            foreach(string dataItems in allData) {
+                //retrieve test data
+                var items = Regex.Matches(dataItems, @"\w+");
+                string input = items[1].Value;
+                string expected = items[2].Value;
+                //record test result
+                string output = Int32.Parse(items[0].Value) == 0 ? Reverse(input) : ToUpperCase(input);
+                pass += output == expected ? 1 : 0;
+                fail += output != expected ? 1 : 0;
+                result.Append(output == expected ? "Good test data\n" : "Mismatch! Bad test data\n");
+            }
+            //summarize test result
+            result.Append("Of " + allData.Length + " tests, " + pass + " succeeded, " + fail + " failed.");
+
+            return result.ToString();
         }
         /*
          * reverse a string
