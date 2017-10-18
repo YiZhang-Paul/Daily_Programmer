38644f6608afc510f6770516acad8a335d308554
YI ZHANG <scaccterran@gmail.com>
2017-10-18 17:53:08 -0400

    all done

--- challenge_121/easy/bytelandianExchange/bytelandianExchange/Program.cs
+++ challenge_121/easy/bytelandianExchange/bytelandianExchange/Program.cs
@@ -8,7 +8,31 @@ namespace bytelandianExchange {
     class Program {
         static void Main(string[] args) {
  
-            
+            //default input
+            int input1 = 7;
+            //challenge input
+            int input2 = 1000;
+
+            Console.WriteLine(ExchangeCoin(input1));
+            Console.WriteLine(ExchangeCoin(input2));
+        }
+        /*
+         * Bytelandian exchange
+         * @param {int} [coins] - initial coins inserted into exchange machine
+         *
+         * @return {int} [number of 0-valued coins at the end of exchange]
+         */
+        public static int ExchangeCoin(int coins) {
+            //coins exchanged
+            int[] exchanged = new int[] { coins / 2, coins / 3, coins / 4 };
+            int totalZero = 0;
+
+            foreach(int coin in exchanged) {
+                //keep exchanging non 0-valued coins and count 0-valued coins received
+                totalZero += coin == 0 ? 1 : ExchangeCoin(coin);
+            }
+
+            return totalZero;
         }
     }
 }
