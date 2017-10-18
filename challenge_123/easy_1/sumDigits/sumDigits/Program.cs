35714bffe5df3e51348b5ce53f0c98d08163d735
YI ZHANG <scaccterran@gmail.com>
2017-10-17 23:41:43 -0400

    all done

---
+++ challenge_123/easy_1/sumDigits/sumDigits/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumDigits {
+    class Program {
+        static void Main(string[] args) {
+
+            //default input
+            int input1 = 31337;
+            int input2 = 1073741824;
+
+            Console.WriteLine(SumDigits(input1));
+            Console.WriteLine(SumDigits(input2));
+        }
+        /*
+         * sum digits of a number until sum becomes one digit
+         * @param {long} [number] - number to sum digits with
+         *
+         * @return {int} [final sum]
+         */
+        public static int SumDigits(long number) { 
+            
+            do {
+                //sum of digits of current number
+                number = GetDigits(number).Sum();
+
+            } while(number > 9);
+
+            return (int)number;
+        }
+        /*
+         * retrieve all digits of a number
+         * @param {long} [number] - number to check
+         *
+         * @return {int[]} [all digits of the number]
+         */
+        public static int[] GetDigits(long number) {
+            //handle negative numbers
+            number = Math.Abs(number);
+            var digits = new List<int>();
+
+            while(number != 0) {
+                //retrieve each digit of the number
+                int remainder = (int)(number % 10);
+                digits.Insert(0, remainder);
+                number = (number - remainder) / 10;
+            }
+
+            return digits.ToArray();
+        }
+    }
+}
