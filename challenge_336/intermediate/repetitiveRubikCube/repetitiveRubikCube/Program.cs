e038c6feea123adeef34c481af8ab04ec963f828
YI ZHANG <scaccterran@gmail.com>
2017-10-18 23:19:36 -0400

    Rubik Cube class

--- challenge_336/intermediate/repetitiveRubikCube/repetitiveRubikCube/Program.cs
+++ challenge_336/intermediate/repetitiveRubikCube/repetitiveRubikCube/Program.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 namespace repetitiveRubikCube {
     class Program {
         static void Main(string[] args) {
- 
-            
+
+            var cube = new RubikCube();
         }
     }
 }
---
+++ challenge_336/intermediate/repetitiveRubikCube/repetitiveRubikCube/RubikCube.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repetitiveRubikCube {
+    class Face {
+
+        public char Color { get; private set; }
+        public int[,] Content { get; set; }
+
+        public Face() { 
+        }
+        /*
+         * @param {} [] - default face color
+         */
+        public Face(char color) {
+
+            Color = color;
+            Content = new int[,] { 
+                { color, color, color }, 
+                { color, color, color }, 
+                { color, color, color } 
+            }; 
+        }
+    }
+
+    class RubikCube {
+
+        public Dictionary<string, Face> Faces { get; private set; }
+
+        public RubikCube() {
+
+            Faces = new Dictionary<string, Face>();
+            char[] colors = new char[] { 'r', 'b', 'y', 'g', 'w', 'o' };
+            string[] names = new string[] { "up", "down", "left", "right", "front", "back" };
+            //create faces
+            for(int i = 0; i < names.Length; i++) {
+
+                Faces.Add(names[i], new Face(colors[i]));
+            }
+        }
+    }
+}
