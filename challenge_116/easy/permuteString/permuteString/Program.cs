5f0d255e0c98b2a0e9570a62b19aead550048ad7
YI ZHANG <scaccterran@gmail.com>
2017-10-20 19:10:39 -0400

    all done

--- challenge_116/easy/permuteString/permuteString/Program.cs
+++ challenge_116/easy/permuteString/permuteString/Program.cs
@@ -8,7 +8,40 @@ namespace permuteString {
     class Program {
         static void Main(string[] args) {
 
+            //default & bonus input
+            string input1 = "baz";
+            string input2 = "daily";
+            string input3 = "ab";
+            //challenge & bonus input
+            string input4 = "abbccc";
 
+            Console.WriteLine(string.Join(" ", Permute(input1, new HashSet<string>())));
+            Console.WriteLine(string.Join(" ", Permute(input2, new HashSet<string>())));
+            Console.WriteLine(string.Join(" ", Permute(input3, new HashSet<string>())));
+            Console.WriteLine(string.Join(" ", Permute(input4, new HashSet<string>())));
+        }
+        /*
+         * get permutation of a string
+         * @param {string} [input] - string to permute
+         * @param {HashSet<string>} [collection] - collection of all permutations
+         * @param {string} [curStr] - current string permutation
+         *
+         * @return {string[]} [all permutations]
+         */
+        public static string[] Permute(string input, HashSet<string> collection, string curStr = "") { 
+        
+            if(input.Length == 1) {
+                //remove duplicates
+                collection.Add(curStr + input);
+                return null;
+            }
+
+            for(int i = 0; i < input.Length; i++) {
+
+                Permute(input.Substring(0, i) + input.Substring(i + 1), collection, curStr + input[i]);
+            }
+
+            return collection.ToArray();
         }
     }
 }
