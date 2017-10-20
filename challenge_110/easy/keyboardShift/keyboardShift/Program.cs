441e5ab5809d565d015b1dd3e229cfef3e4ff7d0
YI ZHANG <scaccterran@gmail.com>
2017-10-20 01:33:38 -0400

    all done

---
+++ challenge_110/easy/keyboardShift/keyboardShift/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace keyboardShift {
+    class Program {
+        static void Main(string[] args) {
+
+            //challenge input
+            string input1 = "Jr;;p ept;f";
+            string input2 = "Lmiyj od ,u jrtp";
+
+            Console.WriteLine(FixKeyboardShift(input1));
+            Console.WriteLine(FixKeyboardShift(input2));
+        }
+        /*
+         * convert broken keyboard input to original input
+         * @param {string} [input] - input to fix
+         *
+         * @return {string} [fixed input]
+         */
+        public static string FixInput(string input) { 
+
+            string toFix = "wertyuiop[sdfghjkl;xcvbnm,WERTYUIOP{SDFGHJKL:XCVBNM<";
+            string fix = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+
+            if(!Regex.IsMatch(toFix, input)) {
+
+                return input;
+            }
+
+            return fix[Regex.Match(toFix, input).Index].ToString();
+        }
+        /*
+         * fix keyboard shift
+         * @param {string} [shift] - keyboard shift to fix
+         *
+         * @return {string} [fixed keyboard shift]
+         */
+        public static string FixKeyboardShift(string shift) {
+
+            return Regex.Replace(shift, @"[^\d\s]", match => FixInput(match.Value));
+        }
+    }
+}
