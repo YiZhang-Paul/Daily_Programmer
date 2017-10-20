2daf6544cbfe1ffa9d2586692606c9db5350a3c9
YI ZHANG <scaccterran@gmail.com>
2017-10-20 02:46:51 -0400

    all done

--- challenge_112/easy/getURL/getURL/Program.cs
+++ challenge_112/easy/getURL/getURL/Program.cs
@@ -14,9 +14,24 @@ namespace getURL {
             string input2 = "http://en.wikipedia.org/w/index.php?title=Main_Page&action=edit";
             string input3 = "http://en.wikipedia.org/w/index.php?title= hello world!&action=é";
 
-            Console.WriteLine(string.Join("\n", GetParameters(input1)));
-            Console.WriteLine(string.Join("\n", GetParameters(input2)));
-            Console.WriteLine(string.Join("\n", GetParameters(input3)));
+            Console.WriteLine(CheckURL(input1));
+            Console.WriteLine(CheckURL(input2));
+            Console.WriteLine(CheckURL(input3));
+        }
+        /*
+         * validate a given url and retrieve parameters if there is any
+         * @param {string} [url] - url to check
+         *
+         * @return {string} [test result]
+         */
+        public static string CheckURL(string url) { 
+        
+            if(!IsValidURL(url)) {
+
+                return "The Given URL is Invalid.\n";
+            }
+
+            return "The Given URL is Valid. Parameters:\n" + string.Join("\n", GetParameters(url)) + "\n";
         }
         /*
          * validate a URL
@@ -24,10 +39,10 @@ namespace getURL {
          *
          * @return {bool} [test result]
          */
-        public static bool IsValidURL(string url) {
+        public static bool IsValidURL(string url) { 
             //check for space and unicode characters
             if(Regex.IsMatch(url, @"\s|[\u00A0-\u00FF]")) {
-            
+
                 return false;
             }
 
@@ -43,9 +58,10 @@ namespace getURL {
 
             var pairs = new List<string>();
 
-            foreach(Match pair in Regex.Matches(url, @"(?<=\?|&)[^=&]+=[^=&]+(?=&|\b)")) {
+            foreach(Match match in Regex.Matches(url, @"(?<=\?|&)[^=&?]+=[^=&?]+(?=&|\b)")) {
 
-                pairs.Add(Regex.Replace(pair.Value, "=", " : "));
+                string[] items = match.Value.Split('=');
+                pairs.Add(items[0] + ": " + "\"" + items[1] + "\"");
             }
 
             return pairs.ToArray();
