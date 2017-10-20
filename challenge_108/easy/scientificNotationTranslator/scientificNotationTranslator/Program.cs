d020c16b449a6d28e73080fe25e95ac1745a1b0a
YI ZHANG <scaccterran@gmail.com>
2017-10-19 23:34:21 -0400

    convert between scientific notation and expanded form

--- challenge_108/easy/scientificNotationTranslator/scientificNotationTranslator/Program.cs
+++ challenge_108/easy/scientificNotationTranslator/scientificNotationTranslator/Program.cs
@@ -8,9 +8,42 @@ namespace scientificNotationTranslator {
     class Program {
         static void Main(string[] args) {
 
+            //challenge input
+            double input1 = 239487d;
+            double input2 = .654d;
+
             var generator = new NumberGenerator();
-            Console.WriteLine(generator.GetRandom());
-            Console.WriteLine(generator.GetRandomScientificNotation());
+            Console.WriteLine(ToScientificNotation(input1));
+            Console.WriteLine(ToScientificNotation(input2));
+            Console.WriteLine(ToExpandedNumber(ToScientificNotation(input1)));
+            Console.WriteLine(ToExpandedNumber(ToScientificNotation(input2)));
+        }
+        /*
+         * convert number into scientific notation
+         * @param {double} [number] - number to convert
+         *
+         * @return {string} [number in scientific notation]
+         */
+        public static string ToScientificNotation(double number) {
+
+            string leadDigits = Math.Truncate(number).ToString();
+            //change number to scientific notation
+            number = leadDigits == "0" ? number * 10 : number / Math.Pow(10, leadDigits.Length - 1);
+            int power = leadDigits == "0" ? -1 : leadDigits.Length - 1;
+
+            return number + "e" + power;
+        }
+        /*
+         * expand scientific notation
+         * @param {string} [notation] - number to expand
+         *
+         * @return {double} [expanded number]
+         */
+        public static double ToExpandedNumber(string notation) {
+
+            string[] halves = notation.Split('e');
+
+            return Double.Parse(halves[0]) * Math.Pow(10, Int32.Parse(halves[1]));
         }
     }
 }
