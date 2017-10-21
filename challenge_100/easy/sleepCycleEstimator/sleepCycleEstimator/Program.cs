f2ebd90d9ad8d3cfeeff6a3d7ade01ffbd5a1377
YI ZHANG <scaccterran@gmail.com>
2017-10-21 19:32:24 -0400

    all done

--- challenge_100/easy/sleepCycleEstimator/sleepCycleEstimator/Program.cs
+++ challenge_100/easy/sleepCycleEstimator/sleepCycleEstimator/Program.cs
@@ -12,7 +12,7 @@ namespace sleepCycleEstimator {
             Console.WriteLine(string.Join(" ", GetBedTime("6:15 AM", 4, 0)));
             //bonus input
             Console.WriteLine(string.Join(" ", GetBedTime("6:15 AM")));
-            //Console.WriteLine(GetWakeUpTime());
+            Console.WriteLine(string.Join(" ", GetWakeUpTime("9:15 PM")));
         }
         /*
          * estimate time to go to sleep
@@ -28,16 +28,44 @@ namespace sleepCycleEstimator {
 
             if(DateTime.TryParse(wake, out wakeUpTime)) {
 
-                wakeUpTime = wakeUpTime.AddHours(Math.Floor(duration / 1.5) * -1.5);
+                var bedTime = wakeUpTime.AddHours(Math.Floor(duration / 1.5) * -1.5);
                 string[] bedTimes = new string[4];
 
                 for(int i = 0; i < 4; i++) {
 
-                    double totalDuration = (i + 1) * -1.5 - (double)asleep / 60;
-                    bedTimes[i] = wakeUpTime.AddHours(totalDuration).ToShortTimeString();
+                    double totalDuration = (i + 1) * 1.5 + (double)asleep / 60;
+                    bedTimes[i] = bedTime.AddHours(totalDuration * -1).ToShortTimeString();
                 }
 
                 return bedTimes.Reverse().ToArray();
+            }
+
+            return new string[] { "NULL" };
+        }
+        /*
+         * estimate wake up time
+         * @param {string} [bed] - time to go to sleep
+         * @param {int} [duration] - total sleep time (in hours)
+         * @param {int} [asleep] - time used to fall asleep (in minutes)
+         *
+         * @return {string[]} [possible wake up times]
+         */
+        public static string[] GetWakeUpTime(string bed, int duration = 4, int asleep = 40) {
+
+            DateTime bedTime;
+
+            if(DateTime.TryParse(bed, out bedTime)) {
+
+                var wakeUpTime = bedTime.AddHours(Math.Floor(duration / 1.5) * 1.5);
+                string[] wakeUpTimes = new string[4];
+
+                for(int i = 0; i < 4; i++) {
+
+                    double totalDuration = (i + 1) * 1.5 + (double)asleep / 60;
+                    wakeUpTimes[i] = wakeUpTime.AddHours(totalDuration).ToShortTimeString();
+                }
+
+                return wakeUpTimes;
             }
 
             return new string[] { "NULL" };
