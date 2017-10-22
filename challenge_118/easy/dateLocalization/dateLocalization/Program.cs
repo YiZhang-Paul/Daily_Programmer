6404c7483a240aa35e4f8f33d9d06099cf068c7d
YI ZHANG <scaccterran@gmail.com>
2017-10-22 16:49:16 -0400

    all done

--- challenge_118/easy/dateLocalization/dateLocalization/Program.cs
+++ challenge_118/easy/dateLocalization/dateLocalization/Program.cs
@@ -3,12 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace dateLocalization {
     class Program {
         static void Main(string[] args) {
 
+            //challenge input
+            string input = @"%s.%l
+                             %s:%m:%h %M/%d/%y
+                             The minute is %m! The hour is %h.";
 
+            Console.WriteLine(GetTime(input));
+        }
+        /*
+         * output current time base on a given format
+         * @param {string} [format] - format to follow
+         *
+         * @return {string} [current time in given format]
+         */
+        public static string GetTime(string format) {
+
+            var currentTime = DateTime.Now;
+
+            return Regex.Replace(format, @"%.|(?<=\n)\s+", match => {
+
+                switch(match.Value) {
+
+                    case "%l":
+
+                        return currentTime.Millisecond.ToString();
+
+                    case "%s":
+
+                        return currentTime.Second.ToString();
+
+                    case "%m":
+
+                        return currentTime.Minute.ToString();
+
+                    case "%h":
+
+                        return (currentTime.Hour > 12 ? currentTime.Hour - 12 : currentTime.Hour).ToString();
+
+                    case "%H":
+
+                        return currentTime.Hour.ToString();
+
+                    case "%c":
+
+                        return currentTime.Hour >= 12 ? "PM" : "AM"; 
+
+                    case "%d":
+
+                        return currentTime.Day.ToString();
+
+                    case "%M":
+
+                        return currentTime.Month.ToString();
+
+                    case "%y":
+
+                        return currentTime.Year.ToString();
+
+                    default :
+
+                        return "";
+                }
+            });
         }
     }
 }
