d0163e967431aa717a3ce5bafd94921edc27720b
YI ZHANG <scaccterran@gmail.com>
2017-10-21 20:55:02 -0400

    all done

---
+++ challenge_102/easy/diceRoller/diceRoller/Dice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace diceRoller {
+    class Dice {
+
+        private Random _random = new Random();
+        /*
+         * roll dice
+         * @param {int} [min] - minimum roll
+         * @param {int} [max] - maximum roll
+         *
+         * @return {int} [dice roll]
+         */
+        public int Roll(int min = 0, int max = 10) {
+
+            return _random.Next(min, max + 1);
+        }
+        /*
+         * process dice notation
+         * @param {string} [notation] - dice notation to process
+         *
+         * @return {double} [result of dice roll]
+         */
+        public double ProcessNotation(string notation) {
+
+            int a = Regex.IsMatch(notation, @"\d+d") ? Int32.Parse(Regex.Match(notation, @"\d+").Value) : 1;
+            int b = Int32.Parse(Regex.Match(notation, @"(?<=d)\d+").Value);
+            int c = Regex.IsMatch(notation, "[+-]") ? Int32.Parse(Regex.Match(notation, @"[+-]\d+").Value) : 0;
+
+            return new double[a].Select(roll => Roll(1, b)).Sum() + c;        
+        }
+    }
+}
---
+++ challenge_102/easy/diceRoller/diceRoller/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diceRoller {
+    class Program {
+        static void Main(string[] args) {
+
+            var dice = new Dice();
+
+            //challenge input
+            Console.WriteLine(dice.ProcessNotation("d8"));
+            Console.WriteLine(dice.ProcessNotation("10d6-2"));
+            Console.WriteLine(dice.ProcessNotation("d20+7"));
+        }
+    }
+}
