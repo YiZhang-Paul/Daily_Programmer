17b17109adff0aeea73e7df5279182c0ed34bbb0
YI ZHANG <scaccterran@gmail.com>
2017-10-23 04:43:32 -0400

    retrieve text file paths

--- challenge_097/easy/concatenateDirectory/concatenateDirectory/Program.cs
+++ challenge_097/easy/concatenateDirectory/concatenateDirectory/Program.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace concatenateDirectory {
     class Program {
         static void Main(string[] args) {
 
+            //challenge input
+            Console.WriteLine(string.Join(" ", GetTextFiles()));
+        }
 
+        public static string[] GetTextFiles(string directory = null) {
+
+            return Directory.GetFiles(directory ?? AppDomain.CurrentDomain.BaseDirectory, "*.txt");
         }
     }
 }
