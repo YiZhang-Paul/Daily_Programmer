7c82abd1d027033ccb5e09a32548d5bd636a0472
YI ZHANG <scaccterran@gmail.com>
2017-10-24 23:53:57 -0400

    all done

--- challenge_092/easy/numberDisplay/numberDisplay/NumberDisplay.cs
+++ challenge_092/easy/numberDisplay/numberDisplay/NumberDisplay.cs
@@ -7,41 +7,46 @@ using System.Threading.Tasks;
 namespace numberDisplay {
     class NumberDisplay {
 
-        private string _patternZero = "abcdef";
-        private string _patternOne = "bc";
-        private string _patternTwo = "abdeg";
-        private string _patternThree = "abcdg";
-        private string _patternFour = "bcfg";
-        private string _patternFive = "acdfg";
-        private string _patternSix = "acdefg";
-        private string _patternSeven = "abc";
-        private string _patternEight = "abcdefg";
-        private string _patternNine = "abcdfg";
-
         public string[] Patterns { get; private set; }
+        public int Scale { get; private set; }
 
         public NumberDisplay() {
 
             Patterns = new string[] { 
-                _patternZero, _patternOne, _patternTwo, _patternThree, _patternFour, 
-                _patternFive, _patternSix, _patternSeven, _patternEight, _patternNine 
+                "abcdef",   //0
+                "bc",       //1
+                "abdeg",    //2
+                "abcdg",    //3
+                "bcfg",     //4
+                "acdfg",    //5
+                "acdefg",   //6
+                "abc",      //7
+                "abcdefg",  //8
+                "abcdfg"    //9
             };
         }
         /// <summary>
-        /// retrieve a grid-like structure to hold all number display
+        /// set scale of number display
         /// </summary>
         /// <param name="scale">display scale</param>
-        /// <returns>number display grids</returns>
-        public char[,] GetGrids(int scale) { 
-        
-            return new char[scale * 2 + 3, scale + 2];
+        public void SetScale(int scale) {
+
+            Scale = scale;
         }
         /// <summary>
-        /// show content on grids
+        /// retrieve a grid-like structure to hold all number display 
+        /// </summary>
+        /// <param name="digits">total number of digits to display</param>
+        /// <returns>number display grids</returns>
+        public char[,] GetGrids(int digits) {
+
+            return new char[Scale * 2 + 3, digits * (Scale + 3) - 1];
+        }
+        /// <summary>
+        /// show content on display grids
         /// </summary>
         /// <param name="grids">display grids</param>
-        /// <returns>grids content</returns>
-        public string ShowGrids(char[,] grids) {
+        public void ShowGrids(char[,] grids) {
 
             var content = new StringBuilder();
 
@@ -55,116 +60,123 @@ namespace numberDisplay {
                 content.Append("\n");
             }
 
-            return content.ToString();
+            Console.WriteLine(content.ToString());
         }
         /// <summary>
-        /// display a digit on console
+        /// get all digits in a number
         /// </summary>
-        /// <param name="digit">digit to display</param>
-        /// <param name="scale">display scale</param>
-        public void DisplayDigit(int digit, int scale) { 
+        /// <param name="number">number to check</param>
+        /// <returns>all digits in the given number</returns>
+        public int[] GetDigits(int number) { 
         
-            char[,] grids = GetGrids(scale);
-            string pattern = Patterns[digit];
+            var digits = new List<int>();
 
-            foreach(char segment in pattern) {
+            while(number != 0) {
 
-                ActivateSegment(segment, grids);                
+                digits.Add(number % 10);
+                number = (number - number % 10) / 10;
             }
 
-            Console.Write(ShowGrids(grids));
+            digits.Reverse();
+
+            return digits.ToArray();
         }
         /// <summary>
         /// display number on console
         /// </summary>
         /// <param name="number">number to display</param>
         /// <param name="scale">display scale</param>
-        public void DisplayNumber(int number, int scale = 2) { 
+        public void DisplayNumber(int number, int scale = 2) {
 
-            do {
-                //retrieve every digit
-                int digit = number % 10;
-                DisplayDigit(digit, scale);
-                number = (number - digit) / 10;
+            SetScale(scale);
+            int column = 0; //current column
+            char[,] grids = GetGrids(number.ToString().Length);
+            
+            foreach(int digit in GetDigits(number)) {
+                //display digits on display grids
+                foreach(char segment in Patterns[digit]) {
 
-            } while(number != 0);
+                    ActivateSegment(segment, column, grids);
+                }
+                //move current position on display grids
+                column += Scale + 3;
+            }
+
+            ShowGrids(grids);
         }
         /// <summary>
         /// fill a horizontal segment on display grids
         /// </summary>
         /// <param name="row">row on grids</param>
+        /// <param name="column">current column on display grids</param>
         /// <param name="grids">display grids</param>
-        public void FillRowSegment(int row, char[,] grids) {
+        public void FillRowSegment(int row, int column, char[,] grids) {
 
-            grids[row, 0] = '+';
-            grids[row, grids.GetLength(1) - 1] = '+';
+            for(int i = column; i <= column + Scale + 1; i++) {
 
-            for(int i = 1; i < grids.GetLength(1) - 1; i++) {
-
-                grids[row, i] = '-';
+                grids[row, i] = i == column || i == column + Scale + 1 ? '+' : '-';
             }
         }
         /// <summary>
         /// fill a vertical segment on display grids
         /// </summary>
-        /// <param name="column">column on grids</param>
+        /// <param name="column">current column on display grids</param>
+        /// <param name="offset">total columns to the right of current column</param>
         /// <param name="upper">upper/lower half on grids</param>
         /// <param name="grids">display grids</param>
-        public void FillColumnSegment(int column, bool upper, char[,] grids) {
+        public void FillColumnSegment(int column, int offset, bool upper, char[,] grids) {
 
-            grids[(grids.GetLength(0) - 1) / 2, column] = '+';
-            grids[upper ? 0 : grids.GetLength(0) - 1, column] = '+';
-
-            int start = upper ? 1 : (grids.GetLength(0) + 1) / 2;
-            int end = upper ? (grids.GetLength(0) - 1) / 2 - 1 : grids.GetLength(0) - 2;
+            int start = upper ? 0 : (grids.GetLength(0) - 1) / 2;
+            int end = upper ? (grids.GetLength(0) - 1) / 2 : grids.GetLength(0) - 1;
 
             for(int i = start; i <= end; i++) {
 
-                grids[i, column] = '|';
+                grids[i, column + offset] = i == start || i == end ? '+' : '|';
             }
         }
         /// <summary>
         /// activate a segment on number display
         /// </summary>
         /// <param name="segment">segment to activate</param>
+        /// <param name="column">current column on display grids</param>
         /// <param name="grids">grids containing all segments</param>
-        public void ActivateSegment(char segment, char[,] grids) { 
+        public void ActivateSegment(char segment, int column, char[,] grids) { 
         
             switch(segment) {
             
                 case 'a' :
 
-                    FillRowSegment(0, grids);
+                    FillRowSegment(0, column, grids);
                     break;
 
                 case 'b':
 
-                    FillColumnSegment(grids.GetLength(1) - 1, true, grids);
+                    FillColumnSegment(column, Scale + 1, true, grids);
                     break;
 
                 case 'c':
 
-                    FillColumnSegment(grids.GetLength(1) - 1, false, grids);
+                    FillColumnSegment(column, Scale + 1, false, grids);
                     break;
 
                 case 'd':
 
-                    FillRowSegment(grids.GetLength(0) - 1, grids);
+                    FillRowSegment(grids.GetLength(0) - 1, column, grids);
                     break;
 
                 case 'e':
 
-                    FillColumnSegment(0, false, grids);
+                    FillColumnSegment(column, 0, false, grids);
                     break;
 
                 case 'f':
 
-                    FillColumnSegment(0, true, grids);
+                    FillColumnSegment(column, 0, true, grids);
                     break;
 
                 case 'g':
 
-                    FillRowSegment((grids.GetLength(0) - 1) / 2, grids);
+                    FillRowSegment((grids.GetLength(0) - 1) / 2, column, grids);
                     break;
             }
         }
--- challenge_092/easy/numberDisplay/numberDisplay/Program.cs
+++ challenge_092/easy/numberDisplay/numberDisplay/Program.cs
@@ -10,20 +10,14 @@ namespace numberDisplay {
 
             //challenge & bonus input
             var display = new NumberDisplay();
-            //display.DisplayNumber(5362);
-            //display.DisplayNumber(5362, 3);
-            //display.DisplayNumber(5362, 4);
-            //display.DisplayNumber(5362, 5);
-            display.DisplayDigit(0, 2);
-            display.DisplayDigit(1, 2);
-            display.DisplayDigit(2, 2);
-            display.DisplayDigit(3, 2);
-            display.DisplayDigit(4, 2);
-            display.DisplayDigit(5, 2);
-            display.DisplayDigit(6, 2);
-            display.DisplayDigit(7, 2);
-            display.DisplayDigit(8, 2);
-            display.DisplayDigit(9, 2);
+            display.DisplayNumber(5362);
+            display.DisplayNumber(5362, 3);
+            display.DisplayNumber(5362, 4);
+            display.DisplayNumber(5362, 5);
+            display.DisplayNumber(4097);
+            display.DisplayNumber(4097, 3);
+            display.DisplayNumber(4097, 4);
+            display.DisplayNumber(4097, 5);
         }
     }
 }
