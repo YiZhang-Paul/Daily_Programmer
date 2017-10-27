1148e9520f7f9e1044f866adbffa2d31c942ac76
YI ZHANG <scaccterran@gmail.com>
2017-10-27 01:42:42 -0400

    all done

--- challenge_087/easy/rectangleIntersection/rectangleIntersection/Program.cs
+++ challenge_087/easy/rectangleIntersection/rectangleIntersection/Program.cs
@@ -9,7 +9,13 @@ namespace rectangleIntersection {
         static void Main(string[] args) {
 
             //challenge input
-            
+            var rectangle = new Rectangle(3, 3, 10, 10);
+            var overlap = rectangle.GetOverlapRectangle(new Rectangle(6, 6, 12, 12));
+            Console.WriteLine(Rectangle.ShowRectangle(overlap));
+
+            rectangle = new Rectangle(4, 4, 5, 5);
+            overlap = rectangle.GetOverlapRectangle(new Rectangle(6, 6, 10, 10));
+            Console.WriteLine(Rectangle.ShowRectangle(overlap));
         }
     }
 }
--- challenge_087/easy/rectangleIntersection/rectangleIntersection/Rectangle.cs
+++ challenge_087/easy/rectangleIntersection/rectangleIntersection/Rectangle.cs
@@ -26,28 +26,72 @@ namespace rectangleIntersection {
             Bottom = bottom;
         }
         /// <summary>
+        /// show rectangle coordinates in string format
+        /// </summary>
+        /// <param name="rectangle">rectangle to display</param>
+        /// <returns>rectangle coordinates in string format</returns>
+        public static string ShowRectangle(Rectangle rectangle) {
+
+            if(rectangle == null) {
+
+                return "No Rectangle";
+            }
+
+            int[] coordinates = new int[] { 
+                rectangle.Left, rectangle.Top, rectangle.Right, rectangle.Bottom 
+            };
+
+            return "Rect(" + string.Join(", ", coordinates) + ")";
+        }
+        /// <summary>
+        /// check if a rectangle is contained by target rectangle
+        /// </summary>
+        /// <param name="rectangle">current rectangle</param>
+        /// <param name="target">target rectangle</param>
+        /// <returns>test result</returns>
+        public bool IsContained(Rectangle rectangle, Rectangle target) { 
+        
+            if(rectangle.Left <= target.Left && rectangle.Top <= target.Top && 
+               rectangle.Right >= target.Right && rectangle.Bottom >= target.Bottom) {
+
+                return true;
+            }
+
+            return false;
+        }
+        /// <summary>
         /// check if current rectangle intersects with another rectangle
         /// </summary>
         /// <param name="target">target rectangle</param>
         /// <returns>test result</returns>
-        public bool HasIntersect(Rectangle target) { 
+        public bool IsIntersect(Rectangle target) { 
         
-            if(Right <= target.Left || Left >= target.Right || Top >= target.Bottom || Bottom <= target.Top) {
+            if(Right <= target.Left || Left >= target.Right || 
+               Top >= target.Bottom || Bottom <= target.Top) {
 
                 return false;
             }
 
-            if(Left < target.Left && Top < target.Top && Right > target.Right && Bottom > target.Bottom) {
+            return !IsContained(this, target) && !IsContained(target, this);
+        }
+        /// <summary>
+        /// retrieve overlapping rectangle with another rectangle
+        /// </summary>
+        /// <param name="target">target rectangle</param>
+        /// <returns>overlapping rectangle</returns>
+        public Rectangle GetOverlapRectangle(Rectangle target) {
 
-                return false;
+            if(!IsIntersect(target)) {
+
+                return null;
             }
 
-            if(Left > target.Left && Top > target.Top && Right < target.Right && Bottom < target.Bottom) {
-
-                return false;
-            }
-
-            return true;
+            return new Rectangle(
+                Math.Max(Left, target.Left),
+                Math.Max(Top, target.Top),
+                Math.Min(Right, target.Right),
+                Math.Min(Bottom, target.Bottom)
+            );
         }
     }
 }
