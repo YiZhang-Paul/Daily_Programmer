232a60aa77946f6b1fcb77ddfabb633ae318e15b
YI ZHANG <scaccterran@gmail.com>
2017-10-28 22:47:02 -0400

    all done

---
+++ challenge_080/easy/anagrams/anagrams/AnagramFinder.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace anagrams {
+    class AnagramFinder {
+
+        public string[] List { get; private set; }
+        public Dictionary<string, List<string>> SortedWords { get; private set; } 
+        /// <param name="name">word list file name</param>
+        public AnagramFinder(string name) {
+
+            List = GetWordList(name);
+            SortedWords = SortAllWords(List);
+        }
+        /// <summary>
+        /// retrieve word list
+        /// </summary>
+        /// <param name="name">word list file name</param>
+        /// <returns></returns>
+        public string[] GetWordList(string name) { 
+        
+            try {
+
+                return File.ReadAllLines(name);
+            }
+            catch(Exception exception) {
+
+                Console.WriteLine("File not found.");
+                Console.WriteLine(exception.Message);
+            }
+
+            return new string[0];
+        }
+        /// <summary>
+        /// sort all letters in a word in alphabetical order
+        /// </summary>
+        /// <param name="word">word to sort</param>
+        /// <returns>sorted word</returns>
+        public string SortLetters(string word) {
+
+            return string.Join("", word.OrderBy(letter => Char.ConvertToUtf32(letter.ToString(), 0)));
+        }
+        /// <summary>
+        /// group all words with same letters together
+        /// </summary>
+        /// <param name="words">all words in the word list</param>
+        /// <returns>grouped words</returns>
+        public Dictionary<string, List<string>> SortAllWords(string[] words) { 
+        
+            var groups = new Dictionary<string, List<string>>();
+
+            foreach(string word in words) {
+
+                string sorted = SortLetters(word);
+                groups[sorted] = groups.ContainsKey(sorted) ? groups[sorted] : new List<string>();
+                groups[sorted].Add(word);
+            }
+
+            return groups;
+        }
+        /// <summary>
+        /// retrieve anagrams of a word
+        /// </summary>
+        /// <param name="word">word to examine</param>
+        /// <returns>all anagrams of the given word</returns>
+        public string[] GetAnagrams(string word) {
+
+            string sorted = SortLetters(word).ToLower();
+
+            return SortedWords.ContainsKey(sorted) ? SortedWords[sorted].ToArray() : new string[] { "No Anagram Found." };
+        }
+        /// <summary>
+        /// retrieve anagram family with given size in place
+        /// </summary>
+        /// <param name="place"></param>
+        /// <returns></returns>
+        public string[] GetLargestFamily(int place = 1) {
+
+            return SortedWords.OrderByDescending(group => group.Value.Count)
+                              .Take(place)
+                              .Last().Value
+                              .ToArray();
+        }
+    }
+}
--- challenge_080/easy/anagrams/anagrams/Program.cs
+++ challenge_080/easy/anagrams/anagrams/Program.cs
@@ -8,7 +8,21 @@ namespace anagrams {
     class Program {
         static void Main(string[] args) {
 
+            var finder = new AnagramFinder("wordList.txt");
 
+            //default input
+            string input1 = "LEPROUS";
+            Console.WriteLine(string.Join(" ", finder.GetAnagrams(input1)));
+            //challenge input
+            string input2 = "TRIANGLE";
+            Console.WriteLine(string.Join(" ", finder.GetAnagrams(input2)));
+            string input3 = "PAGERS";
+            Console.WriteLine(string.Join(" ", finder.GetAnagrams(input3)));
+            string input4 = "AMBLERS";
+            Console.WriteLine(string.Join(" ", finder.GetAnagrams(input4)));
+            //bonus input
+            Console.WriteLine("Largest Family: " + string.Join(" ", finder.GetLargestFamily()));
+            Console.WriteLine("Second Largest Family: " + string.Join(" ", finder.GetLargestFamily(2)));
         }
     }
 }
