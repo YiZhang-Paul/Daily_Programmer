e83c5884051e55523d7c787568b79d775107b08e
YI ZHANG <scaccterran@gmail.com>
2017-10-28 23:34:36 -0400

    calculate Riemann sum

--- challenge_081/easy/numericalCalculus/numericalCalculus/Program.cs
+++ challenge_081/easy/numericalCalculus/numericalCalculus/Program.cs
@@ -10,14 +10,16 @@ namespace numericalCalculus {
 
             //challenge input
             Console.WriteLine(string.Join(" ", GetDerivatives(-1, 1, new double[] { -1.0, -0.5, 0, 0.5, 1.0 })));
+            //bonus 1 input
+            Console.WriteLine(string.Join(" ", GetRiemannSum(-1, 1, new double[] { -1.0, -0.5, 0, 0.5, 1.0 })));
         }
         /// <summary>
         /// calculate derivatives of a series of given points
         /// </summary>
-        /// <param name="xMin"></param>
-        /// <param name="xMax"></param>
-        /// <param name="yValues"></param>
-        /// <returns></returns>
+        /// <param name="xMin">minimum X-Coordinate of interval</param>
+        /// <param name="xMax">maximum X-Coordinate of interval</param>
+        /// <param name="yValues">Y-Coordinate of each point</param>
+        /// <returns>derivatives on each point</returns>
         public static double[] GetDerivatives(double xMin, double xMax, double[] yValues) {
 
             double interval = (xMax - xMin) / (yValues.Length - 1);
@@ -29,6 +31,25 @@ namespace numericalCalculus {
             }
 
             return derivatives;
+        }
+        /// <summary>
+        /// calculate definite integral of a given antiderivative
+        /// </summary>
+        /// <param name="xMin">minimum X-Coordinate of interval</param>
+        /// <param name="xMax">maximum X-Coordinate of interval</param>
+        /// <param name="yValues">Y-Coordinate of each point</param>
+        /// <returns>Riemann sum</returns>
+        public static double GetRiemannSum(double xMin, double xMax, double[] yValues) {
+
+            double interval = (xMax - xMin) / (yValues.Length - 1);
+            double riemannSum = 0;
+
+            for(int i = 0; i < yValues.Length - 1; i++) {
+
+                riemannSum += Math.Abs(yValues[i]) * interval;
+            }
+
+            return riemannSum;
         }
     }
 }
