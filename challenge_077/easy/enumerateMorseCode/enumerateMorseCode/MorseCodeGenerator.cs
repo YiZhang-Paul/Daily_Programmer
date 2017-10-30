1a8b9db1f7e4251687d428770f1bc4fc9b3fd2b7
YI ZHANG <scaccterran@gmail.com>
2017-10-30 07:51:55 -0400

    all done

---
+++ challenge_077/easy/enumerateMorseCode/enumerateMorseCode/MorseCodeGenerator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumerateMorseCode {
+    class MorseCodeGenerator {
+
+        private char[] _codes = new char[] { '.', '-' };
+        /// <summary>
+        /// retrieve size of code
+        /// </summary>
+        public int GetSize(char code) {
+
+            return code == '.' ? 1 : 2;
+        }
+        /// <summary>
+        /// retrieve sub-patterns used to construct final code pattern
+        /// </summary>
+        /// <param name="size">total size of final code pattern</param>
+        public KeyValuePair<int, List<string>>[] GetSubPatterns(int size) {
+
+            var patterns = new Dictionary<int, List<string>> { { 0, new List<string> { "" } } };
+
+            for(int i = 1; i < size; i++) {
+
+                patterns[i] = new List<string>();
+
+                foreach(char code in _codes) {
+                
+                    if(patterns.ContainsKey(i - GetSize(code))) {
+
+                        patterns[i].AddRange(patterns[i - GetSize(code)].Select(pattern => pattern + code));
+                    }
+                }
+            }
+
+            return patterns.Where(pair => _codes.Any(code => GetSize(code) + pair.Key == size)).ToArray();
+        }
+        /// <summary>
+        /// retrieve all morse code patterns with given size
+        /// </summary>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public string[] GetMorseCodes(int size) {
+
+            var output = new List<string>();
+
+            foreach(var pair in GetSubPatterns(size)) {
+
+                char curCode = _codes.First(code => GetSize(code) + pair.Key == size);
+
+                while(pair.Value.Count > 0) {
+
+                    output.Add(pair.Value.Last() + curCode);
+                    pair.Value.RemoveAt(pair.Value.Count - 1);
+                }
+            }
+
+            return output.ToArray();
+        }
+    }
+}
--- challenge_077/easy/enumerateMorseCode/enumerateMorseCode/Program.cs
+++ challenge_077/easy/enumerateMorseCode/enumerateMorseCode/Program.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace enumerateMorseCode {
     class Program {
         static void Main(string[] args) {
 
+            var watch = new Stopwatch();
+            var generator = new MorseCodeGenerator();
 
-        }
+            //default input
+            string[] codeSet1 = generator.GetMorseCodes(5);
+            Console.WriteLine("Total Codes: " + codeSet1.Length + ";\n" + string.Join(" ", codeSet1));
+            //challenge input
+            string[] codeSet2 = generator.GetMorseCodes(10);
+            Console.WriteLine("Total Codes: " + codeSet2.Length + ";\n" + string.Join(" ", codeSet2));
+            //bonus input -> average time: ~18000ms (~1.8 seconds)
+            watch.Start();
+            string[] codeSet3 = generator.GetMorseCodes(35);
+            watch.Stop();
+            Console.WriteLine("Total Codes: " + codeSet3.Length + ";\nTotal Time: " + watch.ElapsedMilliseconds + "ms");
+        }  
     }
 }
