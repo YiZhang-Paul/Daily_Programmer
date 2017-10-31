6fecf5f148b29caea2bbe121ad364814d27a450d
YI ZHANG <scaccterran@gmail.com>
2017-10-31 13:49:43 -0400

    all done

--- challenge_338/easy/dayOfTheWeek/dayOfTheWeek/Program.cs
+++ challenge_338/easy/dayOfTheWeek/dayOfTheWeek/Program.cs
@@ -20,13 +20,83 @@ namespace dayOfTheWeek {
             Console.WriteLine(GetDayInWeek("1953 3 6"));
             Console.WriteLine(GetDayInWeek("2100 1 9"));
             Console.WriteLine(GetDayInWeek("2202 12 15"));
-            Console.WriteLine(GetDayInWeek("7032 3 26"));
+            Console.WriteLine(GetDayInWeek("7032 3 26") + "\n");
+            //using Zeller's congruence
+            Console.WriteLine(ZellersDayInWeek("2017 10 30"));
+            Console.WriteLine(ZellersDayInWeek("2016 2 29"));
+            Console.WriteLine(ZellersDayInWeek("2015 2 28"));
+            Console.WriteLine(ZellersDayInWeek("29 4 12"));
+            Console.WriteLine(ZellersDayInWeek("570 11 30"));
+            Console.WriteLine(ZellersDayInWeek("1066 9 25"));
+            Console.WriteLine(ZellersDayInWeek("1776 7 4"));
+            Console.WriteLine(ZellersDayInWeek("1933 1 30"));
+            Console.WriteLine(ZellersDayInWeek("1953 3 6"));
+            Console.WriteLine(ZellersDayInWeek("2100 1 9"));
+            Console.WriteLine(ZellersDayInWeek("2202 12 15"));
+            Console.WriteLine(ZellersDayInWeek("7032 3 26"));
         }
         /// <summary>
-        /// retrieve total days in a month, in a given year
+        /// calculate day in week using Zeller's congruence
         /// </summary>
-        public static int GetDaysInMonth(int year, int month) { 
+        public static string ZellersDayInWeek(string date) {
+
+            string[] daysInWeek = new string[] {
+            
+                "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"
+            };
+            int[] dates = date.Split(' ').Select(Int32.Parse).ToArray();
+            int year = dates[0] - (dates[1] <= 2 ? 1 : 0);
+            int month = dates[1] + (dates[1] <= 2 ? 12 : 0);
+
+            return daysInWeek[(dates[2] + 13 * (month + 1) / 5 + year + year / 4 + year / 400 - year / 100) % 7];
+        }
+        /// <summary>
+        /// determine day in the week for a given date string
+        /// </summary>
+        public static string GetDayInWeek(string date) {
+
+            int[] dates = date.Split(' ').Select(Int32.Parse).ToArray();
+            int elapsed = TotalElapsedDay(dates[0], dates[1], dates[2]);
+            var daysInWeek = new string[] {
+            
+                "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+            };
+
+            return daysInWeek[(dates[0] < 1970 ? (10 - elapsed % 7) % 7 : (3 + elapsed % 7) % 7) - 1];
+        }
+        /// <summary>
+        /// calculate total day passed/prior to 1970 January 1st
+        /// </summary>
+        public static int TotalElapsedDay(int year, int month, int day) {
+
+            int elapsed = year >= 1970 ? ElapsedDayInYear(year, month, day) : RestDayInYear(year, month, day);
+            int start = year >= 1970 ? 1970 : year + 1;
+            int end = year >= 1970 ? year - 1 : 1969;
+
+            for(int i = start; i <= end; i++) {
+
+                elapsed += IsLeapYear(i) ? 366 : 365;
+            }
+
+            return elapsed;
+        }
+        /// <summary>
+        /// check if a year is leap year
+        /// </summary>
+        public static bool IsLeapYear(int year) {
         
+            if(year % 4 != 0) {
+            
+                return false;
+            }
+            //leap year if divisible by 4 but not 100, or by all 4, 100 and 400
+            return year % 100 != 0 || year % 400 == 0;
+        }
+        /// <summary>
+        /// retrieve total days in a given month, in a given year
+        /// </summary>
+        public static int GetDaysInMonth(int year, int month) {
+
             var daysInMonth = new Dictionary<int, int> {
             
                 {1, 31}, {2, IsLeapYear(year) ? 29 : 28}, {3, 31}, {4, 30}, {5, 31},
@@ -34,18 +104,6 @@ namespace dayOfTheWeek {
             };
 
             return daysInMonth[month];
-        }
-        /// <summary>
-        /// check if a year is leap year
-        /// </summary>
-        public static bool IsLeapYear(int year) { 
-        
-            if(year % 4 != 0) {
-
-                return false;
-            }
-            //leap year if divisible by 4 but not 100, or by all 4, 100 and 400
-            return year % 100 != 0 || year % 400 == 0;
         }
         /// <summary>
         /// calculate total elapsed days in a given year
@@ -67,36 +125,6 @@ namespace dayOfTheWeek {
         public static int RestDayInYear(int year, int month, int day) {
 
             return (IsLeapYear(year) ? 366 : 365) - ElapsedDayInYear(year, month, day);
-        }
-        /// <summary>
-        /// calculate total day passed/prior to 1970 January 1st
-        /// </summary>
-        public static int TotalElapsedDay(int year, int month, int day) {
-
-            int elapsed = year >= 1970 ? ElapsedDayInYear(year, month, day) : RestDayInYear(year, month, day);
-            int start = year >= 1970 ? 1970 : year + 1;
-            int end = year >= 1970 ? year - 1 : 1969;
- 
-            for(int i = start; i <= end; i++) {
-            
-                elapsed += IsLeapYear(i) ? 366 : 365;
-            }
-            
-            return elapsed;
-        }
-        /// <summary>
-        /// determine day in the week for a given date string
-        /// </summary>
-        public static string GetDayInWeek(string dateString) {
-        
-            int[] dates = dateString.Split(' ').Select(Int32.Parse).ToArray();
-            int elapsed = TotalElapsedDay(dates[0], dates[1], dates[2]);
-            var daysInWeek = new string[] { 
-                
-                "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
-            };
-            
-            return daysInWeek[(dates[0] < 1970 ? (10 - elapsed % 7) % 7 : (3 + elapsed % 7) % 7) - 1];
         }
     }
 }
