d8da13a5c6256a3ca5945b3d8b647ab2a8ed4dd0
YI ZHANG <scaccterran@gmail.com>
2017-10-31 04:40:38 -0400

    all done

---
+++ challenge_073/easy/reversePolishNotation/reversePolishNotation/Calculator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace reversePolishNotation {
+    class Calculator {
+        /// <summary>
+        /// evaluate arithmetic expressions
+        /// </summary>
+        /// <param name="second">second operand</param>
+        /// <param name="first">first operand</param>
+        public double Evaluate(string operators, double second, double first) { 
+        
+            switch(operators) {
+            
+                case "+" : case "-" :
+
+                    return first + second * (operators == "+" ? 1 : -1);
+
+                case "*" : case "/" :
+
+                    return first * Math.Pow(second, operators == "*" ? 1 : -1);
+
+                case "^" :
+
+                    return Math.Pow(first, second);
+            }
+
+            return 0;
+        }
+        /// <summary>
+        /// calculate result using Reverse Polish Notation
+        /// </summary>
+        public double Calculate(string expression) {
+
+            var stack = new Stack<double>();
+
+            foreach(Match match in Regex.Matches(expression, @"[+-]?\d*\.?\d+|[+-/*^]")) {
+
+                if(Regex.IsMatch(match.Value, @"\d")) {
+                
+                    stack.Push(Double.Parse(match.Value));
+                }
+                else if(stack.Count >= 2) {
+                
+                    stack.Push(Evaluate(match.Value, stack.Pop(), stack.Pop()));
+                }
+            }
+
+            return stack.Peek();
+        }
+    }
+}
---
+++ challenge_073/easy/reversePolishNotation/reversePolishNotation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reversePolishNotation {
+    class Program {
+        static void Main(string[] args) {
+
+            var calculator = new Calculator();
+            
+            //challenge input
+            Console.WriteLine(calculator.Calculate("3 4 * 6 2 - +"));
+            Console.WriteLine(calculator.Calculate("3 4 / 6 2 * +"));
+            //bonus input
+            Console.WriteLine(calculator.Calculate("3 4 ^"));
+            Console.WriteLine(calculator.Calculate("3 4 ^ 5 / 3 4 - +"));
+        }
+    }
+}
