1783e00774c75da6c9e0492bc77d53a28fce5acd
YI ZHANG <scaccterran@gmail.com>
2017-11-01 05:46:12 -0400

    all done

---
+++ challenge_067/easy/reverseInteger/reverseInteger/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverseInteger {
+    class Program {
+        static void Main(string[] args) {
+
+            //challenge input
+            Console.WriteLine(ReverseInteger(13));
+        }
+        /// <summary>
+        /// reverse integer in binary form and return corresponding decimal value
+        /// </summary>
+        public static uint ReverseInteger(int number) {
+
+            string binary = Convert.ToString(number, 2).PadLeft(32, '0');
+
+            return Convert.ToUInt32(string.Join("", binary.Reverse()), 2);
+        }
+    }
+}
