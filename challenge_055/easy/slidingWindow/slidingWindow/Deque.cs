467b6fc056c5e3f6481a6753e24f60d9397344e5
YI ZHANG <scaccterran@gmail.com>
2017-11-02 09:30:40 -0400

    all done

---
+++ challenge_055/easy/slidingWindow/slidingWindow/Deque.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace slidingWindow {
+    class Deque<T> {
+
+        private LinkedList<T> _queue = new LinkedList<T>();
+
+        public int Count { get { return _queue.Count; } }
+        public bool IsEmpty { get { return Count == 0; } }
+        /// <summary>
+        /// clear all items in queue
+        /// </summary>
+        public void Clear() {
+
+            _queue.Clear();
+        }
+        /// <summary>
+        /// examine item at the start of queue
+        /// </summary>
+        public T PeekStart() {
+
+            return _queue.First();
+        }
+        /// <summary>
+        /// examine item at the end of queue
+        /// </summary>
+        public T PeekEnd() {
+
+            return _queue.Last();
+        }
+        /// <summary>
+        /// insert item to the front of queue
+        /// </summary>
+        public void PushStart(T item) {
+
+            _queue.AddFirst(item);
+        }
+        /// <summary>
+        /// insert item to the end of queue
+        /// </summary>
+        public void PushEnd(T item) {
+
+            _queue.AddLast(item);
+        }
+        /// <summary>
+        /// remove item at the start of queue
+        /// </summary>
+        public T PopStart() {
+
+            var item = _queue.First();
+            _queue.RemoveFirst();
+
+            return item;
+        }
+        /// <summary>
+        /// remove item at the end of queue
+        /// </summary>
+        public T PopEnd() {
+
+            var item = _queue.Last();
+            _queue.RemoveLast();
+
+            return item;
+        }
+    }
+}
---
+++ challenge_055/easy/slidingWindow/slidingWindow/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace slidingWindow {
+    class Program {
+        static void Main(string[] args) {
+
+            int[] input = new int[] { 1, 5, 2, 81, 22, 113, 10, 11, 5 };
+
+            //challenge input
+            Console.WriteLine(string.Join(" ", SlidingWindowMinMax(input, 3, true)));
+            Console.WriteLine(string.Join(" ", SlidingWindowMinMax(input, 3)));
+        }
+        /// <summary>
+        /// find minimum value in each window slide
+        /// </summary>
+        /// <param name="size">window size</param>
+        /// <param name="getMin">find minimum value when true, maximum value otherwise</param>
+        public static int[] SlidingWindowMinMax(int[] sequence, int size, bool getMin = false) { 
+        
+            var window = new Deque<int>();
+            var output = new List<int>();
+
+            for(int i = 0; i < sequence.Length; i++) {
+
+                if(i >= size) {
+                    //record minimum value for previous window
+                    output.Add(sequence[window.PeekStart()]);
+                    //remove elements out of window
+                    while(!window.IsEmpty && window.PeekStart() <= i - size) {
+
+                        window.PopStart();
+                    }
+                }
+                //remove useless elements
+                while(!window.IsEmpty && getMin == sequence[i] < sequence[window.PeekEnd()]) {
+
+                    window.PopEnd();
+                }
+
+                window.PushEnd(i);
+            }
+            
+            output.Add(sequence[window.PeekStart()]);
+
+            return output.ToArray();
+        }
+    }
+}
