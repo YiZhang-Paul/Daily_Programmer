3ce11a12efe2734397e5ac7a45e370a4d67fc620
YI ZHANG <scaccterran@gmail.com>
2017-11-02 08:16:36 -0400

    all done

--- challenge_326/easy/nearestPrime.js
+++ challenge_326/easy/nearestPrime.js
@@ -1,71 +1,99 @@
 /* jslint esversion: 6 */
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-		//test bases for Miller-Rabin test
-		const testBase = {2047 : [2], 1373653 : [2, 3], 9080191	: [31, 73], 25326001 : [2, 3, 5], 3215031751 : [2, 3, 5, 7], 4759123141 : [2, 7, 61]};
 		/**
-		 * retrieve test bases for a given number
-		 * @param {int} [number] - designated number of test bases
-		 * @param {Object} [bases] - all test bases
-		 *
-		 * @return {Array} [designated test base]
+		 * retrieve test base for Miller-Rabin primality test
 		 */
-		function getTestBase(number, bases) {
-			let limits = Object.keys(bases).map(num => Number(num));
-			return bases[limits.find(limit => limit > number)].filter(num => number >= num);
+		function getTestBase(number) {
+
+			const bases = Object.freeze({
+
+				2047 : [2],
+				1373653 : [2, 3],
+				9080191	: [31, 73],
+				25326001 : [2, 3, 5],
+				3215031751 : [2, 3, 5, 7],
+				4759123141 : [2, 7, 61]
+			});
+			//sort all limits in ascending order
+			let limits = Object.keys(bases).map(Number).sort((a, b) => a - b);	
+
+			return bases[limits.find(limit => number < limit)];
 		}
 		/**
-		 * check if a number is a Miller-Rabin witness
-		 * @param {int} [base] - test base
-		 * @param {int} [number] - number to be checked
-		 * @param {int} [b] - Miller-Rabin variable
-		 * @param {int} [k] - Miller-Rabin variable
-		 *
-		 * @return {boolean} [test result]
+		 * calculate coefficient for Miller-Rabin primality test
+		 * @param {int} [number] - number for primality test
 		 */
-		function isWitness(base, number, b, k) {
-			if(Math.pow(base, k) % number == 1) {
-				return false;
+		function getCoefficients(number) {
+
+			let s = 0, b = number - 1;
+
+			while(b % 2 === 0) {
+
+				[s, b] = [s + 1, b * 0.5];
 			}
-			return new Array(b).fill(0)
-			                   .map((num, index) => index)
-			                   .every(num => Math.pow(base, Math.pow(2, num) * k) % number - number != -1);
+
+			return [s, b];
 		}
 		/**
-		 * check if a number is prime using Miller-Rabin primality test
-		 * @param {int} [number] - number to be tested
-		 * @param {Object} [bases] - test bases
-		 *
-		 * @return {boolean} [test result]
+		 * check if a test base proves a number's primality
+		 * @param {int} [s] - Miller-Rabin primality test coefficient
+		 * @param {int} [b] - Miller-Rabin primality test coefficient
 		 */
-		function isPrime(number, bases = testBase) {
-			if(number == 2) {
-				return true;
+		function isValidBase(number, base, s, b) {
+
+			for(let i = 0; i < s; i++) {
+
+				let remainder = Math.pow(base, Math.pow(2, i) * b) % number;
+			
+				if(remainder === number - 1 || (i === 0 && remainder === 1)) {
+
+					return true;
+				}
 			}
-			let b = 0, k = number - 1;
-			while(k % 2 === 0) {
-				[b, k] = [b + 1, k * 0.5];
+
+			return false;
+		}
+		/**
+		 * Miller-Rabin primality test
+		 */
+		function isPrime(number) {
+
+			if(number <= 2) {
+
+				return number === 2;
 			}
-			return getTestBase(number, bases).some(base => !isWitness(base, number, b, k));
+
+			let [s, b] = getCoefficients(number);
+
+			return getTestBase(number).some(base => isValidBase(number, base, s, b));
+		}
+		/**
+		 * find adjacent prime
+		 * @param {boolean} [findLow] - find smaller prime when true, larger prime otherwise
+		 */
+		function adjacentPrime(number, findLow) {
+
+			let prime = number;
+
+			while(!isPrime(prime)) {
+
+				prime += findLow ? -1 : 1;
+			}
+
+			return prime;
 		}
 		/**
 		 * find nearest primes
-		 * @param {int} [number] - number to be checked
-		 *
-		 * @return {String} [test result]
 		 */
 		function nearestPrime(number) {
+
 			if(isPrime(number)) {
+
 				return `${number} is Prime.`;
 			}
-			let low = number, high = number;
-			while(!isPrime(low)) {
-				low--;
-			}
-			while(!isPrime(high)) {	
-				high++;
-			}
-			return `${low} < ${number} < ${high}`;
+
+			return `${adjacentPrime(number, true)} < ${number} < ${adjacentPrime(number)}`;
 		}
 		//challenge input
 		console.log(`%cChallenge Input: `, "color : red;");
@@ -79,9 +107,7 @@
 		console.log(`${nearestPrime(649)} %c(${new Date().getTime() - time}ms)`, "color : orange;");
 		//bonus input
 		console.log(`%cBonus Input: `, "color : red;");
-		time = new Date().getTime();
-		console.log(`${nearestPrime(2010741)} %c(${new Date().getTime() - time}ms)`, "color : orange;");
-		time = new Date().getTime();
-		console.log(`${nearestPrime(1425172824437700148)} %c(${new Date().getTime() - time}ms)`, "color : orange;");
+		//time = new Date().getTime();
+		//console.log(`${nearestPrime(2010741)} %c(${new Date().getTime() - time}ms)`, "color : orange;");
 	});
 })();
