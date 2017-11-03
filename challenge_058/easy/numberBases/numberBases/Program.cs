3715c25b02131881c2ed7e151155ccebe9bd3e9e
YI ZHANG <scaccterran@gmail.com>
2017-11-03 11:22:42 -0400

    all done

--- challenge_058/easy/numberBases/numberBases/Program.cs
+++ challenge_058/easy/numberBases/numberBases/Program.cs
@@ -3,14 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Diagnostics;
 
 namespace numberBases {
     class Program {
         static void Main(string[] args) {
-
-            var watch = new Stopwatch();
-            watch.Start();
 
             //default input
             Console.WriteLine(DecimalToBase(1234, 2));
@@ -21,20 +17,19 @@ namespace numberBases {
             Console.WriteLine(DecimalToBase(19959694, 35));
             Console.WriteLine(DecimalToBase(376609378180550, 29));
             //bonus input
-
-            watch.Stop();
-            Console.WriteLine(watch.ElapsedMilliseconds + "ms");
+            Console.WriteLine(string.Join(" ", GetPalindromicBases(15167)));
+            Console.WriteLine(string.Join(" ", GetPalindromicBases(10858)));
         }
         /// <summary>
         /// generate a list of powers of a given base that is smaller than given number
         /// </summary>
-        public static IEnumerable<long> GetBasePowers(long number, int target) { 
+        public static IEnumerable<long> GetBasePowers(long number, int targetBase) {
 
             int power = 0;
 
-            while((long)Math.Pow(target, power) <= number) {
+            while((long)Math.Pow(targetBase, power) <= number) {
 
-                yield return (long)Math.Pow(target, power++);
+                yield return (long)Math.Pow(targetBase, power++);
             }
         }
         /// <summary>
@@ -42,15 +37,15 @@ namespace numberBases {
         /// </summary>
         public static char GetNumberChar(int number) { 
         
-            return number > 9 ? Char.ConvertFromUtf32(65 + number - 10)[0] : number.ToString()[0];
+            return number > 9 ? Char.ConvertFromUtf32(55 + number)[0] : number.ToString()[0];
         }
         /// <summary>
         /// convert decimal number to target base
         /// </summary>
-        public static string DecimalToBase(long number, int target) {
+        public static string DecimalToBase(long number, int targetBase) {
 
             var result = new StringBuilder();
-            var powers = GetBasePowers(number, target).OrderByDescending(power => power);
+            var powers = GetBasePowers(number, targetBase).OrderByDescending(value => value);
 
             foreach(long power in powers) {
 
@@ -59,6 +54,30 @@ namespace numberBases {
             }
 
             return result.ToString();
+        }
+        /// <summary>
+        /// find all bases where a decimal number is palindromic
+        /// </summary>
+        public static int[] GetPalindromicBases(int number) {
+
+            return Enumerable.Range(2, 35)
+                             .Where(curBase => IsPalindrome(DecimalToBase(number, curBase)))
+                             .ToArray();
+        }
+        /// <summary>
+        /// check if a number is palindromic
+        /// </summary>
+        public static bool IsPalindrome(string number) {
+
+            for(int i = 0; i < number.Length - 1 - i; i++) {
+
+                if(number[i] != number[number.Length - 1 - i]) {
+                
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
