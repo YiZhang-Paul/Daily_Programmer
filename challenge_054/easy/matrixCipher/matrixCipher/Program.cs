b4b45d5f3adbd7fc544c465b121dd66fa3dd1e32
YI ZHANG <scaccterran@gmail.com>
2017-11-03 12:45:19 -0400

    decode message

--- challenge_054/easy/matrixCipher/matrixCipher/Program.cs
+++ challenge_054/easy/matrixCipher/matrixCipher/Program.cs
@@ -11,6 +11,15 @@ namespace matrixCipher {
             //challenge input
             Console.WriteLine(Encode("The cake is a lie!", 3));
             Console.WriteLine(Encode("The cake is a lie!", 7));
+            Console.WriteLine(Decode("T_kiaihces_eea__l!", 3));
+            Console.WriteLine(Decode("Telh_ieie_s!c_vaamk_z", 7));
+        }
+        /// <summary>
+        /// retrieve a random letter
+        /// </summary>
+        public static char GetLetter(Random random) {
+
+            return Char.ConvertFromUtf32(random.Next(0, 26) + 97)[0];
         }
         /// <summary>
         /// encrypt message using matrix cipher
@@ -24,13 +33,30 @@ namespace matrixCipher {
 
                 for(int j = 0; j < Math.Ceiling((double)message.Length / key); j++) {
 
-                    char character = i + j * key >= message.Length ? 
-                        Char.ConvertFromUtf32(random.Next(0, 26) + 97)[0] : message[i + j * key];
+                    char character = i + j * key >= message.Length ? GetLetter(random) : message[i + j * key];
                     encoded.Append(character == ' ' ? '_' : character);
                 }
             }
 
             return encoded.ToString();
+        }
+        /// <summary>
+        /// decrypt message using matrix cipher
+        /// </summary>
+        public static string Decode(string encoded, int key) {
+
+            var decoded = new StringBuilder();
+            int rows = (int)Math.Ceiling((double)encoded.Length / key);
+
+            for(int i = 0; i < rows; i++) {
+
+                for(int j = 0; j < key; j++) {
+
+                    decoded.Append(encoded[i + j * rows] == '_' ? ' ' : encoded[i + j * rows]);
+                }
+            }
+
+            return decoded.ToString();
         }
     }
 }
