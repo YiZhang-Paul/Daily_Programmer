07679421005a0ebf83d125eddf3d19c3c5280021
YI ZHANG <scaccterran@gmail.com>
2017-11-03 22:07:38 -0400

    all done

---
+++ challenge_046/easy/populationCount/populationCount/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace populationCount {
+    class Program {
+        static void Main(string[] args) {
+
+            //challenge input
+            Console.WriteLine(GetPopulation(23));
+        }
+        /// <summary>
+        /// calculate population count of a number
+        /// </summary>
+        public static int GetPopulation(int number) {
+
+            return Convert.ToString(number, 2).Count(digit => digit == '1');
+        }
+    }
+}
