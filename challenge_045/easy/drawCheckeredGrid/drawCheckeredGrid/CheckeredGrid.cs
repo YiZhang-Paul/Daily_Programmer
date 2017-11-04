8cc562c3635d8e8c18f27605cf8c0428013cfcc4
YI ZHANG <scaccterran@gmail.com>
2017-11-03 22:46:03 -0400

    all done

---
+++ challenge_045/easy/drawCheckeredGrid/drawCheckeredGrid/CheckeredGrid.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drawCheckeredGrid {
+    class CheckeredGrid {
+        /// <summary>
+        /// draw checkered grid
+        /// </summary>
+        /// <param name="width">grid width</param>
+        public string DrawGrid(int row, int column, int width = 3) {
+
+            string lineSplit = "".PadLeft((width + 1) * column + 1, '*');
+            string evenRow = GetEvenRow(column, width);
+            string oddRow = GetOddRow(column, width);
+            var grid = new StringBuilder(lineSplit + "\n");
+
+            for(int i = 0; i < row; i++) {
+
+                grid.Append((i % 2 == 0 ? evenRow : oddRow) + "\n" + lineSplit + "\n");
+            }
+
+            return grid.ToString();
+        }
+        /// <summary>
+        /// retrieve even numbered row
+        /// </summary>
+        public string GetEvenRow(int column, int width) {
+
+            string row = Enumerable.Range(1, column)
+                                   .Select(value => "*".PadRight(width + 1, value % 2 == 0 ? '#' : ' '))
+                                   .Aggregate("", (acc, val) => acc + val) + "*";
+
+            return string.Join("\n", new string[width].Select(index => row));
+        }
+        /// <summary>
+        /// retrieve odd numbered row
+        /// </summary>
+        public string GetOddRow(int column, int width) {
+
+            string row = Enumerable.Range(1, column)
+                                   .Select(value => "*".PadRight(width + 1, value % 2 == 0 ? ' ' : '#'))
+                                   .Aggregate("", (acc, val) => acc + val) + "*";
+
+            return string.Join("\n", new string[width].Select(index => row));
+        }
+    }
+}
---
+++ challenge_045/easy/drawCheckeredGrid/drawCheckeredGrid/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drawCheckeredGrid {
+    class Program {
+        static void Main(string[] args) {
+
+            var grid = new CheckeredGrid();
+
+            //challenge input
+            Console.WriteLine(grid.DrawGrid(3, 8)); ;
+            //bonus input
+            Console.WriteLine(grid.DrawGrid(2, 5, 5)); ;
+        }
+    }
+}
