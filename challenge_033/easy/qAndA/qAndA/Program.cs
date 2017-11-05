524115e1f397049af57b25b17e8bb4059b5362a8
YI ZHANG <scaccterran@gmail.com>
2017-11-05 13:30:05 -0500

    all done

---
+++ challenge_033/easy/qAndA/qAndA/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qAndA {
+    class Program {
+        static void Main(string[] args) {
+
+            //challenge & bonus input
+            var game = new QAGame("text.txt");
+            game.Run();
+        }
+    }
+}
---
+++ challenge_033/easy/qAndA/qAndA/QAGame.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace qAndA {
+    class QAGame {
+
+        private Random _random = new Random();
+
+        public Dictionary<string, string> QuestionSet { get; private set; }
+
+        public QAGame(string fileName) {
+
+            QuestionSet = GetQuestionSet(fileName);
+        }
+        /// <summary>
+        /// retrieve question set
+        /// </summary>
+        public Dictionary<string, string> GetQuestionSet(string fileName) {
+
+            var questions = new Dictionary<string, string>();
+
+            try {
+
+                foreach(string line in File.ReadAllLines(fileName)) {
+
+                    string[] question = line.Split(',');
+                    questions.Add(question[0], question[1]);
+                }
+            }
+            catch(Exception exception) {
+
+                Console.WriteLine("File not found.");
+                Console.WriteLine(exception.Message);
+            }
+
+            return questions;
+        }
+        /// <summary>
+        /// pick random question
+        /// </summary>
+        public string PickQuestion() {
+
+            var questions = QuestionSet.Keys.ToList();
+
+            return questions[_random.Next(0, questions.Count)];
+        }
+        /// <summary>
+        /// run questions and answers game
+        /// </summary>
+        public void Run() {
+
+            while(true) {
+
+                string question = PickQuestion();
+                Console.WriteLine("> " + question);
+                string input = Console.ReadLine().Trim();
+
+                if(input == "exit") {
+
+                    break;
+                }
+
+                Console.WriteLine(input == QuestionSet[question] ? "Correct!" : "Wrong!");
+            }
+        }
+    }
+}
