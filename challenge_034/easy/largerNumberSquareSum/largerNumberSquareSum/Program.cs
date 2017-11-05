dbd89e067b371bdf8495ad2d9139cfb981cd0e84
YI ZHANG <scaccterran@gmail.com>
2017-11-05 13:47:06 -0500

    all done

---
+++ challenge_034/easy/largerNumberSquareSum/largerNumberSquareSum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace largerNumberSquareSum {
+    class Program {
+        static void Main(string[] args) {
+
+            //challenge input
+            Console.WriteLine(LargerSquareSum(1, 2, 3));
+            Console.WriteLine(LargerSquareSum(3, 2, 3));
+            Console.WriteLine(LargerSquareSum(1, 21, 1));
+            Console.WriteLine(LargerSquareSum(1, 21, 2));
+        }
+        /// <summary>
+        /// calculate the sum of the squares of the two larger numbers
+        /// </summary>
+        public static int LargerSquareSum(int number1, int number2, int number3) {
+
+            var numbers = new int[] { number1, number2, number3 }.OrderBy(value => value);
+
+            return numbers.Skip(1).Aggregate(0, (sum, number) => sum + (int)Math.Pow(number, 2));
+        }
+    }
+}
