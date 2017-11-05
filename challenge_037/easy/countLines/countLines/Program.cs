724a110b2621ae6cdc5aa6edcbc9a3f5dd6de3b5
YI ZHANG <scaccterran@gmail.com>
2017-11-05 09:41:41 -0500

    all done

---
+++ challenge_037/easy/countLines/countLines/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace countLines {
+    class Program {
+        static void Main(string[] args) {
+
+            //challenge & bonus input
+            Console.WriteLine(LineAndWordCount("text.txt"));
+        }
+        /// <summary>
+        /// get line count and word count of a file
+        /// </summary>
+        public static string LineAndWordCount(string fileName) {
+
+            var result = new StringBuilder();
+
+            try {
+
+                var lines = File.ReadAllLines(fileName);
+
+                return result.Append("Total Lines: " + lines.Count() + "\n")
+                             .Append("Total Words: " + Regex.Matches(string.Join(" ", lines), @"\S+").Count)
+                             .ToString();
+            }
+            catch(Exception exception) {
+
+                Console.WriteLine("File not found.");
+                Console.WriteLine(exception.Message);
+            }
+
+            return result.ToString();
+        }
+    }
+}
