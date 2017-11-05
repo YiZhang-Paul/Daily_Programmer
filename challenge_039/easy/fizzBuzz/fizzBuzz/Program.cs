04b03ca57550d400c408c77e23a3c510f99d53de
YI ZHANG <scaccterran@gmail.com>
2017-11-05 09:26:59 -0500

    all done

---
+++ challenge_039/easy/fizzBuzz/fizzBuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizzBuzz {
+    class Program {
+        static void Main(string[] args) {
+
+            //challenge input
+            FizzBuzz(20);
+            FizzBuzz(100);
+        }
+        /// <summary>
+        /// Fizz Buzz
+        /// </summary>
+        public static void FizzBuzz(int limit) {
+
+            for(int i = 1; i <= limit; i++) {
+
+                if(i % 3 == 0 || i % 5 == 0) {
+
+                    Console.WriteLine(i % 15 == 0 ? "FizzBuzz" : (i % 3 == 0 ? "Fizz" : "Buzz"));
+                    continue;
+                }
+
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
