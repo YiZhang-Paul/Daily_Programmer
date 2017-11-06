344e36f3b0f36fabfed45555c0069789c03d50b7
YI ZHANG <scaccterran@gmail.com>
2017-11-06 12:34:55 -0500

    all done

---
+++ challenge_027/easy/analyzeYear/analyzeYear/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace analyzeYear {
+    class Program {
+        static void Main(string[] args) {
+
+            //challenge input
+            Console.WriteLine(AnalyzeYear(1996) + "\n");
+            Console.WriteLine(AnalyzeYear(1900));
+        }
+        /// <summary>
+        /// check if a year is a leap year
+        /// </summary>
+        public static bool IsLeapYear(int year) { 
+        
+            if(year % 4 != 0) {
+
+                return false;
+            }
+            //divided by 4 but not 100, or by all 4, 100 and 400
+            return year % 100 != 0 || year % 400 == 0; 
+        }
+        /// <summary>
+        /// find century in which the year is included
+        /// </summary>
+        public static int GetCentury(int year) {
+
+            int century = (year - year % 100) / 100;
+
+            return year == century * 100 ? century : century + 1;
+        }
+        /// <summary>
+        /// find out century in which the year is included
+        /// and determine if the year is a leap year
+        /// </summary>
+        public static string AnalyzeYear(int year) {
+
+            return string.Join("\n", new string[] {
+            
+                "Enter Year: " + year,
+                "Century: " + GetCentury(year),
+                "Leap Year: " + (IsLeapYear(year) ? "Yes" : "No")
+            });
+        }
+    }
+}
---
+++ challenge_027/easy/analyzeYear/analyzeYear/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("analyzeYear")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("analyzeYear")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("514b4f68-d6f8-463d-9080-234998ce567e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
