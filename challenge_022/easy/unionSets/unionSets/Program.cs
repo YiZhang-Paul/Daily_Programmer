0ba11711caf22dffca182c31d34b7359317f7bc4
YI ZHANG <scaccterran@gmail.com>
2017-11-06 13:28:02 -0500

    all done

---
+++ challenge_022/easy/unionSets/unionSets/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unionSets {
+    class Program {
+        static void Main(string[] args) {
+
+            string[] list1 = new string[] { "a", "b", "c", "1", "4" };
+            string[] list2 = new string[] { "a", "x", "34", "4", "41" };
+            
+            //challenge input
+            Console.WriteLine(string.Join(" ", UnionLists(list1, list2)));
+            Console.WriteLine(string.Join(" ", list1.Union(list2)));
+        }
+        /// <summary>
+        /// union two lists
+        /// </summary>
+        public static T[] UnionLists<T>(IList<T> list1, IList<T> list2) { 
+        
+            var inList1 = new HashSet<T>(list1);
+            var noInList1 = list2.Where(item => !inList1.Contains(item));
+
+            return new List<T>(list1).Concat(noInList1).ToArray();
+        }
+    }
+}
