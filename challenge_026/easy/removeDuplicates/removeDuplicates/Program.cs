be444f20de76ea21c09dfae71ebf766e53555ad2
YI ZHANG <scaccterran@gmail.com>
2017-11-06 12:15:53 -0500

    all done

---
+++ challenge_026/easy/removeDuplicates/removeDuplicates/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace removeDuplicates {
+    class Program {
+        static void Main(string[] args) {
+
+            //challenge input
+            Console.WriteLine(string.Join(" ", RemoveDuplicate("balloons")));
+            Console.WriteLine(string.Join(" ", RemoveDuplicate("ddaaiillyypprrooggrraammeerr")));
+            Console.WriteLine(string.Join(" ", RemoveDuplicate("aabbccddeded")));
+            Console.WriteLine(string.Join(" ", RemoveDuplicate("flabby aapples")));
+        }
+        /// <summary>
+        /// remove duplicates in string
+        /// </summary>
+        public static string[] RemoveDuplicate(string word) {
+
+            char letter = word[0];
+            var original = new StringBuilder(letter.ToString());
+            var duplicate = new StringBuilder();
+
+            for(int i = 1; i < word.Length; i++) {
+
+                bool hasDuplicate = word[i] == letter;
+                (hasDuplicate ? duplicate : original).Append(word[i]);
+                letter = hasDuplicate ? letter : word[i];
+            }
+
+            return new string[] { "\"" + original.ToString() + "\"", "\"" + duplicate.ToString() + "\"" };
+        }
+    }
+}
