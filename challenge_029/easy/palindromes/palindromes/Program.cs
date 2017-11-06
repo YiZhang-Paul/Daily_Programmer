2a7f48e0d8d66b671f098d8deab2cd23d4395f12
YI ZHANG <scaccterran@gmail.com>
2017-11-06 14:34:05 -0500

    all done

---
+++ challenge_029/easy/palindromes/palindromes/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace palindromes {
+    class Program {
+        static void Main(string[] args) {
+
+            //challenge input
+            Console.WriteLine(IsPalindrome("hannah"));
+            Console.WriteLine(IsPalindrome("12321"));
+            Console.WriteLine(IsPalindrome("123321"));
+            Console.WriteLine(IsPalindrome("123421"));
+            //bonus input
+            Console.WriteLine(IsPalindrome(ReadFile("poem.txt")));
+        }
+        /// <summary>
+        /// read file
+        /// </summary>
+        public static string ReadFile(string fileName) {
+
+            try {
+
+                return File.ReadAllText(fileName); 
+            }
+            catch(Exception exception) {
+
+                Console.WriteLine("File not found.");
+                Console.WriteLine(exception.Message);
+            }
+
+            return "";
+        }
+        /// <summary>
+        /// check if a text is palindromic
+        /// </summary>
+        public static bool IsPalindrome(string text) {
+
+            text = Regex.Replace(text.ToLower(), @"\W", "");
+
+            for(int i = 0; i < text.Length - 1 - i; i++) {
+
+                if(text[i] != text[text.Length - 1 - i]) {
+                
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
