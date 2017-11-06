0b06e6a615b5ba1d4465e8b7d5f44926810c61df
YI ZHANG <scaccterran@gmail.com>
2017-11-06 13:58:07 -0500

    all done

---
+++ challenge_020/easy/printPrimes/printPrimes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace printPrimes {
+    class Program {
+        static void Main(string[] args) {
+
+            //challenge input
+            Console.WriteLine(string.Join("\n", GetPrimes(2000)));
+        }
+        /// <summary>
+        /// find all primes up to a given limit
+        /// </summary>
+        public static int[] GetPrimes(int limit) {
+
+            var tester = new PrimalityTester();
+
+            return Enumerable.Range(1, limit).Where(number => tester.IsPrime(number)).ToArray();
+        }
+    }
+}
