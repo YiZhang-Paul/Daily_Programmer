617297344f03c1a541169b27a7d8471f7be270c6
YI ZHANG <scaccterran@gmail.com>
2017-11-07 12:17:09 -0500

    all done

---
+++ challenge_016/easy/removeDuplicate/removeDuplicate/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace removeDuplicate {
+    class Program {
+        static void Main(string[] args) {
+
+            //challenge input
+            Console.WriteLine(RemoveDuplicate("Daily Programmer", "aeiou "));
+        }
+        /// <summary>
+        /// remove characters in the first string that also appear in second string
+        /// </summary>
+        public static string RemoveDuplicate(string string1, string string2) {
+
+            var duplicate = new HashSet<char>(string2);
+            var output = new StringBuilder();
+
+            foreach(char letter in string1) {
+            
+                if(!duplicate.Contains(letter)) {
+                
+                    output.Append(letter);
+                }
+            }
+
+            return output.ToString();
+        }
+    }
+}
