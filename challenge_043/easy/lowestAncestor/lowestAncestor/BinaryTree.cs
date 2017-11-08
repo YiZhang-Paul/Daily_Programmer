dcbba116eab94eeeca98aa0aafa8820316e4a2e0
YI ZHANG <scaccterran@gmail.com>
2017-11-08 15:25:54 -0500

    all done

--- challenge_043/easy/lowestAncestor/lowestAncestor/BinaryTree.cs
+++ challenge_043/easy/lowestAncestor/lowestAncestor/BinaryTree.cs
@@ -7,11 +7,13 @@ using System.Threading.Tasks;
 namespace lowestAncestor {
     class BinaryTree {
 
+        public Dictionary<int, Node> Nodes { get; private set; }
         public Node Root { get; private set; }
         public int IDs { get; private set; }
 
         public BinaryTree(int[] values) {
 
+            Nodes = new Dictionary<int, Node>();
             Root = CreateNode(values[0]);
             AddNodes(values.Skip(1).ToArray());
         }
@@ -20,25 +22,10 @@ namespace lowestAncestor {
         /// </summary>
         public Node CreateNode(int value, Node parent = null) {
 
-            return new Node(IDs++, value, parent);
-        }
-        /// <summary>
-        /// add new nodes to tree
-        /// </summary>
-        public Node AddNode(int value, Node root = null) { 
-        
-            root = root ?? Root;
+            var node = new Node(IDs++, value, parent);
+            Nodes.Add(node.ID, node);
 
-            if(value < root.Value) {
-            
-                root.Left = root.Left == null ? CreateNode(value, root) : AddNode(value, root.Left);
-            }
-            else {
-            
-                root.Right = root.Right == null ? CreateNode(value, root) : AddNode(value, root.Right);
-            }
-
-            return root;
+            return node;
         }
         /// <summary>
         /// add a list of nodes to tree
@@ -49,6 +36,55 @@ namespace lowestAncestor {
 
                 AddNode(value);
             }
+        }
+        /// <summary>
+        /// add a new node to tree
+        /// </summary>
+        public Node AddNode(int value, Node root = null) {
+
+            root = root ?? Root;
+
+            if(value < root.Value) {
+
+                root.Left = root.Left == null ? CreateNode(value, root) : AddNode(value, root.Left);
+            }
+            else { 
+            
+                root.Right = root.Right == null ? CreateNode(value, root) : AddNode(value, root.Right);
+            }
+
+            return root;
+        }
+        /// <summary>
+        /// check if a node is parent of another given node
+        /// </summary>
+        public bool IsParent(Node parent, Node child) { 
+        
+            if(parent == null || parent.ID == child.ID) {
+
+                return parent != null;
+            }
+
+            return IsParent(parent.Left, child) || IsParent(parent.Right, child);
+        }
+        /// <summary>
+        /// find lowest common ancestor of given nodes
+        /// </summary>
+        public Node LowestAncestor(Node node1, Node node2) { 
+        
+            if(node1.Parent == null || node2.Parent == null) {
+
+                return null;
+            }
+
+            var parent = node1.Parent;
+
+            while(parent != null && (parent == node2 || !IsParent(parent, node2))) {
+
+                parent = parent.Parent;
+            }
+
+            return parent;
         }
     }
 }
--- challenge_043/easy/lowestAncestor/lowestAncestor/Program.cs
+++ challenge_043/easy/lowestAncestor/lowestAncestor/Program.cs
@@ -8,9 +8,15 @@ namespace lowestAncestor {
     class Program {
         static void Main(string[] args) {
 
-            int[] values = new int[] { 5, 2, 3, 1, 1, 6, 8, 4, 2, 7 };
+            var tree = new BinaryTree(new int[] { 5, 2, 3, 1, 1, 6, 8, 4, 2, 7 });
 
-            var tree = new BinaryTree(values);
+            //challenge input
+            var parent = tree.LowestAncestor(tree.Nodes[3], tree.Nodes[1]);
+            Console.WriteLine("{ID: " + parent.ID + ", Value: " + parent.Value + "}");
+            parent = tree.LowestAncestor(tree.Nodes[6], tree.Nodes[9]);
+            Console.WriteLine("{ID: " + parent.ID + ", Value: " + parent.Value + "}");
+            parent = tree.LowestAncestor(tree.Nodes[4], tree.Nodes[2]);
+            Console.WriteLine("{ID: " + parent.ID + ", Value: " + parent.Value + "}");
         }
     }
 }
