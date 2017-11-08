4e60dede84ea79899b9e3913b3758363ebca9e68
YI ZHANG <scaccterran@gmail.com>
2017-11-08 17:56:31 -0500

    construct proper prefix table

---
+++ challenge_059/easy/stringSearching/stringSearching/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringSearching {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine(string.Join(" ", GetPrefixTable("abcdabca")));
+            Console.WriteLine(string.Join(" ", GetPrefixTable("aabaabaaa")));
+        }
+        /// <summary>
+        /// build proper prefix table for a given search pattern
+        /// </summary>
+        public static int[] GetPrefixTable(string pattern) { 
+        
+            int[] prefix = new int[pattern.Length];
+
+            for(int k = 1, j = 0, i = 1; k < pattern.Length; k++) {
+
+                while(j != 0 && pattern[j] != pattern[i]) {
+
+                    j = prefix[j - 1];
+                }
+
+                if(pattern[j] == pattern[i]) {
+
+                    prefix[k] = j + 1;
+                    j++;
+                }
+
+                i++;
+            }
+
+            return prefix;
+        }
+    }
+}
