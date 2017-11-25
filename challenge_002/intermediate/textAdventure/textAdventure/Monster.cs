23c1f1ab0acc15bc80514f4a24797173d1253f51
YI ZHANG <scaccterran@gmail.com>
2017-11-25 02:10:41 -0500

    base game

---
+++ challenge_002/intermediate/textAdventure/textAdventure/Monster.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace textAdventure {
+    class Monster : Player {
+
+        private Random _random = new Random();
+
+        public Monster() {
+
+            Name = GetName();
+            var that = this;
+        }
+
+        public string GetName() {
+
+            try {
+
+                string[] names = File.ReadAllLines("MonsterNames.txt");
+
+                return names[_random.Next(0, names.Length)];
+            }
+            catch(Exception exception) {
+                
+                Console.WriteLine("File not Found.");
+                Console.WriteLine(exception.Message);
+            }
+
+            return null;
+        }
+
+        public int DropPotions() {
+
+            return _random.Next(0, 3);
+        }
+    }
+}
---
+++ challenge_002/intermediate/textAdventure/textAdventure/Player.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace textAdventure {
+    class Player {
+
+        private Random _random = new Random();
+
+        public string Name { get; set; }
+        public int Health { get; private set; }
+        public int Damage { get; private set; }
+        public int Potions { get; set; }
+        public bool InCombat { get; set; }
+        public bool IsAlive { get; private set; }
+        public bool HealthFull { get { return Health == 100; } }
+
+        public Player() {
+
+            Health = 100;
+            Damage = 15;
+            Potions = 3;
+            IsAlive = true;
+        }
+
+        public Player(string name) {
+
+            Name = name;
+            Health = 100;
+            Damage = 15;
+            Potions = 3;
+            IsAlive = true;
+        }
+
+        public void Attack(Player enemy) {
+
+            enemy.Health = Math.Max(0, enemy.Health - Damage);
+            Console.WriteLine(Name + " Dealt " + Damage + " Damage to " + enemy.Name + "!");
+            Console.WriteLine(Name + " Health: " + Health + "; " + enemy.Name + " Health: " + enemy.Health);
+
+            if(enemy.Health == 0) {
+
+                enemy.IsAlive = false;
+            }
+        }
+
+        public void UsePotion() { 
+        
+            if(Potions > 0 && Health < 100) {
+
+                Potions--;
+                Health = Math.Min(100, Health + 40);
+            }
+        }
+
+        public bool Escape(Player enemy) { 
+        
+            if(Health >= enemy.Health) {
+
+                return _random.Next(0, 100) <= 49;
+            }
+
+            return _random.Next(0, 100) <= Math.Truncate((double)Health / enemy.Health * 100);
+        }
+    }
+}
