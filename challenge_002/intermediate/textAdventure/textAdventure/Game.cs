ba5c626384487f9843f69b1e35390179d6cb9ca4
YI ZHANG <scaccterran@gmail.com>
2017-11-25 03:57:41 -0500

    handle all generic commands

--- challenge_002/intermediate/textAdventure/textAdventure/Game.cs
+++ challenge_002/intermediate/textAdventure/textAdventure/Game.cs
@@ -3,23 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace textAdventure {
     class Game {
 
         public User User { get; private set; }
+        public bool Running { get; private set; }
 
         public Game() {
 
             Initialize();
-        }
-
-        public void ShowIntroduction() {
-
-            Console.WriteLine("> Hello, {0}! Welcome to Text Adventure Game!", User.Name);
-            Console.WriteLine("> To Check All Available Commands, Enter \"man\" During the Game.\n");
-            ShowCommands();
-            Console.WriteLine("> There are 10 Rooms Ahead. Your Goal is to Get Through All Doors and Survive.");
         }
 
         public void ShowCommands() {
@@ -38,15 +32,54 @@ namespace textAdventure {
             Console.WriteLine("> Commands:\n" + commands + "\n");
         }
 
+        public void ShowIntroduction() {
+
+            Console.WriteLine("> Hello, {0}! Welcome to Text Adventure Game!", User.Name);
+            Console.WriteLine("> To Check All Available Commands, Enter \"man\" During the Game.\n");
+            ShowCommands();
+            Console.WriteLine("> There are 10 Rooms Ahead. Your Goal is to Get Through All Doors and Survive.");
+        }
+
         public void Initialize() {
 
             User = new User();
             ShowIntroduction();
+            Running = true;
+        }
+
+        public string GetInput() {
+
+            Console.WriteLine("> What to do?");
+            string move = Console.ReadLine().Trim().ToLower();
+
+            return Regex.IsMatch(move, "^[qfahec]|man$") ? move : GetInput();
+        }
+
+        public void EndGame() {
+
+            Running = false;
+            Console.WriteLine("> Game Terminated.");
+        }
+
+        public void HandleGenericCommands(string command) {
+
+            if(command == "q") EndGame();
+            else if(command == "man") ShowCommands();
+            else if(command == "h") User.UsePotion();
+            else if(command == "c") User.ShowStats();
         }
 
         public void Run() {
 
-            
+            while(Running) {
+
+                string move = GetInput();
+
+                if(Regex.IsMatch(move, "^[qhc]|man$")) {
+                
+                    HandleGenericCommands(move);
+                }
+            }
         }
     }
 }
--- challenge_002/intermediate/textAdventure/textAdventure/User.cs
+++ challenge_002/intermediate/textAdventure/textAdventure/User.cs
@@ -8,10 +8,13 @@ namespace textAdventure {
     class User : Player {
 
         public override int Damage { get; set; }
+        public int Potion { get; set; }
+        public bool InCombat { get; private set; }
 
         public User() {
 
             Damage = 15;
+            Potion = 3;
         }
 
         public override string GetName() {
@@ -20,6 +23,29 @@ namespace textAdventure {
             string name = Console.ReadLine().Trim();
 
             return name != "" ? name : GetName();
+        }
+
+        public void ShowStats() {
+
+            Console.WriteLine("Health: {0}; Potions: {1}", Health, Potion);
+        }
+
+        public void UsePotion() { 
+        
+            if(Potion == 0) {
+
+                Console.WriteLine("Not Enough Potion.");
+            }
+            else if(HealthFull) {
+
+                Console.WriteLine("Health Already Full.");
+            }
+            else {
+
+                Potion--;
+                Health = Math.Min(Health + 40, 100);
+                Console.WriteLine("Health Restored to {0}; Potions Remain: {1}", Health, Potion);
+            }
         }
     }
 }
