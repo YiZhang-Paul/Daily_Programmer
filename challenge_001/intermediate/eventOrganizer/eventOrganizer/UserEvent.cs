3efaaf739b92a027d451a12b7667ad5d10939385
YI ZHANG <scaccterran@gmail.com>
2017-11-26 13:07:14 -0500

    user event class

---
+++ challenge_001/intermediate/eventOrganizer/eventOrganizer/UserEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eventOrganizer {
+    class UserEvent {
+
+        public string Title { get; private set; }
+        public DateTime Date { get; private set; }
+        public string Description { get; private set; }
+
+        public UserEvent(string title, DateTime date, string description) {
+
+            Title = title;
+            Date = date;
+            Description = description;
+        }
+    }
+}
