31683f365fb3b39890a4f7e4bb07d4871980a3a8
YI ZHANG <scaccterran@gmail.com>
2017-11-30 00:48:51 -0500

    all done

--- challenge_003/intermediate/ciphers/ciphers/Program.cs
+++ challenge_003/intermediate/ciphers/ciphers/Program.cs
@@ -10,8 +10,8 @@ namespace ciphers {
         static void Main(string[] args) {
 
             string toEncode = "Welcome to cipher day! Create a program that can take a piece of text and encrypt it with an alphabetical substitution cipher. This can ignore white space, numbers, and symbols. for extra credit, make it encrypt whitespace, numbers, and symbols! for extra extra credit, decode someone elses cipher!";
-            string toDecode = "Zhofrph wr flskhu gdb! Fuhdwh d surjudp wkdw fdq wdnh d slhfh ri whaw dqg hqfubsw lw zlwk dq doskdehwlfdo vxevwlwxwlrq flskhu. Wklv fdq ljqruh zklwh vsdfh, qxpehuv, dqg vbperov. iru hawud fuhglw, pdnh lw hqfubsw zklwhvsdfh, qxpehuv, dqg vbperov! iru hawud hawud fuhglw, ghfrgh vrphrqh hovhv flskhu!";
-            //challenge input
+            string toDecode = "Zhofrph#wr#flskhu#gdb$#Fuhdwh#d#surjudp#wkdw#fdq#wdnh#d#slhfh#ri#whaw#dqg#hqfubsw#lw#zlwk#dq#doskdehwlfdo#vxevwlwxwlrq#flskhu1#Wklv#fdq#ljqruh#zklwh#vsdfh/#qxpehuv/#dqg#vbperov1#iru#hawud#fuhglw/#pdnh#lw#hqfubsw#zklwhvsdfh/#qxpehuv/#dqg#vbperov$#iru#hawud#hawud#fuhglw/#ghfrgh#vrphrqh#hovhv#flskhu$";
+            //challenge & bonus input
             Console.WriteLine(Encode(toEncode, 3) + "\n");
             Console.WriteLine(Decode(toDecode, 3));
         }
@@ -29,52 +29,53 @@ namespace ciphers {
             return Char.ConvertFromUtf32(baseCode + (charCode - baseCode + key) % 26)[0];
         }
 
+        private static char ShiftCharacter(char character, int key) { 
+        
+            int[] forbidCodes = new int[] { 65, 97, 128 };
+            int[] validCodes = new int[] { 91, 123, 0 };
+            int charCode = Char.ConvertToUtf32(character.ToString(), 0);
+
+            for(int i = 0; i < 3; i++) {
+
+                if(charCode < forbidCodes[i] && charCode + key >= forbidCodes[i]) {
+
+                    key -= forbidCodes[i] - charCode;
+                    charCode = validCodes[i];
+                    i = -1;
+                }
+            }
+
+            return Char.ConvertFromUtf32(charCode + key)[0];
+        }
+
         private static string Encode(string text, int key) {
 
             return Regex.Replace(text, ".", match => {
 
-                char character = match.Value[0];
+                if(Char.IsLetter(match.Value[0])) {
 
-                return Char.IsLetter(character) ? ShiftLetter(character, key).ToString() : ShiftCharacter(character, key).ToString();
+                    return ShiftLetter(match.Value[0], key).ToString();
+                }
+                else {
+
+                    return ShiftCharacter(match.Value[0], key).ToString();
+                }
             });
         }
 
         private static string Decode(string text, int key) {
 
-            return Encode(text, 26 - key);
-        }
+            return Regex.Replace(text, ".", match => {
 
-        private static char ShiftCharacter(char character, int key) { 
-        
-            int charCode = Char.ConvertToUtf32(character.ToString(), 0);
+                if(Char.IsLetter(match.Value[0])) {
 
-            while(key != 0) {
-            
-                if(charCode + key > 127) {
-
-                    key -= 127 - charCode + 1;
-                    charCode = 0;
-                    continue;
+                    return ShiftLetter(match.Value[0], 26 - key).ToString();
                 }
+                else {
 
-                if(charCode + key > 64) {
-
-                    key -= 64 - charCode + 1;
-                    charCode = 91;
-                    continue;
+                    return ShiftCharacter(match.Value[0], 76 - key).ToString();
                 }
-
-                if(charCode + key > 96) {
-
-                    key -= 96 - charCode + 1;
-                    charCode = 123;
-                    continue;
-                }
-
-                charCode += key;
-            }
-
-            return Char.ConvertFromUtf32(charCode)[0];
+            });
         }
     }
 }
