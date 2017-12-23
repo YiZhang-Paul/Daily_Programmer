79d14873a5eb5bbe72a7e50d9d21fba9c3835753
YI ZHANG <scaccterran@gmail.com>
2017-12-23 16:42:55 -0500

    number to English translator

---
+++ challenge_008/intermediate/printNumberInEnglish/printNumberInEnglish/NumberToEnglishConverter.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace printNumberInEnglish {
+    class NumberToEnglishConverter {
+
+        private Dictionary<int, string> Translation { get; set; }
+
+        public NumberToEnglishConverter(string[] translations) {
+
+            Translation = GetTranslationTable(translations);
+            var that = ToEnglish(20125);
+        }
+
+        private Dictionary<int, string> GetTranslationTable(string[] translations) {
+
+            var table = new Dictionary<int, string>();
+
+            foreach(string translation in translations) {
+
+                if(Regex.IsMatch(translation, @"\d")) {
+
+                    int value = int.Parse(Regex.Match(translation, @"\d+").Value);
+
+                    if(!table.ContainsKey(value)) {
+
+                        table.Add(value, Regex.Match(translation, @"[^\d\s]+").Value);
+                    }
+                }
+            }
+
+            return table;
+        }
+
+        private int[] DecomposeNumber(int number) { 
+        
+            var decomposed = new List<int>();
+            string numberString = string.Join("", number.ToString().Reverse());
+
+            for(int i = 0; i < numberString.Length; i++) {
+
+                int digit = int.Parse(numberString[i].ToString());
+
+                if(digit != 0) {
+
+                    decomposed.Add(digit * (int)Math.Pow(10, i));
+                }
+            }
+
+            decomposed.Reverse();
+
+            return decomposed.ToArray();
+        }
+
+        private string GetTranslation(int number) { 
+        
+            if(Translation.ContainsKey(number)) {
+            
+                return Translation[number];
+            }
+
+            int leadDigit = int.Parse(number.ToString()[0].ToString());
+            
+            return Translation[leadDigit] + "-" + Translation[number / leadDigit];
+        }
+
+        public string ToEnglish(int number) {
+
+            var translation = DecomposeNumber(number).Select(GetTranslation);
+
+            return string.Join(", ", translation);
+        }
+    }
+}
---
+++ challenge_008/intermediate/printNumberInEnglish/printNumberInEnglish/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace printNumberInEnglish {
+    class Program {
+        static void Main(string[] args) {
+
+            string[] translations = ReadFile("translations.txt");
+            var toEnglishConverter = new NumberToEnglishConverter(translations);
+        }
+
+        private static string[] ReadFile(string fileName) {
+
+            try {
+
+                return File.ReadAllLines(fileName);
+            }
+            catch(Exception exception) {
+
+                Console.WriteLine("File not Found.");
+                Console.WriteLine(exception.Message);
+                throw exception;
+            }
+        }
+    }
+}
