accc9b0624ca3cfaa75093ecfa58663e079a47fb
YI ZHANG <scaccterran@gmail.com>
2017-12-27 19:25:39 -0500

    all done

---
+++ challenge_013/intermediate/reverseText/reverseText/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace reverseText {
+    class Program {
+        static void Main(string[] args) {
+
+            //challenge input
+            WriteReverseText("hello!");
+        }
+
+        private static string ReverseText(string text) {
+
+            return string.Join("", text.Reverse());
+        }
+
+        private static void WriteReverseText(string text, string fileName = "output.txt") {
+
+            try {
+
+                File.WriteAllText(fileName, ReverseText(text));
+            }
+            catch(Exception exception) {
+
+                Console.WriteLine(exception.Message);
+                throw exception;
+            }
+        }
+    }
+}
