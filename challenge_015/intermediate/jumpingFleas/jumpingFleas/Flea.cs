03c5993dfecfd76342b8724c37ddcabf016ef8ba
YI ZHANG <scaccterran@gmail.com>
2017-12-28 21:15:03 -0500

    all done

---
+++ challenge_015/intermediate/jumpingFleas/jumpingFleas/Flea.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jumpingFleas {
+    class Flea {
+
+        private Random Random { get; set; }
+
+        public Tuple<int, int> Location { get; private set; }
+
+        public Flea(int row, int column, Random random) {
+
+            Location = new Tuple<int, int>(row, column);
+            Random = random;
+        }
+
+        private Tuple<int, int>[] GetDestinations(int maxRow, int maxColumn) {
+
+            int row = Location.Item1;
+            int column = Location.Item2;
+            var destinations = new List<Tuple<int, int>>();
+
+            if(row > 0) destinations.Add(new Tuple<int, int>(row - 1, column));
+            if(row < maxRow - 1) destinations.Add(new Tuple<int, int>(row + 1, column));
+            if(column > 0) destinations.Add(new Tuple<int, int>(row, column - 1));
+            if(column < maxColumn - 1) destinations.Add(new Tuple<int, int>(row, column + 1));
+
+            return destinations.ToArray();
+        }
+
+        public void Jump(int maxRow, int maxColumn) {
+
+            var destinations = GetDestinations(maxRow, maxColumn);
+            Location = destinations[Random.Next(0, destinations.Length)];
+        }
+    }
+}
---
+++ challenge_015/intermediate/jumpingFleas/jumpingFleas/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jumpingFleas {
+    class Program {
+        static void Main(string[] args) {
+
+            //challenge input
+            Console.WriteLine(CountAverageUnoccupiedGrids(100, 50, 30, 30));
+        }
+
+        private static Flea[] PutFleas(int row, int column, Random random) { 
+        
+            var fleas = new List<Flea>();
+
+            for(int i = 0; i < row; i++) {
+
+                for(int j = 0; j < column; j++) {
+
+                    fleas.Add(new Flea(i, j, random));
+                }
+            }
+
+            return fleas.ToArray();
+        }
+
+        private static void FleasJump(Flea[] fleas, int row, int column) { 
+        
+            foreach(var flea in fleas) {
+
+                flea.Jump(row, column);
+            }
+        }
+
+        private static int CountEmptyGrids(Flea[] fleas, int row, int column) {
+
+            var occupied = new HashSet<string>();
+
+            foreach(var flea in fleas) {
+
+                occupied.Add(flea.Location.Item1 + "," + flea.Location.Item2);
+            }
+
+            return row * column - occupied.Count;
+        }
+
+        private static double CountAverageUnoccupiedGrids(int simulations, int bellRings, int row, int column) {
+
+            var unoccupied = 0;
+            var random = new Random();
+
+            for(int i = 0; i < simulations; i++) {
+
+                var fleas = PutFleas(row, column, random);
+
+                for(int j = 0; j < bellRings; j++) {
+
+                    FleasJump(fleas, row, column);
+                }
+
+                unoccupied += CountEmptyGrids(fleas, row, column);
+            }
+
+            return (double)unoccupied / simulations;
+        }
+    }
+}
