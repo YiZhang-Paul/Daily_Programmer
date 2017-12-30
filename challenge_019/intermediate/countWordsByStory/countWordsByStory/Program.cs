1d987acb8b45bc9a125db2b95f993b3cc1d9509c
YI ZHANG <scaccterran@gmail.com>
2017-12-29 23:53:37 -0500

    retrieve story titles

---
+++ challenge_019/intermediate/countWordsByStory/countWordsByStory/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace countWordsByStory {
+    class Program {
+        static void Main(string[] args) {
+
+            string text = ReadText("book.txt");
+            string[] titles = GetTitles(text);
+        }
+
+        private static string ReadText(string fileName) {
+
+            try {
+
+                return File.ReadAllText(fileName);
+            }
+            catch(Exception exception) {
+
+                Console.WriteLine("File not Found.");
+                Console.WriteLine(exception.Message);
+                throw exception;
+            }
+        }
+
+        private static string[] GetTitles(string text) {
+
+            string[] chapters = { 
+
+                "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII" 
+            };
+
+            return Regex.Match(text, string.Join(@"\.[^\n]+\n\s*", chapters) + @"\.[^\n]+\n")
+                        .Value
+                        .Split('\r')
+                        .Select(line => line.Trim())
+                        .Where(line => line != "")
+                        .ToArray();
+        }
+    }
+}
