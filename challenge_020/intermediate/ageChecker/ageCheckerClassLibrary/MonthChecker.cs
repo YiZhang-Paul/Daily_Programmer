621aaf9e5d7eeb92597b644ef0d9a02b7e13fd2e
YI ZHANG <scaccterran@gmail.com>
2017-12-31 23:01:50 -0500

    month checker

---
+++ challenge_020/intermediate/ageChecker/ageCheckerClassLibrary/MonthChecker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ageCheckerClassLibrary {
+    public class MonthChecker {
+
+        private Dictionary<int, int> _daysInMonth = new Dictionary<int, int>() { 
+                                     
+            { 1, 31 },
+            { 3, 31 },
+            { 4, 30 },
+            { 5, 31 },
+            { 6, 30 },
+            { 7, 31 },
+            { 8, 31 },
+            { 9, 30 },
+            { 10, 31 },
+            { 11, 30 },
+            { 12, 31 }
+        };
+
+        private bool IsLeapYear(int year) { 
+        
+            if(year % 4 != 0) {
+
+                return false;
+            }
+
+            return year % 100 != 0 || year % 400 == 0;
+        }
+
+        public int GetDaysInMonth(int month, int year) {
+
+            if(month == 2) {
+
+                return IsLeapYear(year) ? 29 : 28;
+            }
+
+            return _daysInMonth[month];
+        }
+    }
+}
---
+++ challenge_020/intermediate/ageChecker/ageCheckerTests/MonthCheckerTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ageCheckerClassLibrary;
+
+namespace ageCheckerTests {
+    [TestClass]
+    public class MonthCheckerTest {
+
+        MonthChecker checker;
+
+        [TestInitialize]
+        public void Setup() {
+
+            checker = new MonthChecker();
+        }
+
+        [TestMethod]
+        public void FebruaryDaysInLeapYear() {
+
+            int days = checker.GetDaysInMonth(2, 2016);
+
+            Assert.AreEqual(29, days);
+        }
+
+        [TestMethod]
+        public void FebruaryDaysInNonLeapYear() {
+
+            int days = checker.GetDaysInMonth(2, 2017);
+
+            Assert.AreEqual(28, days);
+        }
+
+        [TestMethod]
+        public void DaysInMonthOtherThanFebruary() {
+
+            int days = checker.GetDaysInMonth(1, 2017);
+
+            Assert.AreEqual(31, days);
+        }
+    }
+}
