0a4fe622ad61ec76b32fea81097fa33304d6c79d
YI ZHANG <scaccterran@gmail.com>
2018-01-09 23:56:39 -0500

    find letters in sentence

---
+++ challenge_346/easy/cryptarithmeticSolver/CryptarithmeticSolverClassLibrary/Utility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace CryptarithmeticSolverClassLibrary {
+    public class Utility {
+
+        public char[] GetLetters(string input) {
+
+            input = Regex.Replace(input.ToLower(), @"\s", "");
+
+            return new HashSet<char>(input).ToArray();
+        }
+    }
+}
---
+++ challenge_346/easy/cryptarithmeticSolver/CryptarithmeticSolverTest/UtilityTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using CryptarithmeticSolverClassLibrary;
+
+namespace CryptarithmeticSolverTest {
+    [TestClass]
+    public class UtilityTest {
+
+        Utility utility;
+
+        [TestInitialize]
+        public void Setup() {
+
+            utility = new Utility();
+        }
+
+        [TestMethod]
+        public void GetLettersFromEmptyString() {
+
+            Assert.AreEqual(0, utility.GetLetters(string.Empty).Length);
+            Assert.AreEqual(0, utility.GetLetters("".PadLeft(10, " "[0])).Length);
+        }
+
+        [TestMethod]
+        public void GetLettersFromStringWithSameCasing() {
+
+            char[] letters = utility.GetLetters("out of ruff");
+
+            Assert.AreEqual(5, letters.Length);
+            Assert.IsTrue(letters.Contains('o'));
+            Assert.IsTrue(letters.Contains('u'));
+            Assert.IsTrue(letters.Contains('t'));
+            Assert.IsTrue(letters.Contains('r'));
+            Assert.IsTrue(letters.Contains('f'));
+        }
+
+        [TestMethod]
+        public void GetLettersFromStringWithDifferentCasing() {
+
+            char[] letters = utility.GetLetters("oUt Of ruFf");
+
+            Assert.AreEqual(5, letters.Length);
+            Assert.IsTrue(letters.Contains('o'));
+            Assert.IsTrue(letters.Contains('u'));
+            Assert.IsTrue(letters.Contains('t'));
+            Assert.IsTrue(letters.Contains('r'));
+            Assert.IsTrue(letters.Contains('f'));
+        }
+    }
+}
