caecc8e23268a8d686e44d6a5f5a792400c2e1c5
YI ZHANG <scaccterran@gmail.com>
2018-01-14 03:11:26 -0500

    fermat's primality tester

---
+++ challenge_346/intermediate/fermatLittleTheorem/PrimalityTesterClassLibrary/FermatPrimalityTester.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace PrimalityTesterClassLibrary {
+    public class FermatPrimalityTester : IBigIntegerPrimalityTester {
+
+        private Random _random = new Random();
+
+        private IPrimalityTester BasicTester { get; set; }
+
+        public FermatPrimalityTester(IPrimalityTester basicTester) {
+
+            BasicTester = basicTester;
+        }
+
+        private List<int> GetTestNumbers(BigInteger number) {
+
+            int maxNumber = 0;
+
+            try {
+
+                maxNumber = (int)number - 1;
+            }
+            catch(Exception) {
+
+                maxNumber = 1000000;
+            }
+
+            return Enumerable.Range(1, maxNumber).ToList();
+        }
+
+        private int PickTestNumber(List<int> numbers) {
+
+            int index = _random.Next(0, numbers.Count);
+            int number = numbers[index];
+            numbers.RemoveAt(index);
+
+            return number;
+        }
+
+        private double GetProbability(int passes) {
+
+            return 1 - Math.Pow(2, -passes);
+        }
+
+        private bool IsCongruent(int testNumber, BigInteger number) {
+
+            return BigInteger.ModPow(testNumber, number, number).Equals(testNumber);
+        }
+
+        public bool IsPrime(BigInteger number, double certainty) { 
+        
+            if(number <= 100) {
+
+                return BasicTester.IsPrime((int)number);
+            }
+
+            var testNumbers = GetTestNumbers(number);
+            int testPasses = 0;
+
+            while(GetProbability(testPasses) <= certainty && testNumbers.Count > 0) {
+            
+                if(!IsCongruent(PickTestNumber(testNumbers), number)) {
+
+                    return false;
+                }
+
+                testPasses++;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ challenge_346/intermediate/fermatLittleTheorem/PrimalityTesterTest/FermatPrimalityTesterTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrimalityTesterClassLibrary;
+using Moq;
+
+namespace PrimalityTesterTest {
+    [TestClass]
+    public class FermatPrimalityTesterTest {
+
+        Mock<IPrimalityTester> basicTester;
+        FermatPrimalityTester fermatTester;
+
+        [TestInitialize]
+        public void Setup() {
+
+            basicTester = new Mock<IPrimalityTester>();
+            fermatTester = new FermatPrimalityTester(basicTester.Object);
+        }
+
+        [TestMethod]
+        public void IsNotPrime() {
+
+            basicTester.Setup(mock => mock.IsPrime(It.IsAny<int>()))
+                       .Returns(false);
+
+            Assert.IsFalse(fermatTester.IsPrime(4, 0.75));
+            Assert.IsFalse(fermatTester.IsPrime(4, 0.99));
+            Assert.IsFalse(fermatTester.IsPrime(15, 0.75));
+            Assert.IsFalse(fermatTester.IsPrime(15, 0.99));
+            Assert.IsFalse(fermatTester.IsPrime(99, 0.75));
+            Assert.IsFalse(fermatTester.IsPrime(99, 0.99));
+            var bigInteger = BigInteger.Parse("95647806479275528135733781266203904794419584591201");
+            Assert.IsFalse(fermatTester.IsPrime(bigInteger, 0.75));
+            Assert.IsFalse(fermatTester.IsPrime(bigInteger, 0.99));
+        }
+
+        [TestMethod]
+        public void IsPrime() {
+
+            basicTester.Setup(mock => mock.IsPrime(It.IsAny<int>()))
+                       .Returns(true);
+
+            Assert.IsTrue(fermatTester.IsPrime(2, 0.75));
+            Assert.IsTrue(fermatTester.IsPrime(2, 0.99));
+            Assert.IsTrue(fermatTester.IsPrime(3, 0.75));
+            Assert.IsTrue(fermatTester.IsPrime(3, 0.99));
+            Assert.IsTrue(fermatTester.IsPrime(5, 0.75));
+            Assert.IsTrue(fermatTester.IsPrime(5, 0.99));
+            var bigInteger = BigInteger.Parse("95647806479275528135733781266203904794419563064407");
+            Assert.IsTrue(fermatTester.IsPrime(bigInteger, 0.75));
+            Assert.IsTrue(fermatTester.IsPrime(bigInteger, 0.99));
+        }
+    }
+}
