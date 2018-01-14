d366c797b09a8db851ad2af2e1f79a251c9dd342
YI ZHANG <scaccterran@gmail.com>
2018-01-13 23:42:39 -0500

    base challenge

---
+++ challenge_346/intermediate/fermatTheorem/FermatPrimalityTesterClassLibrary/FermatPrimalityTester.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FermatPrimalityTesterClassLibrary {
+    public class FermatPrimalityTester {
+
+        private Random _random = new Random();
+
+        private int GetRandomNumber(List<int> numbers) {
+
+            int index = _random.Next(0, numbers.Count);
+            int number = numbers[index];
+            numbers.RemoveAt(index);
+
+            return number;
+        }
+
+        private double GetProbability(int passes) { 
+        
+            return 1 - Math.Pow(2, -passes);
+        }
+
+        public bool IsPrime(int number, double certainty) {
+
+            var choices = Enumerable.Range(1, number - 1).ToList();
+            int passes = 0;
+
+            while(GetProbability(passes) <= certainty) {
+            
+                int choice = GetRandomNumber(choices);
+
+                if(Math.Pow(choice, number) % number != choice) {
+                
+                    return false;
+                }
+
+                passes++;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ challenge_346/intermediate/fermatTheorem/FermatPrimalityTesterTest/FermatPrimalityTesterTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FermatPrimalityTesterClassLibrary;
+
+namespace FermatPrimalityTesterTest {
+    [TestClass]
+    public class FermatPrimalityTesterTest {
+
+        FermatPrimalityTester tester;
+
+        [TestInitialize]
+        public void Setup() {
+
+            tester = new FermatPrimalityTester();
+        }
+
+        [TestMethod]
+        public void IsNotPrime() {
+
+            Assert.IsFalse(tester.IsPrime(4, 0.9));
+            Assert.IsFalse(tester.IsPrime(4, 0.75));
+            Assert.IsFalse(tester.IsPrime(15, 0.9));
+            Assert.IsFalse(tester.IsPrime(15, 0.75));
+            Assert.IsFalse(tester.IsPrime(99, 0.9));
+            Assert.IsFalse(tester.IsPrime(99, 0.75));
+        }
+
+        [TestMethod]
+        public void IsPrime() {
+
+            Assert.IsTrue(tester.IsPrime(2, 0.9));
+            Assert.IsTrue(tester.IsPrime(2, 0.75));
+            Assert.IsTrue(tester.IsPrime(3, 0.9));
+            Assert.IsTrue(tester.IsPrime(3, 0.75));
+            Assert.IsTrue(tester.IsPrime(5, 0.9));
+            Assert.IsTrue(tester.IsPrime(5, 0.75));
+        }
+    }
+}
