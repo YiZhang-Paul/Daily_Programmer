6020f7b857e59ea4c7734dc620e29730d62be9b5
YI ZHANG <scaccterran@gmail.com>
2018-01-14 02:44:44 -0500

    basic primality tester

---
+++ challenge_346/intermediate/fermatLittleTheorem/PrimalityTesterClassLibrary/PrimalityTester.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimalityTesterClassLibrary {
+    public class PrimalityTester : IPrimalityTester {
+
+        private IEnumerable<int> GetFactors(int number) {
+
+            return Enumerable.Range(2, Math.Max(0, number - 2));
+        }
+
+        public bool IsPrime(int number) {
+
+            if(number <= 2) {
+
+                return number == 2;
+            }
+
+            return GetFactors(number).All(factor => number % factor != 0);
+        }
+    }
+}
---
+++ challenge_346/intermediate/fermatLittleTheorem/PrimalityTesterTest/PrimalityTesterTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrimalityTesterClassLibrary;
+
+namespace PrimalityTesterTest {
+    [TestClass]
+    public class PrimalityTesterTest {
+
+        PrimalityTester tester;
+
+        [TestInitialize]
+        public void Setup() {
+
+            tester = new PrimalityTester();
+        }
+
+        [TestMethod]
+        public void IsNotPrime() {
+
+            Assert.IsFalse(tester.IsPrime(1));
+            Assert.IsFalse(tester.IsPrime(4));
+            Assert.IsFalse(tester.IsPrime(15));
+            Assert.IsFalse(tester.IsPrime(99));
+            Assert.IsFalse(tester.IsPrime(150));
+            Assert.IsFalse(tester.IsPrime(212));
+        }
+
+        [TestMethod]
+        public void IsPrime() {
+
+            Assert.IsTrue(tester.IsPrime(2));
+            Assert.IsTrue(tester.IsPrime(3));
+            Assert.IsTrue(tester.IsPrime(5));
+            Assert.IsTrue(tester.IsPrime(7));
+            Assert.IsTrue(tester.IsPrime(97));
+            Assert.IsTrue(tester.IsPrime(113));
+        }
+    }
+}
