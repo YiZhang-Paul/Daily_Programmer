5b5f1af748fb5d740e0e8adfc59e00736112dcc4
YI ZHANG <scaccterran@gmail.com>
2018-01-17 10:09:15 -0500

    new solution for base challenge

--- challenge_305/intermediate/bestConjunction.js
+++ challenge_305/intermediate/bestConjunction.js
@@ -1,122 +1,137 @@
 /* jslint esversion: 6 */
 (() => {
-  document.addEventListener("DOMContentLoaded", () => {
-  	/**
-  	 * retrieve word list
-  	 * @param String
-  	 *
-  	 * url : URL of word list file
-  	 *
-  	 * returns array []
-  	 */
-  	function getWordList(url) {
-  		return new Promise((resolve, reject) => {
-  			let xhttp = window.XMLHttpRequest ? new window.XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
-  			xhttp.onreadystatechange = function() {
-  				if(this.readyState == 4 && this.status == 200) {
-  					resolve(this.responseText.split("\n").map(word => word.trim()).filter(word => word && !/\W/.test(word)));
-  				}
-  			};
-  			xhttp.open("GET", url, true);
-  			xhttp.send();
-  		});
-  	} 
-  	/**
-  	 * slice a string in all possible ways
-  	 * @param int, String, int, int, array []
-  	 *
-  	 * minSize  : minimum length of word slice
-  	 * string   : string to be sliced
-  	 * share    : number of letters can be shared be between sub-words
-  	 * curLen   : current total length of slices
-  	 * curSlice : current slice pattern
-  	 *
-  	 * returns array []
-  	 */
-  	function sliceString(minSize, string, share, curLen = share, curSlice = []) {
-  		if(curLen == string.length || string.length - curLen + share < minSize) {
-  			return curLen == string.length ? curSlice : null;
-  		}
-  		let slices = [];
-  		for(let i = minSize; i <= string.length - curLen + share; i++) {
-  			let result = sliceString(minSize, string, share, curLen + i - share, [...curSlice, string.slice(curLen - share, curLen + i - share)]);
-  			if(result) {
-  				if(Array.isArray(result[0])) {
-  					slices.push(...result);
-  				} else {
-  					slices.push(result);
-  				}
-  			}
-  		}
-  		return slices;	
-  	}
-  	/**
-  	 * check if all slices of a word 
-  	 * can be found in the dictionary
-  	 * @param array [], obj {}
-  	 *
-  	 * slices     : word slices
-  	 * dictionary : dictionary of all words
-  	 *
-  	 * returns boolean
-  	 */ 
-  	function isValidSlice(slices, dictionary) {
-  		return !slices.some(slice => !dictionary.has(slice));
-  	} 
-  	/**
-  	 * find maximum conjunction for a given word
-  	 * @param int, String, obj {}, int
-  	 *
-  	 * minSize    : minimum length of sub-words
-  	 * word       : word to be checked
-  	 * dictionary : dictionary of all words
-  	 * share      : number of letters can be shared be between sub-words
-  	 *
-  	 * returns array [] 
-  	 */
-  	function maxConjunction(minSize, word, dictionary, share) {
-  		return sliceString(minSize, word, share)
-  					   .sort((a, b) => b.length - a.length)
-  		  		   .find(slices => isValidSlice(slices, dictionary)) || [];
-  	} 
-  	/**
-  	 * find best conjunction from a dictionary
-  	 * @param int, array [], int
-  	 *
-  	 * minSize : minimum length of sub-words
-  	 * list    : list of all words
-  	 * share   : number of letters can be shared be between sub-words
-  	 *
-  	 * returns array []
-  	 */
-  	function bestConjunction(minSize, list, share = 0) {
-  		let descList = list.filter(word => word.length >= minSize).sort((a, b) => b.length - a.length);
-  		let dictionary = new Set(descList);
-  		let best = [];
-  		for(let i = 0; i < descList.length; i++) {
-  			if(Math.floor(descList[i].length / minSize) > best.length) {
-  				let conjunction = maxConjunction(minSize, descList[i], dictionary, share);
-  				best = conjunction.length > best.length ? conjunction : best; 
-  			}
-  		}
-  		return best;
-  	} 
-  	//challenge & bonus input
- 		getWordList("wordList.txt").then(list => {
- 			let displayResult = (minSize, share = 0) => {
- 				let time = new Date().getTime();
- 				let result = bestConjunction(minSize, list, share);
- 				console.log(`minSize %c${minSize}%c: ${result.join("")} %c(${result.length}: ${result.join(", ")})`, "color : red;", "", "color : yellow;");
- 				console.log(`Time Spent: %c${new Date().getTime() - time}ms`, "color : red;");
- 			};
- 			console.log(`%cChallenge Input: `, "color : red;");
- 			for(let i = 2; i <= 10; i++) {
- 				displayResult(i);
- 			}
- 			console.log(`%cBonus Input: `, "color : red;");
- 			for(let i = 3; i <= 10; i++) {
- 				displayResult(i, 1);
- 			}
- 		});	
-  });
-})();  	
+  	document.addEventListener("DOMContentLoaded", () => {
+
+		function formatWords(words) {
+
+			return words.split("\n")
+						.map(word => word.trim())
+						.filter(word => /\w/.test(word));
+		}
+
+		function getWords(url) {
+
+			return new Promise((resolve, reject) => {
+
+				let xhttp = window.XMLHttpRequest ?
+					new window.XMLHttpRequest() :
+					new ActiveXObject("Microsoft.XMLHTTP");
+
+				xhttp.onreadystatechange = function() {
+
+					if(this.readyState === 4 && this.status === 200) {
+
+						let words = formatWords(this.responseText);
+						resolve(new Set(words));
+					}
+				};
+
+				xhttp.open("GET", url, true);
+				xhttp.send();
+			});
+		}
+
+		function addSubWords(word, minSize, allWords, subWords) {
+
+			for(let i = minSize; i <= word.length; i++) {
+
+				const subWord = word.slice(0, i);
+
+				if(allWords.has(subWord)) {
+
+					subWords.add(subWord);
+				}
+			}
+		}
+
+		function findSubWords(word, minSize, allWords) {
+
+			let subWords = new Set();
+
+			for(let i = 0; i <= word.length - minSize; i++) {
+
+				addSubWords(word.slice(i), minSize, allWords, subWords);
+			}
+
+			return [...subWords];
+		}
+
+		function hasSubWord(word, subWords) {
+
+			return subWords.some(subWord => word.includes(subWord));
+		}
+
+		function removeSubWord(word, subWord) {
+
+			return word.replace(subWord, "");
+		}
+
+		function excludeElement(array, index) {
+
+			return [...array.slice(0, index), ...array.slice(index + 1)];
+		}
+
+		function findLongestArray(arrays) {
+
+			return arrays.sort((a, b) => b.length - a.length)[0];
+		}
+
+		function findBestConjunction(word, minSize, allWords) {
+
+			let subWords = findSubWords(word, minSize, allWords);
+
+			return findLongestArray(findConjunctions(word, subWords));
+		}
+
+		function findConjunctions(word, subWords, current = [], conjunctions = []) {
+
+			if(!hasSubWord(word, subWords)) {
+
+				if(word.length === 0) {
+
+					conjunctions.push(current);
+				}
+
+				return null;
+			}
+
+			for(let i = 0; i < subWords.length; i++) {
+
+				if(word.indexOf(subWords[i]) !== 0) {
+
+					continue;
+				}
+
+				const otherLetter = removeSubWord(word, subWords[i]);
+				let otherSubWord = excludeElement(subWords, i);
+				let newCurrent = [...current, subWords[i]];
+				findConjunctions(otherLetter, otherSubWord, newCurrent, conjunctions);
+			}
+
+			return conjunctions;
+		}
+
+		function showResult(word, minSize, result) {
+
+			console.log(`%cMinimum Size ${minSize}: ${word} (${result.length} words: ${result.join(", ")})`, "color : yellow;");
+		}
+
+		//challenge & bonus input
+		getWords("wordList.txt").then(words => {
+
+			//default input
+			let time = new Date().getTime();
+			console.log(`%cDefault Input: `, "color : red;");
+			showResult("something", 2, findBestConjunction("something", 2, words));
+			showResult("awesomeness", 2, findBestConjunction("awesomeness", 2, words));
+
+			//challenge input
+			console.log(`%cChallenge Input: `, "color : red;");
+			showResult("disproportionateness", 3, findBestConjunction("disproportionateness", 3, words));
+			showResult("dishonorableness", 4, findBestConjunction("dishonorableness", 4, words));
+
+			console.log(`%cBonus Input: `, "color : red;");
+			console.log(`Time Spent: ${new Date().getTime() - time}ms`);
+		});
+  	});
+})();
