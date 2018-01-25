191f2a8199fb68eb499bd83a0ecaf026d90b53e9
YI ZHANG <scaccterran@gmail.com>
2018-01-24 21:55:46 -0500

    all done

--- challenge_348/easy/rabbitProblem.js
+++ challenge_348/easy/rabbitProblem.js
@@ -1,24 +1,27 @@
 class RabbitTracker {
     constructor(males, females) {
         this._totalBorned = 0;
-        this.maleTracker = this.createTracker(males);
-        this.femaleTracker = this.createTracker(females);
-        console.log(this.totalAlive);
+        this._maleTracker = this.createTracker(males);
+        this._femaleTracker = this.createTracker(females);
+        this._totalBorned += males + females;
     }
     get totalBorned() {
         return this._totalBorned;
     }
     get totalAlive() {
         let alive = 0;
-        [this.maleTracker, this.femaleTracker].forEach(tracker => {
+        [this._maleTracker, this._femaleTracker].forEach(tracker => {
             tracker.forEach(months => {
                 alive += this.sum(months);
             });
         });
         return alive;
     }
+    get totalDeath() {
+        return this.totalBorned - this.totalAlive;
+    }
     sum(array) {
-        return array.reduce((sum, current) => sum + current, 0);
+        return array.reduce((total, current) => total + current, 0);
     }
     createTracker(rabbits) {
         let tracker = new Map();
@@ -28,6 +31,58 @@ class RabbitTracker {
         tracker.get(0)[2] = rabbits;
         return tracker;
     }
+    isFertile(month, year) {
+        return year >= 1 ? true : month >= 4;
+    }
+    getOffspring() {
+        let offspring = 0;
+        this._femaleTracker.forEach((months, year) => {
+            for (let i = 0; i < months.length; i++) {
+                if (this.isFertile(i, year)) {
+                    offspring += months[i] * 14;
+                }
+            }
+        });
+        return offspring;
+    }
+    trackOffspring() {
+        const offspring = this.getOffspring();
+        this._totalBorned += offspring;
+        this._maleTracker.get(0).unshift(offspring / 14 * 5);
+        this._femaleTracker.get(0).unshift(offspring / 14 * 9);
+    }
+    trackDeath(tracker) {
+        tracker.get(tracker.size - 1).pop();
+    }
+    trackAge() {
+        [this._maleTracker, this._femaleTracker].forEach(tracker => {
+            this.trackDeath(tracker);
+            tracker.forEach((months, year) => {
+                if (tracker.has(year + 1)) {
+                    tracker.get(year + 1).unshift(months.pop());
+                }
+            });
+        });
+    }
+    trackMonth() {
+        this.trackOffspring();
+        this.trackAge();
+    }
 }
-let tracker = new RabbitTracker(2, 4);
+function checkDominate(males, females, goal) {
+    let tracker = new RabbitTracker(males, females);
+    let months = 0;
+    while (tracker.totalAlive < goal) {
+        tracker.trackMonth();
+        months++;
+    }
+    return [months, tracker.totalDeath];
+}
+function showResult(results) {
+    console.log(`%cIt Takes %c${results[0]} %cMonths to Dominate Earth; %c${results[1]} %cRabbits Died in the Process.`, "color : violet", "color : yellow", "color : violet", "color : yellow", "color : violet");
+}
+//challenge & bonus input
+console.log(`%cChallenge & Bonus `, "color : red;");
+showResult(checkDominate(2, 4, 1000000000));
+showResult(checkDominate(2, 4, 15000000000));
 //# sourceMappingURL=rabbitProblem.js.map
--- challenge_348/easy/rabbitProblem.ts
+++ challenge_348/easy/rabbitProblem.ts
@@ -1,15 +1,14 @@
 class RabbitTracker {
 
     private _totalBorned: number = 0;
-
-    public maleTracker: Map<number, number[]>;
-    public femaleTracker: Map<number, number[]>;
+    private _maleTracker: Map<number, number[]>;
+    private _femaleTracker: Map<number, number[]>;
 
     constructor(males: number, females: number) {
 
-        this.maleTracker = this.createTracker(males);
-        this.femaleTracker = this.createTracker(females);
-        console.log(this.totalAlive);
+        this._maleTracker = this.createTracker(males);
+        this._femaleTracker = this.createTracker(females);
+        this._totalBorned += males + females;
     }
 
     get totalBorned(): number {
@@ -21,7 +20,7 @@ class RabbitTracker {
 
         let alive: number = 0;
 
-        [this.maleTracker, this.femaleTracker].forEach(tracker => {
+        [this._maleTracker, this._femaleTracker].forEach(tracker => {
 
             tracker.forEach(months => {
 
@@ -32,9 +31,14 @@ class RabbitTracker {
         return alive;
     }
 
+    get totalDeath(): number {
+
+        return this.totalBorned - this.totalAlive;
+    }
+
     sum(array: number[]): number {
 
-        return array.reduce((sum, current) => sum + current, 0);
+        return array.reduce((total, current) => total + current, 0);
     }
 
     createTracker(rabbits: number): Map<number, number[]> {
@@ -50,6 +54,93 @@ class RabbitTracker {
 
         return tracker;
     }
+
+    isFertile(month: number, year: number): boolean {
+
+        return year >= 1 ? true : month >= 4;
+    }
+
+    getOffspring(): number {
+
+        let offspring: number = 0;
+
+        this._femaleTracker.forEach((months, year) => {
+
+            for(let i = 0; i < months.length; i++) {
+
+                if(this.isFertile(i, year)) {
+
+                    offspring += months[i] * 14;
+                }
+            }
+        });
+
+        return offspring;
+    }
+
+    trackOffspring(): void {
+
+        const offspring = this.getOffspring();
+        this._totalBorned += offspring;
+        this._maleTracker.get(0).unshift(offspring / 14 * 5);
+        this._femaleTracker.get(0).unshift(offspring / 14 * 9);
+    }
+
+    trackDeath(tracker: Map<number, number[]>): void {
+
+        tracker.get(tracker.size - 1).pop();
+    }
+
+    trackAge(): void {
+
+        [this._maleTracker, this._femaleTracker].forEach(tracker => {
+
+            this.trackDeath(tracker);
+
+            tracker.forEach((months, year) => {
+
+                if(tracker.has(year + 1)) {
+
+                    tracker.get(year + 1).unshift(months.pop());
+                }
+            });
+        });
+    }
+
+    trackMonth(): void {
+
+        this.trackOffspring();
+        this.trackAge();
+    }
 }
 
-let tracker = new RabbitTracker(2, 4);
+function checkDominate(males: number, females: number, goal: number): number[] {
+
+    let tracker = new RabbitTracker(males, females);
+    let months: number = 0;
+
+    while(tracker.totalAlive < goal) {
+
+        tracker.trackMonth();
+        months++;
+    }
+
+    return [months, tracker.totalDeath];
+}
+
+function showResult(results: number[]) {
+
+    console.log(
+        `%cIt Takes %c${results[0]} %cMonths to Dominate Earth; %c${results[1]} %cRabbits Died in the Process.`,
+        "color : violet",
+        "color : yellow",
+        "color : violet",
+        "color : yellow",
+        "color : violet"
+    );
+}
+
+//challenge & bonus input
+console.log(`%cChallenge & Bonus `, "color : red;");
+showResult(checkDominate(2, 4, 1000000000));
+showResult(checkDominate(2, 4, 15000000000));
