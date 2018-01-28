105ef4b78e3c0a67a151028b52fb30c6a79c1163
YI ZHANG <scaccterran@gmail.com>
2018-01-28 18:03:25 -0500

    all done

--- challenge_030/intermediate/luhnAlgorithm.ts
+++ challenge_030/intermediate/luhnAlgorithm.ts
@@ -10,14 +10,14 @@ function sum(numbers: number[]): number {
 
 function getAlternateSum(digits: number[]): number {
 
-    let alternateDigits = "";
+    let doubledDigits = "";
 
     for(let i = 0; i < digits.length; i++) {
 
-        alternateDigits += digits[i] * (i % 2 === 0 ? 2 : 1);
+        doubledDigits += digits[i] * (i % 2 === 0 ? 2 : 1);
     }
 
-    return sum(parseDigits(alternateDigits));
+    return sum(parseDigits(doubledDigits));
 }
 
 function getCheckSum(number: string): number {
@@ -27,4 +27,13 @@ function getCheckSum(number: string): number {
     return 10 - getAlternateSum(digits) % 10;
 }
 
-console.log(getCheckSum("4992739871"));
+function isValidCheckSum(number: string): boolean {
+
+    return number.slice(-1)[0] === String(getCheckSum(number.slice(0, -1)));
+}
+
+//challenge input
+console.log(`%cChallenge Input:`, "color : red;");
+console.log(`Check Sum for ${4992739871}: %c${getCheckSum("4992739871")}`, "color : yellow;");
+console.log(`49927398716 Is Valid: %c${isValidCheckSum("49927398716")}`, "color : yellow;");
+console.log(`49927398715 Is Valid: %c${isValidCheckSum("49927398715")}`, "color : yellow;");
