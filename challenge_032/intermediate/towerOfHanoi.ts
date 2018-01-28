3494a86b24c2851f1cd07de2b397dc580491c864
YI ZHANG <scaccterran@gmail.com>
2018-01-28 15:58:40 -0500

    all done

--- challenge_032/intermediate/towerOfHanoi.ts
+++ challenge_032/intermediate/towerOfHanoi.ts
@@ -45,12 +45,12 @@ class HanoiTower {
 
         this.disks = this.getDisks(total);
         this.rods = this.getRods();
-        this.putDisks();
+        this.insertDisks();
     }
 
     get isSolved(): boolean {
 
-        return this.rods[2].size === this.disks.length;
+        return this.rods.slice(-1)[0].size === this.disks.length;
     }
 
     private getDisks(total: number): number[] {
@@ -62,7 +62,7 @@ class HanoiTower {
 
     private getRods(): Stack<number>[] {
 
-        let rods:Stack<number>[] = [];
+        let rods: Stack<number>[] = [];
 
         for(let i = 0; i < 3; i++) {
 
@@ -72,11 +72,73 @@ class HanoiTower {
         return rods;
     }
 
-    private putDisks(): void {
+    private insertDisks(): void {
 
         this.rods[0].pushList(this.disks.reverse());
     }
+
+    private findSourceAndDestination(rod1: Stack<number>, rod2: Stack<number>): Stack<number>[] {
+
+        let source: Stack<number>, destination: Stack<number>;
+
+        if(rod1.isEmpty || rod2.isEmpty) {
+
+            source = rod1.isEmpty ? rod2 : rod1;
+            destination = rod1.isEmpty ? rod1 : rod2;
+        }
+        else {
+
+            source = rod1.peek() < rod2.peek() ? rod1 : rod2;
+            destination = rod1.peek() < rod2.peek() ? rod2 : rod1;
+        }
+
+        return [source, destination];
+    }
+
+    private moveDisk(source: Stack<number>, destination: Stack<number>): void {
+
+        destination.push(source.pop());
+    }
+
+    private tryMoveDisk(rod1: Stack<number>, rod2: Stack<number>): void {
+
+        if(rod1.isEmpty && rod2.isEmpty) {
+
+            throw "Illegal Move.";
+        }
+
+        let [source, destination] = this.findSourceAndDestination(rod1, rod2);
+        this.moveDisk(source, destination);
+    }
+
+    private getAffectedRods(step: number): Stack<number>[] {
+
+        let indexes = this.disks.length % 2 === 0 ?
+            [[0, 1], [0, 2], [1, 2]][step] :
+            [[0, 2], [0, 1], [1, 2]][step];
+
+        return [this.rods[indexes[0]], this.rods[indexes[1]]];
+    }
+
+    public solve(): number {
+
+        let steps = 0;
+
+        while(!this.isSolved) {
+
+            let [rod1, rod2] = this.getAffectedRods(steps++ % 3);
+            this.tryMoveDisk(rod1, rod2);
+        }
+
+        return steps;
+    }
 }
 
-let tower = new HanoiTower(6);
-console.log(tower);
+//challenge input
+console.log(`%cChallenge Input:`, "color : red;");
+let tower = new HanoiTower(3);
+console.log(`3 Disks -> %c${tower.solve()} Steps`, "color : yellow;");
+tower = new HanoiTower(6);
+console.log(`6 Disks -> %c${tower.solve()} Steps`, "color : yellow;");
+tower = new HanoiTower(10);
+console.log(`10 Disks -> %c${tower.solve()} Steps`, "color : yellow;");
