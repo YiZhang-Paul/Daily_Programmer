fda73428b4bcaabf612d2fda320573281a6a636b
YI ZHANG <scaccterran@gmail.com>
2018-02-05 20:48:54 -0500

    get digits of a number

--- challenge_039/intermediate/kaprekarNumber/main.c
+++ challenge_039/intermediate/kaprekarNumber/main.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+int countDigits(int);
+int getDigitsOnRight(int, int);
+int getDigitsOnLeft(int, int);
 
 int main() {
 
-    printf("Hello world!\n");
+    printf("%d ", getDigitsOnRight(153, 2));
+    printf("%d", getDigitsOnLeft(157551, 3));
 
     return 0;
+}
+
+int countDigits(int number) {
+
+    return (int)log10((double)number) + 1;
+}
+
+int getDigitsOnRight(int number, int digits) {
+
+    if(digits > countDigits(number)) {
+
+        return 0;
+    }
+
+    return number % (int)(pow(10, digits) + 0.5);
+}
+
+int getDigitsOnLeft(int number, int digits) {
+
+    int length = countDigits(number);
+
+    if(digits > length) {
+
+        return 0;
+    }
+
+    return number / (int)(pow(10, length - digits) + 0.5);
 }
