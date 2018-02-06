54e5260d8a9683f4074b14631308c494b77e7ff9
YI ZHANG <scaccterran@gmail.com>
2018-02-05 21:18:14 -0500

    all done

--- challenge_039/intermediate/kaprekarNumber/main.c
+++ challenge_039/intermediate/kaprekarNumber/main.c
@@ -2,37 +2,27 @@
 #include <stdlib.h>
 #include <math.h>
 
-int countDigits(int);
-int getDigitsOnRight(int, int);
-int getDigitsOnLeft(int, int);
+int getLength(int);
+int digitsOnLeft(int, int);
+int digitsOnRight(int, int);
 int isKaprekar(int);
+void findKaprekar(int);
 
 int main() {
 
-    printf("%d", isKaprekar(9));
-    printf("%d", isKaprekar(297));
+    findKaprekar(1000);
 
     return 0;
 }
 
-int countDigits(int number) {
+int getLength(int number) {
 
     return (int)log10(number) + 1;
 }
 
-int getDigitsOnRight(int number, int digits) {
+int digitsOnLeft(int number, int digits) {
 
-    if(digits > countDigits(number)) {
-
-        return 0;
-    }
-
-    return number % (int)(pow(10, digits) + 0.5);
-}
-
-int getDigitsOnLeft(int number, int digits) {
-
-    int length = countDigits(number);
+    int length = getLength(number);
 
     if(digits > length) {
 
@@ -42,12 +32,33 @@ int getDigitsOnLeft(int number, int digits) {
     return number / (int)(pow(10, length - digits) + 0.5);
 }
 
+int digitsOnRight(int number, int digits) {
+
+    if(digits > getLength(number)) {
+
+        return 0;
+    }
+
+    return number % (int)(pow(10, digits) + 0.5);
+}
+
 int isKaprekar(int number) {
 
-    int length = countDigits(number);
+    int digits = getLength(number);
     int square = (int)(pow(number, 2) + 0.5);
-    int leftDigits = getDigitsOnLeft(square, countDigits(square) - length);
-    int rightDigits = getDigitsOnRight(square, length);
+    int leftDigits = digitsOnLeft(square, getLength(square) - digits);
+    int rightDigits = digitsOnRight(square, digits);
 
     return leftDigits + rightDigits == number;
+}
+
+void findKaprekar(int limit) {
+
+    for(int i = 1; i <= limit; i++) {
+
+        if(isKaprekar(i)) {
+
+            printf("%d\n", i);
+        }
+    }
 }
