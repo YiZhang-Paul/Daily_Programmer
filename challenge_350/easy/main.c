412082f2ac9f279a1e8d0c41e85b754a3368623f
YI ZHANG <scaccterran@gmail.com>
2018-02-07 03:36:32 -0500

    sort bookshelf

--- challenge_350/easy/main.c
+++ challenge_350/easy/main.c
@@ -6,10 +6,16 @@
 #include "bookshelf.h"
 #include "utility.h"
 
-struct bookshelf * createShelves(int * widths, int total);
-void freeShelves(struct bookshelf *, total);
+struct bookshelf * createShelves(int *, int);
+int compareShelves(const void *, const void *);
+void sortShelves(struct bookshelf *, int);
+void freeShelves(struct bookshelf *, int);
 
 int main(void) {
+
+    int widths[] = { 150, 150, 300, 150, 150 };
+    struct bookshelf * shelves = createShelves(widths, sizeof(widths) / sizeof(int));
+    sortShelves(shelves, sizeof(widths) / sizeof(int));
 
     return 0;
 }
@@ -24,6 +30,19 @@ struct bookshelf * createShelves(int * widths, int total) {
     }
 
     return shelves;
+}
+
+int compareShelves(const void * a, const void * b) {
+
+    struct bookshelf *bookshelf1 = (struct bookshelf *)a;
+    struct bookshelf *bookshelf2 = (struct bookshelf *)b;
+
+    return bookshelf2->width - bookshelf1->width;
+}
+
+void sortShelves(struct bookshelf * shelves, int total) {
+
+    qsort(shelves, total, sizeof(struct bookshelf), compareShelves);
 }
 
 void freeShelves(struct bookshelf * shelves, int total) {
