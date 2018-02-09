21c18aa9505476dc799115a98c4ac3d8e38fe4f7
YI ZHANG <scaccterran@gmail.com>
2018-02-09 06:42:35 -0500

    find best way to fill books

--- challenge_350/easy/main.c
+++ challenge_350/easy/main.c
@@ -5,6 +5,8 @@
 #include "headers/permute.h"
 #include "headers/book.h"
 
+int * solve(int *, int, int *, struct book *, int);
+
 int main(void) {
 
     char inputFile[] = "input1.txt";
@@ -20,13 +22,65 @@ int main(void) {
     createBooks(bookInformation, books, totalBooks);
 
     int *range = getRange(0, totalBooks);
-    int permutation[factorial(totalBooks)][totalBooks];
-    permute((int *)permutation, range, totalBooks);
+    int placements[factorial(totalBooks)][totalBooks];
+    permute((int *)placements, range, totalBooks);
+
+    int *result = solve(shelves, sizeof(shelves) / sizeof(int), (int *)placements, books, totalBooks);
+
+    for(int i = 0; i < totalBooks; i++) {
+
+        printf("%d ", result[i]);
+    }
 
     freeLines(shelfInformation, 1);
     freeLines(bookInformation, totalBooks);
     freeBooks(books, totalBooks);
     free(range);
+    free(result);
 
     return 0;
+}
+
+int * solve(int * shelves, int totalShelves, int * placements, struct book * books, int totalBooks) {
+
+    int *result = NULL;
+    const int totalPlacements = factorial(totalBooks);
+
+    for(int i = 0, currentMin = 0; i < totalPlacements; i++) {
+
+        int shelfIndex = 0;
+        int remainWidth = shelves[shelfIndex];
+
+        for(int j = 0; j < totalBooks; j++) {
+
+            if(remainWidth >= books[placements[i * totalBooks + j]].width) {
+
+                remainWidth -= books[placements[i * totalBooks + j]].width;
+
+                continue;
+            }
+
+            if(++shelfIndex == totalShelves) {
+
+                j = totalBooks;
+            }
+        }
+
+        if(shelfIndex + 1 < currentMin || currentMin == 0) {
+
+            currentMin = shelfIndex + 1;
+
+            if(result == NULL) {
+
+                result = (int *)malloc(totalBooks * sizeof(int));
+            }
+
+            for(int k = 0; k < totalBooks; k++) {
+
+                result[k] = placements[i * totalBooks + k];
+            }
+        }
+    }
+
+    return result;
 }
