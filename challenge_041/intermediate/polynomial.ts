ef9f967704086a74c5566183dbb9af34cf462f1b
YI ZHANG <scaccterran@gmail.com>
2018-02-13 08:42:53 -0500

    reduce terms with same degree

--- challenge_041/intermediate/polynomial.ts
+++ challenge_041/intermediate/polynomial.ts
@@ -30,6 +30,40 @@ export default class Polynomial {
         return terms.sort((a, b) => a.power - b.power);
     }
 
+    private groupTerms(terms: Term[]): Map<number, Term[]> {
+
+        let groups = new Map<number, Term[]>();
+
+        terms.forEach(term => {
+
+            if(!groups.has(term.power)) {
+
+                groups.set(term.power, new Array<Term>());
+            }
+
+            groups.get(term.power).push(term);
+        });
+
+        return groups;
+    }
+
+    private addTerms(terms: Term[]): Term {
+
+        if(terms.length === 1) {
+
+            return terms[0];
+        }
+
+        return terms.reduce((result, term) => result.add(term));
+    }
+
+    private reduceTerms(terms: Term[]): Term[] {
+
+        let groups = Array.from(this.groupTerms(terms));
+
+        return groups.map(group => this.addTerms(group[1]));
+    }
+
     public multiply(other: Polynomial): Polynomial {
 
         let terms: Term[] = [];
@@ -42,6 +76,6 @@ export default class Polynomial {
             });
         });
 
-        return new Polynomial(terms);
+        return new Polynomial(this.reduceTerms(terms));
     }
 }
