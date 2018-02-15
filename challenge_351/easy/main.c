defa03b953095ddd58515dbe819c5530f7b16869
YI ZHANG <scaccterran@gmail.com>
2018-02-15 00:05:51 -0500

    team structure

--- challenge_351/easy/main.c
+++ challenge_351/easy/main.c
@@ -3,6 +3,7 @@
 
 #define TRUE 1
 #define FALSE 0
+#define TEAM_SIZE 11
 
 struct player {
 
@@ -11,9 +12,18 @@ struct player {
     int out;
 };
 
+struct team {
+
+    struct player members[TEAM_SIZE];
+    int playerLeft;
+};
+
 struct player createPlayer(int);
+struct team createTeam(void);
 
 int main(void) {
+
+    struct team team = createTeam();
 
     return 0;
 }
@@ -27,4 +37,18 @@ struct player createPlayer(int number) {
     player.out = FALSE;
 
     return player;
+}
+
+struct team createTeam(void) {
+
+    struct team team;
+
+    for(int i = 0; i < TEAM_SIZE; i++) {
+
+        team.members[i] = createPlayer(i + 1);
+    }
+
+    team.playerLeft = TEAM_SIZE;
+
+    return team;
 }
