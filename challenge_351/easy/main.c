feaeac354cc74fb35f926ccaa984d3ccd2b6b0c9
YI ZHANG <scaccterran@gmail.com>
2018-02-14 23:59:14 -0500

    player structure

--- challenge_351/easy/main.c
+++ challenge_351/easy/main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TRUE 1
+#define FALSE 0
+
+struct player {
+
+    int number;
+    int score;
+    int out;
+};
+
+struct player createPlayer(int);
+
 int main(void) {
 
     return 0;
+}
+
+struct player createPlayer(int number) {
+
+    struct player player;
+
+    player.number = number;
+    player.score = 0;
+    player.out = FALSE;
+
+    return player;
 }
