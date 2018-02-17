8b62f03364053587b6fcd660005fadd2dc5d3505
YI ZHANG <scaccterran@gmail.com>
2018-02-17 12:46:39 -0500

    exchange method

--- challenge_351/intermediate/permutationMadness.ts
+++ challenge_351/intermediate/permutationMadness.ts
@@ -28,7 +28,7 @@ class Item {
 class IndexTracker {
 
     private _items: Item[];
-    private _tracker: Map<Item, number>;
+    private _tracker: Map<number, Item>;
 
     constructor(items: string) {
 
@@ -52,23 +52,33 @@ class IndexTracker {
 
         if(this._tracker === undefined) {
 
-            this._tracker = new Map<Item, number>();
+            this._tracker = new Map<number, Item>();
         }
 
         for(let i = 0; i < items.length; i++) {
 
-            items[i].index = i;
-            this._tracker.set(items[i], i);
+            this.changeIndex(items[i], i);
         }
+    }
+
+    private parseNumbers(input: string): number[] {
+
+        return input.match(/\d+/g).map(match => Number.parseInt(match));
+    }
+
+    private changeIndex(item: Item, index: number): void {
+
+        item.index = index;
+        this._tracker.set(index, item);
     }
 
     private spin(total: number): void {
 
         let items: Item[] = [];
 
-        this._tracker.forEach((index, item) => {
+        this._tracker.forEach((item, index) => {
 
-            items[item.index] = item;
+            items[index] = item;
         });
 
         this.setTracker([
@@ -78,6 +88,15 @@ class IndexTracker {
         ]);
     }
 
+    private exchange(index1: number, index2: number): void {
+        //must keep reference to original items before exchanging
+        let item1 = this._tracker.get(index1);
+        let item2 = this._tracker.get(index2);
+
+        this.changeIndex(item1, index2);
+        this.changeIndex(item2, index1);
+    }
+
     public processMove(move: string): void {
 
         switch(move[0]) {
@@ -85,10 +104,24 @@ class IndexTracker {
             case "s" :
 
                 this.spin(Number.parseInt(move[1]));
+
+                break;
+
+            case "x" :
+
+                let indexes = this.parseNumbers(move);
+                this.exchange(indexes[0], indexes[1]);
+
+                break;
+
+            case "p" :
+
+                break;
         }
     }
 }
 
 let tracker = new IndexTracker("abcde");
 tracker.processMove("s2");
+tracker.processMove("x3/4");
 console.log(tracker);
