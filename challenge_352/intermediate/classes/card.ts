5c0b7324e74313f62f56a77ee1f5f614e2aaa4a0
YI ZHANG <scaccterran@gmail.com>
2018-02-22 11:37:23 -0500

    card class

---
+++ challenge_352/intermediate/classes/card.ts
@@ -0,0 +1,32 @@
+export default class Card {
+
+    private _resources = new Set<string>();
+
+    constructor(resources: string) {
+
+        this.addResources(resources);
+    }
+
+    get powerLevel(): number {
+
+        return this._resources.size;
+    }
+
+    private formatResource(resource: string): string {
+
+        return resource.trim().toUpperCase();
+    }
+
+    private addResources(resources: string): void {
+
+        resources.match(/[A-Za-z]/g).forEach(symbol => {
+
+            this._resources.add(this.formatResource(symbol));
+        });
+    }
+
+    public hasResource(resource: string): boolean {
+
+        return this._resources.has(this.formatResource(resource));
+    }
+}
---
+++ challenge_352/intermediate/resourceAllocation.ts
@@ -0,0 +1,5 @@
+import Card from "classes/card";
+
+let card = new Card("W/B/S/O");
+
+console.log(card);
