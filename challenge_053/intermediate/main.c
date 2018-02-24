651c6ba071b48398564d2994d4e0e63ea4c191bf
YI ZHANG <scaccterran@gmail.com>
2018-02-24 14:27:43 -0500

    find sum of 1000 largest random numbers

--- challenge_053/intermediate/main.c
+++ challenge_053/intermediate/main.c
@@ -2,22 +2,18 @@
 #include <stdlib.h>
 #include <sys/timeb.h>
 
-#define DEFAULT_SEED 123456789
-
 int seed = 0;
 
 int getRandom(void);
 long long getTime(void);
+int compare(const void *, const void *);
+long long sum(int *, int);
+long long sum1000LargestRandomNumbers(void);
 
 int main(void) {
 
     const long long now = getTime();
-
-    for(int i = 0; i < 10000000; i++) {
-
-        getRandom();
-    }
-
+    printf("Sum of 1000 Largest Random Numbers: %lld\n", sum1000LargestRandomNumbers());
     printf("Time Spent: %lldms\n", getTime() - now);
 
     return 0;
@@ -27,7 +23,7 @@ int getRandom(void) {
 
     if(seed == 0) {
 
-        seed = DEFAULT_SEED;
+        seed = 123456789;
     }
     else {
 
@@ -43,4 +39,38 @@ long long getTime(void) {
     ftime(&now);
 
     return (long long)now.time * 1000 + now.millitm;
+}
+
+int compare(const void * a, const void * b) {
+
+    return *(int *)b - *(int *)a;
+}
+
+long long sum(int * numbers, int total) {
+
+    long long result = 0;
+
+    for(int i = 0; i < total; i++) {
+
+        result += numbers[i];
+    }
+
+    return result;
+}
+
+long long sum1000LargestRandomNumbers(void) {
+
+    const int total = 10000000;
+    int *numbers = (int *)malloc(total * sizeof(int));
+
+    for(int i = 0; i < total; i++) {
+
+        *(numbers + i) = getRandom();
+    }
+
+    qsort(numbers, total, sizeof(int), compare);
+    long long result = sum(numbers, 1000);
+    free(numbers);
+
+    return result;
 }
