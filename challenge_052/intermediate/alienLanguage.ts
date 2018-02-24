4b36ec527ac3cc73f8221b59685cdb8585a0c394
YI ZHANG <scaccterran@gmail.com>
2018-02-24 16:25:16 -0500

    trie class

---
+++ challenge_052/intermediate/alienLanguage.ts
@@ -0,0 +1,4 @@
+import Trie from "classes/trie";
+
+let trie = new Trie();
+console.log(trie);
---
+++ challenge_052/intermediate/classes/trie.ts
@@ -0,0 +1,46 @@
+class Node {
+
+    private _keys = new Map<string, Node>();
+
+    public hasKey(key: string): boolean {
+
+        return this._keys.has(key);
+    }
+
+    public addKey(key: string): void {
+
+        this._keys.set(key, new Node());
+    }
+
+    public getKey(key: string): Node {
+
+        if(!this.hasKey(key)) {
+
+            throw "Key not found.";
+        }
+
+        return this._keys.get(key);
+    }
+}
+
+export default class Trie {
+
+    private _root = new Node();
+
+    public insert(word: string): void {
+
+        let node = this._root;
+
+        for(let i = 0; i < word.length; i++) {
+
+            const key = word[i];
+
+            if(!node.hasKey(key)) {
+
+                node.addKey(key);
+            }
+
+            node = node.getKey(key);
+        }
+    }
+}
