f3b3e08c4b2f6d2cd5658ad678d9db868b2109fe
YI ZHANG <scaccterran@gmail.com>
2018-02-25 23:56:52 -0500

    custom number

--- challenge_058/intermediate/main.c
+++ challenge_058/intermediate/main.c
@@ -1,7 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct customNumber {
+
+    int *digits;
+    int length;
+};
+
+struct customNumber readCustomNumber(int);
+void printCustomNumber(struct customNumber *);
+void freeCustomNumber(struct customNumber *);
+
 int main(void) {
 
+    struct customNumber number = readCustomNumber(2133);
+
+    printCustomNumber(&number);
+
+    freeCustomNumber(&number);
+
     return 0;
+}
+
+struct customNumber readCustomNumber(int number) {
+
+    struct customNumber customNumber;
+    customNumber.digits = (int *)malloc(150 * sizeof(int));
+    customNumber.length = 0;
+
+    while(number != 0) {
+
+        customNumber.digits[customNumber.length++] = number % 10;
+        number = (number - number % 10) / 10;
+    }
+
+    return customNumber;
+}
+
+void printCustomNumber(struct customNumber * number) {
+
+    for(int i = number->length - 1; i >= 0; i--) {
+
+        printf("%d", number->digits[i]);
+    }
+}
+
+void freeCustomNumber(struct customNumber * number) {
+
+    free(number->digits);
 }
