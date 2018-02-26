192b7c62eb8d56822041fb68cc7fe3b3d88d002b
YI ZHANG <scaccterran@gmail.com>
2018-02-25 21:33:48 -0500

    all done

--- challenge_059/intermediate/nonogram.ts
+++ challenge_059/intermediate/nonogram.ts
@@ -5,74 +5,68 @@ function parseNumbers(input: string): number[] {
 
 function readMatrix(input: string): number[][] {
 
-    return input.split("\n").map(row => {
-
-        return parseNumbers(row.trim());
-    });
+    return input.split("\n").map(row => parseNumbers(row));
 }
 
-function getColumn(matrix: number[][], index: number): number[] {
+function getColumn(matrix: number[][], columnIndex: number): number[] {
 
-    return matrix.map(row => row[index]);
+    return matrix.map(row => row[columnIndex]);
 }
 
-function countContiguous(numbers: number[]): number[] {
+//count total number of each contiguous groups in an array
+function countContiguous(array: number[], value: number): number[] {
 
     let contiguous: number[] = [];
-    let counter = 0;
 
-    for(let i = 0; i < numbers.length; i++) {
+    for(let i = 0, counter = 0; i < array.length; i++) {
 
-        if(numbers[i] === 1) {
+        if(array[i] === value) {
 
             counter++;
-
-            continue;
         }
+        //when other values are found or reaching end of array
+        if(array[i] !== value || i === array.length - 1) {
 
-        if(counter > 0) {
+            if(counter > 0) {
 
-            contiguous.push(counter);
+                contiguous.push(counter);
+            }
+
+            counter = 0;
         }
-
-        counter = 0;
-    }
-
-    if(counter > 0) {
-
-        contiguous.push(counter);
     }
 
     return contiguous;
 }
 
-function createNonogram(input: string): string {
+function createNonogram(input: string, contiguous: number): string {
 
     let matrix = readMatrix(input);
-    let result = "";
+    let nonogram = "";
+    //create nonogram by column
+    for(let i = 0; i < matrix[0].length; i++) {
 
-    matrix[0].forEach((column, index) => {
+        let column = getColumn(matrix, i);
+        nonogram += `${countContiguous(column, contiguous).join(" ")}\n`;
+    }
 
-        result += `${countContiguous(getColumn(matrix, index)).join(" ")}\n`;
-    });
-
-    result += "\n";
-
+    nonogram += "\n";
+    //create nonogram by row
     matrix.forEach(row => {
 
-        result += `${countContiguous(row).join(" ")}\n`;
+        nonogram += `${countContiguous(row, contiguous).join(" ")}\n`;
     });
 
-    return result;
+    return nonogram;
 }
-
-
-const matrix = `0 1 1 1 1 0
-              1 0 0 1 1 1
-              1 0 1 1 1 1
-              1 1 1 1 1 1
-              0 1 1 1 1 0`;
 
 //challenge input
 console.log(`%cChallenge Input:`, "color : red;");
-console.log(createNonogram(matrix));
+
+const matrix = `0 1 1 1 1 0
+                1 0 0 1 1 1
+                1 0 1 1 1 1
+                1 1 1 1 1 1
+                0 1 1 1 1 0`;
+
+console.log(createNonogram(matrix, 1));
