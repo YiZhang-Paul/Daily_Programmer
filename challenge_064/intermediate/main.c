70f6b22a7fb6bf5d2c190513b12f183ae1a9820a
YI ZHANG <scaccterran@gmail.com>
2018-02-28 00:35:05 -0500

    reverse string

--- challenge_064/intermediate/main.c
+++ challenge_064/intermediate/main.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void swap(char *, char *);
+void reverse(char *);
 
 int main(void) {
 
+    char input[] = "FourscoreandsevenyearsagoourfaathersbroughtforthonthiscontainentanewnationconceivedinzLibertyanddedicatedtothepropositionthatallmenarecreatedequalNowweareengagedinagreahtcivilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth";
+    char copy[strlen(input)];
+    strcpy(copy, input);
+
+    printf("%s\n", input);
+    reverse(copy);
+    printf("%s\n", copy);
+
     return 0;
+}
+
+void swap(char * index1, char * index2) {
+
+    char temporary = *index1;
+    *index1 = *index2;
+    *index2 = temporary;
+}
+
+void reverse(char * text) {
+
+    char *start = text;
+    char *end = text + strlen(text) - 1;
+
+    while(start < end) {
+
+        swap(start++, end--);
+    }
 }
