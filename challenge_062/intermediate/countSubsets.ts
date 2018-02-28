6982616b6558e0a3a370633ba24cde5ae6fd9dc5
YI ZHANG <scaccterran@gmail.com>
2018-02-28 18:22:12 -0500

    all done

--- challenge_062/intermediate/countSubsets.ts
+++ challenge_062/intermediate/countSubsets.ts
@@ -1,64 +1,94 @@
-function insertIndex(sums: Map<number, number[]>, sum: number, index: number): void {
+//push given value into array associated with given key
+function insert(map: Map<number, number[]>, key: number, value: number): void {
 
-    if(!sums.has(sum)) {
+    if(!map.has(key)) {
 
-        sums.set(sum, new Array<number>());
+        map.set(key, new Array<number>());
     }
 
-    sums.get(sum).push(index);
+    map.get(key).push(value);
 }
 
-function countValues(sums: Map<number, number[]>, values: Set<number>): number {
+//count total number of values in a map with optional key filter
+function countTotal(map: Map<number, number[]>, keys: Set<number>): number {
 
     let total = 0;
 
-    sums.forEach((indexes, sum) => {
+    map.forEach((values, key) => {
 
-        if(values.has(sum)) {
+        if(keys === undefined || keys.has(key)) {
 
-            total += indexes.length;
+            total += values.length;
         }
     });
 
     return total;
 }
 
-let time = new Date().getTime();
-let input = [3, 4, 9, 14, 15, 19, 28, 37, 47, 50, 54, 56, 59, 61, 70, 73, 78, 81, 92, 95, 97, 99];
-//let input = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009, 1013, 1019, 1021];
-let total = 0;
-let values = new Set(input);
-let sums = new Map<number, number[]>();
+function getInitialSums(input: number[]): Map<number, number[]> {
 
-for(let i = 0; i < input.length - 1; i++) {
+    let sums = new Map<number, number[]>();
 
-    insertIndex(sums, input[i], i + 1);
+    for(let i = 0; i < input.length - 1; i++) {
+
+        insert(sums, input[i], i + 1);
+    }
+
+    return sums;
 }
 
-do {
+function updateSums(sums: Map<number, number[]>, oldSum: number, startIndex: number, input: number[]): void {
 
-    let newSums = new Map<number, number[]>();
+    const maxValue = input.slice(-1)[0];
+    //no sum should exceed largest number in input set
+    if(oldSum + input[startIndex] > maxValue) {
 
-    sums.forEach((indexes, sum) => {
+        return;
+    }
 
-        for(let i = indexes.length - 1; i >= 0; i--) {
+    for(let i = startIndex; i < input.length - 1; i++) {
 
-            if(sum + input[indexes[i]] > input.slice(-1)[0]) {
+        if(oldSum + input[i] <= maxValue) {
 
-                break;
-            }
-
-            for(let j = indexes[i]; j < input.length - 1; j++) {
-
-                insertIndex(newSums, sum + input[j], j + 1);
-            }
+            insert(sums, oldSum + input[i], i + 1);
         }
-    });
+    }
+}
 
-    sums = newSums;
-    total += countValues(sums, values);
+//count total number of subsets whose largest number is the sum of other numbers
+function countSubset(input: number[]): number {
 
-} while(sums.size > 0);
+    let sums = getInitialSums(input);
+    let inputValues = new Set(input);
+    let subsets = 0;
 
-console.log(total);
-console.log(new Date().getTime() - time + "ms");
+    while(sums.size > 0) {
+
+        let newSums = new Map<number, number[]>();
+
+        sums.forEach((indexes, sum) => {
+            //update sums and last index of number used to achieve such sums
+            for(let i = indexes.length - 1; i >= 0; i--) {
+
+                updateSums(newSums, sum, indexes[i], input);
+            }
+        });
+
+        sums = newSums;
+        subsets += countTotal(sums, inputValues);
+    }
+
+    return subsets;
+}
+
+//challenge input
+console.log(`%cChallenge Input:`, "color : red;");
+const time = new Date().getTime();
+
+let input = [1, 2, 3, 4, 6];
+console.log(countSubset(input));
+
+input = [3, 4, 9, 14, 15, 19, 28, 37, 47, 50, 54, 56, 59, 61, 70, 73, 78, 81, 92, 95, 97, 99];
+console.log(countSubset(input));
+
+console.log(`%cTime Spent: %c${new Date().getTime() - time}ms`, "color : yellow;", "color : violet;");
