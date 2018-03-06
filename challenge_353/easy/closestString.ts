9cb3da0540ae0ad7112b8d644808792de039b1d1
YI ZHANG <scaccterran@gmail.com>
2018-03-06 01:15:35 -0500

    all done

--- challenge_353/easy/closestString.ts
+++ challenge_353/easy/closestString.ts
@@ -3,20 +3,49 @@ function readText(input: string): string[] {
     return input.split("\n").map(text => text.trim());
 }
 
+//calculate Hamming distance between two strings
 function getDistance(text1: string, text2: string): number {
 
-    let distance = 0;
+    return Array.prototype.filter.call(text1, (character, index) => {
 
-    for(let i = 0; i < text1.length; i++) {
+        return text2[index] !== character;
 
-        if(text1[i] !== text2[i]) {
+    }).length;
+}
 
-            distance++;
+//calculate average distance between a string with other strings in the same group
+function getAverageDistance(texts: string[], index: number): number {
+
+    return texts.reduce((distance, text) => {
+
+        return distance + getDistance(text, texts[index]);
+
+    }, 0) / (texts.length - 1);
+}
+
+function findCenter(input: string): string {
+
+    let texts = readText(input);
+    let center = "";
+
+    for(let i = 0, distance = 0; i < texts.length; i++) {
+
+        const average = getAverageDistance(texts, i);
+
+        if(distance === 0 || average < distance) {
+
+            distance = average;
+            center = texts[i];
         }
     }
 
-    return distance;
+    return center;
 }
+
+//challenge input
+console.log(`%cChallenge Input:`, "color : red;");
+
+const time = new Date().getTime();
 
 let input = `ATCAATATCAA
              ATTAAATAACT
@@ -25,7 +54,51 @@ let input = `ATCAATATCAA
              TCCCATCCTTT
              ACTTCAATATA`;
 
-let text = readText(input);
+console.log(findCenter(input));
 
-console.log(getDistance(text[0], text[0]));
-console.log(getDistance(text[0], text[1]));
+input = `CTCCATCACAC
+         AATATCTACAT
+         ACATTCTCCAT
+         CCTCCCCACTC`;
+
+console.log(findCenter(input));
+
+input = `AACACCCTATA
+         CTTCATCCACA
+         TTTCAATTTTC
+         ACAATCAAACC
+         ATTCTACAACT
+         ATTCCTTATTC
+         ACTTCTCTATT
+         TAAAACTCACC
+         CTTTTCCCACC
+         ACCTTTTCTCA
+         TACCACTACTT`;
+
+console.log(findCenter(input));
+
+input = `ACAAAATCCTATCAAAAACTACCATACCAAT
+         ACTATACTTCTAATATCATTCATTACACTTT
+         TTAACTCCCATTATATATTATTAATTTACCC
+         CCAACATACTAAACTTATTTTTTAACTACCA
+         TTCTAAACATTACTCCTACACCTACATACCT
+         ATCATCAATTACCTAATAATTCCCAATTTAT
+         TCCCTAATCATACCATTTTACACTCAAAAAC
+         AATTCAAACTTTACACACCCCTCTCATCATC
+         CTCCATCTTATCATATAATAAACCAAATTTA
+         AAAAATCCATCATTTTTTAATTCCATTCCTT
+         CCACTCCAAACACAAAATTATTACAATAACA
+         ATATTTACTCACACAAACAATTACCATCACA
+         TTCAAATACAAATCTCAAAATCACCTTATTT
+         TCCTTTAACAACTTCCCTTATCTATCTATTC
+         CATCCATCCCAAAACTCTCACACATAACAAC
+         ATTACTTATACAAAATAACTACTCCCCAATA
+         TATATTTTAACCACTTACCAAAATCTCTACT
+         TCTTTTATATCCATAAATCCAACAACTCCTA
+         CTCTCAAACATATATTTCTATAACTCTTATC
+         ACAAATAATAAAACATCCATTTCATTCATAA
+         CACCACCAAACCTTATAATCCCCAACCACAC`;
+
+console.log(findCenter(input));
+
+console.log(`%cTime Spent: %c${new Date().getTime() - time}ms`, "color : yellow;", "color : violet;");
