d8f0f45ba3c7d63371faed8dc05f49e135ece36a
YI ZHANG <scaccterran@gmail.com>
2018-04-06 10:41:00 -0400

    all done

--- challenge_355/easy/main.c
+++ challenge_355/easy/main.c
@@ -2,6 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 
+int findIndex(char *, char);
+char * copy(char *, int, int);
+int letterToIndex(char);
+char indexToLetter(int);
+char * transform(char *, char func(char, char));
+char encodeLetter(char, char);
+char decodeLetter(char, char);
+char * encode(char *);
+char * decode(char *);
+void showResult(char *, char * func(char *));
+
+int main(void) {
+
+    showResult("snitch thepackagehasbeendelivered", encode);
+    showResult("bond theredfoxtrotsquietlyatmidnight", encode);
+    showResult("train murderontheorientexpress", encode);
+    showResult("garden themolessnuckintothegardenlastnight", encode);
+    showResult("cloak klatrgafedvtssdwywcyty", decode);
+    showResult("python pjphmfamhrcaifxifvvfmzwqtmyswst", decode);
+    showResult("moore rcfpsgfspiecbcc", decode);
+
+    return 0;
+}
+
+//find index of first occurrence of given letter in text
 int findIndex(char * input, char letter) {
 
     for(int i = 0; i < strlen(input); i++) {
@@ -15,6 +40,7 @@ int findIndex(char * input, char letter) {
     return -1;
 }
 
+//copy a portion of text
 char * copy(char * input, int start, int end) {
 
     const int length = end - start + 1;
@@ -34,48 +60,50 @@ char indexToLetter(int index) {
     return 'a' + index;
 }
 
-char * encode(char * input) {
+char * transform(char * input, char func(char, char)) {
 
     const int splitIndex = findIndex(input, ' ');
     char *keyword = copy(input, 0, splitIndex - 1);
     char *message = copy(input, splitIndex + 1, strlen(input) - 1);
-    char *encoded = malloc(strlen(message) + 1);
+    char *transformed = malloc(strlen(message) + 1);
 
     for(int i = 0; i < strlen(message); i++) {
 
         const char key = keyword[i % strlen(keyword)];
-        const int index = (letterToIndex(message[i]) + letterToIndex(key));
-        encoded[i] = indexToLetter(index % 26);
+        //encode/decode letter
+        transformed[i] = func(key, message[i]);
     }
 
-    encoded[strlen(message)] = '\0';
+    transformed[strlen(message)] = '\0';
 
     free(keyword);
     free(message);
 
-    return encoded;
+    return transformed;
+}
+
+char encodeLetter(char key, char letter) {
+
+    const int index = letterToIndex(letter) + letterToIndex(key);
+
+    return indexToLetter(index % 26);
+}
+
+char decodeLetter(char key, char letter) {
+
+    const int index = letterToIndex(letter) - letterToIndex(key);
+
+    return indexToLetter((index + 26) % 26);
+}
+
+char * encode(char * input) {
+
+    return transform(input, encodeLetter);
 }
 
 char * decode(char * input) {
 
-    const int splitIndex = findIndex(input, ' ');
-    char *keyword = copy(input, 0, splitIndex - 1);
-    char *message = copy(input, splitIndex + 1, strlen(input) - 1);
-    char *decoded = malloc(strlen(message) + 1);
-
-    for(int i = 0; i < strlen(message); i++) {
-
-        const char key = keyword[i % strlen(keyword)];
-        const int index = letterToIndex(message[i]) - letterToIndex(key) + 26;
-        decoded[i] = indexToLetter(index % 26);
-    }
-
-    decoded[strlen(message)] = '\0';
-
-    free(keyword);
-    free(message);
-
-    return decoded;
+    return transform(input, decodeLetter);
 }
 
 void showResult(char * input, char * func(char *)) {
@@ -84,18 +112,4 @@ void showResult(char * input, char * func(char *)) {
     printf("%s\n", result);
 
     free(result);
-}
-
-int main(void) {
-
-    showResult("snitch thepackagehasbeendelivered", encode);
-    showResult("bond theredfoxtrotsquietlyatmidnight", encode);
-    showResult("train murderontheorientexpress", encode);
-    showResult("garden themolessnuckintothegardenlastnight", encode);
-
-    showResult("cloak klatrgafedvtssdwywcyty", decode);
-    showResult("python pjphmfamhrcaifxifvvfmzwqtmyswst", decode);
-    showResult("moore rcfpsgfspiecbcc", decode);
-
-    return 0;
 }
