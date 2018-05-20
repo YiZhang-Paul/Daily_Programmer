1a62903d9b48d824418e9e9aa7f1e9218c1c5daa
YI ZHANG <scaccterran@gmail.com>
2018-05-19 23:58:26 -0400

    find anchor day and dooms day

--- challenge_086/intermediate/main.c
+++ challenge_086/intermediate/main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int getAnchorDay(int);
+int getDoomsday(int);
+
 int main(void) {
 
     return 0;
+}
+
+int getAnchorDay(int year) {
+
+    int anchors[4] = { 2, 0, 5, 3 };
+
+    return anchors[year / 100 % 4];
+}
+
+int getDoomsday(int year) {
+
+    const int lastTwo = year % 100;
+    const int anchor = getAnchorDay(year);
+
+    return (lastTwo / 12 + lastTwo % 12 + lastTwo % 12 / 4 + anchor) % 7;
 }
