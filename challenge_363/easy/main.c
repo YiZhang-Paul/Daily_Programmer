a0d10e019542aebd223c0b4a5632182adad91243
YI ZHANG <scaccterran@gmail.com>
2018-06-12 19:12:16 -0400

    all done

--- challenge_363/easy/main.c
+++ challenge_363/easy/main.c
@@ -3,51 +3,54 @@
 #include <string.h>
 #include <stdbool.h>
 
-bool isValidIndex(int, int);
-bool checkEI(char *, int, int);
-bool checkIE(char *, int, int);
-bool isValidI(char *, int, int);
+#define INPUT_FILE "input.txt"
+#define LINE_LENGTH 256
+
+bool hasIndex(int, int);
+bool checkEAndI(char *, int, int);
+bool checkIAndE(char *, int, int);
+bool isValidIndex(char *, int, int);
 bool isValidWord(char *);
+void countValidWord(char *, int *, int *);
 
 int main(void) {
 
-    printf("%d\n", isValidWord("a"));
-    printf("%d\n", isValidWord("zombie"));
-    printf("%d\n", isValidWord("transceiver"));
-    printf("%d\n", isValidWord("veil"));
-    printf("%d\n", isValidWord("icier"));
+    int valid = 0;
+    int invalid = 0;
+    countValidWord(INPUT_FILE, &valid, &invalid);
+    printf("Valid Words: %d; Invalid Words: %d", valid, invalid);
 
     return 0;
 }
 
-bool isValidIndex(int index, int length) {
+bool hasIndex(int index, int length) {
 
     return index >= 0 && index < length;
 }
 
-bool checkEI(char * word, int index, int length) {
+bool checkEAndI(char * word, int index, int length) {
 
-    if(!isValidIndex(index - 1, length) || word[index - 1] != 'e') {
-
-        return true;
-    }
-
-    return isValidIndex(index - 2, length) && word[index - 2] == 'c';
-}
-
-bool checkIE(char * word, int index, int length) {
-
-    if(!isValidIndex(index + 1, length) || word[index + 1] != 'e') {
+    if(!hasIndex(index - 1, length) || word[index - 1] != 'e') {
 
         return true;
     }
 
-    return !isValidIndex(index - 1, length) || word[index - 1] != 'c';
+    return hasIndex(index - 2, length) && word[index - 2] == 'c';
 }
 
-bool isValidI(char * word, int index, int length) {
+bool checkIAndE(char * word, int index, int length) {
 
-    return checkEI(word, index, length) && checkIE(word, index, length);
+    if(!hasIndex(index + 1, length) || word[index + 1] != 'e') {
+
+        return true;
+    }
+
+    return !hasIndex(index - 1, length) || word[index - 1] != 'c';
+}
+
+bool isValidIndex(char * word, int index, int length) {
+
+    return checkEAndI(word, index, length) && checkIAndE(word, index, length);
 }
 
 bool isValidWord(char * word) {
@@ -56,11 +59,33 @@ bool isValidWord(char * word) {
 
     for(int i = 0; i < length; i++) {
 
-        if(word[i] == 'i' && !isValidI(word, i, length)) {
+        if(word[i] == 'i' && !isValidIndex(word, i, length)) {
 
             return false;
         }
     }
 
     return true;
+}
+
+void countValidWord(char * fileName, int * valid, int * invalid) {
+
+    *valid = 0;
+    *invalid = 0;
+    FILE *file = fopen(fileName, "r");
+
+    if(file) {
+
+        char line[LINE_LENGTH];
+
+        while(!feof(file)) {
+
+            fgets(line, LINE_LENGTH, file);
+            const bool result = isValidWord(line);
+            *valid += result ? 1 : 0;
+            *invalid += result ? 0 : 1;
+        }
+    }
+
+    fclose(file);
 }
