7045c03f2a6a4824ae66812ac23783babdd6df90
YI ZHANG <scaccterran@gmail.com>
2018-07-13 07:54:13 -0400

    all done

--- challenge_365/easy/main.c
+++ challenge_365/easy/main.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+long long calculate(int, int, int);
 
 int main(void) {
 
+    printf("%lld\n", calculate(2, 1, 4));
+    printf("%lld\n", calculate(2, 2, 4));
+    printf("%lld\n", calculate(2, 3, 3));
+    printf("%lld\n", calculate(-1, 3, 3));
+    printf("%lld\n", calculate(1, 1, 0));
+    printf("%lld\n", calculate(1, 2, 0));
+
     return 0;
+}
+
+long long calculate(int base, int arrows, int power) {
+
+    if(arrows == 1) {
+
+        return pow(base, power);
+    }
+
+    if(arrows >= 1 && power <= 0) {
+
+        return 1;
+    }
+
+    return calculate(base, arrows - 1, calculate(base, arrows, power - 1));
 }
