b0c474a876e8cf500dd97acdcf61ff5ff2a463a1
YI ZHANG <scaccterran@gmail.com>
2018-09-26 22:43:00 -0400

    O(m*n) solution for optional challenge 2

--- challenge_366/easy/solution/src/OptionChallengeTwo.hs
+++ challenge_366/easy/solution/src/OptionChallengeTwo.hs
@@ -2,8 +2,10 @@ module OptionChallengeTwo
 ( showResult
 ) where
 
+import Data.List
 import Data.Maybe
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 import qualified OptionChallengeOne as OpOne
 
 showResult :: FilePath -> IO ()
@@ -14,21 +16,29 @@ showResult filePath = do
                             wordsWith5Funnels = filter (totalFunnels 5 groups) allWords
                         print wordsWith5Funnels
 
-addToGroup :: Map.Map Int [String] -> String -> Map.Map Int [String]
-addToGroup wordMap word
-    | isNothing group = Map.insert key [word] wordMap
-    | otherwise       = Map.insert key (word:fromJust group) wordMap
+addToSet :: Map.Map Int (Set.Set String) -> String -> Map.Map Int (Set.Set String)
+addToSet groups word
+    | isNothing group = Map.insert key (Set.insert word Set.empty) groups
+    | otherwise       = Map.insert key (Set.insert word (fromJust group)) groups
     where key   = length word
-          group = Map.lookup key wordMap
+          group = Map.lookup key groups
 
-groupWords :: [String] -> Map.Map Int [String]
-groupWords = foldl addToGroup Map.empty
+groupWords :: [String] -> Map.Map Int (Set.Set String)
+groupWords = foldl addToSet Map.empty
 
-findFunnels :: String -> Map.Map Int [String] -> [String]
-findFunnels word wordMap
+dropOneLetter :: String -> Int -> [String]
+dropOneLetter word index
+    | totalLetters == 0     = []
+    | index == totalLetters = []
+    | otherwise             = dropped:dropOneLetter word (index + 1)
+    where totalLetters = length word
+          dropped      = let (x, y) = splitAt index word in x ++ tail y
+
+findFunnels :: String -> Map.Map Int (Set.Set String) -> [String]
+findFunnels word groups
     | isNothing group = []
-    | otherwise       = OpOne.funnels word (fromJust group)
-    where group = Map.lookup (length word - 1) wordMap
+    | otherwise       = filter (`Set.member` fromJust group) (nub (dropOneLetter word 0))
+    where group = Map.lookup (length word - 1) groups
 
-totalFunnels :: Int -> Map.Map Int [String] -> String -> Bool
-totalFunnels total wordMap word = length (findFunnels word wordMap) == total
+totalFunnels :: Int -> Map.Map Int (Set.Set String) -> String -> Bool
+totalFunnels total groups word = length (findFunnels word groups) == total
