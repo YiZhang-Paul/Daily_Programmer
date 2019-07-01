491b6108a2722a1440861d045ce3327776165960
YI ZHANG <scaccterran@gmail.com>
2019-07-01 09:57:53 -0400

    all done

--- challenge_371/easy/main.go
+++ challenge_371/easy/main.go
@@ -12,13 +12,22 @@ func main() {
 	showValidateResult([]int{5, 3, 1, 4, 2, 8, 6, 3})
 	showValidateResult([]int{5, 8, 2, 4, 7, 1, 3, 6})
 	showValidateResult([]int{4, 3, 1, 8, 1, 3, 5, 2})
+	// bonus challenge
+	showFixResult([]int{8, 6, 4, 2, 7, 1, 3, 5})
+	showFixResult([]int{8, 5, 1, 3, 6, 2, 7, 4})
+	showFixResult([]int{4, 6, 8, 3, 1, 2, 5, 7})
+	showFixResult([]int{7, 1, 3, 6, 8, 5, 2, 4})
 }
 
 func showValidateResult(position []int) {
-	fmt.Printf("validateNQueens(%v) => %t\n", position, validateNQueens(position))
+	fmt.Printf("isValidateNQueens(%v) => %t\n", position, isValidateNQueens(position))
 }
 
-func validateNQueens(position []int) bool {
+func showFixResult(position []int) {
+	fmt.Printf("fixNQueens(%v) => %v\n", position, fixNQueens(position))
+}
+
+func isValidateNQueens(position []int) bool {
 	for i, valueI := range position {
 		for j, valueJ := range position[i+1:] {
 			if valueI == valueJ || abs(valueI-valueJ) == j+1 {
@@ -27,6 +36,35 @@ func validateNQueens(position []int) bool {
 		}
 	}
 	return true
+}
+
+func fixNQueens(position []int) []int {
+	for i, valueI := range position {
+		for j, valueJ := range position[i+1:] {
+			if abs(valueI-valueJ) != j+1 {
+				continue
+			}
+			if tryFixNQueens(position, i) || tryFixNQueens(position, i+j+1) {
+				return position
+			}
+		}
+	}
+	return position
+}
+
+func tryFixNQueens(position []int, index int) bool {
+	for i := range position {
+		swap(position, index, i)
+		if isValidateNQueens(position) {
+			return true
+		}
+		swap(position, index, i)
+	}
+	return false
+}
+
+func swap(list []int, indexA int, indexB int) {
+	list[indexA], list[indexB] = list[indexB], list[indexA]
 }
 
 func abs(value int) int {
--- challenge_371/easy/main_test.go
+++ challenge_371/easy/main_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestValidateNQueens(t *testing.T) {
+func TestIsValidateNQueens(t *testing.T) {
 	cases := []struct {
 		position []int
 		expected bool
@@ -14,8 +14,28 @@ func TestValidateNQueens(t *testing.T) {
 		{[]int{4, 3, 1, 8, 1, 3, 5, 2}, false},
 	}
 	for _, c := range cases {
-		if actual := validateNQueens(c.position); c.expected != actual {
-			t.Errorf("validateNQueens(%v) == %t, expected %t", c.position, actual, c.expected)
+		if actual := isValidateNQueens(c.position); c.expected != actual {
+			t.Errorf("isValidateNQueens(%v) == %t, expected %t", c.position, actual, c.expected)
+		}
+	}
+}
+
+func TestFixNQueens(t *testing.T) {
+	cases := []struct {
+		position, expected []int
+	}{
+		{[]int{8, 6, 4, 2, 7, 1, 3, 5}, []int{4, 6, 8, 2, 7, 1, 3, 5}},
+		{[]int{8, 5, 1, 3, 6, 2, 7, 4}, []int{8, 4, 1, 3, 6, 2, 7, 5}},
+		{[]int{4, 6, 8, 3, 1, 2, 5, 7}, []int{4, 6, 8, 3, 1, 7, 5, 2}},
+		{[]int{7, 1, 3, 6, 8, 5, 2, 4}, []int{7, 3, 1, 6, 8, 5, 2, 4}},
+	}
+	for _, c := range cases {
+		actual := fixNQueens(c.position)
+		for i, value := range actual {
+			if c.expected[i] != value {
+				t.Errorf("fixNQueens(%v) == %v, expected %v", c.position, actual, c.expected)
+				break
+			}
 		}
 	}
 }
