514e62f74f8d733e422f9e79c940d4350fc75d02
YI ZHANG <scaccterran@gmail.com>
2019-07-01 18:34:32 -0400

    all done

--- challenge_376/intermediate/main.go
+++ challenge_376/intermediate/main.go
@@ -19,48 +19,25 @@ func showResult(start, end int64) {
 }
 
 func totalLeaps(start, end int64) int64 {
-	if start == end {
+	var (
+		d4      = totalDivisions(start, end, 4)
+		d100    = totalDivisions(start, end, 100)
+		dMod200 = totalDivisions(start-200, end-200, 900)
+		dMod600 = totalDivisions(start-600, end-600, 900)
+	)
+	return d4 + dMod200 + dMod600 - d100
+}
+
+func totalDivisions(min, max, division int64) int64 {
+	a, b := min/division, max/division
+	if a*division < min {
+		a++
+	}
+	if b*division == max {
+		b--
+	}
+	if b < a {
 		return 0
 	}
-	total, four1, four2 := int64(0), start/4, end/4
-	if four1*4 < start {
-		four1++
-	}
-	if four2*4 == end {
-		four2--
-	}
-	if four2 >= four1 {
-		total += four2 - four1 + 1
-	}
-	hundred1, hundred2 := start/100, end/100
-	if hundred1*100 < start {
-		hundred1++
-	}
-	if hundred2*100 == end {
-		hundred2--
-	}
-	if hundred2 >= hundred1 {
-		total -= hundred2 - hundred1 + 1
-	}
-	nine200Y1, nine200Y2 := (start-200)/900, (end-200)/900
-	if nine200Y1*900 < start-200 {
-		nine200Y1++
-	}
-	if nine200Y2*900 == end-200 {
-		nine200Y2--
-	}
-	if nine200Y2 >= nine200Y1 {
-		total += nine200Y2 - nine200Y1 + 1
-	}
-	nine600Y1, nine600Y2 := (start-600)/900, (end-600)/900
-	if nine600Y1*900 < start-600 {
-		nine600Y1++
-	}
-	if nine600Y2*900 == end-600 {
-		nine600Y2--
-	}
-	if nine600Y2 >= nine600Y1 {
-		total += nine600Y2 - nine600Y1 + 1
-	}
-	return total
+	return b - a + 1
 }
