ef9fcf2e0fa94662759b7dc4ee0ff58ecf6815d2
YI ZHANG <scaccterran@gmail.com>
2019-11-02 19:58:24 -0400

    bonus challenge

--- challenge_050/intermediate/main.go
+++ challenge_050/intermediate/main.go
@@ -7,21 +7,21 @@ import (
 )
 
 func main() {
-	fmt.Println(getFileTree("D:\\Resources"))
+	fmt.Println(getFileTree("D:\\Resources", 2))
 }
 
-func getFileTree(path string) string {
+func getFileTree(path string, depth int) string {
 	if !filepath.IsAbs(path) {
 		return "Invalid path. Must be an absolute path."
 	}
-	var tree, err = generateTree(path, "..")
+	var tree, err = generateTree(path, "..", depth)
 	if err != nil {
 		return "Failed to read specified directory."
 	}
 	return "+-" + filepath.Base(path) + "/\n" + tree
 }
 
-func generateTree(path string, aligners string) (string, error) {
+func generateTree(path string, aligners string, depth int) (string, error) {
 	var (
 		tree       = ""
 		infos, err = ioutil.ReadDir(path)
@@ -31,14 +31,14 @@ func generateTree(path string, aligners string) (string, error) {
 	}
 	for i, info := range infos {
 		tree += aligners + "+-" + info.Name() + "/\n"
-		if !info.IsDir() {
+		if !info.IsDir() || len(aligners) == depth*2 {
 			continue
 		}
 		var aligner = "|."
 		if i == len(infos)-1 {
 			aligner = ".."
 		}
-		var subTree, _ = generateTree(path+"\\"+info.Name(), aligners+aligner)
+		var subTree, _ = generateTree(path+"\\"+info.Name(), aligners+aligner, depth)
 		tree += subTree
 	}
 	return tree, nil
