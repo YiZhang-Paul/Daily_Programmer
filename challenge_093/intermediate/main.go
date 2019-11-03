b8fedd0db00415f0488c2ecf6ee55b940c68d0e8
YI ZHANG <scaccterran@gmail.com>
2019-11-03 16:48:43 -0500

    naive implementation of z-order curve

--- challenge_093/intermediate/main.go
+++ challenge_093/intermediate/main.go
@@ -1,23 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	var square = getSquare(15)
+	var (
+		total  = 260
+		square = getSquare(total)
+		width  = len(strconv.Itoa(total))
+		x      = 0
+		y      = 0
+	)
+	for i := 0; i < total; i++ {
+		var value = strconv.Itoa(i)
+		square[y][x] = strings.Repeat(" ", width-len(value)+1) + value
+		x, y = nextPosition(x, y, i+1)
+	}
 	for _, row := range square {
-		for i := 0; i < len(row); i++ {
-			fmt.Print("x")
+		for _, content := range row {
+			fmt.Print(content)
 		}
 		fmt.Print("\n")
 	}
 }
 
 func getSquare(total int) [][]string {
-	var square = make([][]string, 0)
-	if total == 1 {
-		return append(square, make([]string, 1))
-	}
-	var width = 2
+	var (
+		square = make([][]string, 0)
+		width  = 1
+	)
 	for width*width < total {
 		width *= 2
 	}
@@ -25,4 +39,39 @@ func getSquare(total int) [][]string {
 		square = append(square, make([]string, width))
 	}
 	return square
+}
+
+func nextPosition(x, y, total int) (int, int) {
+	if x%2 == 0 {
+		return x + 1, y
+	}
+	if y%2 == 0 {
+		return x - 1, y + 1
+	}
+	if x/2%2 == 0 {
+		return x + 1, y - 1
+	}
+	if y/2%2 == 0 {
+		return x - 3, y + 1
+	}
+	if total%64 == 48 || total%64 == 16 {
+		return x + 1, y - 3
+	}
+	if total%64 == 32 {
+		return x - 7, y + 1
+	}
+	if y >= x {
+		return x + 1, ternaryInt(x == y, 0, (y+1)/2)
+	}
+	if (x+1)/(y+1) != 4 {
+		return 0, y + 1
+	}
+	return (x + 1) / 2, y + 1
+}
+
+func ternaryInt(condition bool, whenTrue, whenFalse int) int {
+	if condition {
+		return whenTrue
+	}
+	return whenFalse
 }
