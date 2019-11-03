4be65ab0c14adc845f27b3fe1f91f89b05799823
YI ZHANG <scaccterran@gmail.com>
2019-11-02 20:12:31 -0400

    all done

--- challenge_050/intermediate/main.go
+++ challenge_050/intermediate/main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
+	"strings"
 )
 
 func main() {
+	fmt.Println(getFileTree("D:\\Resources", -1))
+	fmt.Printf("------------------------------\n\n")
 	fmt.Println(getFileTree("D:\\Resources", 2))
 }
 
@@ -23,23 +26,27 @@ func getFileTree(path string, depth int) string {
 
 func generateTree(path string, aligners string, depth int) (string, error) {
 	var (
-		tree       = ""
+		tree       strings.Builder
 		infos, err = ioutil.ReadDir(path)
 	)
 	if err != nil {
-		return tree, err
+		return tree.String(), err
 	}
 	for i, info := range infos {
-		tree += aligners + "+-" + info.Name() + "/\n"
-		if !info.IsDir() || len(aligners) == depth*2 {
-			continue
+		tree.WriteString(aligners + "+-" + info.Name() + "/\n")
+		if info.IsDir() && (depth < 0 || len(aligners)/2 < depth) {
+			var subPath = filepath.Join(path, info.Name())
+			var aligner = ternaryString(i == len(infos)-1, "..", "|.")
+			var subTree, _ = generateTree(subPath, aligners+aligner, depth)
+			tree.WriteString(subTree)
 		}
-		var aligner = "|."
-		if i == len(infos)-1 {
-			aligner = ".."
-		}
-		var subTree, _ = generateTree(path+"\\"+info.Name(), aligners+aligner, depth)
-		tree += subTree
 	}
-	return tree, nil
+	return tree.String(), nil
+}
+
+func ternaryString(condition bool, whenTrue, whenFalse string) string {
+	if condition {
+		return whenTrue
+	}
+	return whenFalse
 }
